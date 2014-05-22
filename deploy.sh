12e026ee32a4c6399394a0743e7fb4bb2a3726b2
Minh Tran <mtgred@gmail.com>
2014-05-22 17:00:12 +0200

    added 'cljsbuild clean' to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 # git remote add openshift -f <openshift git repo>
 
+lein cljsbuild clean
 lein cljsbuild once prod &
 git push openshift
 rhc scp netrunner upload resources/js/app.js app-root/repo/resources/js
