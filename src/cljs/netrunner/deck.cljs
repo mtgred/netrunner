a2e94cd1d9607c6aa6137b2005cf21ed09640d1e
Minh Tran <mtgred@gmail.com>
2014-06-06 21:37:45 +0200

    fix issue with edge case in card lookup

--- src/cljs/netrunner/deck.cljs
+++ src/cljs/netrunner/deck.cljs
@@ -31,6 +31,9 @@
   (let [name (:title (first cards))]
     (every? #(= (:title %) name) cards)))
 
+(defn found? [query cards]
+  (some #(if (= (.toLowerCase (:title %)) query) %) cards))
+
 (defn match [query cards]
   (filter #(if (= (.indexOf (.toLowerCase (:title %)) query) -1) false true) cards))
 
@@ -39,18 +42,19 @@
         cards (:cards @cb/app-state)]
     (if-let [card (some #(when (= (-> % :title .toLowerCase) q) %) cards)]
       card
-      (loop [i 3 matches cards]
-        (cond (zero? (count matches)) query
-              (or (= (count matches) 1) (identical-cards? matches)) (first matches)
-              (<= i (count query)) (recur (inc i) (match (subs q 0 i) matches))
-              :else query)))))
+      (loop [i 2 matches cards]
+        (let [subquery (subs q 0 i)]
+         (cond (zero? (count matches)) query
+               (or (= (count matches) 1) (identical-cards? matches)) (first matches)
+               (found? subquery matches) (found? subquery matches)
+               (<= i (count query)) (recur (inc i) (match subquery matches))
+               :else query))))))
 
 (defn parse-line [line]
   (let [tokens (split line " ")
         qty (js/parseInt (first tokens))
         cardname (join " " (rest tokens))]
     (when-not (js/isNaN qty)
-      (println tokens)
       {:qty qty :card (lookup cardname)})))
 
 (defn parse-deck [deck]
