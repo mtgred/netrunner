7289a0180d0fdfd9b6aab85a4660bc0b185a83cb
Minh Tran <mtgred@gmail.com>
2014-07-21 23:34:00 +0200

    small improvements

--- src/cljs/netrunner/about.cljs
+++ src/cljs/netrunner/about.cljs
@@ -9,7 +9,7 @@
    (sab/html
     [:div.about.panel.blue-shade
      [:h3 "About"]
-     [:p "This a website created by an avid Netrunner player from Belgium. The goal is to provide a great way to create and test of Netrunner decks online."]
+     [:p "This website is created by an avid Netrunner player from Belgium. The goal is to provide a great way to create and test of Netrunner decks online."]
 
      [:h3 "Contact"]
      [:p "Twitter: " [:a {:href "http://twitter.com/mtgred"} "@mtgred"]]
@@ -22,18 +22,17 @@
      [:p "Bug reports and feature suggestions can be filled on "
       [:a {:href "https://github.com/mtgred/netrunner/issues"} "Github"] "."]
 
-     [:p "Manabase is built using Clojurescript, Om, Socket.io, Node.js"
-      "If you are proficient with these technologies and would like to contribute to the development, feel free to get in touch."]
+     [:p "Manabase is built using Clojurescript, React, Socket.io, Node.js. If you are proficient with some of these technologies and would like to contribute to the development, feel free to get in touch."]
 
      [:h3 "Credits"]
-     [:p "The Card data and images are provided by "
+     [:p "The card data and images are provided by "
       [:a {:href "http://netrunnerdb.com"} "netrunnerdb.com"]
       ". Big thanks to "
       [:a {:href "http://twitter.com/alsciende"} "@alsciende"]
       " for creating and maintaining such a great website."]
 
      [:h3 "Disclaimer"]
-     [:p "Netrunner and Android are trademarks of Fantasy Flight Publishing, Inc. and/or Wizards of the Coast LLC. (c) 2013 Wizards."]
+     [:p "Netrunner and Android are trademarks of Fantasy Flight Publishing, Inc. and/or Wizards of the Coast LLC."]
      [:p "This is website is not affiliated with Fantasy Flight Games or Wizards of the Coast."]])))
 
 (om/root about app-state {:target (. js/document (getElementById "about"))})
