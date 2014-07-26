db1ae800609cbe72d17ca70216d85af8ba9dc02a
Minh Tran <mtgred@gmail.com>
2014-07-26 19:31:52 +0200

    small issue fixes

--- src/cljs/netrunner/cardbrowser.cljs
+++ src/cljs/netrunner/cardbrowser.cljs
@@ -145,7 +145,7 @@
 
     om/IRenderState
     (render-state [this state]
-      (.select (js/$ ".search"))
+      (.focus (js/$ ".search"))
       (sab/html
        [:div.cardbrowser
         [:div.blue-shade.panel.filters
