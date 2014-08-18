bb1b6199b7ba3fe94747dc4d67bb9e6eb240ae71
Minh Tran <mtgred@gmail.com>
2014-08-18 03:29:30 +0200

    Identity ability handling

--- src/node/game/core.cljs
+++ src/node/game/core.cljs
@@ -10,73 +10,81 @@
         runner (some #(when (= (:side %) "Runner") %) players)
         corp-deck (create-deck (:deck corp))
         runner-deck (create-deck (:deck runner))
-        state {:gameid gameid
-               :log log
-               :corp {:user (:user corp)
-                      :identity (get-in corp [:deck :identity])
-                      :deck (drop 5 corp-deck)
-                      :hand (take 5 corp-deck)
-                      :discard []
-                      :rfg []
-                      :remote-servers []
-                      :click 3
-                      :credit 5
-                      :bad-publicity 0
-                      :agenda-point 0
-                      :max-hand-size 5
-                      :keep false}
-               :runner {:user (:user runner)
-                        :identity (get-in runner [:deck :identity])
-                        :deck (drop 5 runner-deck)
-                        :hand (take 5 runner-deck)
-                        :discard []
-                        :rfg []
-                        :rig {:programs [] :resources [] :hardware []}
-                        :click 4
-                        :credit 5
-                        :memory 4
-                        :link 0
-                        :tag 0
-                        :agenda-point 0
-                        :max-hand-size 5
-                        :brain-damage 0
-                        :keep false}}]
-    (swap! game-states assoc gameid (atom state))))
+        corp-identity (get-in corp [:deck :identity])
+        runner-identity (get-in runner [:deck :identity])
+        state (atom {:gameid gameid
+                     :log log
+                     :corp {:user (:user corp)
+                            :identity corp-identity
+                            :deck (drop 5 corp-deck)
+                            :hand (take 5 corp-deck)
+                            :discard []
+                            :rfg []
+                            :remote-servers []
+                            :click 3
+                            :credit 5
+                            :bad-publicity 0
+                            :agenda-point 0
+                            :max-hand-size 5
+                            :keep false}
+                     :runner {:user (:user runner)
+                              :identity runner-identity
+                              :deck (drop 5 runner-deck)
+                              :hand (take 5 runner-deck)
+                              :discard []
+                              :rfg []
+                              :rig {:programs [] :resources [] :hardware []}
+                              :click 4
+                              :credit 5
+                              :memory 4
+                              :link 0
+                              :tag 0
+                              :agenda-point 0
+                              :max-hand-size 5
+                              :brain-damage 0
+                              :keep false}})]
+    (when-let [corp-init (get-in game.cards/cards [(:title corp-identity) :game-init])]
+      (corp-init state :corp nil))
+    (when-let [runner-init (get-in game.cards/cards [(:title runner-identity) :game-init])]
+      (runner-init state :runner nil))
+    (swap! game-states assoc gameid state)))
+
+(def reset-value
+  {:corp {:credit 5 :bad-publicity 0 :max-hand-size 5}
+   :runner {:credit 5 :link 0 :memory 4 :max-hand-size 5}})
 
 (defn say! [state side args]
   (let [user (or (:user args) (get-in @state [side :user]))]
     (swap! state update-in [:log] #(conj % {:user user :text (:text args)}))))
 
+(defn system-msg [state side text]
+  (let [username (get-in @state [side :user :username])]
+    (say! state side {:user "__system__" :text (str username " " text)})))
+
 (defn mulligan! [state side args]
   (let [player (side @state)
         deck (shuffle (concat (:deck player) (:hand player)))]
+    (swap! state update-in [side] #(merge % (side reset-value)))
     (swap! state assoc-in [side :hand] (take 5 deck))
     (swap! state assoc-in [side :deck] (drop 5 deck))
     (swap! state assoc-in [side :keep] true)
-    (say! state side {:user "__system__"
-                      :text (str (get-in player [:user :username]) " takes a mulligan.")})))
+    (when-let [init-fn (get-in game.cards/cards [(get-in player [:identity :title]) :game-init])]
+      (init-fn state side nil))
+    (system-msg state side  "takes a mulligan.")))
 
 (defn keep! [state side args]
   (swap! state assoc-in [side :keep] true)
-  (say! state side {:user "__system__"
-                    :text (str (get-in @state [side :user :username]) " keeps his or her hand.")}))
+  (system-msg state side "keeps his or her hand."))
 
 (defn draw!
   ([state side] (draw! state side 1))
   ([state side n]
      (let [deck (get-in @state [side :deck])]
        (swap! state update-in [side :hand] #(concat % (take n deck))))
-     (swap! state update-in [side :deck] (partial drop n))
-     (say! state side {:user "__system__"
-                       :text (str (get-in @state [side :user :username]) " draws " n " card" (if (> n 1) "s." "."))})))
+     (swap! state update-in [side :deck] (partial drop n))))
 
-(defn gain!
-  ([state side resource] (gain! state side resource 1))
-  ([state side resource n]
-     (swap! state update-in [side resource] #(+ % n))
-     (say! state side {:user "__system__"
-                       :text (str (get-in @state [side :user :username])
-                                  " gains " n (name resource) (if (> n 1) "s." "."))})))
+(defn gain! [state side resource n]
+  (swap! state update-in [side resource] #(+ % n)))
 
 (defn pay!
   ([state side & args]
@@ -89,5 +97,4 @@
 (defn purge! [state side]
   (let [cards (get-in state [:runner :rig :programs])]
     ;; (filter (fn [card] (some #(= % "virus") (:subtype card))) cards)
-    (say! state side {:user "__system__"
-                      :text (str (get-in @state [side :user :username]) " purges viruses.")})))
+    ))
