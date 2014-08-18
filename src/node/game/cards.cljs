77799879b255928891b3a39e448b0e6db6283737
Minh Tran <mtgred@gmail.com>
2014-08-18 13:19:45 +0200

    More identity start of game abilities implementation

--- src/node/game/cards.cljs
+++ src/node/game/cards.cljs
@@ -6,8 +6,29 @@
   {"Andromeda: Dispossessed Ristie"
    {:game-init (do! {:effect [(gain :link 1) (draw 4)]})}
 
+   "Chaos Theory: Wünderkind"
+   {:game-init (do! {:effect [(gain :memory 1)]})}
+
+   "GRNDL: Power Unleashed"
+   {:game-init (do! {:effect [(gain :credit 5 :bad-publicity 1)]})}
+
+   "NBN: The World is Yours*"
+   {:game-init (do! {:effect [(gain :max-hand-size 1)]})}
+
+   ;; partial implementation
+   "Exile: Streethawk"
+   {:game-init (do! {:effect [(gain :link 1)]})}
+
+   "Iain Stirling: Retired Spook"
+   {:game-init (do! {:effect [(gain :link 1)]})}
+
    "Kate \"Mac\" McCaffrey: Digital Tinker"
    {:game-init (do! {:effect [(gain :link 1)]})}
 
-   "GRNDL: Power Unleashed"
-   {:game-init (do! {:effect [(gain :credit 5 :bad-publicity 1)]})}})
+   "Nasir Meidan: Cyber Explorer"
+   {:game-init (do! {:effect [(gain :link 1)]})}
+
+   "Reina Roja: Freedom Fighter"
+   {:game-init (do! {:effect [(gain :link 1)]})}
+
+   })
