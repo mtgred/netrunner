972ac734bb71f9ca3a4c090acd332bf59db12675
Minh Tran <mtgred@gmail.com>
2014-08-26 02:35:00 +0200

    deploy script update

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ rhc scp netrunner upload game.js app-root/data
 lein cljsbuild clean
 lein cljsbuild once prod &
 git push openshift master
-rhc scp netrunner upload resources/js/app.js app-root/repo/resources/js
+rhc scp netrunner upload resources/public/js/app.js app-root/repo/resources/public/js
