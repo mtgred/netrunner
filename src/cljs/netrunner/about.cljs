ff84a3746a13b1d2a1c63658f8944a199a58bb2d
Minh Tran <mtgred@gmail.com>
2014-09-19 16:03:04 +0200

    about page update

--- src/cljs/netrunner/about.cljs
+++ src/cljs/netrunner/about.cljs
@@ -12,18 +12,20 @@
      [:p "This website is created by an avid Netrunner player from Belgium. The goal is to provide a great way to create and test Netrunner decks online."]
 
      [:h3 "Contact"]
-     [:p "Twitter: " [:a {:href "http://twitter.com/mtgred"} "@mtgred"]]
+     [:p "Twitter: " [:a {:href "http://twitter.com/mtgred" :target "_blank"} "@mtgred"]]
      [:p "Email: " [:a {:href "mailto:mtgred@gmail.com"} "mtgred@gmail.com"]]
 
      [:h3 "Development"]
 
-     [:p "The card browser and deck builder are ready for use. The gameplay functionnalities are currently under development. " [:a {:href "https://www.dropbox.com/s/rpkhxafgile5spp/Cards%20status.xlsx"} "Card implementation status"]]
+     [:p "The card browser and deck builder are ready for use. The gameplay functionnalities are currently under development. Card automation "
+      [:a {:href "https://www.dropbox.com/s/rpkhxafgile5spp/Cards%20status.xlsx"
+           :target "_blank"} "status"] "."]
 
      [:p "The code is open source and available on "
-      [:a {:href "https://github.com/mtgred/netrunner"} "Github"] "."]
+      [:a {:href "https://github.com/mtgred/netrunner" :target "_blank"} "Github"] "."]
 
      [:p "Bug reports and feature suggestions can be submitted "
-      [:a {:href "https://github.com/mtgred/netrunner/issues"} "here"] "."]
+      [:a {:href "https://github.com/mtgred/netrunner/issues" :target "_blank"} "here"] "."]
 
      [:h3 "Disclaimer"]
      [:p "Netrunner and Android are trademarks of Fantasy Flight Publishing, Inc. and/or Wizards of the Coast LLC."]
