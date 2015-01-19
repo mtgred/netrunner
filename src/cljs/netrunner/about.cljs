87797e6938962f2823b79a972dca0952f0af3b4d
Minh Tran <mtgred@gmail.com>
2015-01-19 20:19:28 +0100

    frequently asked questions

--- src/cljs/netrunner/about.cljs
+++ src/cljs/netrunner/about.cljs
@@ -27,6 +27,22 @@
      [:p "Bug reports and feature suggestions can be submitted "
       [:a {:href "https://github.com/mtgred/netrunner/issues" :target "_blank"} "here"] "."]
 
+     [:h3 "Frequently Asked Questions"]
+     [:ul.list
+      [:li
+       [:h4 "How can I change my avatar?"]
+       [:p "Go to " [:a {:href "http://gravatar.com" :target "_blank"} "gravatar.com"]
+        " and create an account with the same email used to register on Jinteki.net."]]
+      [:li
+       [:h4 "How do I quit a game when it's finished?"]
+       [:p "A quit button will be added in the future. In the meanwhile, simply refresh your browser."]]
+      [:li
+       [:h4 "How can I make donation?"]
+       [:p "If you would like to contribute to running costs of Jinteki.net, my PayPal account is mtgred@gmail.com"]]
+      [:li
+       [:h4 "What is the best supported browser?"]
+       [:p "Google Chrome on a desktop/laptop is recommended. Safari and Firefox are supported. Drag and dropping cards during gameplay sometime causes Firefox to navigate to the card image and disconnect from the game. Internet Explorer is not supported."]]]
+
      [:h3 "Disclaimer"]
      [:p "Netrunner and Android are trademarks of Fantasy Flight Publishing, Inc. and/or Wizards of the Coast LLC."]
      [:p "This is website is not affiliated with Fantasy Flight Games or Wizards of the Coast."]])))
