c90772982b4eef69c8698b1b99a7fc285affca0d
nealterrell <neal.terrell@csulb.edu>
2015-07-03 08:12:36 -0700

    Auto-pump first try.

--- src/cljs/netrunner/gameboard.cljs
+++ src/cljs/netrunner/gameboard.cljs
@@ -155,6 +155,11 @@
         side (if (#{"HQ" "R&D" "Archives"} server) "Corp" "Runner")]
     (send-command "move" {:card card :server server})))
 
+(defn ability-costs [ab]
+  (when-let [cost (:cost ab)]
+    (str (clojure.string/join "" (for [c (partition 2 cost)]
+                                   (str (second c) " [" (capitalize (name (first c))) "]"))) ": ")))
+
 (defn card-view [{:keys [zone code type abilities counter advance-counter advancementcost current-cost subtype
                          advanceable rezzed strength current-strength title remotes selected hosted rec-counter]
                   :as cursor}
@@ -201,10 +206,13 @@
                     [:div {:on-click #(do (send-command action {:card @cursor}))} (capitalize action)])
                   actions)
              (map-indexed
-              (fn [i label]
-                [:div {:on-click #(do (send-command "ability" {:card @cursor :ability i})
-                                      (-> (om/get-node owner "abilities") js/$ .fadeOut))
-                       :dangerouslySetInnerHTML #js {:__html (add-symbols label)}}])
+              (fn [i ab]
+                (if (:auto-boost ab)
+                  [:div {:on-click #(do (send-command "auto-pump" {:card @cursor}))
+                         :dangerouslySetInnerHTML #js {:__html (add-symbols (str (ability-costs ab) (:label ab)))}}]
+                  [:div {:on-click #(do (send-command "ability" {:card @cursor :ability i})
+                                        (-> (om/get-node owner "abilities") js/$ .fadeOut))
+                         :dangerouslySetInnerHTML #js {:__html (add-symbols (str (ability-costs ab) (:label ab)))}}]))
               abilities)]))
         (when (= (first zone) "servers")
           (cond
