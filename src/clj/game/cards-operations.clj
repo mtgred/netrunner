92c1228d3c8f95c499ed9c2294c45cb702a55dbf
Joel Koepp <jkoepp@gmail.com>
2015-07-24 08:50:30 -0500

    implement Hellion Alpha Test

--- src/clj/game/cards-operations.clj
+++ src/clj/game/cards-operations.clj
@@ -89,6 +89,13 @@
    "Hedge Fund"
    {:effect (effect (gain :credit 9))}
 
+   "Hellion Alpha Test"
+   {:req (req (:installed-resource runner-reg))
+    :trace {:base 2 :choices {:req #(and (:installed %) (= (:type %) "Resource"))}
+            :msg (msg "add " (:title target) " to the top of the Stack")
+            :effect (effect (move :runner target :deck true))
+            :unsuccessful {:msg "take 1 bad publicity" :effect (effect (gain :corp :bad-publicity 1))}}}
+
    "Housekeeping"
    {:events {:runner-install {:req (req (= side :runner)) :choices (req (:hand runner))
                               :prompt "Choose a card to trash for Housekeeping" :once :per-turn
