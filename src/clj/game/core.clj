f3c959ee987669c3aa69dddd382847ff122fecab
nealterrell <neal.terrell@csulb.edu>
2015-08-02 18:24:43 -0700

    Fix copy/paste error.

--- src/clj/game/core.clj
+++ src/clj/game/core.clj
@@ -832,9 +832,10 @@
 (defn access-helper-hq [cards]
   {:prompt "Select a card to access."
    :choices (concat (when (some #(= (first (:zone %)) :hand) cards) ["Card from hand"])
-                    (map #(if (:rezzed %) (:title %) "Upgrade in HQ") (filter #(= (last (:zone %)) :content) cards)))
+                    (map #(if (:rezzed %) (:title %) "Unrezzed upgrade in HQ")
+                         (filter #(= (last (:zone %)) :content) cards)))
    :effect (req (case target
-                  "Upgrade in HQ"
+                "Unrezzed upgrade in HQ"
                   ; accessing an unrezzed upgrade
                   (let [unrezzed (filter #(and (= (last (:zone %)) :content) (not (:rezzed %))) cards)]
                     (if (= 1 (count unrezzed))
@@ -877,9 +878,10 @@
 (defn access-helper-rd [cards]
   {:prompt "Select a card to access."
    :choices (concat (when (some #(= (first (:zone %)) :deck) cards) ["Card from deck"])
-                    (map #(if (:rezzed %) (:title %) "Upgrade in R&D") (filter #(= (last (:zone %)) :content) cards)))
+                    (map #(if (:rezzed %) (:title %) "Unrezzed upgrade in R&D")
+                         (filter #(= (last (:zone %)) :content) cards)))
    :effect (req (case target
-                  "Upgrade in R&D"
+                  "Unrezzed upgrade in R&D"
                   ; accessing an unrezzed upgrade
                   (let [unrezzed (filter #(and (= (last (:zone %)) :content) (not (:rezzed %))) cards)]
                     (if (= 1 (count unrezzed))
@@ -922,10 +924,10 @@
 (defn access-helper-archives [cards]
   {:prompt "Select a card to access. You must access all cards."
    :choices (map #(if (= (last (:zone %)) :content)
-                   (if (:rezzed %) (:title %) "Upgrade in Archives")
+                   (if (:rezzed %) (:title %) "Unrezzed upgrade in Archives")
                    (:title %)) cards)
    :effect (req (case target
-                  "Upgrade in Archives"
+                  "Unrezzed upgrade in Archives"
                   ; accessing an unrezzed upgrade
                   (let [unrezzed (filter #(and (= (last (:zone %)) :content) (not (:rezzed %))) cards)]
                     (if (= 1 (count unrezzed))
