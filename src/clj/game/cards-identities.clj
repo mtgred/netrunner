04c5fc462ddff079c1e6930e7c5280ba30f1bd78
JHonaker <honaker215@gmail.com>
2015-08-24 18:37:28 -0400

    Removed unneeded calls to :once :per-turn

--- src/clj/game/cards-identities.clj
+++ src/clj/game/cards-identities.clj
@@ -85,7 +85,7 @@
 
    "Hayley Kaplan: Universal Scholar"
    {:events {:runner-install
-             {:optional {:prompt (msg "Install another " (:type target) " from Grip?") :once :per-turn
+             {:optional {:prompt (msg "Install another " (:type target) " from Grip?")
                          :req (req (and (first-event state side :runner-install) ;; If this is the first installation of the turn
                                         (some #(= (:type  %) (:type target)) (:hand runner)))) ;; and there are additional cards of that type in hand
                          :effect (req (let [type (:type target)]
