86dc507631c9026bafcbc798e296111fa82922f0
Joel Koepp <jkoepp@gmail.com>
2015-08-27 10:31:01 -0500

    improvement to Bad Times

--- src/clj/game/cards-operations.clj
+++ src/clj/game/cards-operations.clj
@@ -27,7 +27,13 @@
               (* 3 (:advance-counter target)) " [Credits]")}
 
    "Bad Times"
-   {:req (req tagged)}
+   {:req (req tagged)
+    :msg "force the Runner to lose 2[mu] until the end of the turn"
+    :effect (req (lose state :runner :memory 2)
+                 (when (< (:memory runner) 0)
+                  (system-msg state :runner "must trash programs to free up [mu]")))
+    :end-turn {:effect (req (gain state :runner :memory 2)
+                            (system-msg state :runner "regains 2[mu]"))}}
 
    "Beanstalk Royalties"
    {:effect (effect (gain :credit 3))}
