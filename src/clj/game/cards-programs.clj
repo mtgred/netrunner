45d3ecaf033b98936f5c008b7a77e012c997fe17
Joel Koepp <jkoepp@gmail.com>
2015-09-12 22:20:26 -0500

    enforce targeting of a Barrier with Surfer

--- src/clj/game/cards-programs.clj
+++ src/clj/game/cards-programs.clj
@@ -434,25 +434,26 @@
                                                               :yes-ability {:msg "jack out"
                                                                             :effect (effect (jack-out nil))}}}
                                                   card nil))}]}
+
    "Surfer"
    {:abilities [{:cost [:credit 2]
-                 :req (req (and (:run @state) (:rezzed current-ice)))
-                 :label "Swap a piece of barrier ice currently being encountered with a piece of ice directly before or after it"
+                 :req (req (and (:run @state) (:rezzed current-ice) (has? current-ice :subtype "Barrier")))
+                 :label "Swap the barrier ICE currently being encountered with a piece of ICE directly before or after it"
                  :effect (req (let [cice current-ice]
                                 (resolve-ability
                                   state side
-                                  {:prompt (msg "Choose an ice before or after " (:title cice))
-                                   :choices {:req #(and (= (:type %) "ICE") 
+                                  {:prompt (msg "Choose an ICE before or after " (:title cice))
+                                   :choices {:req #(and (= (:type %) "ICE")
                                                         (= (:zone %) (:zone cice))
                                                         (= 1 (abs (- (ice-index state %) (ice-index state cice)))))}
-                                   :msg "swap a piece of barrier ice"
+                                   :msg "swap a piece of barrier ICE"
                                    :effect (req (let [tgtndx (ice-index state target)
                                                       oldndx (ice-index state cice)]
                                                   (swap! state update-in (cons :corp (:zone cice))
                                                       #(assoc % tgtndx cice))
                                                   (swap! state update-in (cons :corp (:zone cice))
                                                       #(assoc % oldndx target))
-                                                  (swap! state update-in [:run] 
+                                                  (swap! state update-in [:run]
                                                       #(assoc % :position (inc tgtndx)))
                                                   (update-ice-strength state side (cons :corp (:zone cice)))
                                                   (update-run-ice state side)))}
