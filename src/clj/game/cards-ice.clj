c3898fa78165bc64eb83d7c8c88f207415ce95d0
Joel Koepp <jkoepp@gmail.com>
2015-09-22 10:56:58 -0500

    add watch state to IQ

--- src/clj/game/cards-ice.clj
+++ src/clj/game/cards-ice.clj
@@ -322,9 +322,16 @@
                          :effect (effect (damage :brain 1 {:card card}) (tag-runner :runner 1))}}]}
 
    "IQ"
-   {:abilities [end-the-run]
+   {:effect (req (add-watch state (keyword (str "iq" (:cid card)))
+                   (fn [k ref old new]
+                     (let [handsize (count (get-in new [:corp :hand]))]
+                       (when (not= (count (get-in old [:corp :hand])) handsize)
+                         (update! ref side (assoc (get-card ref card) :strength-bonus handsize))
+                         (update-ice-strength ref side (get-card ref card)))))))
+    :abilities [end-the-run]
     :strength-bonus (req (count (:hand corp)))
-    :rez-cost-bonus (req (count (:hand corp)))}
+    :rez-cost-bonus (req (count (:hand corp)))
+    :leave-play (req (remove-watch state (keyword (str "iq" (:cid card)))))}
 
    "Information Overload"
    {:abilities [{:label "Trace 1 - Give the Runner 1 tag"
