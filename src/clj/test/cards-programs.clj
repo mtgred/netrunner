4c4af11594184cc81d455d1faad45031eccffd64
nealterrell <neal.terrell@csulb.edu>
2015-09-22 18:11:44 -0700

    Tests for Djinn, Virus Breeding Ground, Issue #750.

--- src/clj/test/cards-programs.clj
+++ src/clj/test/cards-programs.clj
@@ -1,5 +1,56 @@
 (in-ns 'test.core)
 
+(deftest djinn-host-chakana
+  "Djinn - Hosted Chakana does not disable advancing agendas. Issue #750"
+  (do-game
+    (new-game (default-corp [(qty "Priority Requisition" 1)])
+              (default-runner [(qty "Djinn" 1) (qty "Chakana" 1)]))
+    (play-from-hand state :corp "Priority Requisition" "New remote")
+    (take-credits state :corp 2)
+    (play-from-hand state :runner "Djinn")
+    (let [djinn (get-in @state [:runner :rig :program 0])
+          agenda (get-in @state [:corp :servers :remote 0 :content 0])]
+      (is agenda "Agenda was installed")
+      (card-ability state :runner djinn 1)
+      (core/resolve-prompt state :runner {:card (find-card "Chakana" (:hand (get-runner)))})
+      (let [chak (first (:hosted (refresh djinn)))]
+        (is (= "Chakana" (:title chak)) "Djinn has a hosted Chakana")
+        (core/add-prop state :runner (first (:hosted (refresh djinn))) :counter 3) ; manually add 3 counters
+        (take-credits state :runner 2)
+        (core/advance state :corp {:card agenda})
+        (is (= 1 (:advance-counter (refresh agenda))) "Agenda was advanced")))))
+
+(deftest djinn-host-program
+  "Djinn - Host a non-icebreaker program"
+  (do-game
+    (new-game (default-corp)
+              (default-runner [(qty "Djinn" 1) (qty "Chakana" 1)]))
+    (take-credits state :corp)
+    (play-from-hand state :runner "Djinn")
+    (is (= 3 (:memory (get-runner))))
+    (let [djinn (get-in @state [:runner :rig :program 0])]
+      (card-ability state :runner djinn 1)
+      (core/resolve-prompt state :runner {:card (find-card "Chakana" (:hand (get-runner)))})
+      (is (= 3 (:memory (get-runner))) "No memory used to host on Djinn")
+      (is (= "Chakana" (:title (first (:hosted (refresh djinn))))) "Djinn has a hosted Chakana")
+      (is (= 1 (:credit (get-runner))) "Full cost to host on Djinn"))))
+
+(deftest djinn-tutor-virus
+  "Djinn - Tutor a virus program"
+  (do-game
+    (new-game (default-corp)
+              (default-runner [(qty "Djinn" 1) (qty "Parasite" 1)]))
+    (take-credits state :corp)
+    (play-from-hand state :runner "Djinn")
+    (core/move state :runner (find-card "Parasite" (:hand (get-runner))) :deck)
+    (is (zero? (count (:hand (get-runner)))) "No cards in hand after moving Parasite to deck")
+    (let [djinn (get-in @state [:runner :rig :program 0])]
+      (card-ability state :runner djinn 0)
+      (core/resolve-prompt state :runner {:card (find-card "Parasite" (:deck (get-runner)))})
+      (is (= "Parasite" (:title (first (:hand (get-runner))))) "Djinn moved Parasite to hand")
+      (is (= 2 (:credit (get-runner))) "1cr to use Djinn ability")
+      (is (= 2 (:click (get-runner))) "1click to use Djinn ability"))))
+
 (deftest magnum-opus-click
   "Magnum Opus - Gain 2 cr"
   (do-game
--- src/clj/test/cards-resources.clj
+++ src/clj/test/cards-resources.clj
@@ -10,18 +10,53 @@
     (let [kati (get-in @state [:runner :rig :resource 0])]
       (card-ability state :runner kati 0)
       (is (= 2 (:click (get-runner))))
-      (is (= 3 (:counter (core/get-card state kati))) "Store 3cr on Kati")
-      (card-ability state :runner (core/get-card state kati) 0)
+      (is (= 3 (:counter (refresh kati))) "Store 3cr on Kati")
+      (card-ability state :runner kati 0)
       (is (= 2 (:click (get-runner))) "Second use of Kati should not be allowed")
-      (is (= 3 (:counter (core/get-card state kati))) "Second use of Kati should not be allowed")
+      (is (= 3 (:counter (refresh kati))) "Second use of Kati should not be allowed")
       (take-credits state :runner 2)
       (is (= 5 (:credit (get-runner))) "Pass turn, take 2cr")
       (take-credits state :corp)
-      (card-ability state :runner (core/get-card state kati) 0)
-      (is (= 6 (:counter (core/get-card state kati))) "Store 3cr more on Kati")
+      (card-ability state :runner kati 0)
+      (is (= 6 (:counter (refresh kati))) "Store 3cr more on Kati")
       (take-credits state :runner 3)
       (is (= 8 (:credit (get-runner))) "Pass turn, take 3cr")
       (take-credits state :corp)
-      (card-ability state :runner (core/get-card state kati) 1)
+      (card-ability state :runner (refresh kati) 1)
       (is (= 14 (:credit (get-runner))) "Take 6cr from Kati")
-      (is (zero? (:counter (core/get-card state kati))) "No counters left on Kati"))))
+      (is (zero? (:counter (refresh kati))) "No counters left on Kati"))))
+
+(deftest virus-breeding-ground-gain
+  "Virus Breeding Ground - Gain counters"
+  (do-game
+    (new-game (default-corp) (default-runner [(qty "Virus Breeding Ground" 1)]))
+    (take-credits state :corp)
+    (play-from-hand state :runner "Virus Breeding Ground")
+    (let [vbg (get-in @state [:runner :rig :resource 0])]
+      (is (zero? (get vbg :counter 0)) "Virus Breeding Ground starts with 0 counters")
+      (take-credits state :runner 3)
+      (take-credits state :corp)
+      (is (= 1 (get (refresh vbg) :counter 0)) "Virus Breeding Ground gains 1 counter per turn")
+      (take-credits state :runner 3)
+      (take-credits state :corp)
+      (is (= 2 (get (refresh vbg) :counter 0)) "Virus Breeding Ground gains 1 counter per turn"))))
+
+(deftest virus-breeding-ground-gain
+  "Virus Breeding Ground - Move counters"
+  (do-game
+    (new-game (default-corp) (default-runner [(qty "Virus Breeding Ground" 1) (qty "Hivemind" 1)]))
+    (take-credits state :corp)
+    (play-from-hand state :runner "Virus Breeding Ground")
+    (play-from-hand state :runner "Hivemind")
+    (let [hive (get-in @state [:runner :rig :program 0])
+          vbg (get-in @state [:runner :rig :resource 0])]
+      (is (= 1 (get hive :counter 0)) "Hivemind starts with 1 counter")
+      (is (zero? (get vbg :counter 0)) "Virus Breeding Ground starts with 0 counters")
+      (take-credits state :runner 3)
+      (take-credits state :corp)
+      (is (= 1 (get (refresh vbg) :counter 0)) "Virus Breeding Ground gains 1 counter per turn")
+      (card-ability state :runner vbg 0)
+      (core/select state :runner {:card (refresh hive)})
+      (is (= 2 (get (refresh hive) :counter 0)) "Hivemind gained 1 counter")
+      (is (= 0 (get (refresh vbg) :counter 0)) "Virus Breeding Ground lost 1 counter"))))
+
