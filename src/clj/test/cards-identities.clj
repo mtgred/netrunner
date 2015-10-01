82762f72e020aef22e27395b48bb2dc5f79817db
Mike Harris <mharris717@gmail.com>
2015-10-01 17:04:37 -0400

    Couple tests for Nasir's ability

--- src/clj/test/cards-identities.clj
+++ src/clj/test/cards-identities.clj
@@ -28,3 +28,39 @@
     (play-from-hand state :runner "Magnum Opus")
     (is (= 1 (count (get-in @state [:runner :rig :program]))))
     (is (= 0 (:credit (get-runner))))))
+
+(deftest nasir-ability-basic
+  "Nasir Ability - Basic"
+  (do-game
+    (new-game 
+      (default-corp [(qty "Ice Wall" 3)]) 
+      (make-deck "Nasir Meidan: Cyber Explorer" []))
+    (play-from-hand state :corp "Ice Wall" "HQ")
+    (take-credits state :corp)
+
+    (core/click-run state :runner {:server "HQ"})
+    (let [iwall (get-in @state [:corp :servers :hq :ices 0])
+          nasir (get-in @state [:runner :identity])]
+      (core/rez state :corp iwall)
+      (is (= 5 (:credit (get-runner))))
+      (card-ability state :runner nasir 0)
+      (is (= 1 (:credit (get-runner)))))))
+
+(deftest nasir-ability-xanadu
+  "Nasir Ability - Xanadu"
+  (do-game
+    (new-game 
+      (default-corp [(qty "Ice Wall" 1)]) 
+      (make-deck "Nasir Meidan: Cyber Explorer" [(qty "Xanadu" 1)]))
+    (play-from-hand state :corp "Ice Wall" "HQ")
+    (take-credits state :corp)
+
+    (swap! state assoc-in [:runner :credit] 6)
+    (play-from-hand state :runner "Xanadu")
+    (core/click-run state :runner {:server "HQ"})
+    (let [iwall (get-in @state [:corp :servers :hq :ices 0])
+          nasir (get-in @state [:runner :identity])]
+      (core/rez state :corp iwall)
+      (is (= 3 (:credit (get-runner))))
+      (card-ability state :runner nasir 0)
+      (is (= 2 (:credit (get-runner)))))))
