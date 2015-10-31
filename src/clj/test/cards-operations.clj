4dc8e3cd7310665645ffa58774aefcb38a2bb7e3
Joel Koepp <jkoepp@gmail.com>
2015-10-31 10:20:36 -0500

    test for Diversified Portfolio

--- src/clj/test/cards-operations.clj
+++ src/clj/test/cards-operations.clj
@@ -1,5 +1,18 @@
 (in-ns 'test.core)
 
+(deftest diversified-portfolio
+  (do-game
+    (new-game (default-corp [(qty "Diversified Portfolio" 1) (qty "Paper Wall" 1)
+                             (qty "PAD Campaign" 3)])
+              (default-runner))
+    (core/gain state :corp :click 2)
+    (play-from-hand state :corp "Paper Wall" "New remote")
+    (play-from-hand state :corp "PAD Campaign" "New remote")
+    (play-from-hand state :corp "PAD Campaign" "New remote")
+    (play-from-hand state :corp "PAD Campaign" "New remote")
+    (play-from-hand state :corp "Diversified Portfolio")
+    (is (= 7 (:credit (get-corp))) "Ignored remote with ICE but no server contents")))
+
 (deftest hedge-fund
   (do-game
     (new-game (default-corp) (default-runner))
