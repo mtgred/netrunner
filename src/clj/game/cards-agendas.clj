c26bc495ba8557e9278594db26a8cddbd316d015
Joel Koepp <jkoepp@gmail.com>
2015-11-24 03:58:27 -0600

    add message to Oaktown Renovation

--- src/clj/game/cards-agendas.clj
+++ src/clj/game/cards-agendas.clj
@@ -280,6 +280,7 @@
    "Oaktown Renovation"
    {:install-state :face-up
     :events {:advance {:req (req (= (:cid card) (:cid target)))
+                       :msg (msg "gain " (if (>= (:advance-counter (get-card state card)) 5) "3" "2") " [Credits]")
                        :effect (req (gain state side :credit
                                           (if (>= (:advance-counter (get-card state card)) 5) 3 2)))}}}
 
