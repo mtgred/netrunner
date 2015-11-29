3a290508e27d1aea797c5fc1a7e2cbd8c24d1ec7
Joel Koepp <jkoepp@gmail.com>
2015-11-28 19:28:14 -0600

    test for refresh of recurring credits on cards hosted after install

--- src/clj/test/core-game.clj
+++ src/clj/test/core-game.clj
@@ -19,3 +19,26 @@
     (let [gord (get-in @state [:runner :rig :program 0])]
       (core/trash state :runner gord)
       (is (= 4 (:memory (get-runner))) "Trashing the program restored MU"))))
+
+(deftest refresh-recurring-credits-hosted
+  "host - Recurring credits on cards hosted after install refresh properly"
+  (do-game
+    (new-game (default-corp [(qty "Ice Wall" 3) (qty "Hedge Fund" 3)])
+              (default-runner [(qty "Compromised Employee" 1) (qty "Off-Campus Apartment" 1)]))
+    (play-from-hand state :corp "Ice Wall" "HQ")
+    (take-credits state :corp 2)
+    (play-from-hand state :runner "Off-Campus Apartment")
+    (play-from-hand state :runner "Compromised Employee")
+    (let [iwall (get-in @state [:corp :servers :hq :ices 0])
+          apt (get-in @state [:runner :rig :resource 0])]
+      (card-ability state :runner apt 1) ; use Off-Campus option to host an installed card
+      (prompt-select :runner (find-card "Compromised Employee" (get-in @state [:runner :rig :resource])))
+      (let [cehosted (first (:hosted (refresh apt)))]
+        (card-ability state :runner cehosted 0) ; take Comp Empl credit
+        (is (= 4 (:credit (get-runner))))
+        (is (= 0 (:rec-counter (refresh cehosted))))
+        (core/rez state :corp iwall)
+        (is (= 5 (:credit (get-runner))) "Compromised Employee gave 1 credit from ice rez")
+        (take-credits state :runner)
+        (take-credits state :corp)
+        (is (= 1 (:rec-counter (refresh cehosted))) "Compromised Employee recurring credit refreshed")))))
