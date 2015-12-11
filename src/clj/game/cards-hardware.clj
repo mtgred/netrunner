44a08e4b9ffd0d67fd10d9b1e48c6fa8790e1a20
Joel Koepp <jkoepp@gmail.com>
2015-12-11 15:17:05 -0600

    prevent Dinosaurus from adding MU when hosted icebreaker is trashed

--- src/clj/game/cards-hardware.clj
+++ src/clj/game/cards-hardware.clj
@@ -162,22 +162,29 @@
    "Dinosaurus"
    {:abilities [{:label "Install a non-AI icebreaker on Dinosaurus"
                  :req (req (empty? (:hosted card))) :cost [:click 1]
-                 :prompt "Choose a non-AI icebreaker to install on Dinosaurus from your grip"
+                 :prompt "Choose a non-AI icebreaker in your Grip to install on Dinosaurus"
                  :choices {:req #(and (has? % :subtype "Icebreaker")
                                       (not (has? % :subtype "AI"))
                                       (= (:zone %) [:hand]))}
                  :effect (effect (gain :memory (:memoryunits target))
                                  (runner-install target {:host-card card})
+                                 (update! (assoc (get-card state card) :dino-breaker (:cid target)))
                                  (update-breaker-strength target))}
                 {:label "Host an installed non-AI icebreaker on Dinosaurus"
                  :req (req (empty? (:hosted card)))
-                 :prompt "Choose a program to host on Dinosaurus"
+                 :prompt "Choose an installed non-AI icebreaker to host on Dinosaurus"
                  :choices {:req #(and (has? % :subtype "Icebreaker")
                                       (not (has? % :subtype "AI"))
                                       (:installed %))}
-                 :msg (msg "host " (:title target)) :effect (effect (host card target))}]
+                 :msg (msg "host " (:title target))
+                 :effect (effect (host card target)
+                                 (update! (assoc (get-card state card) :dino-breaker (:cid target)))
+                                 (gain :memory (:memoryunits target)))}]
     :events {:pre-breaker-strength {:req (req (= (:cid target) (:cid (first (:hosted card)))))
-                                    :effect (effect (breaker-strength-bonus 2))}}}
+                                    :effect (effect (breaker-strength-bonus 2))}
+             :card-moved {:req (req (= (:cid target) (:dino-breaker (get-card state card))))
+                          :effect (effect (update! (dissoc card :dino-breaker))
+                                          (lose :memory (:memoryunits target)))}}}
 
    "Doppelgänger"
    {:effect (effect (gain :memory 1)) :leave-play (effect (lose :memory 1))
