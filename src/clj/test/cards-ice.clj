6a488e83d1ccd814ffa12e963f6e5e89f3af4fae
Joel Koepp <jkoepp@gmail.com>
2015-12-21 09:22:47 -0600

    test for Special Offer trashing and updating run position

--- src/clj/test/cards-ice.clj
+++ src/clj/test/cards-ice.clj
@@ -58,6 +58,23 @@
       (take-credits state :corp 2)
       (is (= 5 (:current-strength (refresh lotus))) "Lotus Field strength increased"))))
 
+(deftest special-offer-trash-ice-during-run
+  "Special Offer trashes itself and updates the run position"
+  (do-game
+    (new-game (default-corp [(qty "Ice Wall" 1) (qty "Special Offer" 1)])
+              (default-runner))
+    (play-from-hand state :corp "Ice Wall" "HQ")
+    (play-from-hand state :corp "Special Offer" "HQ")
+    (take-credits state :corp 1)
+    (core/click-run state :runner {:server "HQ"})
+    (is (= 2 (:position (get-in @state [:run]))) "Initial position approaching Special Offer")
+    (let [special (get-in @state [:corp :servers :hq :ices 1])]
+      (core/rez state :corp special)
+      (is (= 4 (:credit (get-corp))))
+      (card-ability state :corp special 0)
+      (is (= 9 (:credit (get-corp))) "Special Offer paid 5 credits")
+      (is (= 1 (:position (get-in @state [:run]))) "Run position updated; now approaching Ice Wall"))))
+
 (deftest tmi
   "TMI ICE test"
   (do-game
@@ -69,7 +86,6 @@
       (prompt-choice :corp 0)
       (prompt-choice :runner 0)
       (is (get-in (refresh tmi) [:rezzed])))))
-
 
 (deftest tmi-derez
   "TMI ICE trace derez"
