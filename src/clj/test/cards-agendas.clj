a3f85731eb4371ac05df9f27711d786e06afe75c
Lukasz Dobrogowski <lukasz.dobrogowski@gmail.com>
2015-12-21 14:17:55 +0100

    added a unit test for 15 minutes

--- src/clj/test/cards-agendas.clj
+++ src/clj/test/cards-agendas.clj
@@ -1,5 +1,42 @@
 (in-ns 'test.core)
 
+(deftest fifteen-minutes
+  "15 Minutes - check if it works correctly from both sides"
+  (do-game
+    (new-game (default-corp [(qty "15 Minutes" 1)]) (default-runner))
+    (play-from-hand state :corp "15 Minutes" "New remote")
+    (take-credits state :corp)
+    ; use 15 minutes to take it away from runner
+    (core/click-run state :runner {:server "Server 1"})
+    (core/no-action state :corp nil)
+    (core/successful-run state :runner nil)
+    (prompt-choice :runner "Steal")
+    (take-credits state :runner)
+    (is (= 1 (:agenda-point (get-runner))))
+    (is (= 1 (count (:scored (get-runner)))))
+    (let [fifm (first (:scored (get-runner)))]
+      (is (= 3 (:click (get-corp))))
+      (is (= 1 (count (:abilities (refresh fifm)))))
+      (card-ability state :corp (refresh fifm) 0)
+      (is (= 0 (:agenda-point (get-runner))))
+      (is (= 0 (count (:scored (get-runner))))))
+    (is (= "15 Minutes" (:title (first (:deck (get-corp))))))
+    (is (= 2 (:click (get-corp))))
+    ; use 15 minutes to take it away from corp (hey, maybe some obscure case happens where corp would want that)
+    (core/click-draw state :corp 1)
+    (play-from-hand state :corp "15 Minutes" "New remote")
+    (take-credits state :runner)
+    (score-agenda state :corp (get-in @state [:corp :servers :remote2 :content 0]))
+    (is (= 1 (:agenda-point (get-corp))))
+    (is (= 1 (count (:scored (get-corp)))))
+    (let [fifm (first (:scored (get-corp)))]
+      (is (= 1 (count (:abilities (refresh fifm)))))
+      (card-ability state :corp (refresh fifm) 0)
+      (is (= 0 (:agenda-point (get-corp))))
+      (is (= 0 (count (:scored (get-corp))))))
+    (is (= "15 Minutes" (:title (first (:deck (get-corp))))))
+    ))
+
 (deftest ancestral-imager
   "Ancestral Imager - damage on jack out"
   (do-game
