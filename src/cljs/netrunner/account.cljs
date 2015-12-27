a63fb6c40ea0ffcadec7491ede93d3966d822278
Jon Cherlin <jonathan.cherlin@domandtom.com>
2015-12-26 23:56:37 -0500

    adding my account navigation

--- src/cljs/netrunner/account.cljs
+++ src/cljs/netrunner/account.cljs
@@ -7,21 +7,42 @@
             [netrunner.appstate :refer [app-state]]
             [netrunner.ajax :refer [POST GET]]))
 
+(def alt-arts-channel (chan))
+(go (let [cards (->> (<! (GET "/data/altarts"))
+                     :json
+                     (filter :versions)
+                     (map #(update-in % [:versions] conj "default"))
+                     (map #(assoc % :title (some (fn [c] (when (= (:code c) (:code %)) (:title c))) (:cards @app-state))))
+                     (into {} (map (juxt :code identity))))]
+      (swap! app-state assoc :alt-arts cards)
+      (put! alt-arts-channel cards)))
+
 (defn handle-post [event owner url ref]
   (.preventDefault event)
   (om/set-state! owner :flash-message "Updating profile...")
   (swap! app-state assoc-in [:options :enablesounds] (om/get-state owner :enablesounds))
   (swap! app-state assoc-in [:options :background] (om/get-state owner :background))
 
-  (let [params (-> event .-target js/$ .serialize)
-        _ (.-serialize (js/$ (.-target event)))] ;; params is nil when built in :advanced mode. This fixes the issue.
-    (go (let [response (<! (POST url params))]
+  (.setItem js/localStorage "enablesounds" (om/get-state owner :enablesounds))
+
+  (let [params (:options @app-state)]
+    (prn params)
+    (go (let [response (<! (POST url params :json))]
           (if (= (:status response) 200)
             (om/set-state! owner :flash-message "Profile updated!")
             (case (:status response)
               401 (om/set-state! owner :flash-message "Invalid login or password")
               421 (om/set-state! owner :flash-message "No account with that email address exists")
               :else (om/set-state! owner :flash-message "Profile updated - Please refresh your browser")))))))
+
+(defn image-url [card version]
+  (str "/img/cards/" card (when-not (= version "default") (str "-" version)) ".png"))
+
+(defn alt-art-name [type]
+  (case type
+    "alt" "Alternate"
+    "wc2015" "World Champion 2015"
+    "Official"))
 
 (defn account-view [user owner]
   (reify
@@ -31,7 +52,10 @@
     om/IWillMount
     (will-mount [this]
       (om/set-state! owner :background (get-in user [:options :background]))
-      (om/set-state! owner :enablesounds (get-in user [:options :enablesounds])))
+      (om/set-state! owner :enablesounds (get-in user [:options :enablesounds]))
+      (go (while true
+            (let [cards (<! alt-arts-channel)]
+              (om/set-state! owner :cards cards)))))
 
     om/IRenderState
     (render-state [this state]
@@ -68,7 +92,33 @@
                                 :on-change #(om/set-state! owner :background (.. % -target -value))
                                 :checked (= (om/get-state owner :background) (:ref option))}]
                 (:name option)]])
-            [:div.button-bar
+
+            (when (:special user)
+              [:div [:h3 {:style {:margin-top "1em"}} "Alt arts"]
+               [:select {:on-change #(do (om/set-state! owner :alt-card (.. % -target -value))
+                                         (om/set-state! owner :alt-card-version
+                                                        (get-in @app-state [:options :alt-arts (keyword (.. % -target -value))] "default")))}
+                (for [card (sort-by :title (vals (:alt-arts @app-state)))]
+                  [:option {:value (:code card)} (:title card)])]
+
+               [:div
+                (for [version (get-in @app-state [:alt-arts (om/get-state owner :alt-card) :versions])]
+                  (let [url (image-url (om/get-state owner :alt-card) version)]
+                    [:div {:style {:float "left" :margin "10px"}}
+                     [:div {:style {:text-align "center"}}
+                      [:div [:label [:input {:type "radio"
+                                             :name "alt-art-radio"
+                                             :value version
+                                             :on-change #(do (om/set-state! owner :alt-card-version (.. % -target -value))
+                                                             (swap! app-state update-in [:options :alt-arts]
+                                                                    assoc (keyword (om/get-state owner :alt-card)) (.. % -target -value))
+                                                             (prn (get-in @app-state [:options :alt-arts])))
+                                             :checked (= (om/get-state owner :alt-card-version) version)}]
+                             (alt-art-name version)]]]
+                     [:div
+                      [:img {:style {:width "150px" :margin-top "5px"} :src url :onError #(-> % .-target js/$ .hide) :onLoad #(-> % .-target js/$ .show)}]]]))]])
+
+            [:div.button-bar {:style {:clear "both"}}
              [:button {:style {:margin "2em 0"}} "Update Profile"]]]]]]))))
 
 (defn unlogged-view [user owner]
--- src/cljs/netrunner/appstate.cljs
+++ src/cljs/netrunner/appstate.cljs
@@ -3,7 +3,8 @@
 (def app-state
   (atom {:active-page "/"
          :user (js->clj js/user :keywordize-keys true)
-         :options (js->clj js/user.options :keywordize-keys true)
+         :options (merge {:background "lobby-bg" :enablesounds (js->clj (.getItem js/localStorage "enablesounds"))}
+                         (:options (js->clj js/user :keywordize-keys true)))
          :cards [] :sets [] :mwl []
          :decks [] :decks-loaded false
          :games [] :gameid nil :messages []}))
