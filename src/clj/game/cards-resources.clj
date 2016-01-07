eaa347a2a4e25e8eaf66f0a299a7fc469237d6cb
Joel Koepp <jkoepp@gmail.com>
2016-01-07 10:27:27 -0600

    add :msg to Hunting Grounds
    fixes #1065

--- src/clj/game/cards-resources.clj
+++ src/clj/game/cards-resources.clj
@@ -252,7 +252,8 @@
    {:abilities [{:label "Prevent a \"when encountered\" ability on a piece of ICE"
                  :msg "prevent a \"when encountered\" ability on a piece of ICE"
                  :once :per-turn}
-                 {:label "Install the top 3 cards of your Stack facedown"
+                 {:label "[Trash]: Install the top 3 cards of your Stack facedown"
+                  :msg "install the top 3 cards of their Stack facedown"
                   :effect (req (trash state side card {:cause :ability-cost})
                                (doseq [c (take 3 (:deck runner))]
                                   (runner-install state side c {:facedown true})))}]}
