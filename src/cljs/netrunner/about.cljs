c5ca8d70fb190b93a70381d2b3cfaa5365a0a92d
Lukasz Dobrogowski <lukasz.dobrogowski@gmail.com>
2016-01-11 16:08:42 +0100

    Moved existing FAQ to the new Help page

--- src/cljs/netrunner/about.cljs
+++ src/cljs/netrunner/about.cljs
@@ -22,7 +22,7 @@
      [:h3 "Development"]
 
      [:p "The gameplay functionalities are currently under development. Check "
-      [:a {:href "https://www.dropbox.com/s/rpkhxafgile5spp/Cards%20status.xlsx"
+      [:a {:href "https://docs.google.com/spreadsheets/d/1ICv19cNjSaW9C-DoEEGH3iFt09PBTob4CAutGex0gnE/pubhtml"
            :target "_blank"} "card automation status"] "."]
 
      [:p "The code is open source and available on "
@@ -31,28 +31,11 @@
      [:p "Bug reports and feature suggestions can be submitted "
       [:a {:href "https://github.com/mtgred/netrunner/issues" :target "_blank"} "here"] "."]
 
-     [:h3 "Frequently Asked Questions"]
-     [:ul.list
-      [:li
-       [:h4 "How can I change my avatar?"]
-       [:p "Go to " [:a {:href "http://gravatar.com" :target "_blank"} "gravatar.com"]
-        " and create an account with the same email used to register on Jinteki.net."]]
-      [:li
-       [:h4 "How can I make a donation?"]
-       [:p "My PayPal account is mtgred@gmail.com."]
-       [:p "If you use Bitcoin, my address is "
-        [:span.bitcoin "1ByjuLNZRvG17YaRKgKUjNi3c4qQ3daX3g" [:img.qr {:src "/img/bitcoin.png"}]] "."]
-       [:p "Contributions are appreciated and help finance fast servers."]]
-      [:li
-       [:h4 "There are too many cards or menu options to fit the screen."]
-       [:p "Decrease the zoom level of your browser and you should be able to see everything."]]
-      [:li
-       [:h4 "What is the best supported browser?"]
-       [:p "Google Chrome on a desktop or laptop is recommended. Firefox and Safari should work fine too."]]
-      [:li
-       [:h4 "Is there documentation on how to use jinteki.net?"]
-       [:p "Read the "
-        [:a {:href "https://github.com/mtgred/netrunner/wiki/Jinteki.net-Guide" :target "_blank"} "Jinteki.net Guide"] " on the Github wiki." ]]]
+     [:p "Donations can be submitted to PayPal account mtgred@gmail.com or to Bitcoin address "
+      [:span.bitcoin "1ByjuLNZRvG17YaRKgKUjNi3c4qQ3daX3g" [:img.qr {:src "/img/bitcoin.png"}]]
+      "."]
+
+     [:p "Contributions are appreciated and help finance fast servers."]
 
      [:h3 "Contributors"]
      [:p "A big props to Neal Terrell (nealpro), Joel Koepp (JoelCFC25) and Dominic Kexel (queueseven) who actively contribute to the success of the project with regular code submissions, the management of the issue tracker and answers to questions in the chat. It would not be the same without your help."]
--- src/cljs/netrunner/help.cljs
+++ src/cljs/netrunner/help.cljs
@@ -8,7 +8,24 @@
   (om/component
     (sab/html
       [:div.help.panel.blue-shade
-       [:h3 "Help"]
-       [:p "Help page test."]])))
+       [:h3 "Frequently Asked Questions"]
+       [:ul.list
+        [:li
+         [:h4 "How can I change my avatar?"]
+         [:p "Go to " [:a {:href "http://gravatar.com" :target "_blank"} "gravatar.com"]
+          " and create an account with the same email as the one used to register on Jinteki.net."]]
+        [:li
+         [:h4 "How can I make a donation?"]
+         [:p "Donation info can be found on the " [:a {:href "/about"} "About"] " page."]]
+        [:li
+         [:h4 "There are too many cards or menu options to fit the screen."]
+         [:p "Decrease the zoom level of your browser and you should be able to see everything."]]
+        [:li
+         [:h4 "What is the best supported browser?"]
+         [:p "Google Chrome on a desktop or laptop is recommended. Firefox and Safari should work fine too."]]
+        [:li
+         [:h4 "Is there documentation on how to use jinteki.net?"]
+         [:p "Read the "
+          [:a {:href "https://github.com/mtgred/netrunner/wiki/Jinteki.net-Guide" :target "_blank"} "Jinteki.net Guide"] " on the Github wiki." ]]]])))
 
 (om/root help app-state {:target (. js/document (getElementById "help"))})
