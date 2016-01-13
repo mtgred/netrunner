54dbc19632366a7e9c5ff0f479dc1416a64ec848
Filip Gokstorp <filip@gokstorp.se>
2016-01-13 14:12:26 +0000

    Adds toast to Nasir.

--- src/clj/game/cards-identities.clj
+++ src/clj/game/cards-identities.clj
@@ -236,16 +236,21 @@
 
    "Nasir Meidan: Cyber Explorer"
    {:effect (effect (gain :link 1))
+    :events {:rez {:req (req (and (:run @state)
+                                  ;; check that the rezzed item is the encountered ice
+                                  (= (:cid target)
+                                     (:cid (get-card state current-ice)))))
+                   :effect (req (toast state :runner "Click Nasir Meidan: Cyber Explorer to lose all credits and gain credits equal to the rez cost of the newly rezzed ice." "info"))}}
     :abilities [{:req (req (and (:run @state)
                                 (:rezzed (get-card state current-ice))))
                  :effect (req (let [current-ice (get-card state current-ice)]
-                           (trigger-event state side :pre-rez-cost current-ice)
-                           (let [cost (rez-cost state side current-ice)]
-                             (lose state side :credit (:credit runner))
-                             (gain state side :credit cost)
-                             (system-msg state side (str "loses all credits and gains " cost
-                                                         " [Credits] from the rez of " (:title current-ice)))
-                             (swap! state update-in [:bonus] dissoc :cost))))}]}
+                                (trigger-event state side :pre-rez-cost current-ice)
+                                (let [cost (rez-cost state side current-ice)]
+                                  (lose state side :credit (:credit runner))
+                                  (gain state side :credit cost)
+                                  (system-msg state side (str "loses all credits and gains " cost
+                                                              " [Credits] from the rez of " (:title current-ice)))
+                                  (swap! state update-in [:bonus] dissoc :cost))))}]}
 
    "NBN: Making News"
    {:recurring 2}
