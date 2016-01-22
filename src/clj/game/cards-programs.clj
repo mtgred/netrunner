9a043eac173cff62d43c5381f79a3b925d990654
Joel Koepp <jkoepp@gmail.com>
2016-01-22 14:28:13 -0600

    remove all automation from Pawn

--- src/clj/game/cards-programs.clj
+++ src/clj/game/cards-programs.clj
@@ -457,26 +457,32 @@
    {:recurring 2}
 
    "Pawn"
-   {:abilities [{:prompt "Host Pawn on the outermost ICE of a central server" :cost [:click 1]
+   {:abilities [{:label "Host Pawn on the outermost ICE of a central server"
+                 :prompt "Host Pawn on the outermost ICE of a central server" :cost [:click 1]
                  :choices {:req #(and (ice? %)
                                       (= (last (:zone %)) :ices)
-                                      (some #{:hq :rd :archives} (rest (butlast (:zone %)))))}
+                                      (is-central? (second (:zone %))))}
                  :msg (msg "host it on " (card-str state target))
-                 :effect (effect (host target card))}]
-    :events {:successful-run
-             {:req (req (ice? card))
-              :effect (req (let [i (ice-index state (:host card))
-                                 nextice (when (> i 0) (nth (get-in @state
-                                                            (vec (concat [:corp] (:zone (:host card))))) (dec i)))]
-                             (if (pos? i)
-                               (host state side nextice card)
-                               (do (resolve-ability state side
-                                     {:prompt "Choose a Caïssa program to install from your Grip or Heap"
-                                      :show-discard true
-                                      :choices {:req #(and (has-subtype? % "Caïssa")
-                                                           (#{[:hand] [:discard]} (:zone %)))}
-                                      :effect (effect (runner-install target {:no-cost true}))} card nil)
-                                    (trash state side card)))))}}}
+                 :effect (effect (host target card))}
+                {:label "Advance to next ICE"
+                 :prompt "Choose the next innermost ICE to host Pawn on it"
+                 :choices {:req #(and (ice? %)
+                                      (= (last (:zone %)) :ices)
+                                      (is-central? (second (:zone %))))}
+                 :msg (msg "host it on " (card-str state target))
+                 :effect (effect (host target card))}
+                {:label "Trash Pawn and install a Caïssa from your Grip or Heap, ignoring all costs"
+                 :effect (req (let [this-pawn (:cid card)]
+                                (resolve-ability
+                                  state side
+                                  {:prompt "Choose a Caïssa program to install from your Grip or Heap"
+                                   :show-discard true
+                                   :choices {:req #(and (has-subtype? % "Caïssa")
+                                                        (not= (:cid %) this-pawn)
+                                                        (#{[:hand] [:discard]} (:zone %)))}
+                                   :msg (msg "install " (:title target))
+                                   :effect (effect (runner-install target {:no-cost true}))} card nil)
+                                (trash state side card)))}]}
 
    "Pheromones"
    {:recurring (req (when (< (get card :rec-counter 0) (:counter card))
