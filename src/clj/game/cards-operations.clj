17f1b971e53250d89c20d9b12e969f88b5018eef
Joel Koepp <jkoepp@gmail.com>
2016-01-29 15:59:03 -0600

    implement Dedication Ceremony and Heritage Committee

--- src/clj/game/cards-operations.clj
+++ src/clj/game/cards-operations.clj
@@ -175,6 +175,12 @@
    "Cyberdex Trial"
    {:effect (effect (purge))}
 
+   "Dedication Ceremony"
+   {:prompt "Choose a faceup card"
+    :choices {:req rezzed?}
+    :msg (msg "place 3 advancement tokens on " (card-str state target))
+    :effect (effect (add-prop :corp target :advance-counter 3 {:placed true}))}
+
    "Defective Brainchips"
    {:events {:pre-damage {:req (req (= target :brain)) :msg "to do 1 additional brain damage"
                           :once :per-turn :effect (effect (damage-bonus :brain 1))}}}
@@ -224,6 +230,15 @@
                             (system-msg (str "adds " (:title target) " to the top of the Stack")))
             :unsuccessful {:msg "take 1 bad publicity"
                            :effect (effect (gain :corp :bad-publicity 1))}}}
+
+   "Heritage Committee"
+   {:effect (effect (draw 3)
+                    (resolve-ability
+                      {:prompt "Choose a card in HQ to put on top of R&D"
+                       :choices (req (:hand corp)) :not-distinct true
+                       :msg "draw 3 cards and add 1 card from HQ to the top of R&D"
+                       :effect (effect (move target :deck {:front true}))}
+                     card nil))}
 
    "Housekeeping"
    {:events {:runner-install {:req (req (= side :runner))
