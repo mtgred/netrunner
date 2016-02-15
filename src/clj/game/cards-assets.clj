f699d5efddde19b534d4380614c9623c823e3a28
Lukasz Dobrogowski <lukasz.dobrogowski@gmail.com>
2016-02-15 21:51:41 +0100

    Implemented Advanced Assembly Lines

--- src/clj/game/cards-assets.clj
+++ src/clj/game/cards-assets.clj
@@ -51,6 +51,20 @@
   {"Adonis Campaign"
    (campaign 12 3)
 
+   "Advanced Assembly Lines"
+   {:effect (effect (gain :credit 3))
+    :msg (msg "gain 3 [Credits]")
+    :abilities [{:label "[Trash]: Install a non-agenda card from HQ"
+                 :effect (effect (trash card) (corp-install target nil))
+                 :msg (msg (corp-install-msg target))
+                 :prompt "Choose a non-agenda card to install from HQ"
+                 :priority true
+                 :req (req (not (:run @state)))
+                 :choices {:req #(and (not (is-type? % "Operation"))
+                                      (not (is-type? % "Agenda"))
+                                      (= (:zone %) [:hand])
+                                      (= (:side %) "Corp"))}}]}
+
    "Aggressive Secretary"
    (advance-ambush 2 {:effect
                       (req (let [agg card
