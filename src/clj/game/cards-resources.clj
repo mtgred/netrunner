810bdfb7779c3f21cd1356829a856e1c92773617
Joel Koepp <jkoepp@gmail.com>
2016-02-21 10:07:47 -0600

    Tallie Perrault should be optional

--- src/clj/game/cards-resources.clj
+++ src/clj/game/cards-resources.clj
@@ -740,10 +740,21 @@
    {:abilities [{:label "Draw 1 card for each Corp bad publicity"
                  :effect (effect (trash card {:cause :ability-cost}) (draw (:bad-publicity corp)))
                  :msg (msg "draw " (:bad-publicity corp) " cards")}]
-    :events {:play-operation {:msg "give the Corp 1 bad publicity and take 1 tag"
-                              :effect (effect (gain :bad-publicity 1) (tag-runner :runner 1))
-                              :req (req (or (has-subtype? target "Black Ops")
-                                            (has-subtype? target "Gray Ops")))}}}
+    :events {:play-operation
+             {:req (req (or (has-subtype? target "Black Ops")
+                            (has-subtype? target "Gray Ops")))
+              :effect (req (show-wait-prompt state :corp "Runner to use Tallie Perrault")
+                           (resolve-ability
+                             state :runner
+                             {:optional
+                              {:prompt "Use Tallie Perrault to give the Corp 1 bad publicity and take 1 tag?"
+                               :player :runner
+                               :yes-ability {:msg "give the Corp 1 bad publicity and take 1 tag"
+                                             :effect (effect (gain :corp :bad-publicity 1)
+                                                             (tag-runner :runner 1)
+                                                             (clear-wait-prompt :corp))}
+                               :no-ability {:effect (effect (clear-wait-prompt :corp))}}}
+                            card nil))}}}
 
    "Technical Writer"
    {:data {:counter-type "Credit"}
