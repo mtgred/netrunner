9350f731e8cc7d6f8ac171369b2847d266334972
Joel Koepp <jkoepp@gmail.com>
2016-02-23 12:38:39 -0600

    use watch state for Resistor strength

--- src/clj/game/cards-ice.clj
+++ src/clj/game/cards-ice.clj
@@ -738,7 +738,14 @@
    {:abilities [end-the-run]}
 
    "Resistor"
-   {:strength-bonus (req (get-in @state [:runner :tag]))
+   {:effect (req (add-watch state (keyword (str "resistor" (:cid card)))
+                   (fn [k ref old new]
+                     (let [tags (get-in new [:runner :tag])]
+                       (when (not= (get-in old [:runner :tag]) tags)
+                         (update! ref side (assoc (get-card ref card) :strength-bonus tags))
+                         (update-ice-strength ref side (get-card ref card)))))))
+    :strength-bonus (req (:tag runner))
+    :leave-play (req (remove-watch state (keyword (str "resistor" (:cid card)))))
     :abilities [(trace-ability 4 end-the-run)]}
 
    "Rototurret"
