7f0eb99ea18634d75ceeba81fa088e73f5a948ae
Joel Koepp <jkoepp@gmail.com>
2016-02-25 20:58:51 -0600

    implement Councilman

--- src/clj/game/cards-resources.clj
+++ src/clj/game/cards-resources.clj
@@ -105,6 +105,24 @@
                    :msg "gain 1 [Credits]"
                    :effect (effect (gain :runner :credit 1))}}}
 
+   "Councilman"
+   {:events {:rez {:req (req (and (#{"Asset" "Upgrade"} (:type target))
+                                  (can-pay? state :runner nil [:credit (rez-cost state :corp target)])))
+                   :effect (req (toast state :runner "Click Councilman to derez the asset or upgrade that was just rezzed" "info"))}}
+    :abilities [{:prompt "Choose an asset or upgrade that was just rezzed"
+                 :choices {:req #(and (rezzed? %)
+                                      (or (is-type? % "Asset") (is-type? % "Upgrade")))}
+                 :effect (req (let [c target
+                                    creds (rez-cost state :corp c)]
+                                (when (can-pay? state side nil [:credit creds])
+                                  (resolve-ability
+                                    state :runner
+                                    {:msg (msg "pay " creds " [Credit] and derez " (:title c) ". Councilman is trashed")
+                                     :effect (req (lose state :runner :credit creds)
+                                                  (derez state :corp c)
+                                                  (trash state side card {:unpreventable true}))}
+                                   card nil))))}]}
+
    "Crash Space"
    {:prevent {:damage [:meat]}
     :recurring 2
