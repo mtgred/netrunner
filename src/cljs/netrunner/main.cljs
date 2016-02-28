8408c34fe8ca4c5a88501a9c6e8c8b39fd2bba08
Dom Tancredi <dom@domandtom.com>
2016-02-28 13:41:11 -0500

    update to main and toast

--- src/cljs/netrunner/main.cljs
+++ src/cljs/netrunner/main.cljs
@@ -31,7 +31,14 @@
 (.setPathPrefix history "")
 (.setEnabled history true)
 
-(go (swap! app-state assoc :version (:json (<! (GET "/data/version")))))
+(go (while true
+      (let [msg (<! socket-channel)]
+        (reset! lock false)
+        (case (:type msg)
+          ("do" "notification" "quit") (do (swap! game-state (if (:diff msg) #(differ/patch @last-state (:diff msg))
+                                                                             #(assoc (:state msg) :side (:side @game-state))))
+                                           (swap! last-state #(identity @game-state)))
+          nil))))
 
 (defn navbar [cursor owner]
   (om/component
@@ -71,12 +78,18 @@
 
 (defn version [cursor owner]
   (reify
+    om/IWillMount
+    (will-mount [this]
+      (let [edit-channel (om/get-state owner :edit-channel)]
+        (go (while true
+            (let [card (<! zoom-channel)]
+              (om/set-state! owner :zoom card))))
 
     om/IDidUpdate
       (did-update [this prev-props prev-state]
         (let [version-val (.html (js/$ "#version-val"))]
-          (when (not= version-val (:version @app-state))
-            (toast (str (str "You have an older version: " (:version @app-state)) version-val2) "warning"))))
+          (cond
+              (not= version-val (:version @app-state)) (toast (str (str "You have an older version: " (:version @app-state)) version-val) "warning"))))
 
     om/IRenderState
       (render-state [this state]
--- src/cljs/netrunner/toast.cljs
+++ src/cljs/netrunner/toast.cljs
@@ -1,21 +1,27 @@
 (ns netrunner.toast)
 
-(def toastr-options (js-obj
-                     "closeButton" false
-                     "debug" false
-                     "newestOnTop" false
-                     "progressBar" false
-                     "positionClass" "toast-card"
-                     "preventDuplicates" true
-                     "onclick" nil
-                     "showDuration" 300
-                     "hideDuration" 3000
-                     "timeOut" 3000
-                     "extendedTimeOut" 1000
-                     "showEasing" "swing"
-                     "hideEasing" "linear"
-                     "showMethod" "fadeIn"
-                     "hideMethod" "fadeOut"))
+(defn toastr-options
+  "Function that generates the correct toastr options for specified settings"
+  [options]
+  (js-obj "closeButton" (:close-button options false)
+          "debug" false
+          "newestOnTop" false
+          "progressBar" false
+          "positionClass" "toast-card"
+          ;; preventDuplicates - identical toasts don't stack when the property is set to true.
+          ;; Duplicates are matched to the previous toast based on their message content.
+          "preventDuplicates" (:prevent-duplicates options true)
+          "onclick" nil
+          "showDuration" 300
+          "hideDuration" 1000
+          ;; timeOut - how long the toast will display without user interaction
+          "timeOut" (:time-out options 3000)
+          ;; extendedTimeOut - how long the toast will display after a user hovers over it
+          "extendedTimeOut" (:time-out options 1000)
+          "showEasing" "swing"
+          "hideEasing" "linear"
+          "showMethod" "fadeIn"
+          "hideMethod" "fadeOut"))
 
 (declare toast)
 
