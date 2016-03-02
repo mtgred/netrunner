ff6d717e1cc0ab1cef3d3c3d4c836a299fd8737a
Joel Koepp <jkoepp@gmail.com>
2016-03-02 08:37:02 -0600

    add runnable-servers to req macro

--- src/clj/game/macros.clj
+++ src/clj/game/macros.clj
@@ -27,6 +27,11 @@
             'installed '(#{:rig :servers} (first (:zone card)))
             'remotes '(get-remote-names @state)
             'servers '(concat ["HQ" "R&D" "Archives"] remotes)
+            'runnable-servers '(let [servers (map zone->name
+                                                  (keys (get-in @state [:corp :servers])))
+                                     restricted (map zone->name
+                                                     (keys (get-in @state [:runner :register :cannot-run-on-server])))]
+                                 (remove (set restricted) (set servers)))
             'tagged '(or (> (:tagged runner) 0) (> (:tag runner) 0))
             'this-server '(let [s (-> card :zone rest butlast)
                                 r (:server run)]
