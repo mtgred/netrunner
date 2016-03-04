0bf9eb77bdf3b1458bb4cb7ff2b5d8920c6b483d
Neal Terrell <neal.terrell@csulb.edu>
2016-03-04 11:42:58 -0800

    Add all-active function.

--- src/clj/game/core-misc.clj
+++ src/clj/game/core-misc.clj
@@ -72,6 +72,15 @@
     (let [servers (->> (:corp @state) :servers seq flatten)]
       (concat (mapcat :content servers) (mapcat :ices servers)))))
 
+(defn all-active
+  "Returns a vector of all active cards for the given side. Active cards are either installed, the identity,
+  or the corp's scored area."
+  [state side]
+  (if (= side :runner)
+    (cons (get-in @state [:runner :identity]) (all-installed state side))
+    (cons (get-in @state [:corp :identity]) (concat (all-installed state side)
+                                                    (get-in @state [:corp :scored])))))
+
 (defn installed-byname
   "Returns a truthy card map if a card matching title is installed"
   [state side title]
