6b561d239fa18fcab87ff7fd119fdae17fa8a295
Joel Koepp <jkoepp@gmail.com>
2016-03-11 19:56:02 -0600

    have all-installed search of Runner cards include programs hosted on ICE

--- src/clj/game/core-misc.clj
+++ src/clj/game/core-misc.clj
@@ -67,8 +67,11 @@
   but not including 'inactive hosting' like Personal Workshop."
   [state side]
   (if (= side :runner)
-    (let [installed (flatten (for [t [:program :hardware :resource]] (get-in @state [:runner :rig t])))]
-      (concat installed (filter :installed (mapcat :hosted installed))))
+    (let [installed (flatten (for [t [:program :hardware :resource]] (get-in @state [:runner :rig t])))
+          hosted-on-ice (->> (:corp @state) :servers seq flatten (mapcat :ices) (mapcat :hosted))]
+      (concat installed
+              (filter :installed (mapcat :hosted installed))
+              (filter #(= (:side %) "Runner") hosted-on-ice)))
     (let [servers (->> (:corp @state) :servers seq flatten)
           content (mapcat :content servers)
           ice (mapcat :ices servers)
