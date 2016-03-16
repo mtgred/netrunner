b385c2f21b059f88163d7b140089eefde5efcf29
Tondo <tatsuya.ahad@gmail.com>
2016-03-16 12:03:02 -0400

    Modified Aesop's to use cid instead of title when checking that it's not targeting itself.

--- src/clj/game/cards-resources.clj
+++ src/clj/game/cards-resources.clj
@@ -23,9 +23,12 @@
 
    "Aesops Pawnshop"
    {:flags {:runner-phase-12 (req (>= 2 (count (all-installed state :runner))))}
-    :abilities [{:msg (msg "trash " (:title target) " and gain 3 [Credits]")
-                 :choices {:req #(and (= (:side %) "Runner") (:installed %) (not= (:title %) "Aesop's Pawnshop"))}
-                 :effect (effect (gain :credit 3) (trash target {:unpreventable true}))}]}
+    :abilities [{:effect (req (resolve-ability
+                                state side
+                                {:msg (msg "trash " (:title target) " and gain 3 [Credits]")
+                                 :choices {:req #(and (= (:side %) "Runner") (:installed %) (not= (:cid card) (:cid %)))}
+                                 :effect (effect (gain :credit 3) (trash target {:unpreventable true}))}
+                               card nil))}]}
 
    "Always Be Running"
    {:abilities [{:once :per-turn
