83149e068b1aaf4abbeadb9d1a2d01430254f198
Joel Koepp <jkoepp@gmail.com>
2016-04-01 11:44:52 -0400

    fix Genetics Pavilion + Fisk Investment Seminar

--- src/clj/game/cards-assets.clj
+++ src/clj/game/cards-assets.clj
@@ -336,9 +336,9 @@
    "Genetics Pavilion"
    {:msg "prevent the Runner from drawing more than 2 cards during their turn"
     :effect (req (max-draw state :runner 2)
-                 (when (= 0 (remaining-draws state side))
-                   (prevent-draw state side)))
-    :events {:runner-turn-begins {:effect (effect (max-draw 2))}}
+                 (when (= 0 (remaining-draws state :runner))
+                   (prevent-draw state :runner)))
+    :events {:runner-turn-begins {:effect (effect (max-draw :runner 2))}}
     :leave-play (req (swap! state update-in [:runner :register] dissoc :max-draw))}
 
    "Ghost Branch"
