5444012dbf8f27018f0cd70288dc4cd978513d9b
Joel Koepp <jkoepp@gmail.com>
2016-04-07 21:17:40 -0500

    fix PAD Factory turn flag

--- src/clj/game/cards-assets.clj
+++ src/clj/game/cards-assets.clj
@@ -602,13 +602,15 @@
                  :label "Place 1 advancement token on a card"
                  :choices {:req #(and (:side % "Corp") (installed? %))}
                  :msg (msg "place 1 advancement token on " (card-str state target))
-                 :effect (effect (add-prop target :advance-counter 1 {:placed true})
-                                 (register-turn-flag!
-                                   target :can-score
-                                   (fn [state side card]
-                                     (if (>= (:advance-counter card) (or (:current-cost card) (:advancementcost card)))
-                                       ((constantly false) (toast state :corp "Cannot score due to PAD Factory." "warning"))
-                                       true))))}]}
+                 :effect (req (add-prop state :corp target :advance-counter 1 {:placed true})
+                              (let [tgtcid (:cid target)]
+                                (register-turn-flag! state side
+                                  target :can-score
+                                  (fn [state side card]
+                                    (if (and (= (:cid card) tgtcid)
+                                             (>= (:advance-counter card) (or (:current-cost card) (:advancementcost card))))
+                                      ((constantly false) (toast state :corp "Cannot score due to PAD Factory." "warning"))
+                                      true)))))}]}
 
    "Pālanā Agroplex"
    (let [ability {:msg "make each player draw 1 card"
