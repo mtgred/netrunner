7c06e3559ba027cbd286cd58a31101fe93317f6f
Joel Koepp <jkoepp@gmail.com>
2016-04-15 15:04:07 -0500

    add re-rez restriction to Councilman

--- src/clj/game/cards-resources.clj
+++ src/clj/game/cards-resources.clj
@@ -118,7 +118,8 @@
    {:events {:rez {:req (req (and (#{"Asset" "Upgrade"} (:type target))
                                   (can-pay? state :runner nil [:credit (rez-cost state :corp target)])))
                    :effect (req (toast state :runner (str "Click Councilman to derez " (card-str state target {:visible true})
-                                                          " that was just rezzed") "info"))}}
+                                                          " that was just rezzed") "info")
+                                (toast state :corp (str "Runner has the opportunity to derez with Councilman.") "error"))}}
     :abilities [{:prompt "Choose an asset or upgrade that was just rezzed"
                  :choices {:req #(and (rezzed? %)
                                       (or (is-type? % "Asset") (is-type? % "Upgrade")))}
@@ -130,6 +131,13 @@
                                     {:msg (msg "pay " creds " [Credit] and derez " (:title c) ". Councilman is trashed")
                                      :effect (req (lose state :runner :credit creds)
                                                   (derez state :corp c)
+                                                  (register-turn-flag! state side
+                                                    card :can-rez
+                                                    (fn [state side card]
+                                                      (if (= (:cid card) (:cid c))
+                                                        ((constantly false)
+                                                         (toast state :corp "Cannot rez the rest of this turn due to Councilman"))
+                                                        true)))
                                                   (trash state side card {:unpreventable true}))}
                                    card nil))))}]}
 
