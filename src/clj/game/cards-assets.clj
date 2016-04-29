a88ed99c3b76158d55a4a0db60ce503e8f57c735
Joel Koepp <jkoepp@gmail.com>
2016-04-29 12:54:04 -0500

    implement Indian Union Stock Exchange

--- src/clj/game/cards-assets.clj
+++ src/clj/game/cards-assets.clj
@@ -384,6 +384,12 @@
       :abilities [choose-ability]
      })
 
+   "Indian Union Stock Exchange"
+   (let [iuse {:req (req (not= (:faction target) (:faction (:identity corp))))
+               :msg "gain 1 [Credits]"
+               :effect (effect (gain :credit 1))}]
+     {:events {:play-operation iuse :rez iuse}})
+
    "Isabel McGuire"
    {:abilities [{:cost [:click 1] :label "Add an installed card to HQ"
                  :choices {:req installed?}
