9305c31706d10828231c2cad7c27d4955d1bc829
kevkcc <kkcchu@gmail.com>
2016-07-09 17:38:33 -0700

    Invasion of privacy rework and test

--- src/clj/game/cards-operations.clj
+++ src/clj/game/cards-operations.clj
@@ -56,7 +56,7 @@
       :choices :credit
       :msg (msg "install and rez " target " advertisements")
       :effect (effect (continue-ability (abhelp 1 target) card nil))})
-   
+
    "Aggressive Negotiation"
    {:req (req (:scored-agenda corp-reg)) :prompt "Choose a card"
     :choices (req (cancellable (:deck corp) :sorted))
@@ -375,11 +375,27 @@
     :msg (msg (corp-install-msg target))}
 
    "Invasion of Privacy"
-   {:trace {:base 2 :msg "reveal the Runner's Grip and trash up to X resources or events"
-            :effect (req (doseq [c (:hand runner)]
-                           (move state side c :play-area))
-                           (system-msg state :corp (str "reveals the Runner's Grip and can trash up to " (- target (second targets)) " resources or events")))
-            :unsuccessful {:msg "take 1 bad publicity" :effect (effect (gain :corp :bad-publicity 1))}}}
+   (letfn [(iop [x]
+             {:delayed-completion true
+              :req (req (pos? (count (filter #(or (is-type? % "Resource")
+                                                  (is-type? % "Event")) (:hand runner)))))
+              :prompt "Choose a resource or event to trash"
+              :msg (msg "trash " (:title target))
+              :choices (req (cancellable
+                              (filter #(or (is-type? % "Resource")
+                                           (is-type? % "Event")) (:hand runner)) :sorted))
+              :effect (req (trash state side target)
+                           (if (pos? x)
+                             (continue-ability state side (iop (dec x)) card nil)
+                             (effect-completed state side eid card)))})]
+     {:trace {:base 2 :msg "reveal the Runner's Grip and trash up to X resources or events"
+              :effect (req (let [x (- target (second targets))]
+                             (system-msg state :corp
+                                         (str "reveals the Runner's Grip ( "
+                                              (join ", " (map :title (:hand runner)))
+                                              " ) and can trash up to " x " resources or events"))
+                             (continue-ability state side (iop (dec x)) card nil)))
+              :unsuccessful {:msg "take 1 bad publicity" :effect (effect (gain :corp :bad-publicity 1))}}})
 
    "Lag Time"
    {:effect (effect (update-all-ice))
