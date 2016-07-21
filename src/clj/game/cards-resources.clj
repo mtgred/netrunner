4941dd7ddc9a481074f2be36a276b77276232c29
Joel Koepp <jkoepp@gmail.com>
2016-07-21 14:39:37 -0500

    revise Beth Kilrain-Chang

--- src/clj/game/cards-resources.clj
+++ src/clj/game/cards-resources.clj
@@ -142,16 +142,20 @@
                   :label "Gain 1 [Credits], draw 1 card, or gain [Click] (start of turn)"
                   :req (req (:runner-phase-12 @state))
                   :effect (req (let [c (:credit corp)
-                                 b (:title card)]
-                             (when (<= 5 c 9)
-                               (do (gain state side :credit 1)
-                                   (system-msg state side (str "uses " b " to gain 1 [Credits]"))))
-                             (when (<= 10 c 14)
-                               (do (draw state side 1)
-                                   (system-msg state side (str "uses " b " to draw 1 card"))))
-                             (when (<= 15 c)
-                               (do (gain state side :click 1)
-                                   (system-msg state side (str "uses " b " to gain [Click]"))))))}]
+                                     b (:title card)]
+                                 (cond
+                                   ;; gain 1 credit
+                                   (<= 5 c 9)
+                                   (do (gain state side :credit 1)
+                                       (system-msg state side (str "uses " b " to gain 1 [Credits]")))
+                                   ;; draw 1 card
+                                   (<= 10 c 14)
+                                   (do (draw state side 1)
+                                       (system-msg state side (str "uses " b " to draw 1 card")))
+                                   ;; gain 1 click
+                                   (<= 15 c)
+                                   (do (gain state side :click 1)
+                                       (system-msg state side (str "uses " b " to gain [Click]"))))))}]
      {:flags {:drip-economy true}
       :abilities [ability]
       :events {:runner-turn-begins ability}})
