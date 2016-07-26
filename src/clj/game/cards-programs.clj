de5a0fc6a075846091dbc8dabdbbaaa4ee94816d
Joel Koepp <jkoepp@gmail.com>
2016-07-26 10:39:09 -0500

    change Diwan to use :server-target

--- src/clj/game/cards-programs.clj
+++ src/clj/game/cards-programs.clj
@@ -152,12 +152,11 @@
    "Diwan"
    {:prompt "Choose the server that this copy of Diwan is targeting:"
     :choices (req servers)
-    :effect (effect (update! (assoc card :named-target target)))
-    :leave-play (effect (update! (dissoc card :named-target)))
+    :effect (effect (update! (assoc card :server-target target)))
     :events {:purge {:effect (effect (trash card))}
              :pre-corp-install {:req (req (let [c target
                                                 serv (:server (second targets))]
-                                            (and (= serv (:named-target card))
+                                            (and (= serv (:server-target card))
                                                  (not (and (is-central? serv)
                                                            (is-type? c "Upgrade"))))))
                                 :effect (effect (install-cost-bonus [:credit 1]))}}}
