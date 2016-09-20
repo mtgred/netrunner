57bcc5f6b98597edab8454cd0de9f2546a816cbf
Joel Koepp <jkoepp@gmail.com>
2016-09-20 10:07:39 -0500

    let Chum target an ICE and give it +2 strength

--- src/clj/game/cards-ice.clj
+++ src/clj/game/cards-ice.clj
@@ -372,7 +372,20 @@
                               card nil))}}
 
    "Chum"
-   {:abilities [(do-net-damage 3)]}
+   {:abilities [{:label "Give +2 strength to next ICE Runner encounters"
+                 :req (req this-server)
+                 :prompt "Choose the ICE the Runner is encountering"
+                 :choices {:req #(and (rezzed? %) (ice? %))}
+                 :msg (msg "give " (:title target) " +2 strength")
+                 :effect (req (let [ice (:cid target)]
+                                (register-events state side
+                                  {:pre-ice-strength {:req (req (= (:cid target) ice))
+                                                      :effect (effect (ice-strength-bonus 2 target))}
+                                   :run-ends {:effect (effect (unregister-events card))}}
+                                 card)
+                                (update-all-ice state side)))}
+                (do-net-damage 3)]
+    :events {:pre-ice-strength nil :run-ends nil}}
 
    "Cobra"
    {:abilities [trash-program (do-net-damage 2)]}
