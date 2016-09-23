df13520433dcaf1539d4c225c48a091cb2fdbc3f
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2016-09-23 18:29:02 +0100

    Add the chosen subtype as a label on Chameleon

--- src/clj/game/cards-icebreakers.clj
+++ src/clj/game/cards-icebreakers.clj
@@ -226,9 +226,9 @@
    {:prompt "Choose one subtype"
     :choices ["Barrier" "Code Gate" "Sentry"]
     :msg (msg "choose " target)
-    :effect (effect (set-prop (get-card state card) :ice-type target))
+    :effect (effect (update! (assoc card :subtype-target target)))
     :events {:runner-turn-ends {:msg "add itself to Grip" :effect (effect (move card :hand))}}
-    :abilities [{:cost [:credit 1] :msg (msg "break 1 " (:ice-type card) " subroutine")}]}
+    :abilities [{:cost [:credit 1] :msg (msg "break 1 " (:subtype-target card) " subroutine")}]}
 
    "Corroder"
    (auto-icebreaker ["Barrier"]
