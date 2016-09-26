331d8701206ac7f25b76d60a94a2715f11cab2a5
Filip Gokstorp <filip@gokstorp.se>
2016-09-26 18:10:29 +0200

    Message for Heartbeat specifying which card was trashed to prevent damage.

--- src/clj/game/cards-hardware.clj
+++ src/clj/game/cards-hardware.clj
@@ -315,7 +315,7 @@
    "Heartbeat"
    {:in-play [:memory 1]
     :prevent {:damage [:meat :net :brain]}
-    :abilities [{:msg "prevent 1 damage"
+    :abilities [{:msg (msg "prevent 1 damage, trashing a facedown " (:title target))
                  :choices {:req #(and (= (:side %) "Runner") (:installed %))}
                  :priority 50
                  :effect (effect (trash target {:unpreventable true})
