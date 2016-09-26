8b0d68ed2746cd5671ce711dec737b6d6fcd04c2
Joel Koepp <jkoepp@gmail.com>
2016-09-25 22:48:47 -0500

    Hatchet Job can only target Runner cards

--- src/clj/game/cards-operations.clj
+++ src/clj/game/cards-operations.clj
@@ -374,6 +374,7 @@
    "Hatchet Job"
    {:trace {:base 5
             :choices {:req #(and (installed? %)
+                                 (card-is? % :side :runner)
                                  (not (has-subtype? % "Virtual")))}
             :msg "add an installed non-virtual card to the Runner's grip"
             :effect (effect (move :runner target :hand true))}}
