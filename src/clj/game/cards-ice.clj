0b0b4b1dceb06927a031e25236c845b00b7a2cbf
Matthew Meyer <mattchainsaw@gmail.com>
2016-09-28 13:34:52 -0700

    improvements to readability

--- src/clj/game/cards-ice.clj
+++ src/clj/game/cards-ice.clj
@@ -828,19 +828,19 @@
    "Mausolus"
    {:advanceable :always
     :subroutines [{:label "Gain 1 [Credits] (Gain 3 [Credits])"
-                   :msg (msg "gain " (if (< (+ (:advance-counter card 0) (:extra-advance-counter card 0)) 3) 1 3) " [Credit]")
-                   :effect (effect (gain :credit (if (< (+ (:advance-counter card 0) (:extra-advance-counter card 0)) 3) 1 3)))}
+                   :msg (msg "gain " (if (> 3 (+ (:advance-counter card 0) (:extra-advance-counter card 0))) 1 2) " [Credits]")
+                   :effect (effect (gain :credit (if (> 3 (+ (:advance-counter card 0) (:extra-advance-counter card 0))) 1 3)))}
                   {:label "Do 1 net damage (Do 3 net damage)"
                    :delayed-completion true
-                   :msg (msg "do " (if (< (+ (:advance-counter card 0) (:extra-advance-counter card 0)) 3) 1 3) " net damage")
-                   :effect (effect (damage eid :net (if (< (+ (:advance-counter card 0) (:extra-advance-counter card 0)) 3) 1 3) {:card card}))}
+                   :msg (msg "do " (if (> 3 (+ (:advance-counter card 0) (:extra-advance-counter card 0))) 1 3) " net damage")
+                   :effect (effect (damage eid :net (if (> 3 (+ (:advance-counter card 0) (:extra-advance-counter card 0))) 1 3) {:card card}))}
                   {:label "Give the Runner 1 tag (and end the run)"
                    :delayed-completion true
                    :msg (msg "give the Runner 1 tag"
-                          (when (>= (+ (:advance-counter card 0) (:extra-advance-counter card 0)) 3) " and end the run"))
-                   :effect (req (if (< (+ (:advance-counter card 0) (:extra-advance-counter card 0)) 3)
-                                  (tag-runner state :runner 1)
-                                  (do (tag-runner state :runner 1) (end-run state side))))}]}
+                          (when (<= 3 (+ (:advance-counter card 0) (:extra-advance-counter card 0))) " and end the run"))
+                   :effect (req (tag-runner state :runner 1)
+                             (when (<= 3 (+ (:advance-counter card 0) (:extra-advance-counter card 0)))
+                                  (end-run state side)))}]}
 
    "Merlin"
    (grail-ice (do-net-damage 2))
