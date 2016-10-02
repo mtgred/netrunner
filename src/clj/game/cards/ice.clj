9da07d3f0fc9264903b4299da27178cc3a98594f
Joel Koepp <jkoepp@gmail.com>
2016-10-02 11:42:24 -0500

    add msg to Yagura if Corp doesn't bottom top card

--- src/clj/game/cards/ice.clj
+++ src/clj/game/cards/ice.clj
@@ -1273,7 +1273,8 @@
                   {:msg "look at the top card of R&D"
                    :optional {:prompt (msg "Move " (:title (first (:deck corp))) " to the bottom of R&D?")
                               :yes-ability {:effect (effect (move (first (:deck corp)) :deck)
-                                                            (do (system-msg state side "uses Yagura to move the top card of R&D to the bottom")))}}}]}
+                                                            (do (system-msg state side "uses Yagura to move the top card of R&D to the bottom")))}
+                              :no-ability {:effect (req (system-msg state :corp (str "does not use Yagura to move the top card of R&D to the bottom")))}}}]}
 
    "Zed 1.0"
    {:subroutines [(do-brain-damage 1)]
