a1545eff2c86de30388009fbddc38a0269ecedb3
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2016-10-03 23:53:05 +0100

    Remove the dependecy on react
    Om includes it already.

--- project.clj
+++ project.clj
@@ -47,7 +47,7 @@
        :compiler {:output-to "resources/public/cljs/app.js"
                   :output-dir "resources/public/cljs"
                   :optimizations :none
-                  :source-map true}},
+                  :source-map true}}
       {:id "prod"
        :source-paths ["src/cljs/netrunner"]
        :compiler {:output-to "resources/public/js/app.js"
@@ -57,7 +57,6 @@
                   :closure-warnings {:externs-validation :off}
                   :externs ["resources/public/lib/jquery/jquery.min.js"
                             "resources/public/lib/jqueryui/jquery-ui.min.js"
-                            "resources/public/lib/react/react.min.js"
                             "resources/public/lib/moment/min/moment.min.js"
                             "resources/public/lib/bootstrap/dist/js/bootstrap.min.js"
                             "node_modules/socket.io-client/socket.io.js"]}}]}
