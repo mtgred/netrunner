18a74a14b18ac4033ec56f5ba3373ad997929f67
kevkcc <kkcchu@gmail.com>
2016-10-06 14:50:19 -0700

    Code review changes

--- src/clj/game/cards/assets.clj
+++ src/clj/game/cards/assets.clj
@@ -91,31 +91,26 @@
    "Alexa Belsky"
    {:abilities [{:label "[Trash]: Shuffle all cards in HQ into R&D"
                  :effect (req (trash state side card)
-                              (if (pos? (:credit runner))
-                                (do (show-wait-prompt state :corp "Runner to decide whether or not to prevent Alexa Belsky")
-                                    (resolve-ability
-                                      state side
-                                      {:prompt "How many credits?"
-                                       :choices :credit :player :runner
-                                       :msg (msg "shuffle " (- (count (:hand corp)) (int (/ target 2))) " card"
-                                                 (when-not (= 1 (- (count (:hand corp)) (int (/ target 2)))) "s")
-                                                 " in HQ into R&D")
-                                       :effect (req (if (pos? (int (/ target 2)))
-                                                      (do (doseq [c (take (- (count (:hand corp)) (int (/ target 2)))
-                                                                          (shuffle (:hand corp)))]
-                                                            (move state :corp c :deck))
-                                                          (shuffle! state :corp :deck)
-                                                          (system-msg state :runner
-                                                                      (str "pays " target " [Credits] to prevent "
-                                                                           (int (/ target 2)) " random card"
-                                                                           (when (> (int (/ target 2)) 1) "s")
-                                                                           " in HQ from being shuffled into R&D")))
-                                                      (shuffle-into-deck state :corp :hand))
-                                                    (clear-wait-prompt state :corp))} card nil))
-                                (resolve-ability
-                                  state side
-                                  {:msg "shuffle all cards in HQ into R&D"
-                                   :effect (effect (shuffle-into-deck :hand))} card nil)))}]}
+                              (do (show-wait-prompt state :corp "Runner to decide whether or not to prevent Alexa Belsky")
+                                  (resolve-ability
+                                    state side
+                                    {:prompt "How many credits?"
+                                     :choices :credit :player :runner
+                                     :msg (msg "shuffle " (- (count (:hand corp)) (quot target 2)) " card"
+                                               (when-not (= 1 (- (count (:hand corp)) (quot target 2))) "s")
+                                               " in HQ into R&D")
+                                     :effect (req (if (pos? (quot target 2))
+                                                    (do (doseq [c (take (- (count (:hand corp)) (quot target 2))
+                                                                        (shuffle (:hand corp)))]
+                                                          (move state :corp c :deck))
+                                                        (shuffle! state :corp :deck)
+                                                        (system-msg state :runner
+                                                                    (str "pays " target " [Credits] to prevent "
+                                                                         (quot target 2) " random card"
+                                                                         (when (> (quot target 2) 1) "s")
+                                                                         " in HQ from being shuffled into R&D")))
+                                                    (shuffle-into-deck state :corp :hand))
+                                                  (clear-wait-prompt state :corp))} card nil)))}]}
 
    "Alix T4LB07"
    {:events {:corp-install {:effect (effect (add-counter card :power 1))}}
