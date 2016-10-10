2a26d59a4bb8f998c09be3f7d5b845ad6b84b4dc
Kevin Powe <dev@kevinpowe.com>
2016-10-10 22:54:59 +1100

    Enhanced testing for Noise's ability accommodating feedback from previous pull request

--- src/clj/test/cards/identities.clj
+++ src/clj/test/cards/identities.clj
@@ -652,20 +652,45 @@
       (prompt-choice :runner "1 [Credits]")
       (is (= 5 (:credit (get-corp))) "Gained 1 credit from psi game"))))
 
-(deftest noise
+(deftest noise-ability
   ;; Noise: Hacker Extraordinaire - Ability
   (do-game
     (new-game
-      (default-corp [(qty "Hedge Fund" 3) (qty "Restructure" 3) (qty "PAD Campaign" 3)])
-      (make-deck "Noise: Hacker Extraordinaire" [(qty "Datasucker" 1) (qty "Easy Mark" 2)]))
+      (default-corp [(qty "Hedge Fund" 3) (qty "Restructure" 3) (qty "PAD Campaign" 3) (qty "Beanstalk Royalties" 2)])
+      (make-deck "Noise: Hacker Extraordinaire" [(qty "Datasucker" 1) (qty "Cache" 1) (qty "Sure Gamble" 1) (qty "Clone Chip" 2) (qty "Sharpshooter" 2)]))
+    (starting-hand state :runner ["Datasucker" "Sure Gamble" "Clone Chip" "Clone Chip" "Cache"])
     (is (= 6 (count (:hand (get-corp)))) "Corp should start with 6 cards in hand")
-    (is (= 3 (count (:deck (get-corp)))) "Corp deck should contain 3 cards")
+    (is (= 5 (count (:deck (get-corp)))) "Corp deck should contain 3 cards")
     (take-credits state :corp)
     (is (= 0 (count (:discard (get-corp)))) "Archives started empty")
     (play-from-hand state :runner "Datasucker")
     (is (= 1 (count (:discard (get-corp)))) "Playing virus should cause card to be trashed from R&D")
-    (play-from-hand state :runner "Easy Mark")
-    (is (= 1 (count (:discard (get-corp)))) "Playing non-virus should not cause card to be trashed from R&D")))
+    (is (= 4 (count (:deck (get-corp)))) "Card trashed to Archives by Noise should come from R&D")
+    (play-from-hand state :runner "Sure Gamble")
+    (is (= 1 (count (:discard (get-corp)))) "Playing non-virus should not cause card to be trashed from R&D")
+    (core/click-draw state :runner nil)
+    (play-from-hand state :runner "Clone Chip")
+    (play-from-hand state :runner "Clone Chip")
+    (trash-from-hand state :runner "Cache")
+    (trash-from-hand state :runner "Sharpshooter")
+    (take-credits state :runner)
+    ;; playing virus via Clone Chip on Corp's turn should trigger Noise ability
+    (let [chip (get-in @state [:runner :rig :hardware 0])]
+      (card-ability state :runner chip 0)
+      (prompt-select :runner (find-card "Cache" (:discard (get-runner))))
+      (let [ds (get-in @state [:runner :rig :program 1])]
+        (is (not (nil? ds)))
+        (is (= (:title ds) "Cache"))))
+    (is (= 2 (count (:discard (get-corp)))) "Playing virus via Clone Chip on corp's turn should trigger Noise ability")
+    (is (= 2 (count (:deck (get-corp)))) "Card trashed to Archives by Noise should come from R&D")
+    ;; playing non-virus via Clone Chip on Corp's turn should NOT trigger Noise ability
+    (let [chip-2 (get-in @state [:runner :rig :hardware 0])]
+      (card-ability state :runner chip-2 0)
+      (prompt-select :runner (find-card "Sharpshooter" (:discard (get-runner))))
+      (let [ss (get-in @state [:runner :rig :program 2])]
+        (is (not (nil? ss)))
+        (is (= (:title ss) "Sharpshooter"))))
+    (is (= 2 (count (:discard (get-corp)))) "Playing non-virus via Clone Chip on corp's turn should not trigger Noise ability")))
 
 (deftest null-ability
   ;; Null ability - once per turn
