018426dee13d16072ade00402c3c59af6d48dbb2
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2016-10-10 18:07:58 +0100

    Make Wetwork Refit remove the first subroutine when leaving play
    Assume that subroutine is the one it added.

--- src/clj/game/cards/operations.clj
+++ src/clj/game/cards/operations.clj
@@ -1000,7 +1000,8 @@
                          (rezzed? %))}
     :msg (msg "give " (card-str state target) "\"[Subroutine] Do 1 brain damage\" before all its other subroutines")
     :effect (effect (update! (assoc target :subroutines (cons (do-brain-damage 1) (:subroutines target))))
-                    (host (get-card state target) (assoc card :zone [:discard] :seen true)))}
+                    (host (get-card state target) (assoc card :zone [:discard] :seen true)))
+    :leave-play (effect (update! (assoc (:host card) :subroutines (rest (:subroutines (:host card))))))}
 
    "Violet Level Clearance"
    {:msg "gain 8 [Credits] and draw 4 cards"
--- src/clj/test/cards/operations.clj
+++ src/clj/test/cards/operations.clj
@@ -1007,9 +1007,12 @@
       (is (not-any? #{"Eli 1.0"} (get-in @state [:runner :prompt :choices]))
           "Unrezzed Eli 1.0 is not a choice to host Wetwork Refit")
       (prompt-choice :corp "Done")
-      (core/gain state :corp :click 3)
+
+      (take-credits state :corp)
+      (take-credits state :runner)
       (core/rez state :corp (refresh eli))
       (core/rez state :corp (refresh vanilla))
+
       (play-from-hand state :corp "Wetwork Refit")
       (prompt-select :corp (refresh eli))
       (is (= "Wetwork Refit" (:title (first (:hosted (refresh eli)))))
@@ -1018,6 +1021,14 @@
           "Eli 1.0 has 2 different subroutines")
       (is (= "Do 1 brain damage" (:label (first (:subroutines (refresh eli)))))
           "Eli 1.0 has a brain damage subroutine as his first subroutine")
+
+      (core/move state :corp (first (:hosted (refresh eli))) :hand)
+      (is (empty? (:hosted (refresh eli))) "No cards are hosted on Eli 1.0")
+      (is (= 1 (count (:subroutines (refresh eli))))
+          "Eli 1.0 has 1 different subroutine")
+      (is (= "End the run" (:label (first (:subroutines (refresh eli)))))
+          "Eli 1.0 has an end the run subroutine as his first subroutine")
+
       (play-from-hand state :corp "Wetwork Refit")
       (prompt-select :corp (refresh vanilla))
       (is (not= "Wetwork Refit" (:title (first (:hosted (refresh vanilla)))))
