7ae21b3014021ebd7cf89f1413b4587cdacdb536
Neal Terrell <neal.terrell@csulb.edu>
2016-10-18 17:57:40 -0700

    Make sounds default enabled.

--- src/cljs/netrunner/appstate.cljs
+++ src/cljs/netrunner/appstate.cljs
@@ -3,7 +3,9 @@
 (def app-state
   (atom {:active-page "/"
          :user (js->clj js/user :keywordize-keys true)
-         :options (merge {:background "lobby-bg" :enablesounds (js->clj (.getItem js/localStorage "enablesounds"))}
+         :options (merge {:background "lobby-bg"
+                          :enablesounds (let [sounds (js->clj (.getItem js/localStorage "enablesounds"))]
+                                          (if (nil? sounds) true sounds))}
                          (:options (js->clj js/user :keywordize-keys true)))
          :cards [] :sets [] :mwl []
          :decks [] :decks-loaded false
