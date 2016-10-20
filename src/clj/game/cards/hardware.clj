23a75b8be27e5ff48ab9ad04bccfdff2cb96d44f
Joel Koepp <jkoepp@gmail.com>
2016-10-20 09:55:25 -0500

    help Blackguard interaction with add'l cost cards

--- src/clj/game/cards/hardware.clj
+++ src/clj/game/cards/hardware.clj
@@ -44,8 +44,26 @@
 
    "Blackguard"
    {:in-play [:memory 2]
-    :events {:expose {:msg (msg "attempt to force the rez of " (:title target))
-                      :effect (effect (rez :corp target))}}}
+    :events {:expose
+             {:msg (msg "attempt to force the rez of " (:title target))
+              :delayed-completion true
+              :effect (req (let [c target
+                                 cdef (card-def c)
+                                 cname (:title c)]
+                             (if (:additional-cost cdef)
+                               (do (show-wait-prompt state :runner (str "Corp to decide if they will rez " cname))
+                                   (continue-ability state side
+                                     {:optional
+                                      {:prompt (msg "Pay additional cost to rez " cname "?")
+                                       :player :corp
+                                       :yes-ability {:effect (effect (rez :corp c)
+                                                                     (clear-wait-prompt :runner))}
+                                       :no-ability {:effect (effect (system-msg :corp (str "declines to pay additional costs"
+                                                                                       " and is not forced to rez " cname))
+                                                                    (clear-wait-prompt :runner))}}}
+                                    card nil))
+                               (do (rez :corp target)
+                                   (effect-completed state side eid)))))}}}
 
    "Bookmark"
    {:abilities [{:label "Host up to 3 cards from your Grip facedown"
