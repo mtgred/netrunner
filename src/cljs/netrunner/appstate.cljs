28e4535011bc8ddc29ca72c78acbde59c8aa060b
Neal Terrell <neal.terrell@csulb.edu>
2016-11-02 16:22:00 -0700

    Correct the lookup of sounds option from localStorage.

--- src/cljs/netrunner/appstate.cljs
+++ src/cljs/netrunner/appstate.cljs
@@ -5,8 +5,8 @@
          :user (js->clj js/user :keywordize-keys true)
          :options (merge {:background "lobby-bg"
                           :opponent-alt-art true
-                          :enablesounds (let [sounds (js->clj (.getItem js/localStorage "sounds"))]
-                                          (if (nil? sounds) true sounds))}
+                          :sounds (let [sounds (js->clj (.getItem js/localStorage "sounds"))]
+                                    (if (nil? sounds) true (= sounds "true")))}
                          (:options (js->clj js/user :keywordize-keys true)))
          :cards [] :sets [] :mwl []
          :decks [] :decks-loaded false
