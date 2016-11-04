4261a8e63af1f6d5177cf89bccfad861a2860e50
Joel Koepp <jkoepp@gmail.com>
2016-11-04 09:17:03 -0500

    implement Best Defense

--- src/clj/game/cards/operations.clj
+++ src/clj/game/cards/operations.clj
@@ -134,6 +134,18 @@
    {:msg "gain 3 [Credits]"
     :effect (effect (gain :credit 3))}
 
+   "Best Defense"
+   {:delayed-completion true
+    :req (req (not-empty (all-installed state :runner)))
+    :effect (req (let [t (:tag runner)]
+                   (continue-ability state side
+                     {:prompt (msg "Choose a Runner card with an install cost of " t " or less to trash")
+                      :choices {:req #(and (installed? %)
+                                           (<= (:cost %) t))}
+                      :msg (msg "trash " (:title target))
+                      :effect (effect (trash target))}
+                    card nil)))}   
+
    "Big Brother"
    {:req (req tagged)
     :msg "give the Runner 2 tags"
