213f1e8b4c22c8b8d16ef8a967402b2e68ca3a99
kevkcc <kkcchu@gmail.com>
2016-11-05 01:00:29 -0700

    Frantic coding tests

--- src/clj/test/cards/events.clj
+++ src/clj/test/cards/events.clj
@@ -426,6 +426,46 @@
     (take-credits state :runner)
     (is (not (:corp-phase-12 @state)) "Employee Strike suppressed Blue Sun step 1.2")))
 
+(deftest frantic-coding-install
+  ;; Frantic Coding - Install 1 program, other 9 cards are trashed
+  (do-game
+    (new-game (default-corp)
+              (default-runner [(qty "Frantic Coding" 1) (qty "Torch" 1) (qty "Corroder" 1)
+                               (qty "Magnum Opus" 1) (qty "Daily Casts" 2) (qty "Sure Gamble" 2)
+                               (qty "John Masanori" 1) (qty "Amped Up" 1) (qty "Wanton Destruction" 1)]))
+    (starting-hand state :runner ["Frantic Coding"])
+    (take-credits state :corp)
+    (play-from-hand state :runner "Frantic Coding")
+    (prompt-choice :runner "OK")
+    (let [get-prompt (fn [] (first (#(get-in @state [:runner :prompt]))))
+          prompt-names (fn [] (map #(:title %) (:choices (get-prompt))))]
+      (is (= (list "Corroder" "Magnum Opus" nil) (prompt-names)) "No Torch in list because can't afford")
+      (is (= 2 (:credit (get-runner))))
+      (is (= 1 (count (:discard (get-runner)))))
+      (prompt-choice :runner (find-card "Magnum Opus" (:deck (get-runner))))
+      (is (= 1 (count (get-in @state [:runner :rig :program]))))
+      (is (= 2 (:credit (get-runner))) "Magnum Opus installed for free")
+      (is (= 10 (count (:discard (get-runner))))))))
+
+(deftest frantic-coding-noinstall
+  ;; Frantic Coding - Don't install anything, all 10 cards are trashed
+  (do-game
+    (new-game (default-corp)
+              (default-runner [(qty "Frantic Coding" 1) (qty "Torch" 1) (qty "Corroder" 1)
+                               (qty "Magnum Opus" 1) (qty "Daily Casts" 2) (qty "Sure Gamble" 2)
+                               (qty "John Masanori" 1) (qty "Amped Up" 1) (qty "Wanton Destruction" 1)]))
+    (starting-hand state :runner ["Frantic Coding"])
+    (take-credits state :corp)
+    (play-from-hand state :runner "Frantic Coding")
+    (prompt-choice :runner "OK")
+    (let [get-prompt (fn [] (first (#(get-in @state [:runner :prompt]))))
+          prompt-names (fn [] (map #(:title %) (:choices (get-prompt))))]
+      (is (= (list "Corroder" "Magnum Opus" nil) (prompt-names)) "No Torch in list because can't afford")
+      (is (= 1 (count (:discard (get-runner)))))
+      (prompt-choice :runner "No install")
+      (is (= 0 (count (get-in @state [:runner :rig :program]))))
+      (is (= 11 (count (:discard (get-runner))))))))
+
 (deftest freelance-coding-contract
   ;; Freelance Coding Contract - Gain 2 credits per program trashed from Grip
   (do-game
