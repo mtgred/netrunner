7d3f8ba1bcea9a5876e325054a678d52b29f4050
Joel Koepp <jkoepp@gmail.com>
2016-11-27 14:53:29 -0600

    fix Showing Off to work with new access routines
    fixes #2177

--- src/clj/game/cards/events.clj
+++ src/clj/game/cards/events.clj
@@ -993,11 +993,17 @@
                       {:replace-access
                        {:msg "access cards from the bottom of R&D"
                         :delayed-completion true
-                        :effect (req (swap! state assoc-in [:corp :deck]
+                        :effect (req (when-completed (resolve-ability state side
+                                                       {:effect (effect (register-events (:events (card-def card))
+                                                                                         (assoc card :zone '(:discard))))}
+                                                      card nil)
+                                                     (do-access state side eid (:server run))))}} card))
+    :events {:pre-access {:silent (req true)
+                          :effect (req (swap! state assoc-in [:corp :deck]
+                                              (rseq (into [] (get-in @state [:corp :deck])))))}
+             :run-ends {:effect (req (swap! state assoc-in [:corp :deck]
                                             (rseq (into [] (get-in @state [:corp :deck]))))
-                                     (do-access state side eid (:server run))
-                                     (swap! state assoc-in [:corp :deck]
-                                            (rseq (into [] (get-in @state [:corp :deck])))))}} card))}
+                                     (unregister-events state side card))}}}
 
    "Singularity"
    {:prompt "Choose a server" :choices (req (filter #(can-run-server? state %) remotes))
