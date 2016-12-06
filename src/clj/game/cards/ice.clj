c08fe5744a877078a6181087e6e9deb105870b22
Joel Koepp <jkoepp@gmail.com>
2016-12-06 12:39:44 -0600

    implement Mind Game

--- src/clj/game/cards/ice.clj
+++ src/clj/game/cards/ice.clj
@@ -925,6 +925,23 @@
    {:subroutines [end-the-run]
     :strength-bonus (req (if (= (second (:zone card)) :hq) 3 0))}
 
+   "Mind Game"
+   {:subroutines [(do-psi {:label "Redirect the run to another server"
+                           :player :corp
+                           :prompt "Choose a server"
+                           :choices (req servers)
+                           :msg (msg "redirect the run to " target)
+                           :effect (req (let [dest (server->zone state target)]
+                                          (swap! state update-in [:run]
+                                                 #(assoc % :position (count (get-in corp (conj dest :ices)))
+                                                           :server (rest dest)))))})]
+    :runner-abilities [{:label "Add an installed card to the bottom of your Stack"
+                        :prompt "Choose one of your installed cards"
+                        :choices {:req #(and (installed? %)
+                                             (= (:side %) "Runner"))}
+                        :effect (effect (move target :deck)
+                                        (system-msg :runner (str "adds " (:title target) " to the bottom of their Stack")))}]}
+
    "Minelayer"
    {:subroutines [{:msg "install an ICE from HQ"
                    :choices {:req #(and (ice? %)
