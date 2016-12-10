5e13686f2ed3e279adfc5ddf68e8ceccd37ef38a
Joel Koepp <jkoepp@gmail.com>
2016-12-09 21:46:45 -0600

    implement Interdiction

--- src/clj/game/cards/events.clj
+++ src/clj/game/cards/events.clj
@@ -648,6 +648,19 @@
     :choices (req runnable-servers)
     :effect (effect (run target nil card))}
 
+   "Interdiction"
+   (let [ab (effect (register-turn-flag!
+                     card :can-rez
+                     (fn [state side card]
+                       (if (and (= (:active-player @state) :runner) (not (ice? card)))
+                         ((constantly false)
+                          (toast state :corp "Cannot rez non-ICE on the Runner's turn due to Interdiction"))
+                         true))))]
+     {:msg "prevent the Corp from rezzing non-ICE cards on the Runner's turn"
+      :effect ab
+      :events {:runner-turn-begins {:effect ab}}
+      :leave-play (req (clear-all-flags-for-card! state side card))})
+
    "Itinerant Protesters"
    {:msg "reduce the Corp's maximum hand size by 1 for each bad publicity"
     :effect (req (lose state :corp :hand-size-modification (:bad-publicity corp))
