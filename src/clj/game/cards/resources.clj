bbef8ed6e1e8acb700804e131a7393830a7d8be2
Joel K <jkoepp@gmail.com>
2016-12-22 20:26:29 -0600

    fix Hunting Grounds + Reaver

--- src/clj/game/cards/resources.clj
+++ src/clj/game/cards/resources.clj
@@ -564,8 +564,8 @@
                  {:label "[Trash]: Install the top 3 cards of your Stack facedown"
                   :msg "install the top 3 cards of their Stack facedown"
                   :effect (req (trash state side card {:cause :ability-cost})
-                               (doseq [c (take 3 (:deck runner))]
-                                  (runner-install state side c {:facedown true})))}]}
+                               (doseq [c (take 3 (get-in @state [:runner :deck]))]
+                                 (runner-install state side c {:facedown true})))}]}
 
    "Ice Analyzer"
    {:implementation "Credit use restriction is not enforced"
