6b6fd627e14968567d5e05da5fae9252a4517dbd
Joel Koepp <jkoepp@gmail.com>
2017-01-05 21:29:10 -0600

    don't let Uninstall target facedown cards

--- src/clj/game/cards/events.clj
+++ src/clj/game/cards/events.clj
@@ -1280,6 +1280,7 @@
 
    "Uninstall"
    {:choices {:req #(and (installed? %)
+                         (not (facedown? %))
                          (#{"Program" "Hardware"} (:type %)))}
     :msg (msg "move " (:title target) " to their Grip")
     :effect (effect (move target :hand))}
