4dae69e770a30c0306d4c32fee4515123a0cac07
Joel Koepp <jkoepp@gmail.com>
2017-01-07 07:42:09 -0600

    implement NASX

--- src/clj/game/cards/assets.clj
+++ src/clj/game/cards/assets.clj
@@ -733,6 +733,25 @@
                  :effect (req (doseq [c targets] (move state side c :deck))
                               (shuffle! state side :deck))}]}
 
+   "NASX"
+   (let [ability {:msg "gain 1 [Credits]"
+                  :label "Gain 1 [Credits] (start of turn)"
+                  :once :per-turn
+                  :effect (effect (gain :credit 1))}]
+     {:derezzed-events {:runner-turn-ends corp-rez-toast}
+      :events {:corp-turn-begins ability}
+      :abilities [ability
+                  {:label "Place 1 power counter" :cost [:credit 1]
+                   :effect (effect (add-counter card :power 1)
+                                   (system-msg (str "places 1 power counter on NASX")))}
+                  {:label "Place 2 power counters" :cost [:credit 2]
+                   :effect (effect (add-counter card :power 2)
+                                   (system-msg (str "places 2 power counters on NASX")))}
+                  {:label "[Trash] and gain 2 [Credits] for each power counter" :cost [:click 1]
+                   :msg (msg "gain " (* 2 (get-in card [:counter :power])) " [Credits]")
+                   :effect (effect (gain :credit (* 2 (get-in card [:counter :power])))
+                                   (trash card))}]})
+
    "Net Police"
    {:recurring (effect (set-prop card :rec-counter (:link runner)))
     :effect (effect (set-prop card :rec-counter (:link runner)))}
