8e7707c042f92d9338d6e2ddfb5e6b70baf4f8e3
Joel Koepp <jkoepp@gmail.com>
2017-01-12 11:41:07 -0600

    implement Aaron Marrón

--- src/clj/game/cards/resources.clj
+++ src/clj/game/cards/resources.clj
@@ -11,6 +11,14 @@
 
 ;;; Card definitions
 (def cards-resources
+  {"Aaron Marrón"
+   (let [am {:effect (effect (add-counter card :power 2)
+                             (system-msg :runner (str "places 2 power counters on Aaron Marrón")))}]
+     {:abilities [{:counter-cost [:power 1]
+                   :msg "remove 1 tag and draw 1 card"
+                   :effect (effect (lose :tag 1) (draw))}]
+      :events {:agenda-scored am :agenda-stolen am}})
+
   {"Access to Globalsec"
    {:in-play [:link 1]}
 
