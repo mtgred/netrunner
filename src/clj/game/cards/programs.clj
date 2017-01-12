863dbbefeb397cc34c5ff596ae71e7c0065d276b
Joel Koepp <jkoepp@gmail.com>
2017-01-12 14:22:18 -0600

    implement Tracker

--- src/clj/game/cards/programs.clj
+++ src/clj/game/cards/programs.clj
@@ -734,6 +734,18 @@
       :events {:runner-turn-begins ability
                :purge {:effect (effect (trash card))}}})
 
+   "Tracker"
+   (let [ability {:prompt "Choose a server for Tracker" :choices (req servers)
+                  :msg (msg "target " target)
+                  :req (req (not (:server-target card)))
+                  :effect (effect (update! (assoc card :server-target target)))}]
+     {:abilities [{:label "Make a run on targeted server" :cost [:click 1 :credit 2]
+                   :req (req (:server-target card))
+                   :msg (msg "make a run on " (:server-target card) ". Prevent the first subroutine that would resolve from resolving")
+                   :effect (effect (run (:server-target card) nil card))}]
+      :events {:runner-turn-begins ability
+               :runner-turn-ends {:effect (effect (update! (dissoc card :server-target)))}}})
+
    "Trope"
    {:events {:runner-turn-begins {:effect (effect (add-counter card :power 1))}}
     :abilities [{:cost [:click 1]
