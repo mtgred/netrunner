57e4cb1881a15c9c04e16571cf1708414e4ed6ba
Joel Koepp <jkoepp@gmail.com>
2017-01-13 20:49:03 -0600

    actually give credits with Peace in Our Time
    fixes #2314

--- src/clj/game/cards/events.clj
+++ src/clj/game/cards/events.clj
@@ -867,7 +867,9 @@
    "Peace in Our Time"
    {:req (req (not (:scored-agenda corp-reg)))
     :msg "gain 10 [Credits]. The Corp gains 5 [Credits]"
-    :effect (req (apply prevent-run-on-server
+    :effect (req (gain state :runner :credit 10)
+                 (gain state :corp :credit 5)
+                 (apply prevent-run-on-server
                         state card (get-zones @state))
                  (register-events state side
                    {:runner-turn-ends {:effect (req (apply enable-run-on-server state card (get-zones @state)))}}
