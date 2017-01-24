914a055a976e985bb4ebd1aca4abd99efb62ebe2
Neal Terrell <neal.terrell@csulb.edu>
2017-01-23 18:28:35 -0800

    Fix #1838, Bhagat first-time issue.

--- src/clj/game/cards/resources.clj
+++ src/clj/game/cards/resources.clj
@@ -114,10 +114,10 @@
                                  (runner-install target))}]}
 
    "Bhagat"
-   {:events {:successful-run {:req (req (= target :hq))
+   {:events {:successful-run {:req (req (= target :hq)
+                                        (first-successful-run-on-server? state :hq))
                               :msg "force the Corp to trash the top card of R&D"
-                              :effect (effect (mill :corp))
-                              :once :per-turn}}}
+                              :effect (effect (mill :corp))}}}
 
    "Bank Job"
    {:data {:counter {:credit 8}}
