bb7ab131ed788f87f50c333a9ce1d40a66d3f68e
danhut <danhut@cisco.com>
2017-01-24 16:39:11 +1100

    deleted leftover comment2

--- src/clj/game/cards/resources.clj
+++ src/clj/game/cards/resources.clj
@@ -997,20 +997,6 @@
                           (join ", " (map :title (take 3 (:deck runner)))))
                    :effect (effect (mill :runner 3))}}
 
-;   "Rolodex"
-;   {:delayed-completion true
-;    :msg "look at the top 5 cards of their Stack"
-;    :effect (req (show-wait-prompt state :corp "Runner to rearrange the top cards of their Stack")
-;                 (let [from (take 5 (:deck runner))]
-;                   (if (pos? (count from))
-;                     (continue-ability state side (reorder-choice :runner :corp from '()
-;                                                                  (count from) from) card nil)
-;                     (do (clear-wait-prompt state :corp)
-;                         (effect-completed state side eid card)))))
-;    :trash-effect {:msg (msg "trash cards from their Stack as Rolodex was trashed "
-;                             (join ", " (map :title (take 3 (:deck runner)))))
-;                   :effect (effect (mill :runner 3))}}
-
    "Sacrificial Clone"
    {:prevent {:damage [:meat :net :brain]}
     :abilities [{:effect (req (doseq [c (concat (get-in runner [:rig :hardware])
