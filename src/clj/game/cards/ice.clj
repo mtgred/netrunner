3ec1a63cc2fe894fb4f8d6cc974f5c5645fb6255
Minh Tran <mtgred@gmail.com>
2017-01-26 03:45:57 +0000

    Macrophage implementation

--- src/clj/game/cards/ice.clj
+++ src/clj/game/cards/ice.clj
@@ -877,6 +877,24 @@
    "Lycan"
    (morph-ice "Sentry" "Code Gate" trash-program)
 
+   "Macrophage"
+   {:subroutines [(trace-ability 4 {:label "Purge virus counters"
+                                    :msg "purge virus counters"
+                                    :effect (effect (purge))})
+                  (trace-ability 3 {:label "Trash a virus"
+                                    :prompt "Choose a virus to trash"
+                                    :msg (msg "trash " (:title target))
+                                    :choices {:req #(and (installed? %)
+                                                         (has? % :subtype "Virus"))}
+                                    :effect (effect (trash target {:cause :subroutine})
+                                                    (clear-wait-prompt :runner))})
+                  (trace-ability 2 {:label "Remove a virus in the Heap from the game"
+                                    :prompt "Choose a virus in the Heap to remove from the game"
+                                    :choices (req (cancellable (filter #(has? % :subtype "Virus") (:discard runner)) :sorted))
+                                    :msg (msg "remove " (:title target) " from the game")
+                                    :effect (effect (move :runner target :rfg))})
+                  (trace-ability 1 end-the-run)]}
+
    "Magnet"
    {:delayed-completion true
     :effect (req (let [magnet card]
