998a1f24963f3b37d95be8e936209d2d9273d759
Jeremy <jalee453@gmail.com>
2017-01-30 17:49:52 -0500

    Fix for Bhagat

--- src/clj/game/cards/resources.clj
+++ src/clj/game/cards/resources.clj
@@ -114,8 +114,8 @@
                                  (runner-install target))}]}
 
    "Bhagat"
-   {:events {:successful-run {:req (req (= target :hq)
-                                        (first-successful-run-on-server? state :hq))
+   {:events {:successful-run {:req (req (and (= target :hq)
+                                             (first-successful-run-on-server? state :hq)))
                               :msg "force the Corp to trash the top card of R&D"
                               :effect (effect (mill :corp))}}}
 
