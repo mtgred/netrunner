654438c4fadb9a60e330448ab7f04d1abc159ad5
danhut <danhut@cisco.com>
2017-02-15 22:33:49 +1100

    removed some code duplication via local function eventmap

--- src/clj/game/cards/hardware.clj
+++ src/clj/game/cards/hardware.clj
@@ -618,24 +618,20 @@
                                                    (trash card {:cause :ability-cost}))} card nil)))}]}
 
    "Recon Drone"
+   ; eventmap uses reverse so we get the most recent event of each kind into map
+   (let [eventmap (fn [s] (into {} (reverse (get s :turn-events))))]
    {:prevent {:damage [:net :brain :meat]}
-    ; eventmap uses reverse so we get the most recent event of each kind into map
-    :abilities [{:req (req (let [eventmap (into {} (reverse (get @state :turn-events)))]
-                        (and
-                             (true? (:access @state))
-                             (= (:cid (second (:pre-damage eventmap)))
-                                (:cid (first (:post-access-card eventmap)))))))
-                :effect (req (let [eventmap (into {} (reverse (get @state :turn-events)))]
-                               (resolve-ability state side
-                                                {:prompt "Choose how much damage to prevent"
-                                                 :priority 50
-                                                 :choices {:number (req (min
-                                                                          (last (:pre-damage eventmap))
-                                                                          (:credit runner)))}
-                                                 :msg (msg "prevent " target " damage")
-                                                 :effect (effect (damage-prevent (first (:pre-damage eventmap))  target)
-                                                                 (lose :credit target)
-                                                                 (trash card {:cause :ability-cost}))} card nil)))}]}
+    :abilities [{:req (req (and (true? (:access @state)) (= (:cid (second (:pre-damage (eventmap @state))))
+                                                            (:cid (first (:post-access-card (eventmap @state)))))))
+                :effect (effect (resolve-ability
+                                  {:prompt "Choose how much damage to prevent"
+                                   :priority 50
+                                   :choices {:number (req (min (last (:pre-damage (eventmap @state)))
+                                                               (:credit runner)))}
+                                   :msg (msg "prevent " target " damage")
+                                   :effect (effect (damage-prevent (first (:pre-damage (eventmap @state))) target)
+                                                   (lose :credit target)
+                                                   (trash card {:cause :ability-cost}))} card nil))}]})
 
    "Record Reconstructor"
    {:events
