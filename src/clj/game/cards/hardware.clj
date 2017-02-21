486e7357a39cb77d6f98b549c6f411c4e8470f0e
danhut <danhut@cisco.com>
2017-02-21 12:48:47 +1100

    improve - only trigger prevent on access

--- src/clj/game/cards/hardware.clj
+++ src/clj/game/cards/hardware.clj
@@ -621,9 +621,8 @@
    "Recon Drone"
    ; eventmap uses reverse so we get the most recent event of each kind into map
    (let [eventmap (fn [s] (into {} (reverse (get s :turn-events))))]
-   {:prevent {:damage [:net :brain :meat]}
-    :abilities [{:req (req (and (true? (:access @state)) (= (:cid (second (:pre-damage (eventmap @state))))
-                                                            (:cid (first (:post-access-card (eventmap @state)))))))
+     {:abilities [{:req (req (and (true? (:access @state)) (= (:cid (second (:pre-damage (eventmap @state))))
+                                                              (:cid (first (:post-access-card (eventmap @state)))))))
                 :effect (effect (resolve-ability
                                   {:prompt "Choose how much damage to prevent"
                                    :priority 50
@@ -632,7 +631,9 @@
                                    :msg (msg "prevent " target " damage")
                                    :effect (effect (damage-prevent (first (:pre-damage (eventmap @state))) target)
                                                    (lose :credit target)
-                                                   (trash card {:cause :ability-cost}))} card nil))}]})
+                                                   (trash card {:cause :ability-cost}))} card nil))}]
+     :events    {:pre-access {:effect (req (doseq [dtype [:net :brain :meat]] (swap! state update-in [:prevent :damage dtype] #(conj % card))))}
+                 :run-ends   {:effect (req (doseq [dtype [:net :brain :meat]] (swap! state update-in [:prevent :damage dtype] #(drop 1 %))))}}})
 
    "Record Reconstructor"
    {:events
