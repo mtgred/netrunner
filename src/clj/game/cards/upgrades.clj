afdc7710f6fb3956db4fc1260d567ee391a4f689
Joel Koepp <jkoepp@gmail.com>
2017-02-23 09:20:11 -0600

    implement Defense Construct

--- src/clj/game/cards/upgrades.clj
+++ src/clj/game/cards/upgrades.clj
@@ -134,6 +134,23 @@
    "Dedicated Technician Team"
    {:recurring 2}
 
+   "Defense Construct"
+   {:advanceable :always
+    :abilities [{:label "[Trash]: Add 1 facedown card from Archives to HQ for each advancement token"
+                 :req (req (and run (= (:server run) [:archives])
+                                (pos? (get-in card [:advance-counter] 0))))
+                 :effect (effect (resolve-ability
+                                   {:show-discard true
+                                    :choices {:max (get-in card [:advance-counter] 0)
+                                              :req #(and (= (:side %) "Corp")
+                                                         (not (:seen %))
+                                                         (= (:zone %) [:discard]))}
+                                              :msg (msg "add " (count targets) " facedown cards in Archives to HQ")
+                                    :effect (req (doseq [c targets]
+                                                   (move state side c :hand)))}
+                                  card nil)
+                                 (trash card))}]}
+
    "Disposable HQ"
    (letfn [(dhq [n i]
              {:req (req (pos? i))
