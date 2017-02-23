99422998e707910d5e29066919bb750e716432f1
Joel Koepp <jkoepp@gmail.com>
2017-02-23 06:58:36 -0600

    implement Zed 2.0

--- src/clj/game/cards/ice.clj
+++ src/clj/game/cards/ice.clj
@@ -1502,4 +1502,10 @@
    "Zed 1.0"
    {:implementation "Restriction on having spent [click] is not implemented"
     :subroutines [(do-brain-damage 1)]
-    :runner-abilities [(runner-break [:click 1] 1)]}})
+    :runner-abilities [(runner-break [:click 1] 1)]}
+
+   "Zed 2.0"
+   {:implementation "Restriction on having spent [click] is not implemented"
+    :subroutines [trash-hardware
+                  (do-brain-damage 2)]
+    :runner-abilities [(runner-break [:click 2] 2)]}})
