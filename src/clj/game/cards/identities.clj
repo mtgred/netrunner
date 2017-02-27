439363e27cc4c7451f5c3d7a253eea526b2a6366
Joel Koepp <jkoepp@gmail.com>
2017-02-27 08:18:09 -0600

    implement Jemison Astronautics

--- src/clj/game/cards/identities.clj
+++ src/clj/game/cards/identities.clj
@@ -330,6 +330,19 @@
                            :once :per-turn
                            :effect (effect (draw 1))}}}
 
+   "Jemison Astronautics: Sacrifice. Audacity. Success."
+   {:events {:corp-forfeit-agenda
+             {:delayed-completion true
+              :effect (req (show-wait-prompt state :runner "Corp to place advancement tokens")
+                           (let [p (inc (get-agenda-points state :corp target))]
+                             (continue-ability state side
+                               {:prompt "Choose a card to place advancement tokens on with Jemison Astronautics: Sacrifice. Audacity. Success."
+                                :choices {:req #(and (installed? %) (= (:side %) "Corp"))}
+                                :msg (msg "place " p " advancement tokens on " (card-str state target))
+                                :effect (effect (add-prop :corp target :advance-counter p {:placed true})
+                                                (clear-wait-prompt :runner))}
+                              card nil)))}}}
+
    "Jesminder Sareen: Girl Behind the Curtain"
    {:events {:pre-tag {:once :per-run
                        :req (req (:run @state))
