1083514966bf6c91fd94bf87478df420893affdb
danhut <danhut@cisco.com>
2017-03-02 09:45:19 +0100

    fix  #2401

--- src/clj/game/cards/resources.clj
+++ src/clj/game/cards/resources.clj
@@ -472,7 +472,7 @@
              :successful-run {:interactive (req true)
                               :optional
                               {:delayed-completion true
-                               :req (req (first-event? state side :successful-run))
+                               :req (req (= 1 (count (get-in @state [:runner :register :successful-run]))))
                                :prompt "Use Find the Truth to look at the top card of R&D?"
                                :yes-ability {:msg "look at the top card of R&D"
                                              :effect (req (prompt! state :runner card (str "The top card of R&D is "
