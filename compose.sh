84f11f2d70ca58a12c08f5eb0c185da0dfe2c72f
Daniel Strong <dstrong@glyx.co.uk>
2017-03-04 01:20:03 +0000

    Improve Dockerfile, compose commands, compose.sh
     - Require volume and set WORKDIR on Dockerfile (saves having to `cd`
       before all the commands in `compose.yml`
     - Use image named netrunner-node rather than build on each
       `docker-compose up`
     - Add flag to `./compose.sh` to build netrunner-node image from
       Dockerfile
     - Move environment variables defined in service commands into compose
       `environment:` fields
     - Separate npm-install service from fetch service (which pulls card
       data)
     - Rename working directory in containers from `/workdir` to `/netrunner`

--- compose.sh
+++ compose.sh
@@ -2,6 +2,31 @@
 
 set -e
 
+usage () {
+    cat<<EOF
+Usage: $0 [-b]
+
+  -b   build netrunner-node image
+EOF
+}
+
+while getopts ':b' o; do
+    case "${o}" in
+        b)
+            BUILD=true
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+if [[ -n ${BUILD} ]]; then
+    docker build -t netrunner-node .
+fi
+
+docker-compose up npm-install
+
 docker-compose up npm
 
 docker-compose up netrunner coffee stylus lein
