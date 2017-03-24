972eec554034afe5f55cf2616871737cd3e93cbb
Joel Koepp <jkoepp@gmail.com>
2017-03-24 14:39:26 -0500

    implement Free Lunch

--- src/clj/game/cards/ice.clj
+++ src/clj/game/cards/ice.clj
@@ -654,6 +654,12 @@
                                                                              (end-run))}
                                                      card nil))})]}
 
+   "Free Lunch"
+   {:abilities [(power-counter-ability {:label "Runner loses 1 [Credits]"
+                                        :msg "make the Runner lose 1 [Credits]"
+                                        :effect (effect (lose :runner :credit 1))})]
+    :subroutines [add-power-counter]}
+
    "Galahad"
    (grail-ice end-the-run)
 
