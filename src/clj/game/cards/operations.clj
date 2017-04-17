b861f9f79dc8c6ac3bed63a8f6f7535b3dd4ea2e
Joel Koepp <jkoepp@gmail.com>
2017-04-17 18:09:59 -0500

    partial implementation of MCA Informant

--- src/clj/game/cards/operations.clj
+++ src/clj/game/cards/operations.clj
@@ -650,6 +650,19 @@
                                       :delayed-completion true
                                       :effect (effect (tag-runner :runner eid 1))}}}}
 
+   "MCA Informant"
+   {:implementation "Runner must deduct 1 click and 2 credits, then trash host manually"
+    :req (req (not-empty (filter #(and (has-subtype? % "Connection")
+                                         (installed? %)) (concat (all-installed state :runner)
+                                                                 (all-installed state :corp)))))
+    :prompt "Choose a connection to host MCA Informant on it"
+    :choices {:req #(and (has-subtype? % "Connection") (installed? %))}
+    :msg (msg "host it on " (card-str state target) ". The Runner has an additional tag")
+    :effect (req (host state side (get-card state target) (assoc card :zone [:discard] :seen true))
+                 (swap! state update-in [:runner :tag] inc))
+    :leave-play (req (swap! state update-in [:runner :tag] dec)
+                     (system-msg state :corp "trashes MCA Informant"))}
+
    "Medical Research Fundraiser"
    {:msg "gain 8 [Credits]. The Runner gains 3 [Credits]"
     :effect (effect (gain :credit 8) (gain :runner :credit 3))}
