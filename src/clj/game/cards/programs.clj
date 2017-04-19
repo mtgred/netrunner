78d0f7f5bfb4a19978df7df76fd1c05d3eca0fb8
Joel Koepp <jkoepp@gmail.com>
2017-04-19 08:26:20 -0500

    fix Self-modifying Code (trashing itself twice)
    fixes #2450

--- src/clj/game/cards/programs.clj
+++ src/clj/game/cards/programs.clj
@@ -690,24 +690,23 @@
                                 (gain state side :credit 1)))}]}
 
    "Self-modifying Code"
-   {:abilities  [{:req (req (not (install-locked? state side)))
-                  :effect (req (when-completed (trash state side card {:cause :ability-cost})
-                                               (continue-ability state side
-                                                                  {:prompt "Choose a program to install"
-                                                                   :msg (req (if (not= target "No install")
-                                                                               (str "install " (:title target))
-                                                                               (str "shuffle their Stack")))
-                                                                   :priority true
-                                                                   :choices (req (cancellable
-                                                                                   (conj (vec (sort-by :title (filter #(is-type? % "Program")
-                                                                                                                      (:deck runner))))
-                                                                                         "No install")))
-                                                                   :cost [:credit 2]
-                                                                   :effect (req (trigger-event state side :searched-stack nil)
-                                                                                (trash state side card {:cause :ability-cost})
-                                                                                (shuffle! state side :deck)
-                                                                                (when (not= target "No install")
-                                                                                  (runner-install state side target)))} card nil)))}]}
+   {:abilities [{:req (req (not (install-locked? state side)))
+                 :effect (req (when-completed (trash state side card {:cause :ability-cost})
+                                              (continue-ability state side
+                                                {:prompt "Choose a program to install"
+                                                 :msg (req (if (not= target "No install")
+                                                             (str "install " (:title target))
+                                                             (str "shuffle their Stack")))
+                                                 :priority true
+                                                 :choices (req (cancellable
+                                                                 (conj (vec (sort-by :title (filter #(is-type? % "Program")
+                                                                                                    (:deck runner))))
+                                                                       "No install")))
+                                                 :cost [:credit 2]
+                                                 :effect (req (trigger-event state side :searched-stack nil)
+                                                              (shuffle! state side :deck)
+                                                              (when (not= target "No install")
+                                                                (runner-install state side target)))} card nil)))}]}
 
    "Sneakdoor Beta"
    {:abilities [{:cost [:click 1]
