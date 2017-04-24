69cd72156335c2d31ce62f170b93df607a5ec3f7
danhut <danhut@cisco.com>
2017-04-24 17:12:06 +1000

    implement Ben Musashi

--- src/clj/test/cards/upgrades.clj
+++ src/clj/test/cards/upgrades.clj
@@ -22,6 +22,56 @@
         (is (get-in (refresh spid) [:rezzed]) "Spiderweb rezzed")
         (is (= 1 (:credit (get-corp))) "Paid only 1 credit to rez")))))
 
+(deftest ben-musashi
+  ;; Ben Musashi - pay 2 net damage to steal from this servr
+  (do-game
+    (new-game (default-corp [(qty "Ben Musashi" 1) (qty "House of Knives" 1)])
+              (default-runner))
+    (play-from-hand state :corp "Ben Musashi" "New remote")
+    (play-from-hand state :corp "House of Knives" "Server 1")
+    (take-credits state :corp 1)
+    (let [bm (get-content state :remote1 0)
+          hok (get-content state :remote1 1)]
+      (core/rez state :corp bm)
+      (run-empty-server state "Server 1")
+      ;; runner now chooses which to access.
+      (prompt-select :runner hok)
+      ;; prompt should be asking for the 5cr cost
+      (is (= "House of Knives" (:title (:card (first (:prompt (get-runner))))))
+          "Prompt to pay 2 net damage")
+      (prompt-choice :runner "No")
+      (is (= 5 (:credit (get-runner))) "Runner did not pay 2 net damage")
+      (is (= 0 (count (:scored (get-runner)))) "No scored agendas")
+      (prompt-select :runner bm)
+      (prompt-choice :runner "No")
+      (run-empty-server state "Server 1")
+      (prompt-select :runner hok)
+      (prompt-choice :runner "Yes")
+      (is (= 2 (count (:discard (get-runner)))) "Runner took 2 net")
+      (is (= 1 (count (:scored (get-runner)))) "1 scored agenda"))))
+
+(deftest ben-musashi-trash
+  ;; Ben Musashi - pay even when trashed
+  (do-game
+    (new-game (default-corp [(qty "Ben Musashi" 3) (qty "House of Knives" 3)])
+              (default-runner))
+    (play-from-hand state :corp "Ben Musashi" "New remote")
+    (play-from-hand state :corp "House of Knives" "Server 1")
+    (take-credits state :corp 1)
+    (core/gain state :runner :credit 1)
+    (let [bm (get-content state :remote1 0)
+          hok (get-content state :remote1 1)]
+      (core/rez state :corp bm)
+      (run-empty-server state "Server 1")
+      ;; runner now chooses which to access.
+      (prompt-select :runner bm)
+      (prompt-choice :runner "Yes") ; pay to trash
+      (prompt-select :runner hok)
+      ;; should now have prompt to pay 2 net for HoK
+      (prompt-choice :runner "Yes")
+      (is (= 2 (count (:discard (get-runner)))) "Runner took 2 net")
+      (is (= 1 (count (:scored (get-runner)))) "1 scored agenda"))))
+
 (deftest bernice-mai
   ;; Bernice Mai - successful and unsuccessful
   (do-game
