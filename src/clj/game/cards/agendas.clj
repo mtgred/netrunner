9c7ce60c6233fa445446a3039e45125ace411645
Joel Koepp <jkoepp@gmail.com>
2017-05-01 13:35:51 -0500

    implement Successful Field Test

--- src/clj/game/cards/agendas.clj
+++ src/clj/game/cards/agendas.clj
@@ -799,6 +799,23 @@
     :msg "do 2 meat damage"
     :effect (effect (damage eid :meat 2 {:card card}))}
 
+   "Successful Field Test"
+   (letfn [(sft [n max] {:prompt "Select a card in HQ to install with Successful Field Test"
+                         :priority -1
+                         :delayed-completion true
+                         :choices {:req #(and (= (:side %) "Corp")
+                                              (not (is-type? % "Operation"))
+                                              (in-hand? %))}
+                         :effect (req (when-completed
+                                        (corp-install state side target nil {:no-install-cost true})
+                                        (if (< n max)
+                                          (continue-ability state side (sft (inc n) max) card nil)
+                                          (effect-completed state side eid card))))})]
+     {:delayed-completion true
+      :msg "install cards from HQ, ignoring all costs"
+      :effect (req (let [max (count (filter #(not (is-type? % "Operation")) (:hand corp)))]
+                     (continue-ability state side (sft 1 max) card nil)))})
+
    "Superior Cyberwalls"
    {:interactive (req true)
     :msg (msg "gain " (reduce (fn [c server]
