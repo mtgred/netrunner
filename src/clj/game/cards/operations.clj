efcfc82aec86dcc530449cd2838e3b077512907a
danhut <danhut@cisco.com>
2017-05-28 22:52:48 +1000

    implement Audacity

--- src/clj/game/cards/operations.clj
+++ src/clj/game/cards/operations.clj
@@ -106,6 +106,25 @@
                    (move state :runner c :rfg))
                  (effect-completed state side eid card))}
 
+   "Audacity"
+   (let [audacity (fn au [n] {:prompt "Choose a card on which to place an advancement"
+                              :choices {:req can-be-advanced?}
+                              :msg (msg "place an advancement token on " (card-str state target))
+                              :effect (req (add-prop state :corp target :advance-counter 1 {:placed true})
+                                           (if (< n 2)
+                                             (continue-ability state side (au (inc n)) card nil)
+                                             (effect-completed state side eid card)))})]
+   {:req (req (let [h (:hand corp)
+                    p (:play-area corp)]
+                ;; this is needed to pass the req check for can-play? and again when card is actually played
+                (if (some #(= (:cid %) (:cid card)) p)
+                  (>= (count h) 2)
+                  (>= (count h) 3))))
+    :effect (req (system-msg state side "trashes all cards in HQ due to Audacity")
+                 (doseq [c (:hand corp)]
+                   (trash state side c {:unpreventable true}))
+                 (continue-ability state side (audacity 1) card nil))})
+
    "Back Channels"
    {:prompt "Choose an installed card in a server to trash"
     :choices {:req #(and (= (last (:zone %)) :content)
