4d8921d3342a623a483aadb90035951e7a839d26
danhut <danhut@cisco.com>
2017-05-29 09:17:57 +1000

    update based on feedback

--- src/clj/game/cards/ice.clj
+++ src/clj/game/cards/ice.clj
@@ -278,7 +278,7 @@
     :runner-abilities [{:label "Take 1 tag"
                         :delayed-completion true
                         :effect (req (system-msg state :runner "takes 1 tag on encountering Authenticator to Bypass it")
-                                     (tag-runner state :runner eid 1))}]
+                                     (tag-runner state :runner eid 1 {:unpreventable true}))}]
     :subroutines [(gain-credits 2)
                   end-the-run]}
 
--- src/clj/game/cards/operations.clj
+++ src/clj/game/cards/operations.clj
@@ -108,13 +108,16 @@
 
    "Audacity"
    (let [audacity (fn au [n] {:prompt "Choose a card on which to place an advancement"
+                              :delayed-completion true
                               :choices {:req can-be-advanced?}
+                              :cancel-effect (req (effect-completed state side eid))
                               :msg (msg "place an advancement token on " (card-str state target))
                               :effect (req (add-prop state :corp target :advance-counter 1 {:placed true})
                                            (if (< n 2)
                                              (continue-ability state side (au (inc n)) card nil)
                                              (effect-completed state side eid card)))})]
-   {:req (req (let [h (:hand corp)
+   {:delayed-completion true
+    :req (req (let [h (:hand corp)
                     p (:play-area corp)]
                 ;; this is needed to pass the req check for can-play? and again when card is actually played
                 (if (some #(= (:cid %) (:cid card)) p)
