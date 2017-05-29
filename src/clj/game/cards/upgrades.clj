674e9f4da7b5a21ddfef73ebb86e858949c8f1d9
danhut <danhut@cisco.com>
2017-05-29 22:43:41 +1000

    implement Bamboo Dome

--- src/clj/game/cards/upgrades.clj
+++ src/clj/game/cards/upgrades.clj
@@ -50,6 +50,37 @@
                                                                   (unregister-events state side card))}} card))}]
     :events {:run-ends nil}}
 
+   "Bamboo Dome"
+   (letfn [(dome [dcard]
+             {:prompt "Select a card to add to HQ"
+              :delayed-completion true
+              :choices {:req #(and (= (:side %) "Corp")
+                                   (= (:zone %) [:play-area]))}
+              :msg "move a card to HQ"
+              :effect (effect (move target :hand)
+                              (continue-ability (put dcard) dcard nil))})
+           (put [dcard]
+             {:prompt "Select first card to put back onto R&D"
+              :delayed-completion true
+              :choices {:req #(and (= (:side %) "Corp")
+                                   (= (:zone %) [:play-area]))}
+              :msg "move remaining cards back to R&D"
+              :effect (effect (move target :deck {:front true})
+                              (move (first (get-in @state [:corp :play-area])) :deck {:front true})
+                              (clear-wait-prompt :runner)
+                              (effect-completed eid dcard))})]
+
+   {:init {:root "R&D"}
+    :abilities [{:cost [:click 1]
+                 :req (req (>= (count (:deck corp)) 3))
+                 :delayed-completion true
+                 :msg (msg (str "reveal " (join ", " (map :title (take 3 (:deck corp)))) " from R&D"))
+                 :label "Reveal the top 3 cards of R&D. Secretly choose 1 to add to HQ. Return the others to the top of R&D, in any order."
+                 :effect (req (doseq [c (take 3 (:deck corp))]
+                                (move state side c :play-area))
+                              (show-wait-prompt state :runner "Corp to use Bamboo Dome")
+                              (continue-ability state side (dome card) card nil))}]})
+
    "Ben Musashi"
    (let [bm {:req (req (or (= (:zone card) (:zone target)) (= (central->zone (:zone target)) (butlast (:zone card)))))
              :effect (effect (steal-cost-bonus [:net-damage 2]))}]
