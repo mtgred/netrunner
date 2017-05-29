b519409643d6621b5bbd7598c80bbd19d7c1b7a1
danhut <danhut@cisco.com>
2017-05-29 21:51:57 +1000

    implement Bloom

--- src/clj/game/cards/ice.clj
+++ src/clj/game/cards/ice.clj
@@ -320,6 +320,38 @@
                                                          (resolve-ability state :runner trash-program card nil)))}
                                         card nil))))}]}
 
+   "Bloom"
+   (let [ice-index (fn [state i] (first (keep-indexed #(when (= (:cid %2) (:cid i)) %1)
+                                                      (get-in @state (cons :corp (:zone i))))))]
+     {:subroutines
+              [{:label "Install a piece of ice from HQ protecting another server, ignoring all costs"
+                :prompt "Choose ICE to install from HQ in another server"
+                :delayed-completion true
+                :choices {:req #(and (ice? %)
+                                     (in-hand? %))}
+                :effect (req (let [this (zone->name (second (:zone card)))
+                                   nice target]
+                               (continue-ability state side
+                                                 {:prompt (str "Choose a location to install " (:title target))
+                                                  :choices (req (remove #(= this %) (corp-install-list state nice)))
+                                                  :delayed-completion true
+                                                  :effect (effect (corp-install nice target {:no-install-cost true}))}
+                                                 card nil)))}
+               {:label "Install a piece of ice from HQ in the next innermost position, protecting this server, ignoring all costs"
+                :prompt "Choose ICE to install from HQ in this server"
+                :delayed-completion true
+                :choices {:req #(and (ice? %)
+                                     (in-hand? %))}
+                :effect (req (let [newice (assoc target :zone (:zone card))
+                                   bndx (ice-index state card)
+                                   ices (get-in @state (cons :corp (:zone card)))
+                                   newices (apply conj (subvec ices 0 bndx) newice (subvec ices bndx))]
+                               (swap! state assoc-in (cons :corp (:zone card)) newices)
+                               (swap! state update-in (cons :corp (:zone target))
+                                      (fn [coll] (remove-once #(not= (:cid %) (:cid target)) coll)))
+                               (card-init state side newice false)
+                               (trigger-event state side :corp-install newice)))}]})
+
    "Brainstorm"
    {:abilities [{:label "Gain subroutines"
                  :msg (msg "gain " (count (:hand runner)) " subroutines")}]
