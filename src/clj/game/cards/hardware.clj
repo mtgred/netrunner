5c6cdc89b8e582fcb66a046e5cf680307033c7cc
Joel Koepp <jkoepp@gmail.com>
2017-07-15 08:58:53 -0500

    add message to Spinal Modem

--- src/clj/game/cards/hardware.clj
+++ src/clj/game/cards/hardware.clj
@@ -838,7 +838,9 @@
    "Spinal Modem"
    {:in-play [:memory 1]
     :recurring 2
-    :events {:successful-trace {:req (req run) :effect (effect (damage eid :brain 1 {:card card}))}}}
+    :events {:successful-trace {:req (req run)
+                                :effect (effect (system-msg (str "suffers 1 brain damage from Spinal Modem"))
+                                                (damage eid :brain 1 {:card card}))}}}
 
    "Sports Hopper"
    {:in-play [:link 1]
