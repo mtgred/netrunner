a465c46ea5ae125e6e00be5059214124bf3b9bda
kevkcc <kkcchu@gmail.com>
2017-07-22 18:07:57 -0700

    Make god of war auto-icebreaker

--- src/clj/game/cards/icebreakers.clj
+++ src/clj/game/cards/icebreakers.clj
@@ -488,20 +488,21 @@
                                  (strength-pump 1 1)]})
 
    "God of War"
-   {:flags {:runner-phase-12 (req true)}
-    :abilities [(strength-pump 2 1)
-                {:counter-cost [:virus 1]
-                 :msg "break 1 subroutine"}
-                {:label "Take 1 tag to place 2 virus counters (start of turn)"
-                 :once :per-turn
-                 :req (req (:runner-phase-12 @state))
-                 :effect (req (when-completed (tag-runner state :runner 1)
-                                              (if (not (get-in @state [:tag :tag-prevent]))
-                                                (do (add-counter state side card :virus 2)
-                                                    (system-msg state side
-                                                                (str "takes 1 tag to place 2 virus counters on God of War"))
-                                                    (effect-completed state side eid))
-                                                (effect-completed state side eid))))}]}
+   (auto-icebreaker ["All"]
+                    {:flags {:runner-phase-12 (req true)}
+                     :abilities [(strength-pump 2 1)
+                                 {:counter-cost [:virus 1]
+                                  :msg "break 1 subroutine"}
+                                 {:label "Take 1 tag to place 2 virus counters (start of turn)"
+                                  :once :per-turn
+                                  :req (req (:runner-phase-12 @state))
+                                  :effect (req (when-completed (tag-runner state :runner 1)
+                                                               (if (not (get-in @state [:tag :tag-prevent]))
+                                                                 (do (add-counter state side card :virus 2)
+                                                                     (system-msg state side
+                                                                                 (str "takes 1 tag to place 2 virus counters on God of War"))
+                                                                     (effect-completed state side eid))
+                                                                 (effect-completed state side eid))))}]})
 
    "Golden"
    (auto-icebreaker ["Sentry"]
