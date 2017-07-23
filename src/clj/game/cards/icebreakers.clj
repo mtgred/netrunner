edc088c91c238b070ce475fb9e780169521ba9de
kevkcc <kkcchu@gmail.com>
2017-07-22 18:07:31 -0700

    Maven implementation

--- src/clj/game/cards/icebreakers.clj
+++ src/clj/game/cards/icebreakers.clj
@@ -615,6 +615,14 @@
                      :abilities [(break-sub 2 1 "code gate")
                                  (strength-pump 1 1)]})
 
+   "Maven"
+   {:abilities [(break-sub 2 1 "ICE")]
+    :events (let [maven {:silent (req true)
+                         :req (req (is-type? target "Program"))
+                         :effect (effect (update-breaker-strength card))}]
+              {:runner-install maven :trash maven :card-moved maven})
+    :strength-bonus (req (count (filter #(is-type? % "Program") (all-installed state :runner))))}
+
    "Morning Star"
    {:abilities [(break-sub 1 0 "barrier")]}
 
