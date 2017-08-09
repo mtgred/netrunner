8c28b52a24e3609aebe219eb2ae131dda6697fb0
danhut <danhut@cisco.com>
2017-08-09 21:03:43 +1000

    implement rover algorithm

--- src/clj/game/cards/operations.clj
+++ src/clj/game/cards/operations.clj
@@ -1036,6 +1036,16 @@
     :msg "shuffle a card from HQ into R&D"
     :effect (final-effect (move target :deck) (shuffle! :deck))}
 
+   "Rover Algorithm"
+   {:choices {:req #(and (ice? %) (rezzed? %))}
+    :msg (msg "host it as a condition counter on " (card-str state target))
+    :effect (final-effect (host target (assoc card :zone [:discard] :seen true :condition true))
+                          (update-ice-strength (get-card state target)))
+    :events {:pass-ice {:req (req (= (:cid target) (:cid (:host card))))
+                                :effect (effect (add-counter card :power 1))}
+             :pre-ice-strength {:req (req (= (:cid target) (:cid (:host card))))
+                                :effect (effect (ice-strength-bonus (get-in card [:counter :power] 0) target))}}}
+
    "Sacrifice"
    {:req (req (pos? (:bad-publicity corp)))
     :delayed-completion true
