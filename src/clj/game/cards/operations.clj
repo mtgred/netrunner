d291bc1e16566ca1154083822dafe28d1d94cd31
danhut <danhut@cisco.com>
2017-08-14 22:24:14 +1000

    implement Restoration

--- src/clj/game/cards/operations.clj
+++ src/clj/game/cards/operations.clj
@@ -1003,6 +1003,26 @@
       :effect (req (move state side target :hand)
                    (resolve-ability state side (replant 1) card nil))})
 
+   "Restoration"
+   {:delayed-completion true
+    :effect (effect (continue-ability {:prompt "Select a card in Archives to install & rez with Restoration"
+                                       :priority -1
+                                       :delayed-completion true
+                                       :show-discard true
+                                       :choices {:req #(and (= (:side %) "Corp")
+                                                            (not (is-type? % "Operation"))
+                                                            (in-discard? %))}
+                                       :effect (req (when-completed
+                                                      (corp-install state side target nil {:install-state :rezzed})
+                                                      (do (system-msg state side (str "uses Restoration to "
+                                                                                      (corp-install-msg target)))
+                                                          (let [leftover (filter #(= (:title target) (:title %)) (-> @state :corp :discard))]
+                                                            (when (seq leftover)
+                                                              (doseq [c leftover]
+                                                                (move state side c :rfg))
+                                                              (system-msg state side "removes " (count leftover) " copies of " (:title target) " from the game")))
+                                                          (effect-completed state side eid card))))} card nil))}
+
    "Restoring Face"
    {:prompt "Choose a Sysop, Executive or Clone to trash"
     :msg (msg "trash " (card-str state target) " to remove 2 bad publicity")
