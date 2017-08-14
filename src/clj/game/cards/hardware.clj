f6ab361986b29420feff00cccbe713e473d4aed5
Filip Gokstorp <filip@gokstorp.se>
2017-08-14 19:07:02 +0200

    Adds :no-trash trigger for non-agenda cards with no trash cost. Makes Maw card discard face-up if it is the card being accessed (triggering the :no-trash event hook). Adds test for this. Fixes #2695

--- src/clj/game/cards/hardware.clj
+++ src/clj/game/cards/hardware.clj
@@ -446,7 +446,11 @@
                                  (not= (first (:zone target)) :discard)))
                   :once :per-turn
                   :msg "force the Corp to trash a random card from HQ"
-                  :effect (req (trash state :corp (first (shuffle (:hand corp)))))}]
+                  :effect (req (let [card-to-trash (first (shuffle (:hand corp)))
+                                     card-seen? (= (:cid target) (:cid card-to-trash))
+                                     card-to-trash (if card-seen? (assoc card-to-trash :seen true)
+                                                                  card-to-trash)]
+                                 (trash state :corp card-to-trash)))}]
      {:in-play [:memory 2]
       :abilities [ability]
       :events {:no-trash ability
