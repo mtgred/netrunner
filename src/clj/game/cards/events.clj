dd594a87b595d8c682b295176eb5416273a4d466
danhut <danhut@cisco.com>
2017-08-14 22:51:39 +1000

    implement Reformation

--- src/clj/game/cards/events.clj
+++ src/clj/game/cards/events.clj
@@ -1164,6 +1164,13 @@
    "Recon"
    {:prompt "Choose a server" :choices (req runnable-servers) :effect (effect (run target nil card))}
 
+   "Reformation"
+   {:prompt "Select two non-rezzed ICE to swap positions"
+    :choices {:req #(and (installed? %) (not (rezzed? %)) (ice? %)) :max 2}
+    :msg (msg "swap the positions of " (card-str state (first targets)) " and " (card-str state (second targets)))
+    :effect (req (when (= (count targets) 2)
+                   (swap-ice state side (first targets) (second targets))))}
+
    "Retrieval Run"
    {:effect (effect (run :archives
                       {:req (req (= target :archives))
