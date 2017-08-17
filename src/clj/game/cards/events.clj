5a235319e3b861e28742f4afb910d1f47481190a
danhut <danhut@cisco.com>
2017-08-17 16:47:37 +1000

    implement Diana's Hunt

--- src/clj/game/cards/events.clj
+++ src/clj/game/cards/events.clj
@@ -311,6 +311,39 @@
      {:delayed-completion true
       :effect (effect (continue-ability (choice all) card nil))})
 
+   "Dianas Hunt"
+   {:implementation "One program per encounter not enforced"
+    :prompt "Choose a server"
+    :msg "make a run and install a program on encounter with each ICE"
+    :choices (req runnable-servers)
+    :delayed-completion true
+    :abilities [{:label "Install a program using Diana's Hunt?"
+                 :delayed-completion true
+                 :effect (effect (resolve-ability
+                                   {:prompt "Choose a program in your Grip to install"
+                                    :choices {:req #(and (is-type? % "Program")
+                                                         (runner-can-install? state side % false)
+                                                         (in-hand? %))}
+                                    :msg (msg "install " (:title target))
+                                    :effect (req (runner-install state side target {:no-cost true})
+                                                 (swap! state update :diana #(conj % target)))}
+                                   card nil))}]
+    :effect (effect (run target nil card)
+                    (prompt! card (str "Click Diana's Hunt in the Temporary Zone to install a Program") ["OK"] {})
+                    (resolve-ability
+                      {:effect (req (let [c (move state side (last (:discard runner)) :play-area)]
+                                      (card-init state side c false)
+                                      (register-events state side
+                                                       {:run-ends {:effect (req (let [hunt (:diana @state)]
+                                                                                  (doseq [c hunt]
+                                                                                    (system-msg state side (str "trashes " (:title c) " at the end of the run from Diana's Hunt"))
+                                                                                    (trash state side (find-cid (:cid c) (all-installed state side)) {:unpreventable true}))
+                                                                                  (swap! state dissoc :diana)
+                                                                                  (unregister-events state side card)
+                                                                                  (trash state side c)))}} c)))}
+                      card nil))
+    :events {:run-ends nil}}
+
    "Diesel"
    {:msg "draw 3 cards" :effect (effect (draw 3))}
 
