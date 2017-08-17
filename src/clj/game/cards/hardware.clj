fc94e28a57cc3c12f1ce52a74305c513d42d8269
Joel Koepp <jkoepp@gmail.com>
2017-08-17 09:58:39 -0500

    flip Archives faceup before ArchInt prompt
    fix #2685

--- src/clj/game/cards/hardware.clj
+++ src/clj/game/cards/hardware.clj
@@ -26,19 +26,19 @@
       :req (req (and (= target :archives)
                      (not= (:max-access run) 0)
                      (not-empty (:discard corp))))
-      :effect (effect (continue-ability
-                        {:optional
-                         {:delayed-completion true
-                          :prompt "Use Archives Interface to remove a card from the game instead of accessing it?"
-                          :yes-ability
-                          {:delayed-completion true
-                           :effect (req (swap! state update-in [:corp :discard] #(map (fn [c] (assoc c :seen true)) %))
-                                        (continue-ability state side
+      :effect (req (swap! state update-in [:corp :discard] #(map (fn [c] (assoc c :seen true)) %))
+                   (continue-ability state side
+                     {:optional
+                      {:delayed-completion true
+                       :prompt "Use Archives Interface to remove a card from the game instead of accessing it?"
+                       :yes-ability
+                       {:delayed-completion true
+                        :effect (effect (continue-ability
                                           {:prompt "Choose a card in Archives to remove from the game instead of accessing"
                                            :choices (req (:discard corp))
                                            :msg (msg "remove " (:title target) " from the game")
                                            :effect (effect (move :corp target :rfg))} card nil))}
-                          :no-ability {:effect (req (effect-completed state side eid))}}} card nil))}}}
+                       :no-ability {:effect (req (effect-completed state side eid))}}} card nil))}}}
 
    "Astrolabe"
    {:in-play [:memory 1]
