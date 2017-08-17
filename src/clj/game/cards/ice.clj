a0263f9e95e125e6d4c1712818bb43984b61b141
danhut <danhut@cisco.com>
2017-08-17 21:14:35 +1000

    implement Sand Storm

--- src/clj/game/cards/ice.clj
+++ src/clj/game/cards/ice.clj
@@ -1446,6 +1446,18 @@
                  :msg (msg "gain " (:advance-counter card 0) " subroutines")}]
     :subroutines [(tag-trace 2)]}
 
+   "Sand Storm"
+   {:subroutines [{:req (req (:run @state))
+                   :label "Move Sand Storm and the run to another server"
+                   :prompt "Choose another server and redirect the run to its outermost position"
+                   :choices (req (cancellable servers))
+                   :msg (msg "move Sand Storm and the run.  The Runner is now running on " target ". Sand Storm is trashed")
+                   :effect (req (let [dest (server->zone state target)]
+                                (trash state side card {:unpreventable true})
+                                (swap! state update-in [:run]
+                                       #(assoc % :position (count (get-in corp (conj dest :ices)))
+                                                 :server (rest dest)))))}]}
+
    "Sapper"
    {:subroutines [trash-program]
     :access {:delayed-completion true
