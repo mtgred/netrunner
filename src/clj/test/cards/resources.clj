8b0804629790bcc6bf3a5d5b6d622c29cd2e5909
Filip Gokstorp <filip@gokstorp.se>
2017-08-24 01:12:32 +0200

    Fix Joshua B. test.

--- src/clj/test/cards/resources.clj
+++ src/clj/test/cards/resources.clj
@@ -715,8 +715,12 @@
     (play-from-hand state :runner "Joshua B.")
     (take-credits state :runner)
     (take-credits state :corp)
-    (prompt-choice :runner "Yes") ; gain the extra click
-    (is (= 5 (:click (get-runner))) "Gained extra click")
+    (is (= 0 (:click (get-runner))) "Runner has 0 clicks")
+    (is (:runner-phase-12 @state) "Runner is in Step 1.2")
+    (card-ability state :runner (get-in @state [:runner :rig :resource 0]) 0)
+    (is (= 1 (:click (get-runner))) "Gained extra click from Joshua")
+    (core/end-phase-12 state :runner nil)
+    (is (= 5 (:click (get-runner))) "Gained normal clicks as well")
     (take-credits state :runner)
     (is (= 1 (:tag (get-runner))) "Took 1 tag")))
 
