08f0a448ae62345d56b84dc3667e2602ea42b40d
Filip Gokstorp <filip@gokstorp.se>
2017-08-23 23:03:04 +0200

    Adds runner phase 1.2 to Joshua B. Fixes #2711.

--- src/clj/game/cards/resources.clj
+++ src/clj/game/cards/resources.clj
@@ -812,11 +812,12 @@
                   :end-turn {:delayed-completion true
                              :effect (effect (tag-runner eid 1))
                              :msg "gain 1 tag"}}]
-   {:events {:runner-turn-begins
-             {:optional {:prompt "Use Joshua B. to gain [Click]?"
-                         :once :per-turn
-                         :yes-ability ability}}}
-    :abilities [ability]})
+     {:flags {:runner-phase-12 (req true)}
+      :events {:runner-turn-begins
+               {:optional {:prompt "Use Joshua B. to gain [Click]?"
+                           :once :per-turn
+                           :yes-ability ability}}}
+      :abilities [ability]})
 
    "Kati Jones"
    {:abilities [{:cost [:click 1]
