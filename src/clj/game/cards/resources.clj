1fdd33200793d79b97f7b6c9b32ee861e4865461
Joel Koepp <jkoepp@gmail.com>
2017-08-23 21:00:44 -0500

    stop Bloo Moose if Blacklist is active
    fix #2712

--- src/clj/game/cards/resources.clj
+++ src/clj/game/cards/resources.clj
@@ -236,7 +236,8 @@
 
    "Bloo Moose"
    {:flags {:runner-phase-12 (req true)}
-    :abilities [{:req (req (:runner-phase-12 @state))
+    :abilities [{:req (req (and (:runner-phase-12 @state)
+                                (not (seq (get-in @state [:runner :locked :discard])))))
                  :once :per-turn
                  :prompt "Choose a card in the Heap to remove from the game and gain 2 [Credits]"
                  :show-discard true
