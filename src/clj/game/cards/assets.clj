0a0264f80d6de8021c291292de5e35679e6564d3
Filip Gokstorp <filip@gokstorp.se>
2017-09-28 22:12:25 +0100

    Fixes missing parenthesis.

--- src/clj/game/cards/assets.clj
+++ src/clj/game/cards/assets.clj
@@ -565,7 +565,7 @@
    "Ghost Branch"
    (advance-ambush 0 {:delayed-completion true
                       :req (req (< 0 (:advance-counter (get-card state card) 0)))
-                      :msg (msg "give the Runner " (quantify (:advance-counter (get-card state card) 0) "tag")
+                      :msg (msg "give the Runner " (quantify (:advance-counter (get-card state card) 0) "tag"))
                       :effect (effect (tag-runner :runner eid (:advance-counter (get-card state card) 0)))})
 
    "GRNDL Refinery"
