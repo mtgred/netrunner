4d592d9b294f8622d0c9cd58737c6caa8dad7b25
Neal Terrell <nealterrell@users.noreply.github.com>
2017-10-02 17:56:39 -0700

    Fix Underway Grid

--- src/clj/game/cards/upgrades.clj
+++ src/clj/game/cards/upgrades.clj
@@ -771,7 +771,7 @@
 
    "Underway Grid"
    {:implementation "Bypass prevention is not implemented"
-    :events {:pre-expose {:req (req (protecting-same-server? card target))
+    :events {:pre-expose {:req (req (same-server? card target))
                           :msg "prevent 1 card from being exposed"
                           :effect (effect (expose-prevent 1))}}}
 
