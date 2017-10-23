0efd404431dc65f08063513171fb0258fd5228c1
Neal Terrell <neal.terrell@csulb.edu>
2017-10-23 08:35:57 -0700

    Rework Standoff. Fix #2901.

--- src/clj/game/cards/agendas.clj
+++ src/clj/game/cards/agendas.clj
@@ -926,30 +926,30 @@
     :effect (effect (damage eid :meat 2 {:card card}))}
 
    "Standoff"
-   (letfn [(stand [state side card]
-             (show-wait-prompt state (other-side side) "trash target choice from Standoff")
-             (resolve-ability state side
-                              {:prompt "Choose an installed card to trash due to Standoff"
-                               :delayed-completion true
-                               :choices {:req #(installed? %)}
-                               :cancel-effect (req (if (= side :runner)
-                                                     (do (draw state :corp)
-                                                         (gain state :corp :credit 5)
-                                                         (clear-wait-prompt state :corp)
-                                                         (system-msg state :runner "declines to trash a card due to Standoff")
-                                                         (system-msg state :corp "draws a card and gains 5 [Credits] from Standoff")
-                                                         (effect-completed state :corp eid card))
-                                                     (do (system-msg state :corp "declines to trash a card from Standoff")
-                                                         (clear-wait-prompt state :runner)
-                                                         (effect-completed state :corp eid card))))
-                               :effect (req (do (system-msg state side (str "trashes " (card-str state target) " due to Standoff"))
-                                                (clear-wait-prompt state (other-side side))
-                                                (trash state side target {:unpreventable true})
-                                                (continue-ability state (other-side side) (stand state (other-side side) card) card nil)))}
-                              card nil))]
+   (letfn [(stand [side]
+             {:delayed-completion true
+              :prompt "Choose one of your installed cards to trash due to Standoff"
+              :choices {:req #(and (installed? %)
+                                   (same-side? side (:side %)))}
+              :cancel-effect (req (if (= side :runner)
+                                    (do (draw state :corp)
+                                        (gain state :corp :credit 5)
+                                        (clear-wait-prompt state :corp)
+                                        (system-msg state :runner "declines to trash a card due to Standoff")
+                                        (system-msg state :corp "draws a card and gains 5 [Credits] from Standoff")
+                                        (effect-completed state :corp eid))
+                                    (do (system-msg state :corp "declines to trash a card from Standoff")
+                                        (clear-wait-prompt state :runner)
+                                        (effect-completed state :corp eid))))
+              :effect (req (do (system-msg state side (str "trashes " (card-str state target) " due to Standoff"))
+                               (clear-wait-prompt state (other-side side))
+                               (trash state side target {:unpreventable true})
+                               (show-wait-prompt state side (str (side-str (other-side side)) " to trash a card for Standoff"))
+                               (continue-ability state (other-side side) (stand (other-side side)) card nil)))})]
      {:interactive (req true)
       :delayed-completion true
-      :effect (req (continue-ability state :runner (stand state :runner card) card nil))})
+      :effect (effect (show-wait-prompt (str (side-str (other-side side)) " to trash a card for Standoff"))
+                      (continue-ability :runner (stand :runner) card nil))})
 
    "Successful Field Test"
    (letfn [(sft [n max] {:prompt "Select a card in HQ to install with Successful Field Test"
