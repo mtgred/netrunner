440223709d9959bf8be88c8f650e999080cae34d
danhut <danhut@cisco.com>
2017-11-15 21:50:52 +1100

    faq 4.1 - the shadow net - do not allow priority events

--- src/clj/game/cards/resources.clj
+++ src/clj/game/cards/resources.clj
@@ -1551,13 +1551,14 @@
                                                        :effect (effect (breaker-strength-bonus 2))}}) card))}}
 
    "The Shadow Net"
-   {:abilities [{:cost [:click 1 :forfeit]
-                 :req (req (< 0 (count (filter #(is-type? % "Event") (:discard runner)))))
-                 :label "Play an event from your Heap, ignoring all costs"
-                 :prompt "Choose an event to play"
-                 :msg (msg "play " (:title target) " from the Heap, ignoring all costs")
-                 :choices (req (cancellable (filter #(is-type? % "Event") (:discard runner)) :sorted))
-                 :effect (effect (play-instant nil target {:ignore-cost true}))}]}
+   (letfn [(events [runner] (filter #(and (is-type? % "Event") (not (has-subtype? % "Priority"))) (:discard runner)))]
+     {:abilities [{:cost [:click 1 :forfeit]
+                   :req (req (< 0 (count (events runner))))
+                   :label "Play an event from your Heap, ignoring all costs"
+                   :prompt "Choose an event to play"
+                   :msg (msg "play " (:title target) " from the Heap, ignoring all costs")
+                   :choices (req (cancellable (events runner) :sorted))
+                   :effect (effect (play-instant nil target {:ignore-cost true}))}]})
 
    "The Supplier"
    (let [ability  {:label "Install a hosted card (start of turn)"
