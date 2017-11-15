2c1976a64a5e17ea0e102c7c901fbb2fcda621ef
danhut <danhut@cisco.com>
2017-11-15 21:36:54 +1100

    faq 4.1 - aginfusion rez requirements and enforce flipping to other server

--- src/clj/game/cards/identities.clj
+++ src/clj/game/cards/identities.clj
@@ -54,9 +54,9 @@
 
    "AgInfusion: New Miracles for a New World"
    {:abilities [{:once :per-turn
-                 :req (req (and (:run @state) (not (rezzed? current-ice))))
+                 :req (req (and (:run @state) (not (rezzed? current-ice)) (can-rez? state side current-ice)))
                  :prompt "Choose another server and redirect the run to its outermost position"
-                 :choices (req (cancellable servers))
+                 :choices (req (cancellable (remove #{(-> @state :run :server central->name)} servers)))
                  :msg (msg "trash the approached ICE. The Runner is now running on " target)
                  :effect (req (let [dest (server->zone state target)]
                                 (trash state side current-ice)
