c98b9e3ba32b9f1c4e6483be948f055d5685e832
Neal Terrell <neal.terrell@csulb.edu>
2017-11-15 17:32:40 -0800

    Load all-cards from database on web server start.

--- src/clj/web/core.clj
+++ src/clj/web/core.clj
@@ -1,7 +1,10 @@
 (ns web.core
   (:require [web.api :refer [app]]
             [immutant.web]
+            [monger.collection :as mc]
+            [jinteki.cards :refer [all-cards]]
             [web.ws :as ws]
+            [web.db :refer [db]]
             [web.chat :as chat]
             [web.lobby :as lobby]
             [web.game :as game]))
@@ -18,7 +21,11 @@
   (immutant.util/set-log-level! :WARN)
   (let [port (or (first args) 4141)]
     (web.db/connect)
+    (let [cards (mc/find-maps db "cards" nil)]
+      (reset! all-cards (into {} (map (juxt :title identity) cards))))
+
     (web.utils/tick lobby/send-lobby 1000)
+
     (reset! server (immutant.web/run app {:port port}))
     (println "Jinteki server running on port" port))
   (ws/start-ws-router!))
