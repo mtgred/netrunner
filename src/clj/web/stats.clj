bd87018080ae3952b10848441ed3be9278f8f71a
danhut <danhut@cisco.com>
2017-11-20 21:58:17 +1100

    optimise number of mongo writes

--- src/clj/web/stats.clj
+++ src/clj/web/stats.clj
@@ -30,73 +30,83 @@
 
 (defn inc-deck-stats
   "Update deck stats for a given counter"
-  [deck-id key]
-  (mc/update db "decks" {:_id (object-id deck-id)} {"$inc" {key 1}})
+  [deck-id record]
+  (mc/update db "decks" {:_id (object-id deck-id)} {"$inc" record})
   (response 200 {:message "OK"}))
 
-(defn inc-game-stats
-  "Update user's game stats for a given counter"
-  [user-id key]
-  (mc/update db "users" {:_id (object-id user-id)} {"$inc" {key 1}})
-  (response 200 {:message "OK"}))
+(defn deck-record-end
+  [all-games gameid p]
+  (let [end-players (get-in @all-games [gameid :ending-players])
+        game-state (get-in @all-games [gameid :state])
+        enable-deckstats (get-in p [:user :options :deckstats])
+        deck-id (get-in p [:deck :_id])
+        winning-deck (:winning-deck-id @game-state)
+        losing-deck (:losing-deck-id @game-state)
+        record (merge (when (and enable-deckstats deck-id)
+                        {:stats.games-completed 1})
+                      (when (and enable-deckstats deck-id (= winning-deck deck-id))
+                        {:stats.wins 1})
+                      (when (and enable-deckstats deck-id (= losing-deck deck-id))
+                        {:stats.loses 1}))]
+    record))
 
 (defn update-deck-stats
   "Update stats for player decks on game ending"
   [all-games gameid]
   (let [start-players (get-in @all-games [gameid :original-players])
-        end-players (get-in @all-games [gameid :ending-players])
-        game-state (get-in @all-games [gameid :state])]
+        end-players (get-in @all-games [gameid :ending-players])]
 
     ;; Increment games commenced stats for each deck that started the game
     (doseq [p start-players]
       (let [enable-deckstats (get-in p [:user :options :deckstats])
             deck-id (get-in p [:deck :_id])]
         (when (and enable-deckstats deck-id)
-          (inc-deck-stats deck-id :stats.games-started))))
+          (inc-deck-stats deck-id '{:stats.games-started 1}))))
 
     ;; Increment games completed stats for each deck that ended the game
-    ;; TODO Merge these operations rather than multiple MONGO writes
     (doseq [p end-players]
-      (let [enable-deckstats (get-in p [:user :options :deckstats])
-            deck-id (get-in p [:deck :_id])
-            winning-deck (:winning-deck-id @game-state)
-            losing-deck (:losing-deck-id @game-state)]
-        (when (and enable-deckstats deck-id)
-          (inc-deck-stats deck-id :stats.games-completed)
-          (when (= winning-deck deck-id)
-            (inc-deck-stats deck-id :stats.wins))
-          (when (= losing-deck deck-id)
-            (inc-deck-stats deck-id :stats.loses)))))))
+      (inc-deck-stats (get-in p [:deck :_id]) (deck-record-end all-games gameid p)))))
+
+(defn inc-game-stats
+  "Update user's game stats for a given counter"
+  [user-id record]
+  (mc/update db "users" {:_id (object-id user-id)} {"$inc" record})
+  (response 200 {:message "OK"}))
+
+(defn game-record-start
+  [all-games gameid p]
+  (let [user-id (get-in p [:user :_id])
+        side-str (:side p)
+        record (merge {:stats.games-started 1}
+                      {(build-stats-kw "stats.games-started-" side-str) 1})]
+    record))
+
+(defn game-record-end
+  [all-games gameid p]
+  (let [game-state (get-in @all-games [gameid :state])
+        username (get-in p [:user :username])
+        enable-userstats (get-in p [:user :options :gamestats])
+        winning-user (:winning-user @game-state)
+        losing-user (:losing-user @game-state)
+        side-str (:side p)
+        record (merge {:stats.games-completed 1}
+                      {(build-stats-kw "stats.games-completed-" side-str) 1}
+                      (when (and (= username winning-user) enable-userstats)
+                                 {:stats.wins 1 (build-stats-kw "stats.wins-" side-str) 1})
+                      (when (and (= username losing-user) enable-userstats)
+                        {:stats.loses 1 (build-stats-kw "stats.loses-" side-str) 1}))]
+    record))
 
 (defn update-game-stats
   "Update game stats for users on game ending"
   [all-games gameid]
   (let [start-players (get-in @all-games [gameid :original-players])
-        end-players (get-in @all-games [gameid :ending-players])
-        game-state (get-in @all-games [gameid :state])]
+        end-players (get-in @all-games [gameid :ending-players])]
 
     ;; Increment games commenced stats for each player that started the game
-    ;; TODO Merge these operations rather than multiple MONGO writes
     (doseq [p start-players]
-      (let [user-id (get-in p [:user :_id])
-            side-str (:side p)]
-        (inc-game-stats user-id :stats.games-started)
-        (inc-game-stats user-id (build-stats-kw "stats.games-started-" side-str))))
+      (inc-game-stats (get-in p [:user :_id]) (game-record-start all-games gameid p)))
 
     ;; Increment games completed stats for each player that ended the game
     (doseq [p end-players]
-      (let [enable-userstats (get-in p [:user :options :gamestats])
-            user-id (get-in p [:user :_id])
-            username (get-in p [:user :username])
-            side-str (:side p)
-            winning-user (:winning-user @game-state)
-            losing-user (:losing-user @game-state)]
-        (inc-game-stats user-id :stats.games-completed)
-        (inc-game-stats user-id (build-stats-kw "stats.games-completed-" side-str))
-        (when enable-userstats)
-          (when (= username winning-user)
-            (inc-game-stats user-id :stats.wins)
-            (inc-game-stats user-id (build-stats-kw "stats.wins-" side-str)))
-          (when (= username losing-user)
-            (inc-game-stats user-id :stats.loses)
-            (inc-game-stats user-id (build-stats-kw "stats.loses-" side-str)))))))
+      (inc-game-stats (get-in p [:user :_id]) (game-record-end all-games gameid p)))))
