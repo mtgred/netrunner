b34c5ed34fc71635912d09b69176c5dcf7b77b13
danhut <danhut@cisco.com>
2017-12-05 18:20:35 +1100

    fix  #2958

--- src/clj/game/cards/resources.clj
+++ src/clj/game/cards/resources.clj
@@ -412,17 +412,20 @@
                  :effect (effect (trash card {:cause :ability-cost}) (damage-prevent :meat 3))}]}
 
    "Dadiana Chacon"
-   (let [ability {:once :per-turn
+   (let [trashme {:effect (effect (system-msg "trashes Dadiana Chacon and suffers 3 meat damage")
+                                  (damage eid :meat 3 {:unboostable true :card card})
+                                  (trash card {:cause :ability-cost}))}
+         ability {:once :per-turn
                   :msg "gain 1 [Credits]"
                   :req (req (< (get-in @state [:runner :credit]) 6))
                   :effect (req (gain state :runner :credit 1))}]
-     {:effect (req (add-watch state :dadiana
-                              (fn [k ref old new]
-                                (when (and (not (zero? (get-in old [:runner :credit])))
-                                           (zero? (get-in new [:runner :credit])))
-                                  (resolve-ability ref side {:effect (effect (system-msg "trashes Dadiana Chacon and suffers 3 meat damage")
-                                                                             (damage eid :meat 3 {:unboostable true :card card})
-                                                                             (trash card {:cause :ability-cost}))} card nil)))))
+     {:effect (req (if (= 0 (get-in @state [:runner :credit]))
+                     (resolve-ability state side trashme card nil)
+                     (add-watch state :dadiana
+                                (fn [k ref old new]
+                                  (when (and (not (zero? (get-in old [:runner :credit])))
+                                             (zero? (get-in new [:runner :credit])))
+                                    (resolve-ability ref side trashme card nil))))))
       :leave-play (req (remove-watch state :dadiana))
       :flags {:drip-economy true}
       :events {:runner-turn-begins ability}})
