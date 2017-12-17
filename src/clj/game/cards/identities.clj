8ce73255e66a1abcbefc453c568c3610976c9d2f
danhut <danhut@cisco.com>
2017-12-17 18:14:34 +1100

    fix #2963

--- src/clj/game/cards/identities.clj
+++ src/clj/game/cards/identities.clj
@@ -772,9 +772,10 @@
                                                 card nil))}}}
 
    "Skorpios Defense Systems: Persuasive Power"
-   {:implementation "Manually triggered, no restriction on which cards in Heap can be targeted"
+   {:implementation "Manually triggered, no restriction on which cards in Heap can be targeted.  Cannot use during a run"
     :abilities [{:label "Remove a card in the Heap that was just trashed from the game"
                  :delayed-completion true
+                 :req  (req (not (:run @state)))
                  :effect (effect (show-wait-prompt :runner "Corp to use Skorpios' ability")
                                  (continue-ability {:prompt "Choose a card in the Runner's Heap that was just trashed"
                                                     :once :per-turn
