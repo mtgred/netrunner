ea29933e6a85b49bb3a486f748f8cd9b45108629
Neal Terrell <nealterrell@users.noreply.github.com>
2017-12-20 09:07:01 -0800

    Indentation.

--- src/clj/game/cards/assets.clj
+++ src/clj/game/cards/assets.clj
@@ -1489,12 +1489,12 @@
     :events {:corp-turn-begins
              {:delayed-completion true
               :effect (req (add-counter state side card :power -1)
-                             (if (<= (get-in card [:counter :power]) 1)
-                               (when-completed
-                                 (trash state side card {:cause :ability-cost})
-                                 (do (system-msg state :corp "uses Urban Renewal to do 4 meat damage")
-                                     (damage state side eid :meat 4 {:card card})))
-                               (effect-completed state side eid)))}}}
+                           (if (<= (get-in card [:counter :power]) 1)
+                             (when-completed
+                               (trash state side card {:cause :ability-cost})
+                               (do (system-msg state :corp "uses Urban Renewal to do 4 meat damage")
+                                   (damage state side eid :meat 4 {:card card})))
+                             (effect-completed state side eid)))}}}
 
    "Victoria Jenkins"
    {:effect (req (lose state :runner :click-per-turn 1)
