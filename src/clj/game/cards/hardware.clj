fbcdf2acaa616aac765f35a02ebc8ae52baa459f
Neal Terrell <neal.terrell@csulb.edu>
2017-12-31 10:35:45 -0800

    Cyberdelia.

--- src/clj/game/cards/hardware.clj
+++ src/clj/game/cards/hardware.clj
@@ -217,6 +217,13 @@
                                                     (get-card state card)))}
     :events {:pre-rez nil :runner-turn-ends nil :corp-turn-ends nil}}
 
+   "Cyberdelia"
+   {:implementation "Credit gain is manually triggered."
+    :in-play [:memory 1]
+    :abilities [{:msg "gain 1 [Credits] for breaking all subroutines on a piece of ice"
+                 :once :per-turn
+                 :effect (effect (gain :credit 1))}]}
+
    "Cyberfeeder"
    {:recurring 1}
 
