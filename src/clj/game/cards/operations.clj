7c8265bc64a34b04d828b95dc51137f6715d2c84
Neal Terrell <neal.terrell@csulb.edu>
2018-01-03 09:41:07 -0800

    Wake Up Call.

--- src/clj/game/cards/operations.clj
+++ src/clj/game/cards/operations.clj
@@ -1458,6 +1458,30 @@
                       :msg (msg "trash " (:title target))
                       :effect (effect (trash target))}}}
 
+   "Wake Up Call"
+   {:req (req (:trashed-card runner-reg-last))
+    :prompt "Select a piece of hardware or non-virtual resource"
+    :choices {:req #(or (hardware? %)
+                        (and (resource? %) (not (has-subtype? % "Virtual"))))}
+    :delayed-completion true
+    :effect (req (let [chosen target
+                       wake card]
+                   (show-wait-prompt state side "Runner to resolve Wake Up Call")
+                   (continue-ability state :runner
+                                     {:prompt (str "Trash " (:title chosen) " or suffer 4 meat damage?")
+                                      :choices [(str "Trash " (:title chosen))
+                                                "4 meat damage"]
+                                      :delayed-completion true
+                                      :effect (req (clear-wait-prompt state :corp)
+                                                   (move state side (last (:discard corp)) :rfg)
+                                                   (if (.startsWith target "Trash")
+                                                     (do (system-msg state side (str "chooses to trash " (:title chosen)))
+                                                         (trash state side eid chosen nil))
+                                                     (do (system-msg state side "chooses to suffer meat damage")
+                                                         (damage state side eid :meat 4 {:card wake
+                                                                                         :unboostable true}))))}
+                                     card nil)))}
+
    "Wetwork Refit"
    {:choices {:req #(and (ice? %)
                          (has-subtype? % "Bioroid")
