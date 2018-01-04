64db69b675688e9c71802d8ee55b6ccb32ab5cde
Neal Terrell <neal.terrell@csulb.edu>
2018-01-03 16:25:28 -0800

    Fix HB: Stronger Together and Employee Strike interaction in #2469.

--- src/clj/game/cards/identities.clj
+++ src/clj/game/cards/identities.clj
@@ -339,7 +339,9 @@
 
    "Haas-Bioroid: Stronger Together"
    {:events {:pre-ice-strength {:req (req (and (ice? target) (has-subtype? target "Bioroid")))
-                                :effect (effect (ice-strength-bonus 1 target))}}}
+                                :effect (effect (ice-strength-bonus 1 target))}}
+    :leave-play (effect (update-all-ice))
+    :effect (effect (update-all-ice))}
 
    "Harishchandra Ent.: Where Youre the Star"
    {:effect (req (when tagged
