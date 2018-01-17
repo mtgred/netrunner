f13f4b63ff4af29cf6f6f150111c0bf8decc0bf6
Chris Greene <archgoon@gmail.com>
2018-01-17 00:00:24 -0800

    Add message to Puffer when adding power counter.
    Fixes #2997

--- src/clj/game/cards/icebreakers.clj
+++ src/clj/game/cards/icebreakers.clj
@@ -782,11 +782,11 @@
                     {:implementation "Memory use must be manually tracked by the Runner"
                      :abilities [(break-sub 1 1 "Sentry")
                                  (strength-pump 2 1)
-                                 {:cost [:click 1]
+                                 {:cost [:click 1] :msg "place one power counter"
                                   :label "Place 1 power counter"
                                   :effect (effect (add-counter card :power 1)
                                                   (update-breaker-strength card))}
-                                 {:cost [:click 1]
+                                 {:cost [:click 1] :msg "remove one power counter"
                                   :label "Remove 1 power counter"
                                   :effect (effect (add-counter card :power -1)
                                                   (update-breaker-strength card))}]
