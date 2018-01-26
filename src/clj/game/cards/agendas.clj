8b22d0f385d1a05668bc3d626063418d0ef4b66c
John Warwick <jwarwick@gmail.com>
2018-01-26 17:20:29 -0500

    Implemented SSL Endorsement

--- src/clj/game/cards/agendas.clj
+++ src/clj/game/cards/agendas.clj
@@ -925,6 +925,21 @@
     :msg "do 2 meat damage"
     :effect (effect (damage eid :meat 2 {:card card}))}
 
+   "SSL Endorsement"
+   (let [add-credits (fn [] (effect (add-counter card :credit 9)))
+         remove-credits {:optional {:req (req (pos? (get-in card [:counter :credit] -1)))
+                                    :prompt "Gain 3 [Credits] from SSL Endorsement?"
+                                    :yes-ability
+                                    {:effect (req (when (pos? (get-in card [:counter :credit] -1))
+                                                    (gain state :corp :credit 3)
+                                                    (system-msg state :corp (str "uses SSL Endorsement to gain 3 [Credits]"))
+                                                    (add-counter state side card :credit -3)))}}}]
+     {:effect (add-credits)
+      :stolen {:effect (add-credits)}
+      :interactive (req true)
+      :events {:corp-turn-begins remove-credits}
+      :flags {:has-events-when-stolen true}})
+
    "Standoff"
    (letfn [(stand [side]
              {:delayed-completion true
