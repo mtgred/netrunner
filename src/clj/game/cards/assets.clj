7b9f179e2c1f01c2d58d23c608edf33f494c2881
Brian Butz <butzopower@gmail.com>
2018-01-28 20:26:01 +0000

    Implement NGO Front

--- src/clj/game/cards/assets.clj
+++ src/clj/game/cards/assets.clj
@@ -979,6 +979,19 @@
                                                    (trigger-event state side :no-trash card))))}
                                card targets))}}
 
+   "NGO Front"
+   (letfn [(builder [cost cred]
+             {
+              :advance-counter-cost cost
+              :effect (effect (trash card {:cause :ability-cost}) (gain :credit cred))
+              :label (str "[Trash]: Gain " cred " [Credits]")
+              :msg (str "gain " cred " [Credits]")
+              })]
+     {
+      :advanceable :always
+      :abilities [(builder 1 5) (builder 2 8)]
+      }
+     )
    "Open Forum"
    {:events {:corp-mandatory-draw {:msg (msg (let [deck (:deck corp)]
                                                (if (pos? (count deck))
