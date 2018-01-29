3111c24add81041ac91bfc544f1c75064a54b463
John Warwick <jwarwick@gmail.com>
2018-01-29 09:03:41 -0500

    Implemented Aimor

--- src/clj/game/cards/ice.clj
+++ src/clj/game/cards/ice.clj
@@ -207,6 +207,20 @@
                            :effect (effect (draw :runner 2))})
                   (do-net-damage 1)]}
 
+   "Aimor"
+   {:subroutines [{:label "Trash the top 3 cards of the Stack. Trash Aimor."
+                   :effect (req (when (not-empty (:deck runner))
+                                  (system-msg state :corp
+                                              (str "uses Aimor to trash "
+                                                   (join ", " (map :title (take 3 (:deck runner))))
+                                                   " from the Runner's Stack"))
+                                  (mill state :runner 3))
+                                (when current-ice
+                                  (no-action state :corp nil)
+                                  (continue state :runner nil))
+                                (trash state side card)
+                                (system-msg state side (str "trashes Aimor")))}]}
+
    "Archangel"
    {:access
     {:delayed-completion true
