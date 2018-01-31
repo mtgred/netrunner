dcc8d1b297f54a6106ed17c29642ef350510ba52
John Warwick <jwarwick@gmail.com>
2018-01-30 21:20:56 -0500

    Added `effect_completed` after move action

--- src/clj/game/cards/operations.clj
+++ src/clj/game/cards/operations.clj
@@ -1399,8 +1399,7 @@
    "Threat Assessment"
    {:req (req (:trashed-card runner-reg-last))
     :prompt "Select an installed Runner card"
-    :choices {:req #(and (= (:side %) "Runner")
-                         (installed? %))}
+    :choices {:req #(and (= (:side %) "Runner") (installed? %))}
     :delayed-completion true
     :effect (req (let [chosen target]
                    (show-wait-prompt state side "Runner to resolve Threat Assessment")
@@ -1413,9 +1412,10 @@
                                                    (move state :corp (last (:discard corp)) :rfg)
                                                    (if (.startsWith target "Move")
                                                      (do (system-msg state side (str "chooses to move " (:title chosen) " to the Stack"))
-                                                       (move state :runner chosen :deck {:front true}))
+                                                       (move state :runner chosen :deck {:front true})
+                                                       (effect-completed state side eid))
                                                      (do (system-msg state side "chooses to take 2 tags")
-                                                         (tag-runner state :runner eid 2))))}
+                                                       (tag-runner state :runner eid 2))))}
                                      card nil)))}
 
    "Threat Level Alpha"
