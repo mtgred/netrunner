aea3fdfc9bf80ffe6009f1a3051eead6901d3567
Neal Terrell <nealterrell@users.noreply.github.com>
2018-02-05 08:27:44 -0800

    Minor formatting.

--- src/clj/game/cards/programs.clj
+++ src/clj/game/cards/programs.clj
@@ -713,18 +713,18 @@
                                :msg (msg "to reveal " (:title target))
                                :effect (req (if-let [guess (get-in card [:special :rng-guess])]
                                               (if (or (= guess (:cost target))
-                                                        (= guess (:advancementcost target)))
+                                                      (= guess (:advancementcost target)))
                                                 (continue-ability state side
                                                                   {:prompt "Choose RNG Key award"
                                                                    :choices ["Gain 3 [Credits]" "Draw 2 cards"]
                                                                    :effect (req (if (= target "Draw 2 cards")
                                                                                   (do (draw state :runner 2)
-                                                                                    (system-msg state :runner "uses RNG Key to draw 2 cards"))
+                                                                                      (system-msg state :runner "uses RNG Key to draw 2 cards"))
                                                                                   (do (gain state :runner :credit 3)
-                                                                                    (system-msg state :runner "uses RNG Key to gain 3 [Credits]"))))}
+                                                                                      (system-msg state :runner "uses RNG Key to gain 3 [Credits]"))))}
                                                                   card nil)
-                                                (effect-completed state side eid card))
-                                              (effect-completed state side eid card)))}
+                                                (effect-completed state side eid))
+                                              (effect-completed state side eid)))}
              :post-access-card {:effect (effect (update! (assoc-in card [:special :rng-guess] nil)))}
              :successful-run {:req (req (let [first-hq (first-successful-run-on-server? state :hq)
                                               first-rd (first-successful-run-on-server? state :rd)]
