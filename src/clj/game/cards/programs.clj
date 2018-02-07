d2590fb5c0354c41678f508749ebe8dfbf78747c
Karl Erik Holter <karl0erik@gmail.com>
2018-02-07 18:33:49 +0100

    Made Wari less of a nested mess

--- src/clj/game/cards/programs.clj
+++ src/clj/game/cards/programs.clj
@@ -880,45 +880,48 @@
                    :effect (effect (gain :click 2))}]}
 
    "Wari"
-   {:events {:successful-run
-             {:interactive (req )
+   (letfn [(prompt-for-subtype []
+             {:prompt "Choose a subtype"
+              :choices ["Barrier" "Code Gate" "Sentry"]
               :delayed-completion true
-              :req (req (and (= target :hq)
-                             (first-successful-run-on-server? state :hq)
-                             (some #(and (ice? %)
-                                         (not (rezzed? %)))
-                                   (all-installed state :corp))))
-              :effect (effect
-                       (continue-ability
-                        {:prompt "Use Wari?"
-                         :choices ["Yes" "No"]
-                         :delayed-completion true
-                         :effect (effect 
-                                  (continue-ability
+              :effect (req
+                       (when-completed (trash state side card {:unpreventable true})
+                         (continue-ability
+                          state side
+                          (expose-and-maybe-bounce target)
+                          card nil)))})
+           
+           (expose-and-maybe-bounce [chosen-subtype]
+             {:choices {:req #(and (ice? %)
+                                   (not (rezzed? %)))}
+              :delayed-completion true
+              :msg (str "name " chosen-subtype)
+              :effect (req
+                       (when-completed (expose state side target)
+                         (do (if (and async-result
+                                      (has-subtype? target chosen-subtype))
+                               (do (move state :corp target :hand)
+                                   (system-msg state :runner
+                                               (str "add " (:title target) " to HQ"))))
+                             (effect-completed state side eid))))})]
+     {:events {:successful-run
+              {:interactive (req true)
+               :delayed-completion true
+               :req (req (and (= target :hq)
+                              (first-successful-run-on-server? state :hq)
+                              (some #(and (ice? %)
+                                          (not (rezzed? %)))
+                                    (all-installed state :corp))))
+               :effect (effect
+                        (continue-ability
+                         {:prompt "Use Wari?"
+                          :choices ["Yes" "No"]
+                          :delayed-completion true
+                          :effect (req
                                    (if (= target "Yes")
-                                     {:prompt "Choose a subtype"
-                                      :choices ["Barrier" "Code Gate" "Sentry"]
-                                      :delayed-completion true
-                                      :effect (effect
-                                               (trash card)
-                                               (continue-ability
-                                                (let [chosen-subtype target]
-                                                  {:choices {:req #(and (ice? %)
-                                                                        (not (rezzed? %)))}
-                                                   :delayed-completion true
-                                                   :msg (str "name " chosen-subtype)
-                                                   :effect (req
-                                                            (when-completed (expose state side target)
-                                                              (if (and async-result
-                                                                       (has-subtype? target chosen-subtype))
-                                                                (continue-ability
-                                                                   state :corp
-                                                                   (let [ice target]
-                                                                     {:effect (effect (move ice :hand))
-                                                                      :msg (str "add " (:title target) " to HQ")})
-                                                                   card nil)
-                                                                (effect-completed state side eid))))})
-                                                card nil))}
-                                     (effect-completed state side eid))
-                                   card nil))}
-                        card nil))}}}})
+                                     (continue-ability
+                                      state side
+                                      (prompt-for-subtype)
+                                      card nil)
+                                     (effect-completed state side eid)))}
+                         card nil))}}})})
