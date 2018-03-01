1de9071c8fe5657dc85a837df0c1ca0f15e8f31f
Filip Gokstorp <filip@gokstorp.se>
2018-03-01 15:25:56 +0000

    Makes AI unboostable.

--- src/clj/game/cards/agendas.clj
+++ src/clj/game/cards/agendas.clj
@@ -114,7 +114,7 @@
                        :prompt "Choose Armed Intimidation score effect"
                        :effect (req (case target
                                       "Suffer 5 meat damage"
-                                      (do (damage state :runner eid :meat 5 {:card card})
+                                      (do (damage state :runner eid :meat 5 {:card card :unboostable true})
                                           (system-msg state :runner "chooses to suffer 5 meat damage from Armed Intimidation"))
                                       "Take 2 tags"
                                       (do (tag-runner state :runner eid 2 {:card card})
