74eb3951c1b9c86f1bfcbd9c8f2244b988471dac
nealterrell <neal.terrell@csulb.edu>
2018-03-01 14:55:20 -0800

    Kuwinda K4H1U3

--- src/clj/game/cards/assets.clj
+++ src/clj/game/cards/assets.clj
@@ -722,6 +722,21 @@
                  :effect (effect (mill :runner)
                                  (trash card {:cause :ability-cost}))}]}
 
+   "Kuwinda K4H1U3"
+   (let [ability {:trace {:base (req (get-in card [:counter :power] 0))
+                          :delayed-completion true
+                          :effect (effect (damage :runner eid :brain 1)
+                                          (trash card))
+                          :msg "do 1 brain damage"
+                          :unsuccessful {:effect (effect (add-counter card :power 1)
+                                                         (system-msg "adds 1 power counter to Kuwinda K4H1U3"))}}}]
+     {:derezzed-events {:runner-turn-ends corp-rez-toast}
+      :events {:corp-turn-begins
+               {:optional {:prompt "Initiate trace with Kuwinda K4H1U3?"
+                           :delayed-completion true
+                           :yes-ability ability}}}
+      :abilities [(assoc ability :label "Trace X - do 1 brain damage (start of turn)")]})
+
    "Lakshmi Smartfabrics"
    {:events {:rez {:effect (effect (add-counter card :power 1))}}
     :abilities [{:req (req (seq (filter #(and (is-type? % "Agenda")
@@ -998,9 +1013,9 @@
               :label (str "[Trash]: Gain " cred " [Credits]")
               :msg (str "gain " cred " [Credits]")})]
      {:advanceable :always
-      :abilities [(builder 1 5) 
+      :abilities [(builder 1 5)
                   (builder 2 8)]})
-   
+
    "Open Forum"
    {:events {:corp-mandatory-draw {:msg (msg (let [deck (:deck corp)]
                                                (if (pos? (count deck))
