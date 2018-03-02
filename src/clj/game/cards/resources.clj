c55d87f43dece818c73ef1e141bf3726b29740b3
Neal Terrell <neal.terrell@csulb.edu>
2018-03-01 20:03:48 -0800

    Crypt

--- src/clj/game/cards/resources.clj
+++ src/clj/game/cards/resources.clj
@@ -431,6 +431,26 @@
                  :msg "prevent up to 3 meat damage"
                  :effect (effect (trash card {:cause :ability-cost}) (damage-prevent :meat 3))}]}
 
+   "Crypt"
+   {:events {:successful-run
+             {:silent (req true)
+              :req (req (= :archives target))
+              :optional {:prompt "Place a virus counter on Crypt?"
+                         :yes-ability {:effect (effect (add-counter card :virus 1)
+                                                       (system-msg "places a virus counter on Crypt"))}}}}
+    :abilities [{:label "[Click][Trash]: install a virus program from the stack"
+                 :prompt "Choose a virus"
+                 :msg (msg "install " (:title target) " from the stack")
+                 :choices (req (cancellable (filter #(and (is-type? % "Program")
+                                                          (has-subtype? % "Virus"))
+                                                    (:deck runner)) :sorted))
+                 :cost [:click 1]
+                 :counter-cost [:virus 3]
+                 :effect (effect (trigger-event :searched-stack nil)
+                                 (shuffle! :deck)
+                                 (runner-install target)
+                                 (trash card {:cause :ability-cost}))}]}
+
    "Dadiana Chacon"
    (let [trashme {:effect (effect (system-msg "trashes Dadiana Chacon and suffers 3 meat damage")
                                   (damage eid :meat 3 {:unboostable true :card card})
