a3e131beb6dba0b9edf3a3cce895b614be45bbc0
Neal Terrell <neal.terrell@csulb.edu>
2018-03-01 20:33:55 -0800

    White Hat

--- src/clj/game/cards/events.clj
+++ src/clj/game/cards/events.clj
@@ -1771,6 +1771,28 @@
                                               (when (pay state :runner card :click n)
                                                 (trash-cards state :corp (take n (shuffle (:hand corp)))))))}} card))}
 
+   "White Hat"
+   (letfn [(finish-choice [choices]
+             (let [choices (filter #(not= "None" %) choices)]
+               (when (not-empty choices)
+                {:effect (req (doseq [c choices] (move state :corp c :deck))
+                              (shuffle! state :corp :deck))
+                 :msg (str "shuffle " (join ", " (map :title choices)) " into R&D")})))
+           (choose-cards [hand chosen]
+             {:prompt "Choose a card in HQ to shuffle into R&D"
+              :player :runner
+              :choices (conj (vec (clojure.set/difference hand chosen))
+                             "None")
+              :delayed-completion true
+              :effect (req (if (and (empty? chosen) (not= "None" target))
+                             (continue-ability state side (choose-cards hand (conj chosen target)) card nil)
+                             (continue-ability state side (finish-choice (conj chosen target)) card nil)))})]
+   {:req (req (some #{:hq :rd :archives} (:successful-run runner-reg)))
+    :trace {:base 3
+            :msg "reveal all cards in HQ"
+            :unsuccessful {:delayed-completion true
+                           :effect (effect (continue-ability :runner (choose-cards (set (:hand corp)) #{}) card nil))}}})
+
    "Windfall"
    {:effect (effect (shuffle! :deck)
                     (resolve-ability
