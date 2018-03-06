7ad250ac60c6928c7bf61c26239749556d26db91
Neal Terrell <neal.terrell@csulb.edu>
2018-03-05 16:56:51 -0800

    Implement No One Home. Improve Snare.

--- src/clj/game/cards/resources.clj
+++ src/clj/game/cards/resources.clj
@@ -1099,6 +1099,30 @@
     :events {:agenda-stolen {:msg "trash itself" :effect (effect (trash card))}}
     :abilities [{:cost [:credit 2] :msg "avoid 1 tag" :effect (effect (tag-prevent 1))}]}
 
+   "No One Home"
+   (letfn [(start-trace [type]
+             (let [message (str "avoid any " (if (= type :net) "amount of net damage" "number of tags"))]
+             {:player :corp
+              :label (str "Trace 0 - if unsuccessful, " message)
+              :trace {:base 0
+                      :priority 11
+                      :unsuccessful {:msg message
+                                     :effect (req (if (= type :net)
+                                                    (damage-prevent state side :net Integer/MAX_VALUE)
+                                                    (tag-prevent state side Integer/MAX_VALUE)))}}}))]
+   {:prevent {:tag [:all]
+              :damage [:net]}
+    :abilities [{:msg "force the Corp to trace"
+                 :delayed-completion true
+                 :once :per-turn
+                 :effect (req (let [type (get-in @state [:prevent :current])]
+                                (when-completed (trash state side card {:unpreventable true})
+                                                (continue-ability state side (start-trace type)
+                                                                  card nil))))}]
+    :events {:pre-resolve-damage {:silent (req true)
+                                  :effect (req (swap! state assoc-in [:per-turn (:cid card)] true))}
+             :pre-resolve-tag {:silent (req true)
+                               :effect (req (swap! state assoc-in [:per-turn (:cid card)] true))}}})
    "Off-Campus Apartment"
    {:flags {:runner-install-draw true}
     :abilities [{:label "Install and host a connection on Off-Campus Apartment"
