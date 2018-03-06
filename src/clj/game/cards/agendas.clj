e0b917f57229c8c00256ed1bb41cb8bf42056e2d
Filip Gokstorp <filip@gokstorp.se>
2018-03-06 19:39:38 +0000

    Fixes AI not clearing Corp prompt.

--- src/clj/game/cards/agendas.clj
+++ src/clj/game/cards/agendas.clj
@@ -112,7 +112,8 @@
                       {:delayed-completion true
                        :choices ["Suffer 5 meat damage" "Take 2 tags"]
                        :prompt "Choose Armed Intimidation score effect"
-                       :effect (req (case target
+                       :effect (req (clear-wait-prompt state :corp)
+                                    (case target
                                       "Suffer 5 meat damage"
                                       (do (damage state :runner eid :meat 5 {:card card :unboostable true})
                                           (system-msg state :runner "chooses to suffer 5 meat damage from Armed Intimidation"))
