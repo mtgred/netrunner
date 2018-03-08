994fb6062e23752518f9dc8de774788accff256e
Filip Gokstorp <filip@gokstorp.se>
2018-03-08 22:02:19 +0000

    Dries up Bazaar and Replicator a bit.

--- src/clj/game/cards/resources.clj
+++ src/clj/game/cards/resources.clj
@@ -197,23 +197,24 @@
                                                        bj nil)))}))))}}}
 
    "Bazaar"
-   {:events
-    {:runner-install
-     {:interactive (req (and (is-type? target "Hardware")
-                             (some #(= (:title %) (:title target)) (:hand runner))))
-      :silent (req (not (and (is-type? target "Hardware")
-                             (some #(= (:title %) (:title target)) (:hand runner)))))
-      :delayed-completion true
-      :req (req (and (is-type? target "Hardware") (= [:hand] (:previous-zone target))))
-      :effect (req (let [hw (:title target)]
-                     (continue-ability state side
-                       {:optional {:req (req (some #(when (= (:title %) hw) %) (:hand runner)))
-                                   :prompt (msg "Install another copy of " hw "?")
-                                   :msg (msg "install another copy of " hw)
-                                   :yes-ability {:delayed-completion true
-                                                 :effect (req (if-let [c (some #(when (= (:title %) hw) %)
-                                                                               (:hand runner))]
-                                                                (runner-install state side eid c nil)))}}} card nil)))}}}
+   (letfn [(hardware-and-in-hand? [target runner]
+             (and (is-type? target "Hardware")
+                  (some #(= (:title %) (:title target)) (:hand runner))))]
+     {:events
+      {:runner-install
+       {:interactive (req (hardware-and-in-hand? target runner))
+        :silent (req (not (hardware-and-in-hand? target runner)))
+        :delayed-completion true
+        :req (req (and (is-type? target "Hardware") (= [:hand] (:previous-zone target))))
+        :effect (req (let [hw (:title target)]
+                       (continue-ability state side
+                                         {:optional {:req (req (some #(when (= (:title %) hw) %) (:hand runner)))
+                                                     :prompt (msg "Install another copy of " hw "?")
+                                                     :msg (msg "install another copy of " hw)
+                                                     :yes-ability {:delayed-completion true
+                                                                   :effect (req (if-let [c (some #(when (= (:title %) hw) %)
+                                                                                                 (:hand runner))]
+                                                                                  (runner-install state side eid c nil)))}}} card nil)))}}})
 
    "Beach Party"
    {:in-play [:hand-size-modification 5]
