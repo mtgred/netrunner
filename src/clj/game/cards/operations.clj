a39768048f6426deaa3fa7867805b4b6c6b7042a
Neal Terrell <neal.terrell@csulb.edu>
2018-03-07 18:59:31 -0800

    Implement Trojan Horse.

--- src/clj/game/cards/operations.clj
+++ src/clj/game/cards/operations.clj
@@ -1523,6 +1523,23 @@
                                        tol nil)))}
                      card nil)))}
 
+   "Trojan Horse"
+   {:req (req (:accessed-cards runner-reg))
+    :trace {:base 4
+            :label "Trace 4 - Trash a program"
+            :delayed-completion true
+            :effect (req (let [exceed (- target (second targets))]
+                           (continue-ability
+                             state side
+                             {:delayed-completion true
+                              :prompt (str "Select a program with an install cost of no more than " exceed "[Credits]")
+                              :choices {:req #(and (is-type? % "Program")
+                                                   (installed? %)
+                                                   (>= exceed (:cost %)))}
+                              :msg (msg "trash " (card-str state target))
+                              :effect (effect (trash eid target nil))}
+                             card nil)))}}
+
    "Ultraviolet Clearance"
    {:delayed-completion true
     :effect (req (gain state side :credit 10)
