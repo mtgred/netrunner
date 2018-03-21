20ab6175d3eb2bbb55ecc2aa4a18e9606e37ef6f
Noah <nbtheduke@gmail.com>
2018-03-20 21:05:44 -0400

    Removed run req, self-targeting

--- src/clj/game/cards/ice.clj
+++ src/clj/game/cards/ice.clj
@@ -1505,14 +1505,13 @@
 
    "Oduduwa"
    {:implementation "Encounter effect is manual"
-    :abilities [{:req (req (and this-server (= (dec (:position run)) (ice-index state card))))
-                 :label "Place 1 advancement counter on Oduduwa"
+    :abilities [{:label "Place 1 advancement counter on Oduduwa"
                  :msg (msg "place 1 advancement counter on Oduduwa")
                  :effect (req (add-prop state side card :advance-counter 1 {:placed true}))}
-                {:req (req (and this-server (= (dec (:position run)) (ice-index state card))))
-                 :label "Place X advancement token on another ICE"
+                {:label "Place X advancement token on another piece of ice"
                  :msg (msg "place " (:advance-counter card 0) " advancement token on " (card-str state target))
-                 :choices {:req ice?}
+                 :choices {:req ice?
+                           :not-self (req (:cid card))}
                  :effect (req (add-prop state side target :advance-counter (:advance-counter card 0) {:placed true}))}]
     :subroutines [end-the-run]}
 
