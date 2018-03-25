b2937de69e67d1d9537894d6dcfa3f5476f3bc54
Neal Terrell <nealterrell@users.noreply.github.com>
2018-03-25 11:16:59 -0700

    Alignment.

--- src/clj/game/cards/events.clj
+++ src/clj/game/cards/events.clj
@@ -414,16 +414,16 @@
                :effect (effect (gain :runner :credit 5))}})
 
    "Diversion of Funds"
-   {:req    (req hq-runnable)
+   {:req (req hq-runnable)
     :effect (effect (run :hq
                          {:req (req (= target :hq))
                           :replace-access
-                            (let [five-or-all (fn [corp] (min 5 (:credit corp)))]
-                              {:delayed-completion true
-                                :msg                (msg "force the Corp to lose " (five-or-all corp)
-                                                        " [Credits], and gain " (five-or-all corp))
-                                :effect             (effect (lose :corp :credit (five-or-all corp))
-                                                            (gain :runner :credit (five-or-all corp)))})}
+                          (let [five-or-all (fn [corp] (min 5 (:credit corp)))]
+                            {:delayed-completion true
+                             :msg (msg "force the Corp to lose " (five-or-all corp)
+                                       " [Credits], and gain " (five-or-all corp))
+                             :effect (effect (lose :corp :credit (five-or-all corp))
+                                             (gain :runner :credit (five-or-all corp)))})}
                       card))}
 
    "Drive By"
