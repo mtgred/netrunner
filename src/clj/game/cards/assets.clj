918ea11393c96c822cfd4b498c74820dd2a3f176
Dan Knutson <dan.knutson@gmail.com>
2018-03-24 19:48:28 -0500

    add False Flag

--- src/clj/game/cards/assets.clj
+++ src/clj/game/cards/assets.clj
@@ -528,6 +528,21 @@
                  :msg (msg "remove " (:advance-counter card) " bad publicity")
                  :effect (effect (trash card) (lose :bad-publicity (:advance-counter card)))}]}
 
+   "False Flag"
+   (letfn [(tag-count [false-flag]
+             (int (Math/floor (/ (:advance-counter false-flag 0)
+                                 2))))]
+     {:advanceable :always
+      :access {:req (req (pos? (:advance-counter (get-card state card) 0)))
+               :msg (msg "give the runner " (quantify (tag-count (get-card state card)) "tag"))
+               :delayed-completion true
+               :effect (effect (tag-runner :runner eid (tag-count (get-card state card))))}
+      :abilities [{:cost [:click 1]
+                   :advance-counter-cost 7
+                   :label "Add False Flag to your score area as an agenda worth 3 agenda points"
+                   :msg "add it to their score area as an agenda worth 3 agenda points"
+                   :effect (effect (as-agenda :corp card 3))}]})
+
    "Franchise City"
    {:events {:access {:req (req (is-type? target "Agenda"))
                       :msg "add it to their score area as an agenda worth 1 agenda point"
