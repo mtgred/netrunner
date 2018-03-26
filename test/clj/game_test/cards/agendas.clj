473241f9e0aca7027a5487456e3f2ffd444d86a7
Noah <nbtheduke@gmail.com>
2018-03-26 15:29:00 -0400

    AR-Enhanced Security

--- test/clj/game_test/cards/agendas.clj
+++ test/clj/game_test/cards/agendas.clj
@@ -105,6 +105,39 @@
       (run-jack-out state)
       (is (= (dec grip) (count (:hand (get-runner)))) "Runner took 1 net damage"))))
 
+(deftest ar-enhanced-security
+  ;; AR-Enhanced Security
+  (do-game
+    (new-game (default-corp [(qty "AR-Enhanced Security" 1) (qty "NGO Front" 3)])
+              (default-runner))
+    (testing "set up"
+      (core/gain state :corp :click 10 :credit 10)
+      (core/gain state :runner :credit 10)
+      (dotimes [_ 3]
+        (play-from-hand state :corp "NGO Front" "New remote"))
+      (take-credits state :corp))
+    (testing "don't take a tag from trashing normally"
+      (run-on state :remote1)
+      (run-successful state)
+      (prompt-choice :runner "Yes")
+      (is (= 1 (count (:discard (get-corp)))) "trashed")
+      (is (= 0 (:tag (get-runner))) "Runner took 0 tags")
+      (take-credits state :runner)
+      (play-and-score state "AR-Enhanced Security")
+      (take-credits state :corp))
+    (testing "gain a tag from first trash"
+      (run-on state :remote2)
+      (run-successful state)
+      (prompt-choice :runner "Yes")
+      (is (= 2 (count (:discard (get-corp)))) "trashed")
+      (is (= 1 (:tag (get-runner))) "Runner took 1 tag"))
+    (testing "don't gain a tag from second trash"
+      (run-on state :remote3)
+      (run-successful state)
+      (prompt-choice :runner "Yes")
+      (is (= 3 (count (:discard (get-corp)))) "trashed")
+      (is (= 1 (:tag (get-runner))) "Runner took 0 tags"))))
+
 (deftest armed-intimidation
   ;; Armed intimidation choices
   (do-game
