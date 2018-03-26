504318ae85770634164b7016e3083ec1e153e8d1
Dan Knutson <dan.knutson@gmail.com>
2018-03-25 21:10:02 -0500

    use req instead of effect

--- src/clj/game/cards/operations.clj
+++ src/clj/game/cards/operations.clj
@@ -742,8 +742,9 @@
      {:req (req tagged)
       :msg (msg (let [c (credit-diff runner)]
                   (str "make the runner lose " c " [Credits], and gain " c " [Credits]")))
-      :effect (effect (gain :corp :credit (credit-diff runner))
-                      (lose :runner :credit (credit-diff runner)))})
+      :effect (req (let [c (credit-diff runner)]
+                     (lose state :runner :credit c)
+                     (gain state :corp :credit c)))})
 
    "Mass Commercialization"
    {:msg (msg "gain " (* 2 (count (filter #(pos? (+ (:advance-counter % 0) (:extra-advance-counter % 0)))
