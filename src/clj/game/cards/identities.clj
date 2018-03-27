2f0a9c475bdc71880563c7658a9dd5ad0ace6cb4
John Warwick <jwarwick@gmail.com>
2018-03-27 17:40:51 -0400

    Cleaning up delayed-completion confusion

--- src/clj/game/cards/identities.clj
+++ src/clj/game/cards/identities.clj
@@ -42,11 +42,10 @@
                          {:optional
                           {:prompt "Expose installed card unless Corp pays 1 [Credits]?"
                            :player :runner
-                           :delayed-completion true
-                           :no-ability {:effect (req (clear-wait-prompt state :corp)
-                                                     (effect-completed state side eid))}
+                           :no-ability {:effect (req (clear-wait-prompt state :corp))}
                            :yes-ability
-                           {:effect (req (clear-wait-prompt state :corp)
+                           {:delayed-completion true
+                            :effect (req (clear-wait-prompt state :corp)
                                          (if (not (can-pay? state :corp nil :credit 1))
                                            (do
                                              (toast state :corp "Cannot afford to pay 1 [Credits] to block card exposure" "info")
@@ -58,16 +57,15 @@
                                                {:optional
                                                 {:prompt "Pay 1 [Credits] to prevent exposure of installed card?"
                                                  :player :corp
-                                                 :delayed-completion true
                                                  :no-ability
-                                                 {:effect (req (expose state side eid itarget)
+                                                 {:delayed-completion true
+                                                  :effect (req (expose state side eid itarget)
                                                                (clear-wait-prompt state :runner))}
                                                  :yes-ability
                                                  {:effect (req (lose state :corp :credit 1)
                                                                (system-msg state :corp (str "spends 1 [Credits] to prevent "
                                                                                             " card from being exposed"))
-                                                               (clear-wait-prompt state :runner)
-                                                               (effect-completed state side eid))}}}
+                                                               (clear-wait-prompt state :runner))}}}
                                                card nil))))}}}
                          card nil)))}}}
 
