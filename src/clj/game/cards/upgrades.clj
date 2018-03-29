e2cbde5eacf82042b9ebfefd43ff830fe674ed8d
John Warwick <jwarwick@gmail.com>
2018-03-29 00:04:51 -0400

    Bryan Stinson installed Currents are removed from game instead of trashed

--- src/clj/game/cards/upgrades.clj
+++ src/clj/game/cards/upgrades.clj
@@ -134,12 +134,13 @@
                  :req (req (and (< (:credit runner) 6)
                                 (< 0 (count (filter #(and (is-type? % "Operation")
                                                           (has-subtype? % "Transaction")) (:discard corp))))))
-                 :label "Play a transaction operation from Archives ignoring all costs and remove it from the game"
+                 :label "Play a transaction operation from Archives, ignoring all costs, and remove it from the game"
                  :prompt "Choose a transaction operation to play"
-                 :msg (msg "play " (:title target) " from Archives ignoring all costs and remove it from the game")
+                 :msg (msg "play " (:title target) " from Archives, ignoring all costs, and removes it from the game")
                  :choices (req (cancellable (filter #(and (is-type? % "Operation")
                                                           (has-subtype? % "Transaction")) (:discard corp)) :sorted))
-                 :effect (effect (play-instant nil target {:ignore-cost true}) (move target :rfg))}]}
+                 :effect (effect (play-instant nil (assoc-in target [:special :rfg-when-trashed] true) {:ignore-cost true})
+                                 (move target :rfg))}]}
 
    "Calibration Testing"
    {:abilities [{:label "[Trash]: Place 1 advancement token on a card in this server"
