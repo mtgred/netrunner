a4586f2ee59b5a01edcaa654824c8f1df12713d4
Noah <nbtheduke@gmail.com>
2018-04-05 20:28:05 -0400

    Fixed cost

--- src/clj/game/cards/programs.clj
+++ src/clj/game/cards/programs.clj
@@ -567,13 +567,13 @@
     :events {:pre-access {:delayed-completion true
                           :req (req (and (pos? (get-in card [:counter :power] 0))
                                          (= target :rd)))
-                          :counter-cost [:power 1]
                           :effect (effect (show-wait-prompt :corp "Runner to use Nyashia")
                                           (continue-ability
                                             {:optional
                                              {:prompt "Spend a power counter on Nyashia to access 1 additional card?"
                                               :yes-ability {:msg "access 1 additional card from R&D"
                                                             :effect (effect (access-bonus 1)
+                                                                            (add-counter card :power -1)
                                                                             (clear-wait-prompt :corp))}
                                               :no-ability {:effect (effect (clear-wait-prompt :corp))}}}
                                             card nil))}}}
