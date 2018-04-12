bcb044e9147deddfbac36279f8638ccc2bfb1d9f
John Warwick <jwarwick@gmail.com>
2018-04-12 17:36:15 +0200

    Reminder toast for Always Be Running

--- src/clj/game/cards/resources.clj
+++ src/clj/game/cards/resources.clj
@@ -99,6 +99,8 @@
 
    "Always Be Running"
    {:implementation "Run requirement not enforced"
+    :events {:runner-turn-begins {:effect (req (toast state :runner
+                                              "Reminder: Always Be Running requires a run on the first click" "info"))}}
     :abilities [{:once :per-turn
                  :cost [:click 2]
                  :msg (msg "break 1 subroutine")}]}
