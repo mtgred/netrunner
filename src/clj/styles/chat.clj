e713443745b451711f347f7887f63d7e5bc604d0
Dan Hutchins <danhut@cisco.com>
2018-04-19 09:32:53 +1000

    fix initial glitches

--- src/clj/styles/chat.clj
+++ src/clj/styles/chat.clj
@@ -1,9 +1,11 @@
 (ns styles.chat
-  (:require [garden.def :refer [defstyles defrule defkeyframes]]
+  (:require [garden.def :refer [defstyles defkeyframes]]
             [garden.selectors :as gs :refer [& nth-child]]
             [garden.units :refer [px]]))
 
-(defn get-delay [i]
+(defn get-delay
+  "Takes a time interval and generates it as a string"
+  [i]
   (str (float (* 0.1 i)) "s"))
 
 (defkeyframes blink
@@ -11,8 +13,9 @@
               [:20% {:opacity 1}]
               [:100% {:opacity 0.2}])
 
-(def animate-dots
-  (for [i (range 1 11)] [(gs/& (gs/nth-child i)) {:animation-delay (get-delay i)}]))
+(def animate-typing
+  (for [i (range 1 11)]
+    [(-> i str nth-child &) {:animation-delay (get-delay i)}]))
 
 (defstyles chat
            [:.typing {:position "absolute"
@@ -24,6 +27,6 @@
             [:span {:animation-name "blink"
                     :animation-duration "1.5s"
                     :animation-iteration-count "infinite"
-                    :animation=fill-mode "both"}]]
-           animate-dots
+                    :animation-fill-mode "both"}
+             animate-typing]]
            blink)
