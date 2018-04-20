30aa2f1b4d59cd11d781f78621bd96176ef91017
Joel Koepp <jkoepp@gmail.com>
2018-04-20 09:00:58 -0500

    fix Trope MU adjustment
    fix #3335, correct indentation

--- src/clj/game/cards/programs.clj
+++ src/clj/game/cards/programs.clj
@@ -959,7 +959,6 @@
                  :label "[Click], remove Trope from the game: Reshuffle cards from Heap back into Stack"
                  :effect (effect
                           (move card :rfg)
-                          (gain :memory 1)
                           (resolve-ability
                            {:show-discard true
                             :choices {:max (min (get-in card [:counter :power] 0) (count (:discard runner)))
@@ -971,16 +970,17 @@
                             :effect (req (doseq [c targets] (move state side c :deck))
                                          (shuffle! state side :deck))}
                            card nil))}]}
-     "Upya"
-     {:implementation "Power counters added automatically"
-      :events {:successful-run {:silent (req true)
-                                :req (req (= target :rd))
-                                :effect (effect (add-counter card :power 1)) }}
-      :abilities [{:cost [:click 1]
-                   :counter-cost [:power 3]
-                   :once :per-turn
-                   :msg "gain [Click][Click]"
-                   :effect (effect (gain :click 2))}]}
+
+   "Upya"
+   {:implementation "Power counters added automatically"
+    :events {:successful-run {:silent (req true)
+                              :req (req (= target :rd))
+                              :effect (effect (add-counter card :power 1)) }}
+    :abilities [{:cost [:click 1]
+                 :counter-cost [:power 3]
+                 :once :per-turn
+                 :msg "gain [Click][Click]"
+                 :effect (effect (gain :click 2))}]}
 
    "Wari"
    (letfn [(prompt-for-subtype []
