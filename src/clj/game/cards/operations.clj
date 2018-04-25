cfb06b01185defe59906d45c02d4d00c905e7652
Joel Koepp <jkoepp@gmail.com>
2018-04-24 21:19:33 -0500

    update Standard Procedure
    remove delayed completion

--- src/clj/game/cards/operations.clj
+++ src/clj/game/cards/operations.clj
@@ -1358,15 +1358,13 @@
 
    "Standard Procedure"
    {:req (req (last-turn? state :runner :successful-run))
-    :delayed-completion true
     :prompt "Choose a card type"
     :choices ["Event" "Hardware" "Program" "Resource"]
     :effect (req (let [n (* 2 (count (filter #(is-type? % target) (:hand runner))))]
                    (gain state :corp :credit n)
                    (system-msg state side (str "uses Standard Procedure to name " target ", reveal "
                                                (join ", " (map :title (:hand runner)))
-                                               " in the Runner's Grip, and gain " n " [Credits]"))
-                   (effect-completed state side eid)))}
+                                               " in the Runner's Grip, and gain " n " [Credits]"))))}
 
    "Stock Buy-Back"
    {:msg (msg "gain " (* 3 (count (:scored runner))) " [Credits]")
