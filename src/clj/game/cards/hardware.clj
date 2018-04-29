55d5650967b3f5aea5eae41387ddd6ff63c7db60
Nikolai Hansen <nicohasa@gmail.com>
2018-04-29 22:43:05 +0200

    Fixed bugs etc
    Added get-card to hosting, otherwise you get bugs if you try
    to host to same card you are already hosted on.
    
    Made gebrselassie remove all-turn strength if it gets moved/removed

--- src/clj/game/cards/hardware.clj
+++ src/clj/game/cards/hardware.clj
@@ -415,13 +415,22 @@
                                                 (system-msg :runner (str "places 1 virus counter on Friday Chip")))}}}}})
 
    "Gebrselassie"
-   {:abilities [{:label "Host Gebrselassie on an installed non-AI icebreaker"
+   {:abilities [{:msg (msg "host it on an installed non-AI icebreaker")
                  :cost [:click 1] 
-                 :choices {:req #(and (installed? %) (has-subtype? % "Icebreaker") (not (has-subtype? % "AI")))}
-                 :effect (effect (host target card))}]
+                 :choices {:req #(and 
+                                   (installed? %) 
+                                   (has-subtype? % "Icebreaker") (not (has-subtype? % "AI")))}
+                 :effect (req (when-let [host (get-card state (:host card))]
+                                (update! state side (dissoc-in host [:pump :all-turn]))
+                                (update-breaker-strength state side host))
+                              (host state side target card))}]
     :events {:pump-breaker {:silent (req true)
-                            :req (req (= (:cid (second target)) (:cid (:host card))))
-                            :effect (effect (update! (update-in (second targets) [:pump :all-turn] (fnil #(+ % (first targets) 0)))))}}}
+                            :req (req (= (:cid (second targets)) (:cid (:host card))))
+                            :effect (effect (update! (update-in (second targets) [:pump :all-turn] (fnil #(+ % (first targets)) 0)))
+                                            (update-breaker-strength (second targets)))}}
+    :leave-play (req (when-let [host (get-card state (:host card))]
+                       (update! state side (dissoc-in host [:pump :all-turn]))
+                       (update-breaker-strength state side host)))}
 
    "GPI Net Tap"
    {:implementation "Trash and jack out effect is manual"
