44fdc8cb9ae5e805fbfe67d9450030a9b3fdff93
John Warwick <jwarwick@gmail.com>
2018-05-02 16:43:04 -0400

    Warden Fatuma: pre-rezzed ice works

--- src/clj/game/cards/assets.clj
+++ src/clj/game/cards/assets.clj
@@ -1681,8 +1681,7 @@
 
    "Warden Fatuma"
    (let [new-sub {:label "[Warden Fatuma] force the Runner to lose 1 [Click], if able"
-                  :msg "force the Runner to lose 1 [Click], if able"
-                  :effect runner-loses-click}]
+                  :msg "force the Runner to lose 1 [Click], if able" }]
 
      (letfn [(all-rezzed-bios [state]
                (filter #(and (ice? %)
@@ -1690,28 +1689,24 @@
                              (rezzed? %))
                        (all-installed state :corp)))
 
-             (remove-one [state ice]
-               (println "Removing" (:title ice))
-               (update! state :corp (-> ice
-                                      (assoc :subroutines (rest (:subroutines ice)))
-                                      (dissoc-in [:special :extra-subs]))))
+             (remove-one [cid state ice]
+               (remove-extra-subs state :corp cid ice))
 
-             (add-one [state ice]
-               (println "Adding" (:title ice))
-               (update! state :corp (-> ice
-                                      (assoc :subroutines (cons new-sub (:subroutines ice)))
-                                      (assoc-in [:special :extra-subs] true))))
+             (add-one [cid state ice]
+               (add-extra-sub state :corp cid ice 0 new-sub))
 
              (update-all [state func] (doseq [i (all-rezzed-bios state)] (func state i)))
              ]
        {:effect (req (system-msg
                        state :corp
                        "uses Warden Fatuma to add \"[Subroutine] The Runner loses [Click], if able\" before all other subroutines")
-                  (update-all state add-one))
+                  (update-all state (partial add-one (:cid card))))
         :leave-play (req (system-msg state :corp "loses Warden Fatuma additional subroutines")
-                      (update-all state remove-one))
+                      (update-all state (partial remove-one (:cid card))))
+        :sub-effect {:msg "force the Runner to lose 1 [Click], if able"
+                     :effect (req (lose state :runner :click 1))}
         :events {:rez {:req (req (and (ice? target) (has-subtype? target "Bioroid")))
-                       :effect (req (add-one state target))}}}))
+                       :effect (req (add-one (:cid card) state target))}}}))
 
    "Watchdog"
    {:events {:pre-rez {:req (req (and (ice? target) (not (get-in @state [:per-turn (:cid card)]))))
