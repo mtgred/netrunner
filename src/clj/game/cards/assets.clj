5ae902b49f724662f07d0fc77f4c9b183f41755e
John Warwick <jwarwick@gmail.com>
2018-05-03 16:48:20 -0400

    Update The Board to not catch agendas going to the Corp

--- src/clj/game/cards/assets.clj
+++ src/clj/game/cards/assets.clj
@@ -1596,17 +1596,22 @@
                  :msg (msg "derez " (:advance-counter card)) :effect (effect (trash card))}]}
 
    "The Board"
-   {:effect (effect (lose :runner :agenda-point (count (:scored runner))))
-    :leave-play (effect (gain :runner :agenda-point (count (:scored runner))))
-    :trash-effect {:when-inactive true
-                   :req (req (:access @state))
-                   :msg "add it to the Runner's score area as an agenda worth 2 agenda points"
-                   :delayed-completion true
-                   :effect (req (as-agenda state :runner eid card 2))}
-    :events {:agenda-stolen {:effect (effect (lose :runner :agenda-point 1))}
-             :card-moved {:req (req (or (some #{:scored} (:zone (first targets)))
-                                        (some #{:scored} (:zone (second targets)))))
-                          :effect (effect ((if (some #{:scored} (:zone (first targets))) gain lose) :runner :agenda-point 1))}}}
+   (let [the-board {:req (req (and (= :runner (:as-agenda-side target))
+                                   (not= (:cid target) (:cid card))))
+                    :effect (effect (lose :runner :agenda-point 1))}]
+         {:effect (effect (lose :runner :agenda-point (count (:scored runner))))
+          :leave-play (effect (gain :runner :agenda-point (count (:scored runner))))
+          :trash-effect {:when-inactive true
+                         :req (req (:access @state))
+                         :msg "add it to the Runner's score area as an agenda worth 2 agenda points"
+                         :delayed-completion true
+                         :effect (req (as-agenda state :runner eid card 2))}
+          :events {:agenda-stolen (dissoc the-board :req)
+                   :as-agenda the-board
+                   :pre-card-moved {:req (req (let [c (first targets)
+                                                    c-cid (:cid c)]
+                                                (some #(when (= c-cid (:cid %)) %) (:scored runner))))
+                                    :effect (req (gain state :runner :agenda-point 1))}}})
 
    "The News Now Hour"
    {:events {:runner-turn-begins {:effect (req (prevent-current state side))}}
