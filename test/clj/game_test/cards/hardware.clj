1f4730dd1325080726cdb010b2a4f46e16b36335
Filip Gokstorp <filip@gokstorp.se>
2018-05-12 18:34:02 +0100

    Tidies up Dinosaurus test a bit.

--- test/clj/game_test/cards/hardware.clj
+++ test/clj/game_test/cards/hardware.clj
@@ -231,46 +231,44 @@
     (is (= 5 (core/available-mu state)) "Gain 1 memory")
     (is (= 3 (:credit (get-runner))) "Got 1c for successful run on Desperado")))
 
-(deftest dinosaurus-adept
-  ;; Dinosaurus - hosting a breaker with strength based on unused MU should calculate correctly
-  (do-game
-    (new-game (default-corp)
-              (default-runner [(qty "Adept" 1)
-                               (qty "Dinosaurus" 1)]))
-    (take-credits state :corp)
-    (core/gain state :runner :credit 5)
-    (play-from-hand state :runner "Dinosaurus")
-    (play-from-hand state :runner "Adept")
-    (is (= 2 (core/available-mu state)) "2 MU used")
-    (let [dino (get-hardware state 0)
-          adpt (get-program state 0)]
-      (is (= 4 (:current-strength (refresh adpt))) "Adept at 4 strength individually")
-      (card-ability state :runner dino 1)
-      (prompt-select :runner (refresh adpt))
-      (let [hosted-adpt (first (:hosted (refresh dino)))]
-        (is (= 4 (core/available-mu state)) "0 MU used")
-        (is (= 8 (:current-strength (refresh hosted-adpt))) "Adept at 8 strength hosted")))))
-
-(deftest dinosaurus-strength-boost-mu-savings
-  ;; Dinosaurus - Boost strength of hosted icebreaker; keep MU the same when hosting or trashing hosted breaker
-  (do-game
-    (new-game (default-corp)
-              (default-runner [(qty "Dinosaurus" 1) (qty "Battering Ram" 1)]))
-    (take-credits state :corp)
-    (core/gain state :runner :credit 5)
-    (play-from-hand state :runner "Dinosaurus")
-    (let [dino (get-in @state [:runner :rig :hardware 0])]
-      (card-ability state :runner dino 0)
-      (prompt-select :runner (find-card "Battering Ram" (:hand (get-runner))))
-      (is (= 2 (:click (get-runner))))
-      (is (= 0 (:credit (get-runner))))
-      (is (= 4 (core/available-mu state)) "Battering Ram 2 MU not deducted from available MU")
-      (let [ram (first (:hosted (refresh dino)))]
-        (is (= 5 (:current-strength (refresh ram)))
-            "Dinosaurus giving +2 strength to Battering Ram")
-        ;; Trash Battering Ram
-        (core/move state :runner (find-card "Battering Ram" (:hosted (refresh dino))) :discard)
-        (is (= 4 (core/available-mu state)) "Battering Ram 2 MU not added to available MU")))))
+(deftest dinosaurus
+  (testing "Hosting a breaker with strength based on unused MU should calculate correctly"
+    (do-game
+      (new-game (default-corp)
+                (default-runner ["Adept" "Dinosaurus"]))
+      (take-credits state :corp)
+      (core/gain state :runner :credit 5)
+      (play-from-hand state :runner "Dinosaurus")
+      (play-from-hand state :runner "Adept")
+      (is (= 2 (core/available-mu state)) "2 MU used")
+      (let [dino (get-hardware state 0)
+            adpt (get-program state 0)]
+        (is (= 4 (:current-strength (refresh adpt))) "Adept at 4 strength individually")
+        (card-ability state :runner dino 1)
+        (prompt-select :runner (refresh adpt))
+        (let [hosted-adpt (first (:hosted (refresh dino)))]
+          (is (= 4 (core/available-mu state)) "0 MU used")
+          (is (= 8 (:current-strength (refresh hosted-adpt))) "Adept at 8 strength hosted")))))
+  (testing "Boost strength of hosted icebreaker; keep MU the same when hosting or trashing hosted breaker"
+    (do-game
+      (new-game (default-corp)
+                (default-runner ["Dinosaurus" "Battering Ram"]))
+      (take-credits state :corp)
+      (core/gain state :runner :credit 5)
+      (play-from-hand state :runner "Dinosaurus")
+      (let [dino (get-in @state [:runner :rig :hardware 0])]
+        (card-ability state :runner dino 0)
+        (prompt-select :runner (find-card "Battering Ram" (:hand (get-runner))))
+        (is (= 2 (:click (get-runner))))
+        (is (= 0 (:credit (get-runner))))
+        (is (= 4 (core/available-mu state)) "Battering Ram 2 MU not deducted from available MU")
+        (let [ram (first (:hosted (refresh dino)))]
+          (is (= 5 (:current-strength (refresh ram)))
+              "Dinosaurus giving +2 strength to Battering Ram")
+          ;; Trash Battering Ram
+          (core/move state :runner (find-card "Battering Ram" (:hosted (refresh dino))) :discard)
+          (is (= 4 (core/available-mu state))
+              "Battering Ram 2 MU not added to available MU when Battering Ram was trashed"))))))
 
 (deftest doppelganger
   ;; Doppelgänger - run again when successful
