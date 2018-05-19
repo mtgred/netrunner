4a2e013211f11f37babf69a8f6f0541fc37f3498
John Warwick <jwarwick@gmail.com>
2018-05-19 11:00:39 -0700

    Implement Mlinzi

--- test/clj/game_test/cards/ice.clj
+++ test/clj/game_test/cards/ice.clj
@@ -777,6 +777,28 @@
     (is (= 2 (count (get-in @state [:corp :servers :hq :ices]))) "2 ICE protecting HQ")
     (is (= 6 (:credit (get-corp))) "Didn't pay 1 credit to install as second ICE")))
 
+(deftest mlinzi
+  ;; Mlinzi - take X net damage or trash the top X+1 cards from the Stack
+  (do-game
+    (new-game (default-corp ["Mlinzi"])
+              (default-runner [(qty "Sure Gamble" 3)]))
+    (starting-hand state :runner ["Sure Gamble"])
+    (play-from-hand state :corp "Mlinzi" "HQ")
+    (take-credits state :corp)
+    (let [ml (get-ice state :hq 0)]
+      (run-on state "HQ")
+      (core/rez state :corp ml)
+      (card-subroutine state :corp (refresh ml) 0)
+      (is (= 1 (count (:hand (get-runner)))) "Runner has 1 card in hand")
+      (prompt-choice-partial :runner "Take")
+      (is (= 1 (count (:discard (get-runner)))) "Runner trashed 1 card")
+      (is (empty? (:hand (get-runner))) "Runner trashed card from hand")
+      (card-subroutine state :corp (refresh ml) 0)
+      (is (= 2 (count (:deck (get-runner)))) "Runner has 2 cards in stack")
+      (prompt-choice-partial :runner "Trash")
+      (is (= 3 (count (:discard (get-runner)))) "Runner trashed 2 cards")
+      (is (empty? (:deck (get-runner))) "Runner trashed card from stack"))))
+
 (deftest morph-ice-subtype-changing
   ;; Morph ice gain and lose subtypes from normal advancements and placed advancements
   (do-game
