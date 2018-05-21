1a2cc34a31b64d0cf9f56a2587eaec32dfd0c884
Noah <nbtheduke@gmail.com>
2018-05-21 18:15:02 -0400

    Levy University

--- test/clj/game_test/cards/assets.clj
+++ test/clj/game_test/cards/assets.clj
@@ -1843,6 +1843,27 @@
       (is (= 8 (:credit (get-corp))))
       (is (= 4 (get-counters (refresh launch) :credit))))))
 
+(deftest levy-university
+  ;; Levy University
+  (do-game
+    (new-game (default-corp ["Levy University" "Ice Wall" (qty "Fire Wall" 10)])
+              (default-runner))
+    (starting-hand state :corp ["Levy University"])
+    (play-from-hand state :corp "Levy University" "New remote")
+    (let [levy (get-content state :remote1 0)
+          number-of-shuffles (count (core/turn-events state :corp :corp-shuffle-deck))]
+      (core/rez state :corp levy)
+      (is (zero? (-> (get-corp) :hand count)) "HQ should be empty")
+      (let [clicks (:click (get-corp))
+            credits (:credit (get-corp))]
+        (card-ability state :corp (refresh levy) 0)
+        (prompt-card :corp (find-card "Ice Wall" (:deck (get-corp))))
+        (is (= (- credits 1) (:credit (get-corp))) "Levy University ability should cost 1 credit")
+        (is (= (- clicks 1) (:click (get-corp))) "Levy University ability should cost 1 click"))
+      (is (= 1 (-> (get-corp) :hand count)) "HQ should have 1 card")
+      (is (= "Ice Wall" (-> (get-corp) :hand first :title)) "HQ should contain Ice Wall")
+      (is (< number-of-shuffles (count (core/turn-events state :corp :corp-shuffle-deck))) "Corp should shuffle deck"))))
+
 (deftest malia-z0l0ka
   ;; Malia Z0L0K4 - blank an installed non-virtual runner resource
   (do-game
