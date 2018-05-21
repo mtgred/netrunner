a152297ac87a707857bcb1494fc93c00627d2b63
John Warwick <jwarwick@gmail.com>
2018-05-21 18:50:41 -0400

    Implement Flame-out

--- src/clj/game/cards/hardware.clj
+++ src/clj/game/cards/hardware.clj
@@ -382,6 +382,62 @@
                  :effect (effect (trash card {:cause :ability-cost})
                                  (damage-prevent :brain 2))}]}
 
+   "Flame-out"
+   (let [turn-end {:delayed-completion true
+                   :effect (req (unregister-events state :runner card)
+                                (if-let [hosted (first (:hosted card))]
+                                  (do
+                                    (system-msg state :runner (str "trashes " (:title hosted) " from Flame-out"))
+                                    (trash state side eid hosted nil))
+                                  (effect-completed state side eid)))}]
+   {:implementation "Credit usage restriction not enforced"
+    :data {:counter {:credit 9}}
+    :abilities [{:label "Take 1[Credits] from Flame-out"
+                 :req (req (and (not-empty (:hosted card))
+                                (pos? (get-in card [:counter :credit] 0))))
+                 :counter-cost [:credit 1]
+                 :effect (req (gain state :runner :credit 1)
+                              (system-msg state :runner "takes 1[Credits] from Flame-out")
+                              (register-events
+                                state :runner
+                                {:runner-turn-ends turn-end
+                                 :corp-turn-ends turn-end}
+                                (get-card state card)))}
+                {:label "Take all [Credits] from Flame-out"
+                 :req (req (and (not-empty (:hosted card))
+                                (pos? (get-in card [:counter :credit] 0))))
+                 :effect (req (let [credits (get-in card [:counter :credit] 0)]
+                                (gain state :runner :credit credits)
+                                (update! state :runner (dissoc-in card [:counter :credit]))
+                                (system-msg state :runner (str "takes " credits "[Credits] from Flame-out"))
+                                (register-events
+                                  state :runner
+                                  {:runner-turn-ends turn-end
+                                   :corp-turn-ends turn-end}
+                                  (get-card state card))))}
+                 {:label "Install a program on Flame-out"
+                 :req (req (empty? (:hosted card)))
+                 :cost [:click 1]
+                 :prompt "Select a program in your Grip to install on Flame-out"
+                 :choices {:req #(and (is-type? % "Program")
+                                      (in-hand? %))}
+                 :effect (effect (runner-install target {:host-card card})
+                                 (update! (assoc-in (get-card state card) [:special :flame-out] (:cid target))))}
+                {:label "Host an installed program on Flame-out"
+                 :req (req (empty? (:hosted card)))
+                 :prompt "Select an installed program to host on Flame-out"
+                 :choices {:req #(and (is-type? % "Program")
+                                      (installed? %))}
+                 :msg (msg "host " (:title target))
+                 :effect (req (->> target
+                                (get-card state)
+                                (host state side card))
+                              (update! state side (assoc-in (get-card state card) [:special :flame-out] (:cid target))))}]
+    :events {:card-moved {:req (req (= (:cid target) (get-in (get-card state card) [:special :flame-out])))
+                          :effect (effect (update! (dissoc-in card [:special :flame-out])))}
+             :runner-turn-ends nil
+             :corp-turn-ends nil}})
+
    "Forger"
    {:prevent {:tag [:all]}
     :in-play [:link 1]
