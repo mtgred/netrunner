835734982e784385b405a0ecb51637fdbe490d0e
Noah <nbtheduke@gmail.com>
2018-05-21 12:05:34 -0400

    Expose

--- test/clj/game_test/cards/assets.clj
+++ test/clj/game_test/cards/assets.clj
@@ -1072,6 +1072,27 @@
         (core/move state :corp (find-card card (:hand (get-corp))) :deck)
         (is (< number-of-shuffles (count (core/turn-events state :corp :corp-shuffle-deck))) "Should be shuffled")))))
 
+(deftest expose
+  ;; Exposé
+  (do-game
+    (new-game (default-corp ["Exposé"])
+              (default-runner))
+    (core/gain state :corp :click 100 :credit 100)
+    (dotimes [i 2]
+      (play-from-hand state :corp "Exposé" "New remote")
+      (let [i (inc i)
+            expose (get-content state (keyword (str "remote" i)) 0)]
+        (core/rez state :corp (refresh expose))
+        (is (zero? (:bad-publicity (get-corp))) "Corp should have 0 bad publicity to start with")
+        (core/gain-bad-publicity state :corp i)
+        (is (= i (:bad-publicity (get-corp))) (str "Corp should gain " i " bad publicity"))
+        (advance state (refresh expose) i)
+        (card-ability state :corp (refresh expose) 0)
+        (is (zero? (:bad-publicity (get-corp))) "Corp should have 0 bad publicity after using Exposé's ability")
+        (is (= 1 (-> (get-corp) :discard count)) "Archives should have 1 card in it")
+        (is (= "Exposé" (-> (get-corp) :discard first :title)) "Only card in Archives should be Exposé")
+        (core/move state :corp (find-card "Exposé" (:discard (get-corp))) :hand)))))
+
 (deftest false-flag
   (testing "when the corp attempts to score False Flag"
     (testing "and False Flag has 7 advancements"
