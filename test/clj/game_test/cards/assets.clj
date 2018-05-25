0153fb29b3e75ff0a0fd49fc57e147a981f6cf1a
Noah <nbtheduke@gmail.com>
2018-05-25 16:37:05 -0400

    Shannon Claire

--- test/clj/game_test/cards/assets.clj
+++ test/clj/game_test/cards/assets.clj
@@ -2955,6 +2955,38 @@
     (play-from-hand state :corp "Pup" "HQ")
     (is (= 1 (count (:discard (get-corp)))) "Server Diagnostics trashed by ICE install")))
 
+(deftest shannon-claire
+  ;; Shannon Claire
+  (do-game
+    (new-game (default-corp ["Shannon Claire" "Hostile Takeover" "Ronin" (qty "Ice Wall" 10)])
+              (default-runner))
+    (starting-hand state :corp ["Shannon Claire" "Ronin"])
+    (core/move state :corp (find-card "Ronin" (:hand (get-corp))) :deck)
+    (play-from-hand state :corp "Shannon Claire" "New remote")
+    (let [shannon (get-content state :remote1 0)]
+      (core/rez state :corp shannon)
+      (is (zero? (count (:hand (get-corp)))) "Corp should have 0 cards in hand to start")
+      (card-ability state :corp shannon 0)
+      (is (= "Ronin" (-> (get-corp) :hand first :title)) "Corp should draw Ronin with Shannon's click ability")
+      (let [number-of-shuffles (count (core/turn-events state :corp :corp-shuffle-deck))]
+        (card-ability state :corp shannon 1)
+        (prompt-card :corp (find-card "Hostile Takeover" (:deck (get-corp))))
+        (is (= "Hostile Takeover" (-> (get-corp) :deck last :title))
+            "Agenda selected with Shannon's R&D ability should be on bottom of deck")
+        (is (< number-of-shuffles (count (core/turn-events state :corp :corp-shuffle-deck))) "Searching R&D should shuffle deck")))
+    (core/move state :corp (find-card "Hostile Takeover" (:deck (get-corp))) :discard)
+    (core/move state :corp (find-card "Shannon Claire" (:discard (get-corp))) :hand)
+    (play-from-hand state :corp "Shannon Claire" "New remote")
+    (let [shannon (get-content state :remote2 0)
+          number-of-shuffles (count (core/turn-events state :corp :corp-shuffle-deck))]
+      (core/rez state :corp shannon)
+      (card-ability state :corp shannon 2)
+      (prompt-card :corp (find-card "Hostile Takeover" (:discard (get-corp))))
+      (is (= "Hostile Takeover" (-> (get-corp) :deck last :title))
+          "Agenda selected with Shannon's Archives ability should be on bottom of deck")
+      (is (= number-of-shuffles (count (core/turn-events state :corp :corp-shuffle-deck)))
+          "Searching Archives shouldn't shuffle deck"))))
+
 (deftest shattered-remains
   ;; Shattered Remains
   (do-game
