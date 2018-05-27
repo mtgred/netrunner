507a7a0202030e0bebf8297ea83c74d21f71ddf7
John Warwick <jwarwick@gmail.com>
2018-05-27 17:34:00 -0400

    Update Mlinzi text

--- src/clj/game/cards/ice.clj
+++ src/clj/game/cards/ice.clj
@@ -1453,8 +1453,7 @@
 
    "Mlinzi"
    (letfn [(net-or-trash [net-dmg mill-cnt]
-             {:label (str "Do " net-dmg " net damage unless the Runner trashes the top "
-                          mill-cnt " cards of the Stack.")
+             {:label (str "Do " net-dmg " net damage.")
               :effect (req (show-wait-prompt state :corp "Runner to choose an option for Mlinzi")
                            (resolve-ability state :runner
                                             {:prompt "Take net damage or trash cards from the Stack?"
@@ -1462,7 +1461,7 @@
                                                        (str "Trash the top " mill-cnt " cards of the Stack")]
                                              :effect (req (if (.startsWith target "Take")
                                                             (do
-                                                              (system-msg state :corp (str "uses Mlinzi to give the Runner "
+                                                              (system-msg state :corp (str "uses Mlinzi to do "
                                                                                            net-dmg " net damage"))
                                                               (clear-wait-prompt state :corp)
                                                               (damage state :runner eid :net net-dmg {:card card}))
