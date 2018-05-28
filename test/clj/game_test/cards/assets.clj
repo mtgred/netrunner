d8a8f8ff98627370d1c2eb541140d0d295544909
Noah <nbtheduke@gmail.com>
2018-05-28 18:44:17 -0400

    Zealous Judge

--- test/clj/game_test/cards/assets.clj
+++ test/clj/game_test/cards/assets.clj
@@ -3737,4 +3737,15 @@
 
 (deftest zealous-judge
   ;; Zealous Judge
-  )
+  (do-game
+    (new-game (default-corp ["Zealous Judge"])
+              (default-runner))
+    (play-from-hand state :corp "Zealous Judge" "New remote")
+    (let [judge (get-content state :remote1 0)]
+      (core/rez state :corp judge)
+      (is (not (:rezzed (refresh judge))) "Zealous Judge can't be rezzed until Runner is tagged")
+      (core/gain state :runner :tag 1)
+      (core/rez state :corp judge)
+      (is (:rezzed (refresh judge)) "Zealous Judge can be rezzed while the Runner is tagged")
+      (card-ability state :corp judge 0)
+      (is (= 2 (:tag (get-runner))) "Runner should gain a tag from Zealous Judge's ability"))))
