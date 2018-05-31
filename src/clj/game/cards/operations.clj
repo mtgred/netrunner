f8fb6ed9cebeab1bb99b2c184890dcdc42d7b87d
John Warwick <jwarwick@gmail.com>
2018-05-31 08:21:37 -0400

    Fixed optional delayed-completion and end-effect

--- src/clj/game/cards/operations.clj
+++ src/clj/game/cards/operations.clj
@@ -1224,25 +1224,25 @@
    "Riot Suppression"
    {:req (req (last-turn? state :runner :trashed-card))
     :delayed-completion true
-    :effect (req (show-wait-prompt state :corp "Runner to decide if they will take 1 brain damage")
-                 (when-completed (resolve-ability
-                                   state :runner
-                                   {:optional
-                                    {:prompt "Take 1 brain damage to prevent having 3 fewer clicks next turn?"
-                                     :player :runner
-                                     :delayed-completion true
-                                     :yes-ability
-                                     {:effect (req (system-msg
-                                                     state :runner
-                                                     "suffers 1 brain damage to prevent losing 3[Click] to Riot Suppression")
-                                                   (damage state :runner eid :brain 1 {:card card}))}
-                                     :no-ability
-                                     {:msg "give the runner 3 fewer [Click] next turn"
-                                      :effect (req (swap! state update-in [:runner :extra-click-temp] (fnil #(- % 3) 0))
-                                                   (effect-completed state side eid))}}}
-                                   card nil)
-                                 (do (clear-wait-prompt state :corp)
-                                   (move state :corp (find-latest state card) :rfg))))}
+    :effect (req (let [c card]
+                   (show-wait-prompt state :corp "Runner to decide if they will take 1 brain damage")
+                   (continue-ability
+                     state :runner
+                     {:optional
+                      {:prompt "Take 1 brain damage to prevent having 3 fewer clicks next turn?"
+                       :player :runner
+                       :end-effect (req (clear-wait-prompt state :corp)
+                                        (move state :corp (find-latest state c) :rfg))
+                       :yes-ability
+                       {:delayed-completion true
+                        :effect (req (system-msg
+                                       state :runner
+                                       "suffers 1 brain damage to prevent losing 3[Click] to Riot Suppression")
+                                     (damage state :runner eid :brain 1 {:card card}))}
+                       :no-ability
+                       {:msg "give the runner 3 fewer [Click] next turn"
+                        :effect (req (swap! state update-in [:runner :extra-click-temp] (fnil #(- % 3) 0)))}}}
+                     card nil)))}
 
    "Rolling Brownout"
    {:msg "increase the play cost of operations and events by 1 [Credits]"
