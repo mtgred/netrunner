a143356372a18f16de3837a681e2be1b55d6d891
Noah <nbtheduke@gmail.com>
2018-06-07 13:55:04 -0400

    On the Lam fix + test

--- src/clj/game/cards/events.clj
+++ src/clj/game/cards/events.clj
@@ -1326,13 +1326,14 @@
     :prompt "Choose a resource to host On the Lam"
     :choices {:req #(and (is-type? % "Resource")
                          (installed? %))}
-    :effect (effect (host target (assoc card :zone [:discard]))
+    :effect (effect (host target (assoc card :zone [:discard] :installed true))
                     (system-msg (str "hosts On the Lam on " (:title target))))
     :interactions {:prevent [{:type #{:net :brain :meat :tag}
                               :req (req true)}]}
     :abilities [{:label "[Trash]: Avoid 3 tags"
                  :msg "avoid up to 3 tags"
-                 :effect (effect (tag-prevent 3) (trash card {:cause :ability-cost}))}
+                 :effect (effect (tag-prevent 3)
+                                 (trash card {:cause :ability-cost}))}
                 {:label "[Trash]: Prevent up to 3 damage"
                  :msg "prevent up to 3 damage"
                  :effect (effect (damage-prevent :net 3)
