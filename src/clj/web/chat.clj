3fc9a358c91b3af0ead46b1bde32bb2401d5c08b
John Warwick <jwarwick@gmail.com>
2018-06-09 22:22:38 -0400

    Rate limit chat messages from an individual user

--- src/clj/web/chat.clj
+++ src/clj/web/chat.clj
@@ -1,6 +1,8 @@
 (ns web.chat
   (:require [buddy.sign.jwt :as jwt]
             [clojure.string :as s]
+            [clj-time.core :as t]
+            [clj-time.coerce :as c]
             [monger.query :as q]
             [monger.collection :as mc]
             [monger.result :refer [acknowledged?]]
@@ -18,12 +20,21 @@
                                             (q/sort (array-map :date -1))
                                             (q/limit 100)))))
 
+(defn- within-rate-limit
+  [username]
+  (let [window (:rate-window chat-config 60)
+        start-date (c/to-date (t/plus (t/now) (t/seconds (- window))))
+        max-cnt (:rate-cnt chat-config 10)
+        msg-cnt (mc/count db "messages" {:username username :date {"$gt" start-date}})]
+    (< msg-cnt max-cnt)))
+
 (defn- insert-msg [{{{:keys [username emailhash]} :user} :ring-req
                     {:keys [:channel :msg]} :?data :as event}]
   (when (and username
              emailhash
              (not (s/blank? msg))
-             (<= (count msg) (:max-length chat-config 144)))
+             (<= (count msg) (:max-length chat-config 144))
+             (within-rate-limit username))
     (let [message {:emailhash emailhash
                    :username  username
                    :msg       msg
