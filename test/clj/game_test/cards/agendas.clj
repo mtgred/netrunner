85a36e673d4705d5648cb2ad696fb7a9735dc40c
Noah <nbtheduke@gmail.com>
2018-06-13 14:58:02 -0400

    Fixed test to handle actually relevant scenario

--- test/clj/game_test/cards/agendas.clj
+++ test/clj/game_test/cards/agendas.clj
@@ -1184,20 +1184,21 @@
 (deftest improved-tracers
   ;; Improved Tracers
   (do-game
-    (new-game (default-corp ["Improved Tracers" "News Hound" "Restructured Datapool"])
+    (new-game (default-corp ["Improved Tracers" "News Hound" "Information Overload"])
               (default-runner))
+    (core/gain state :corp :credit 10)
     (play-from-hand state :corp "News Hound" "HQ")
-    (play-and-score state "Restructured Datapool")
+    (play-from-hand state :corp "Information Overload" "R&D")
     (let [nh (get-ice state :hq 0)
-          rd (get-scored state :corp 0)]
+          io (get-ice state :rd 0)]
       (core/rez state :corp nh)
+      (core/rez state :corp io)
       (is (= 4 (:current-strength (refresh nh))) "Should start with base strength of 4")
-      (is (= 3 (:credit (get-corp))) "Should have 2 credits after rez")
+      (is (= 7 (:credit (get-corp))) "Should have 7 credits after rez")
       (play-and-score state "Improved Tracers")
       (is (= 5 (:current-strength (refresh nh))) "Should gain 1 strength from 4 to 5")
       (take-credits state :corp)
       (run-on state "HQ")
-      (run-phase-43 state)
       (card-subroutine state :corp nh 0)
       (is (= 1 (-> (get-corp) :prompt first :bonus)) "Should gain 1 bonus trace strength")
       (prompt-choice :corp 0)
@@ -1205,16 +1206,15 @@
       (is (= 1 (:tag (get-runner))))
       (run-jack-out state)
       (run-on state "HQ")
-      (run-phase-43 state)
       (card-subroutine state :corp nh 0)
       (is (= 1 (-> (get-corp) :prompt first :bonus))
           "Should gain only 1 bonus trace strength regardless of number of runs in a turn")
       (prompt-choice :corp 0)
       (prompt-choice :runner 0)
       (is (= 2 (:tag (get-runner))))
-      (take-credits state :runner)
-      (card-ability state :corp rd 0)
-      (is (zero? (-> (get-corp) :prompt first :bonus)) "Should gain 0 bonus trace strength"))))
+      (run-on state "R&D")
+      (card-ability state :corp io 1)
+      (is (zero? (-> (get-corp) :prompt first :bonus)) "Should gain 0 bonus trace strength, as it's an encounter ability"))))
 
 (deftest labyrinthine-servers
   ;; Labyrinthine Servers
