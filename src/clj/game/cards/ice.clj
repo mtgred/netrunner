8f72c21714c148f3e6ee5878971121721f4d46de
Neal Terrell <neal.terrell@csulb.edu>
2018-06-25 12:27:40 -0700

    Implement Blockchain.

--- src/clj/game/cards/ice.clj
+++ src/clj/game/cards/ice.clj
@@ -396,6 +396,19 @@
    "Battlement"
    {:subroutines [end-the-run]}
 
+   "Blockchain"
+   (letfn [(sub-count [corp] (int (/ (count (filter #(and (is-type? % "Operation") (has-subtype? % "Transaction"))
+                                                    (:discard corp)))
+                                     2)))]
+     {:abilities [{:label "Gain subroutines"
+                   :msg (msg (let [c (sub-count corp)]
+                               (str "gain " c (pluralize " subroutine" c))))}]
+      :subroutines [{:label "Gain 1 [credits], Runner loses 1 [credits]"
+                     :msg "gain 1 [credits] and force the Runner to lose 1 [credits]"
+                     :effect (effect (gain-credits 1)
+                                     (lose-credits :runner 1))}
+                    end-the-run]})
+
    "Bloodletter"
    {:subroutines [{:label "Runner trashes 1 program or top 2 cards of their Stack"
                    :effect (req (if (empty? (filter #(is-type? % "Program") (all-active-installed state :runner)))
--- test/clj/game_test/cards/ice.clj
+++ test/clj/game_test/cards/ice.clj
@@ -110,6 +110,34 @@
       (run-jack-out state)
       (is (= 1 (:tag (get-runner))) "Run unsuccessful; Runner kept 1 tag"))))
 
+(deftest blockchain
+  (do-game
+    (new-game (default-corp ["Blockchain" (qty "Beanstalk Royalties" 5)])
+              (default-runner))
+    (core/gain state :corp :credit 2 :click 5)
+    (play-from-hand state :corp "Blockchain" "HQ")
+    (let [bc (get-ice state :hq 0)]
+      (core/rez state :corp bc)
+      (card-ability state :corp bc 0)
+      (is (last-log-contains? state "uses Blockchain to gain 0 subroutines") "No subroutines gained because no Transactions are in Archives")
+      (play-from-hand state :corp "Beanstalk Royalties")
+      (card-ability state :corp bc 0)
+      (is (last-log-contains? state "uses Blockchain to gain 0 subroutines") "No subroutines gained because only 1 Transaction is in Archives")
+      (play-from-hand state :corp "Beanstalk Royalties")
+      (card-ability state :corp bc 0)
+      (is (last-log-contains? state "uses Blockchain to gain 1 subroutine") "1 subroutine gained because 2 Transactions are in Archives")
+      (play-from-hand state :corp "Beanstalk Royalties")
+      (card-ability state :corp bc 0)
+      (is (last-log-contains? state "uses Blockchain to gain 1 subroutine") "1 subroutine gained because 3 Transactions are in Archives")
+      (play-from-hand state :corp "Beanstalk Royalties")
+      (card-ability state :corp bc 0)
+      (is (last-log-contains? state "uses Blockchain to gain 2 subroutines") "2 subroutines gained because 4 Transactions are in Archives")
+
+      (is (= 12 (:credit (get-corp))) "Corp has 12 credits from four Beanstalks")
+      (card-subroutine state :corp bc 0)
+      (is (= 13 (:credit (get-corp))) "Corp gained 1 credit from Blockchain")
+      (is (= 4 (:credit (get-runner))) "Runner lost 1 credit from Blockchain"))))
+
 (deftest bullfrog
   ;; Bullfrog - Win psi to move to outermost position of another server and continue run there
   (do-game
