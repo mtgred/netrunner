c3139e721fe59d728e57545ac59686a73ed0db91
John Warwick <jwarwick@gmail.com>
2018-06-26 14:55:27 -0400

    Check runner last turn for Under the Bus. Ensure Connection Resource is target.

--- src/clj/game/cards/operations.clj
+++ src/clj/game/cards/operations.clj
@@ -1699,10 +1699,16 @@
                                              card nil)))}
 
    "Under the Bus"
-   {:req (req (and (:accessed-cards runner-reg)
-                   (not-empty (filter #(has-subtype? % "Connection") (all-active-installed state :runner)))))
+   {:req (req (and (last-turn? state :runner :accessed-cards)
+                   (not-empty (filter
+                                #(and (is-type? % "Resource")
+                                      (has-subtype? % "Connection"))
+                                (all-active-installed state :runner)))))
     :prompt "Choose a connection to trash"
-    :choices {:req #(and (= (:side %) "Runner") (has-subtype? % "Connection") (installed? %))}
+    :choices {:req #(and (= (:side %) "Runner")
+                         (is-type? % "Resource")
+                         (has-subtype? % "Connection")
+                         (installed? %))}
     :msg (msg "trash " (:title target) " and take 1 bad publicity")
     :async true
     :effect (req (wait-for (trash state side target nil)
