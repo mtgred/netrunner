6a5dbc15af2da44a2ad4eb93914baa9b3fbb561d
Neal Terrell <neal.terrell@csulb.edu>
2018-06-26 11:23:16 -0700

    Implement Divert Power.

--- src/clj/game/cards/operations.clj
+++ src/clj/game/cards/operations.clj
@@ -380,6 +380,27 @@
                 " [Credits]")
       :effect (effect (gain-credits (number-of-non-empty-remotes state)))})
 
+   "Divert Power"
+   {:async true
+    :prompt "Select any number of cards to derez"
+    :choices {:req #(and (installed? %)
+                         (rezzed? %))
+              :max (req (count (filter rezzed? (all-installed state :corp))))}
+    :effect (req (doseq [c targets]
+                   (derez state side c))
+                 (let [discount (* -3 (count targets))]
+                   (continue-ability
+                     state side
+                     {:async true
+                      :prompt "Select a card to rez"
+                      :choices {:req #(and (installed? %)
+                                           (= (:side %) "Corp")
+                                           (not (rezzed? %))
+                                           (not (is-type? % "Agenda")))}
+                      :effect (effect (rez-cost-bonus discount)
+                                      (rez eid target nil))}
+                     card nil)))}
+
    "Door to Door"
    {:events {:runner-turn-begins
              {:trace {:base 1
--- test/clj/game_test/cards/operations.clj
+++ test/clj/game_test/cards/operations.clj
@@ -414,6 +414,51 @@
     (play-from-hand state :corp "Diversified Portfolio")
     (is (= 7 (:credit (get-corp))) "Ignored remote with ICE but no server contents")))
 
+(deftest divert-power
+  (do-game
+    (new-game (default-corp [(qty "Divert Power" 2) "Paper Wall" (qty "Eve Campaign" 3) ])
+              (default-runner))
+    (core/gain state :corp :click 3 :credit 11)
+    (play-from-hand state :corp "Paper Wall" "HQ")
+    (play-from-hand state :corp "Eve Campaign" "New remote")
+    (play-from-hand state :corp "Eve Campaign" "New remote")
+    (play-from-hand state :corp "Eve Campaign" "New remote")
+    (let [pw (get-ice state :hq 0)
+          ec1 (get-content state :remote1 0)
+          ec2 (get-content state :remote2 0)
+          ec3 (get-content state :remote3 0)]
+      (core/rez state :corp pw)
+      (core/rez state :corp ec1)
+      (core/rez state :corp ec2)
+      (play-from-hand state :corp "Divert Power")
+      (is (= 4 (:credit (get-corp))) "Corp has 4 credits after rezzes and playing Divert Power")
+      (testing "Choose 2 targets to derez"
+        (prompt-select :corp (refresh pw))
+        (prompt-select :corp (refresh ec1))
+        (prompt-choice :corp "Done"))
+
+      (testing "Choose a target to rez for -6 cost"
+        (prompt-select :corp (refresh ec3)))
+
+      (is (core/rezzed? (refresh ec3)) "Eve Campaign was rezzed")
+      (is (= 4 (:credit (get-corp))) "Rezzed Eve Campaign for 0 credits")
+      (is (not (core/rezzed? (refresh pw))) "Paper Wall was derezzed")
+      (is (not (core/rezzed? (refresh ec1))) "First Eve Campaign was derezzed")
+      (is (= 16 (get-counters (refresh ec3) :credit)) "Eve gained 16 credits on rez")
+
+      (play-from-hand state :corp "Divert Power")
+      (testing "Choose 1 target to derez"
+        (prompt-select :corp (refresh ec2))
+        (prompt-choice :corp "Done"))
+
+      (testing "Choose a target to rez for -3 cost"
+        (prompt-select :corp (refresh ec1)))
+
+      (is (core/rezzed? (refresh ec1)) "First Eve Campaign was rezzed")
+      (is (= 0 (:credit (get-corp))) "Rezzed Eve Campaign for 2 credits")
+      (is (not (core/rezzed? (refresh ec2))) "Second Eve Campaign was derezzed")
+      (is (= 32 (get-counters (refresh ec1) :credit)) "First Eve gained 16  more credits on rez"))))
+
 (deftest door-to-door
   ;; Door to Door
   (do-game
