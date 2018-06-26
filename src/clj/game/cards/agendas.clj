a81fcd5766107e9c6e7295a371e2e9b48e14a2ed
John Warwick <jwarwick@gmail.com>
2018-06-26 18:32:43 -0400

    Handling bad-pub events in Broad Daylight

--- src/clj/game/cards/agendas.clj
+++ src/clj/game/cards/agendas.clj
@@ -292,21 +292,24 @@
                :msg "make the Runner lose 2 tags"}}
 
    "Broad Daylight"
-   {:prompt "Take 1 bad publicity?"
-    :choices ["Yes" "No"]
-    :async true
-    :effect (req (when (= target "Yes")
-                   (wait-for (gain-bad-publicity state :corp 1)
-                             (system-msg state :corp "used Broad Daylight to take 1 bad publicity")))
-                 (let [bp (get-in @state [:corp :bad-publicity] 0)]
-                   (add-counter state :corp card :agenda bp)
-                   (effect-completed state side eid)))
-    :abilities [{:cost [:click 1] :counter-cost [:agenda 1]
-                 :async true
-                 :label "Do 2 meat damage"
-                 :once :per-turn
-                 :msg "do 2 meat damage"
-                 :effect (effect (damage eid :meat 2 {:card card}))}]}
+   (letfn [(add-counters [state side card eid]
+             (let [bp (get-in @state [:corp :bad-publicity] 0)]
+               (add-counter state :corp card :agenda bp)
+               (effect-completed state side eid)))]
+     {:prompt "Take 1 bad publicity?"
+      :choices ["Yes" "No"]
+      :async true
+      :effect (req (when (= target "Yes")
+                     (wait-for (gain-bad-publicity state :corp 1)
+                               (system-msg state :corp "used Broad Daylight to take 1 bad publicity")
+                               (add-counters state side card eid)))
+                (add-counters state side card eid))
+      :abilities [{:cost [:click 1] :counter-cost [:agenda 1]
+                   :async true
+                   :label "Do 2 meat damage"
+                   :once :per-turn
+                   :msg "do 2 meat damage"
+                   :effect (effect (damage eid :meat 2 {:card card}))}]})
 
    "CFC Excavation Contract"
    {:effect (req (let [bios (count (filter #(has-subtype? % "Bioroid") (all-active-installed state :corp)))
--- test/clj/game_test/cards/agendas.clj
+++ test/clj/game_test/cards/agendas.clj
@@ -394,7 +394,37 @@
       (card-ability state :corp (get-scored state :corp 0) 0)
       (is (= 2 (count (:discard (get-runner)))) "Runner took 2 damage")
       (card-ability state :corp (get-scored state :corp 0) 0)
-      (is (= 2 (count (:discard (get-runner)))) "Runner didn't take additional damage"))))
+      (is (= 2 (count (:discard (get-runner)))) "Runner didn't take additional damage")))
+  (testing "bad pub triggers"
+    (do-game
+      (new-game (default-corp ["Broad Daylight" "Broadcast Square"])
+                (default-runner))
+      (core/gain state :corp :bad-publicity 1)
+      (play-from-hand state :corp "Broadcast Square" "New remote")
+      (core/rez state :corp (get-content state :remote1 0))
+      (is (= 1 (:bad-publicity (get-corp))) "Corp start with one bad pub")
+      (play-and-score state "Broad Daylight")
+      (prompt-choice :corp "Yes")
+      (is (= 1 (:bad-publicity (get-corp))) "Doesn't gain additional bad pub yet")
+      (prompt-choice :corp 0)  ;; Corp doesn't pump trace, base 3
+      (prompt-choice :runner 0)  ;; Runner doesn't pump trace; loses trace
+      (is (= 1 (:bad-publicity (get-corp))) "Blocks gaining additional bad pub")
+      (is (= 1 (get-counters (get-scored state :corp 0) :agenda)) "Should gain 1 agenda counter")))
+  (testing "bad pub triggers - more cases"
+    (do-game
+      (new-game (default-corp ["Broad Daylight" "Broadcast Square"])
+                (default-runner))
+      (core/gain state :corp :bad-publicity 1)
+      (play-from-hand state :corp "Broadcast Square" "New remote")
+      (core/rez state :corp (get-content state :remote1 0))
+      (is (= 1 (:bad-publicity (get-corp))) "Corp start with one bad pub")
+      (play-and-score state "Broad Daylight")
+      (prompt-choice :corp "Yes")
+      (is (= 1 (:bad-publicity (get-corp))) "Doesn't gain additional bad pub yet")
+      (prompt-choice :corp 0)  ;; Corp doesn't pump trace, base 3
+      (prompt-choice :runner 5)  ;; Runner pumps trace; wins trace
+      (is (= 2 (:bad-publicity (get-corp))) "Gains additional bad pub")
+      (is (= 2 (get-counters (get-scored state :corp 0) :agenda)) "Should gain 2 agenda counter"))))
 
 (deftest cfc-excavation-contract
   ;; CFC Excavation Contract
