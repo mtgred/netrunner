10c782bf08579a3b7ea674d9e203df2e4e8d4267
Noah <nbtheduke@gmail.com>
2018-06-27 16:42:04 -0400

    Game Changer

--- test/clj/game_test/cards/operations.clj
+++ test/clj/game_test/cards/operations.clj
@@ -1,5 +1,6 @@
 (ns game-test.cards.operations
   (:require [game.core :as core]
+            [game.utils :as utils]
             [game-test.core :refer :all]
             [game-test.utils :refer :all]
             [game-test.macros :refer :all]
@@ -759,6 +760,25 @@
     (prompt-choice :runner 0)
     (prompt-select :corp (get-resource state 0))
     (is (= 2 (-> (get-runner) :discard count)) "Corp should trash Ice Carver from winning Foxfire trace")))
+
+(deftest game-changer
+  (letfn [(game-changer-test [num-agenda]
+            (do-game
+              (new-game (default-corp ["Game Changer" "Hostile Takeover"])
+                        (default-corp [(qty "Fan Site" num-agenda)]))
+              (take-credits state :corp)
+              (core/gain state :runner :click num-agenda)
+              (dotimes [_ num-agenda]
+                (play-from-hand state :runner "Fan Site"))
+              (take-credits state :runner)
+              (play-and-score state "Hostile Takeover")
+              (is (= num-agenda (count (get-scored state :runner))) (str "Runner should have " (utils/quantify num-agenda "Fan Site") " in play"))
+              (let [clicks (:click (get-corp))
+                    n (dec num-agenda)]
+                (play-from-hand state :corp "Game Changer")
+                (is (= (+ n clicks) (:click (get-corp))) (str "Corp should gain " (utils/quantify n "click")))
+                (is (= 1 (-> (get-corp) :rfg count)) "Game Changer should be in rfg zone now"))))]
+    (doall (map game-changer-test (range 5)))))
 
 (deftest hard-hitting-news
   ;; Hard-Hitting News
