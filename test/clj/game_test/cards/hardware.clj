a912ac28de3aa993aa0341e5db4d6239b4ca2466
Nikolai Hansen <nicohasa@gmail.com>
2018-06-29 23:47:01 +0200

    Fixed failing tests

--- test/clj/game_test/cards/hardware.clj
+++ test/clj/game_test/cards/hardware.clj
@@ -809,14 +809,15 @@
   (testing "Play event"
     (do-game
       (new-game (default-corp)
-                (default-runner ["Patchwork" (qty "Sure Gamble" 4)]))
+                (default-runner ["Patchwork" (qty "Sure Gamble" 2) "Easy Mark"]))
       (take-credits state :corp)
       (core/gain state :runner :credit 4)
       (play-from-hand state :runner "Patchwork")
+      (card-ability state :runner (get-hardware state 0) 0)
       (play-from-hand state :runner "Sure Gamble")
       (is (= 5 (:credit (get-runner))) "Runner has not been charged credits yet")
       (is (empty? (:discard (get-runner))) "Sure Gamble is not in heap yet")
-      (prompt-select :runner (find-card "Sure Gamble" (:hand (get-runner))))
+      (prompt-select :runner (find-card "Easy Mark" (:hand (get-runner))))
       (is (= 11 (:credit (get-runner))) "Runner was only charge 3 credits to play Sure Gamble")
       (is (= 2 (count (:discard (get-runner)))) "2 cards now in heap")
       (play-from-hand state :runner "Sure Gamble")
@@ -825,14 +826,15 @@
   (testing "Install a card"
     (do-game
       (new-game (default-corp)
-                (default-runner ["Patchwork" (qty "Cyberfeeder" 4)]))
+                (default-runner ["Patchwork" "Easy Mark" "Cyberfeeder"]))
       (take-credits state :corp)
       (core/gain state :runner :credit 4)
       (play-from-hand state :runner "Patchwork")
+      (card-ability state :runner (get-hardware state 0) 0)
       (play-from-hand state :runner "Cyberfeeder")
       (is (= 5 (:credit (get-runner))) "Runner has not been charged credits yet")
       (is (empty? (:discard (get-runner))) "Cyberfeeder is not in heap yet")
-      (prompt-select :runner (find-card "Cyberfeeder" (:hand (get-runner))))
+      (prompt-select :runner (find-card "Easy Mark" (:hand (get-runner))))
       (is (= 5 (:credit (get-runner))) "Runner was charged 0 credits to play Cyberfeeder"))))
 
 (deftest plascrete-carapace
