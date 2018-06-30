e924fd885cf58e76b6752fae088a5fadfdccab9e
John Warwick <jwarwick@gmail.com>
2018-06-30 17:40:15 -0400

    Implement RaR: Miss Bones

--- src/clj/game/cards/resources.clj
+++ src/clj/game/cards/resources.clj
@@ -1139,6 +1139,17 @@
                  :effect (effect (derez target)
                                  (trash card {:cause :ability-cost}))}]}
 
+   "Miss Bones"
+   {:data {:counter {:credit 12}}
+    :implementation "Credit use restriction not enforced"
+    :abilities [{:counter-cost [:credit 1]
+                 :msg "gain 1 [Credits] for trashing installed cards"
+                 :async true
+                 :effect (req (gain-credits state :runner 1)
+                              (if (zero? (get-counters (get-card state card) :credit))
+                                (trash state :runner eid card {:unpreventable true})
+                                (effect-completed state :runner eid)))}]}
+
    "Motivation"
    (let [ability {:msg "look at the top card of their Stack"
                   :label "Look at the top card of Stack (start of turn)"
