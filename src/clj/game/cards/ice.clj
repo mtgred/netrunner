949808229a67d235ff9068c24f2119ac91097cc2
Filip Gokstorp <filip@gokstorp.se>
2018-07-01 18:19:13 +0200

    Implements and adds test for Hydra.

--- src/clj/game/cards/ice.clj
+++ src/clj/game/cards/ice.clj
@@ -1074,6 +1074,22 @@
    "Hunter"
    {:subroutines [(tag-trace 3)]}
 
+   "Hydra"
+   (letfn [(otherwise-tag [message ability]
+             {:msg (msg (if (is-tagged? @state) message "give the Runner 1 tag"))
+              :label (str (capitalize message) " if the Runner is tagged; otherwise, give the Runner 1 tag")
+              :async true
+              :effect (req (if (is-tagged? @state)
+                             (ability state :runner eid card nil)
+                             (tag-runner state :runner eid 1)))})]
+     {:subroutines [(otherwise-tag "do 3 net damage"
+                                   (req (damage state :runner :net 3 {:card card})))
+                    (otherwise-tag "gain 5 [Credits]"
+                                   (req (gain-credits state :corp 5)
+                                        (effect-completed state side eid)))
+                    (otherwise-tag "end the run"
+                                   (req (end-run state side eid)))]})
+
    "Ice Wall"
    {:advanceable :always
     :subroutines [end-the-run]
