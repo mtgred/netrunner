1ce905afa5c98854c74336fbc7e3984328b7b227
Noah <nbtheduke@gmail.com>
2018-07-08 15:54:57 -0400

    Fast Break

--- test/clj/game_test/cards/operations.clj
+++ test/clj/game_test/cards/operations.clj
@@ -739,6 +739,46 @@
       (is (= 3 (:click (get-corp))))
       (is (= 3 (:click-per-turn (get-corp)))))))
 
+(deftest fast-break
+  ;; Fast Break
+  (do-game
+    (new-game (default-corp ["Fast Break" "Hostile Takeover" "Keegan Lane" "Haas Arcology AI"
+                             "Research Station" (qty "Ice Wall" 10)])
+              (default-runner [(qty "Fan Site" 3)]))
+    (starting-hand state :corp ["Fast Break" "Hostile Takeover" "Keegan Lane"
+                                "Haas Arcology AI" "Research Station"])
+    (take-credits state :corp)
+    (dotimes [_ 3]
+      (play-from-hand state :runner "Fan Site"))
+    (take-credits state :runner)
+    (play-and-score state "Hostile Takeover")
+    (is (= 3 (count (get-scored state :runner))) "Runner should have 3 agendas in score area")
+    (play-from-hand state :corp "Fast Break")
+    (let [hand (-> (get-corp) :hand count)
+          credits (:credit (get-corp))]
+      (prompt-choice :corp 3)
+      (is (= (+ hand 3) (-> (get-corp) :hand count)) "Corp should draw 3 cards from Fast Break")
+      (prompt-choice :corp "New remote")
+      (prompt-select :corp (find-card "Keegan Lane" (:hand (get-corp))))
+      (prompt-select :corp (find-card "Ice Wall" (:hand (get-corp))))
+      (prompt-select :corp (find-card "Ice Wall" (:hand (get-corp))))
+      (is (= (dec credits) (:credit (get-corp))) "Corp should pay 1 credit to install second Ice Wall"))
+    (core/move state :corp (find-card "Fast Break" (:discard (get-corp))) :hand)
+    (play-from-hand state :corp "Fast Break")
+    (let [hand (-> (get-corp) :hand count)
+          credits (:credit (get-corp))]
+      (prompt-choice :corp 0)
+      (is (= hand (-> (get-corp) :hand count)) "Corp should draw no cards as they're allowed to draw no cards")
+      (is (some #{"Server 2"} (:choices (prompt? :corp))) "Corp should be able to choose existing remotes")
+      (prompt-choice :corp "Server 2")
+      (prompt-select :corp (find-card "Haas Arcology AI" (:hand (get-corp))))
+      (prompt-select :corp (find-card "Research Station" (:hand (get-corp))))
+      (is (= 2 (count (get-content state :remote2))) "Corp can't choose Research Station to install in a remote")
+      (prompt-select :corp (find-card "Ice Wall" (:hand (get-corp))))
+      (prompt-choice :corp "Done")
+      (is (= (- credits 2) (:credit (get-corp))) "Corp should pay 2 credits to install third Ice Wall")
+      (is (empty? (:prompt (get-corp))) "Corp should be able to stop installing early"))))
+
 (deftest foxfire
   ;; Foxfire
   (do-game
