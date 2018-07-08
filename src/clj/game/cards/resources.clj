61ec2a0acede3067c4959774ee17af5614fcffde
Noah <nbtheduke@gmail.com>
2018-07-08 18:11:18 -0400

    Removed double-counting accessed cards in Archives

--- src/clj/game/cards/resources.clj
+++ src/clj/game/cards/resources.clj
@@ -842,8 +842,6 @@
    (shard-constructor :archives "access all cards in Archives" {:async true}
                       (req (trash state side card {:cause :ability-cost})
                            (swap! state update-in [:corp :discard] #(map (fn [c] (assoc c :seen true)) %))
-                           (when (:run @state)
-                             (swap! state update-in [:run :cards-accessed :discard] (fnil #(+ % (count (:discard corp))) 0)))
                            (wait-for (trigger-event-sync state side :pre-access :archives)
                                      (resolve-ability state :runner
                                                       (choose-access (get-in @state [:corp :discard])
