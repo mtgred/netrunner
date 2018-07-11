842923f05b1e187cac73e52e84f71efc7e23e813
Karl Erik Holter <karl0erik@gmail.com>
2018-07-11 10:33:06 +0200

    Modified Thimblerig prompt text.

--- src/clj/game/cards/ice.clj
+++ src/clj/game/cards/ice.clj
@@ -2064,7 +2064,7 @@
    {:flags {:corp-phase-12 (req (>= (count (filter ice? (all-installed state :corp))) 2))}
     :implementation "Does not restrict usage of swap ability to start of turn or after pass"
     :abilities [{:label "Swap Thimblerig with a piece of ice"
-                 :prompt "Swap Thimblerig with a piece of ice"
+                 :prompt "Choose a piece of ice to swap Thimblerig with"
                  :choices {:req ice?
                            :not-self (req (:cid card))}
                  :effect (effect (swap-ice card target))}]
