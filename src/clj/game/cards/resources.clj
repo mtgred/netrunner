6634f6a46cad5098a944c598acb689b76c080f79
Nikolai Hansen <nicohasa@gmail.com>
2018-07-12 00:44:46 +0200

    Fixed :async stuff
    Also checks if runner can actually pay before printing message
    and adding install-const-bonus.

--- src/clj/game/cards/resources.clj
+++ src/clj/game/cards/resources.clj
@@ -1980,10 +1980,14 @@
          ability {:choices (req (cancellable (remove #(is-type? % "Event") (:hand runner))))
                   :async true
                   :prompt (msg "Which card to install?")
-                  :effect (req (install-cost-bonus state side [:credit -1])
-                               (if (runner-can-install? state side target)
-                                 (do (system-msg state side "uses Thunder Art Gallery to install a card.")
-                                     (runner-install state side eid target nil))))}]
+                  :effect (req (if (and (runner-can-install? state side target)
+                                        (can-pay? state side target 
+                                                  (install-cost state side target [:credit (dec (:cost target))])))
+                                 (do (install-cost-bonus state side [:credit -1])
+                                     (system-msg state side "uses Thunder Art Gallery to install a card.")
+                                     (runner-install state side eid target nil))
+                                 (effect-completed state side eid)))
+                  :cancel-effect (effect (effect-completed eid))}]
      {:events {:runner-lose-tag (assoc ability :req (req (and (first-event-check state first-event? no-event?) (= side :runner))))
                :runner-prevent (assoc ability :req (req (and (first-event-check state no-event? first-event?) (seq (filter #(some #{:tag} %) targets)))))}})
 
