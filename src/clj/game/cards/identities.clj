95c008c6ec0b7ffbfd984d2bee98ba2bdf8fc64d
Neal Terrell <neal.terrell@csulb.edu>
2018-07-16 09:24:14 -0700

    Implement Acme Consulting.

--- src/clj/game/cards/identities.clj
+++ src/clj/game/cards/identities.clj
@@ -75,6 +75,28 @@
                                                card nil))))}}}
                          card nil)))}}}
 
+   "Acme Consulting: The Truth You Need"
+   (letfn [(activate [state card active]
+             (update! state :corp (assoc-in card [:special :acme-active] active))
+             (swap! state update-in [:runner :additional-tag] (if active inc dec)))
+           (outermost? [run-position run-ices]
+             (and run-position
+                  (pos? run-position)
+                  (= run-position (count run-ices))))]
+     {:implementation "Tag is gained on approach, not on encounter"
+      :events {:run {:effect (req (when (and (outermost? run-position run-ices)
+                                             (rezzed? current-ice))
+                                    (activate state card true)))}
+               :rez {:effect (req (when (outermost? run-position run-ices)
+                                    (activate state card true)))}
+               :derez {:effect (req (when (outermost? run-position run-ices)
+                                      (activate state card false)))}
+               :pass-ice {:effect (req (when (and (outermost? run-position run-ices)
+                                                  (get-in card [:special :acme-active]))
+                                         (activate state card false)))}
+               :end-run {:effect (req (when (get-in card [:special :acme-active])
+                                        (activate state card false)))}}})
+
    "Adam: Compulsive Hacker"
    {:events {:pre-start-game
              {:req (req (= side :runner))
