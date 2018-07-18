f92aeb7b21f78e034cb7d862a6f41fe1f0c3b65d
Neal Terrell <neal.terrell@csulb.edu>
2018-07-18 11:17:59 -0700

    Merge separate files for runner and corp.

--- src/clj/game/quotes.clj
+++ src/clj/game/quotes.clj
@@ -2,21 +2,30 @@
   (:require [aero.core :refer [read-config]]
             [clojure.java.io :as io]))
 
-(let [quotes-corp-filename "data/quotes.edn"
+(let [quotes-corp-filename "data/quotes-corp.edn"
+      quotes-runner-filename "data/quotes-runner.edn"
       quotes-corp (if (.exists (io/file quotes-corp-filename))
                     (read-config quotes-corp-filename)
                     {})
-      generic-key "Default"
-      ]
-  (defonce identity-quotes quotes-corp)
+      quotes-runner (if (.exists (io/file quotes-runner-filename))
+                      (read-config quotes-runner-filename)
+                      {})
+      generic-key "Default"]
+  (defonce identity-quotes (merge quotes-corp quotes-runner))
 
-  (defn make-quote [{player-ident :title} {opp-ident :title}]
+  (defn- choose-and-repeat [options qty]
+    (when (not-empty options)
+      (repeat qty (first (shuffle options)))))
+
+  (defn make-quote [{player-ident :title} {opp-ident :title opp-faction :faction}]
+
     (let [generic (get-in identity-quotes [player-ident generic-key])
-          opp-faction (get-in identity-quotes [player-ident (:faction opp-ident)])
+          opp-faction (get-in identity-quotes [player-ident opp-faction])
           opp-specific (get-in identity-quotes [player-ident opp-ident])
-          one-random (flatten (filter #(when % (-> % shuffle first)) [generic opp-faction opp-specific]))
-          non-blank (filter #(pos? (count %)) one-random)]
-
+          weighted (concat (choose-and-repeat generic 1)
+                           (choose-and-repeat opp-faction 0)
+                           (choose-and-repeat opp-specific 0))
+          non-blank (filter identity weighted)]
       (if (not-empty non-blank)
         (first (shuffle non-blank))
         "NO QUOTE SRY"))))
