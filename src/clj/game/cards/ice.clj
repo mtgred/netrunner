2a159452570491faaa881ee02775a1d3fd22f345
Chris Greene <archgoon@gmail.com>
2018-09-18 01:37:38 -0700

    Use same-card instead.

--- src/clj/game/cards/ice.clj
+++ src/clj/game/cards/ice.clj
@@ -1242,7 +1242,7 @@
 
    "Kakugo"
    {:events {:pass-ice {:async true
-                        :req (req (= (:cid target) (:cid card)))
+                        :req (req (same-card? target card))
                         :msg "do 1 net damage"
                         :effect (effect (damage eid :net 1 {:card card}))}}
     :subroutines [end-the-run]}
