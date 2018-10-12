3a3407c409cd795a0326ac08d027957773c864ca
John Warwick <jwarwick@gmail.com>
2018-10-12 15:45:56 -0400

    Close runner prompt for Remote Enforcement if no ice in deck

--- src/clj/game/cards/agendas.clj
+++ src/clj/game/cards/agendas.clj
@@ -1095,17 +1095,24 @@
    "Remote Enforcement"
    {:interactive (req true)
     :optional {:prompt "Search R&D for a piece of ice to install protecting a remote server?"
-               :yes-ability {:async true
-                             :prompt "Choose a piece of ice"
-                             :choices (req (filter ice? (:deck corp)))
-                             :effect (req (let [chosen-ice target]
-                                            (continue-ability state side
-                                              {:async true
-                                               :prompt (str "Select a server to install " (:title chosen-ice) " on")
-                                               :choices (filter #(not (#{"HQ" "Archives" "R&D"} %))
-                                                                (corp-install-list state chosen-ice))
-                                               :effect (effect (shuffle! :deck)
-                                                               (corp-install eid chosen-ice target {:install-state :rezzed-no-rez-cost}))}
+               :yes-ability
+               {:effect (req (when (not-empty (filter ice? (:deck corp)))
+                                            (continue-ability
+                                              state side
+                                              {
+                                               :async true
+                                               :prompt "Choose a piece of ice"
+                                               :choices (req (filter ice? (:deck corp)))
+                                               :effect (req (let [chosen-ice target]
+                                                              (continue-ability state side
+                                                                                {:async true
+                                                                                 :prompt (str "Select a server to install " (:title chosen-ice) " on")
+                                                                                 :choices (filter #(not (#{"HQ" "Archives" "R&D"} %))
+                                                                                                  (corp-install-list state chosen-ice))
+                                                                                 :effect (effect (shuffle! :deck)
+                                                                                                 (corp-install eid chosen-ice target
+                                                                                                               {:install-state :rezzed-no-rez-cost}))}
+                                                                                card nil)))}
                                               card nil)))}}}
 
    "Research Grant"
