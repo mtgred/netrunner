63b5abb5446c945a923fa92fe21b6a0c6aa9494f
John Warwick <jwarwick@gmail.com>
2018-10-27 11:14:46 +1100

    Made Labor Rights playable with less than 3 cards in the deck

--- src/clj/game/cards/events.clj
+++ src/clj/game/cards/events.clj
@@ -1155,27 +1155,29 @@
                     card nil)))}
 
    "Labor Rights"
-   {:req (req (<= 3 (count (:deck runner))))
-    :effect (req (mill state :runner :runner 3)
-                 (system-msg state :runner "trashes the top 3 cards of their Stack")
-                 (continue-ability
-                   state side
-                   {:prompt "Choose 3 cards to shuffle into the stack"
-                    :show-discard true
-                    :choices {:max 3
-                              :all true
-                              :not-self true
-                              :req #(and (= (:side %) "Runner")
-                                         (in-discard? %))}
-                    :effect (req (doseq [c targets] (move state side c :deck))
-                                 (system-msg state :runner (str "moves " (join ", " (map :title targets)) " from their Heap to their Stack"))
-                                 (shuffle! state :runner :deck)
-                                 (system-msg state :runner "shuffles their Stack")
-                                 (draw state :runner 1)
-                                 (system-msg state :runner "draws 1 card")
-                                 (move state side (find-latest state card) :rfg)
-                                 (system-msg state :runner "removes Labor Rights from the game"))}
-                   card nil))}
+   {:req (req (pos? (+ (count (:deck runner)) (count (:discard runner)))))
+    :effect (req (let [mill-count (min 3 (count (:deck runner)))]
+                   (mill state :runner :runner mill-count)
+                   (system-msg state :runner (str "trashes the top " mill-count (pluralize " card" mill-count) " of their Stack"))
+                   (let [heap-count (min 3 (count (get-in @state [:runner :discard])))]
+                     (continue-ability
+                       state side
+                       {:prompt (str "Choose " heap-count (pluralize " card" heap-count) " to shuffle into the stack")
+                        :show-discard true
+                        :choices {:max heap-count
+                                  :all true
+                                  :not-self true
+                                  :req #(and (= (:side %) "Runner")
+                                             (in-discard? %))}
+                        :effect (req (doseq [c targets] (move state side c :deck))
+                                     (system-msg state :runner (str "moves " (join ", " (map :title targets)) " from their Heap to their Stack"))
+                                     (shuffle! state :runner :deck)
+                                     (system-msg state :runner "shuffles their Stack")
+                                     (draw state :runner 1)
+                                     (system-msg state :runner "draws 1 card")
+                                     (move state side (find-latest state card) :rfg)
+                                     (system-msg state :runner "removes Labor Rights from the game"))}
+                       card nil))))}
 
    "Lawyer Up"
    {:msg "remove 2 tags and draw 3 cards"
