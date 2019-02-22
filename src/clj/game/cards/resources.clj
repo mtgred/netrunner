e9c349e3ad9f64200f8506c0266f71ef3907110d
John Warwick <jwarwick@gmail.com>
2019-02-22 16:39:03 -0500

    Implement Fencer Fueno

--- src/clj/game/cards/resources.clj
+++ src/clj/game/cards/resources.clj
@@ -820,6 +820,38 @@
                              :req (req (installed? card))
                              :effect (req (as-agenda state :runner eid card 0))}}}
 
+   "Fencer Fueno"
+   (let [place-credit {:msg "add 1 [Credits] to itself"
+                       :effect (effect (add-counter card :credit 1))}
+         credit-count (fn [state card] (get-counters (get-card state card) :credit))]
+     {:events {:runner-turn-begins place-credit
+               :agenda-stolen place-credit
+               :runner-turn-ends
+               {:req (req (>= (credit-count state card) 3))
+                :async true
+                :effect (req (show-wait-prompt state :corp "Runner to take decision on Fencer Fueno")
+                             (continue-ability
+                               state side
+                               {:prompt (msg "Pay 1 [Credits] or trash Fencer Fueno?")
+                                :choices (req (if (can-pay? state :runner nil :credit 1)
+                                                ["Pay 1 [Credits]" "Trash"]
+                                                ["Trash"]))
+                                :player :runner
+                                :effect (req (if (= target "Trash")
+                                               (do
+                                                 (trash state :runner card)
+                                                 (system-msg state :runner "trashes Fencer Fueno"))
+                                               (do
+                                                 (pay state :runner card :credit 1)
+                                                 (system-msg state :runner "pays 1 [Credits] to avoid trashing Fencer Fueno")))
+                                             (clear-wait-prompt state :corp))}
+                               card nil))}}
+      :abilities [{:req (req (and (pos? (credit-count state card))
+                                  (:successful run)))
+                   :msg "take 1 [Credits]"
+                   :effect (effect (add-counter card :credit -1)
+                                   (gain-run-credits 1))}]})
+
    "Fester"
    {:events {:purge {:msg "force the Corp to lose 2 [Credits] if able"
                      :effect (effect (pay :corp card :credit 2))}}}
