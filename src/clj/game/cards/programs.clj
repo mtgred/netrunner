d47a51caf6b2ba7c61c1b5e09d48f52ecd17e692
Karl Erik Holter <karl0erik@gmail.com>
2019-02-27 21:03:44 +0100

    Update programs.clj
    Sorry for sneaking in this reorder, but the cards were unordered!!

--- src/clj/game/cards/programs.clj
+++ src/clj/game/cards/programs.clj
@@ -139,7 +139,38 @@
    "Collective Consciousness"
    {:events {:rez {:req (req (ice? target)) :msg "draw 1 card"
                    :effect (effect (draw :runner))}}}
-
+   
+   "Consume"
+   {:events {:runner-trash {:async true
+                            :req (req (some #(card-is? % :side :corp) targets))
+                            :effect (req (let [amt-trashed (count (filter #(card-is? % :side :corp) targets))
+                                               sing-ab {:optional {:prompt "Place a virus counter on Consume?"
+                                                                   :autoresolve (autoresolve-lookup :auto-accept)
+                                                                   :yes-ability {:effect (effect (add-counter :runner card :virus 1))
+                                                                                 :msg "place 1 virus counter on Consume"}}}
+                                               mult-ab {:prompt "Place virus counters on Consume?"
+                                                        :choices {:number (req amt-trashed)
+                                                                  :default (req amt-trashed)}
+                                                        :msg (msg "place " (quantify target "virus counter") " on Consume")
+                                                        :effect (effect (add-counter :runner card :virus target))}
+                                               ab (if (= 1 amt-trashed) sing-ab mult-ab)]
+                                           (continue-ability state side ab card targets)))}}
+    :abilities [{:req (req (pos? (get-virus-counters state card)))
+                 :cost [:click 1]
+                 :label "Gain 2 [Credits] for each hosted virus counter, then remove all virus counters."
+                 :effect (req (gain-credits state side (* 2 (get-virus-counters state card)))
+                              (update! state side (assoc-in card [:counter :virus] 0))
+                              (when-let [hiveminds (filter #(= "Hivemind" (:title %)) (all-active-installed state :runner))]
+                                        (doseq [h hiveminds]
+                                               (update! state side (assoc-in h [:counter :virus] 0)))))
+                 :msg (msg (let [local-virus (get-counters card :virus)
+                                 global-virus (get-virus-counters state card)
+                                 hivemind-virus (- global-virus local-virus)]
+                             (str "gain " (* 2 global-virus) " [Credits], removing " (quantify local-virus "virus counter") " from Consume"
+                             (when (pos? hivemind-virus)
+                                   (str " (and " hivemind-virus " from Hivemind)")))))}
+                {:effect (autoresolve-toggler :auto-accept "adding virus counters")}]}
+   
    "Copycat"
    {:abilities [{:req (req (and (:run @state)
                                 (:rezzed current-ice)))
@@ -191,37 +222,6 @@
                    :msg (msg "install " (:title target))
                    :effect (req (when (can-pay? state side nil :credit (:cost target))
                                   (runner-install state side target)))}]})
-   "Consume"
-   {:events {:runner-trash {:async true
-                            :req (req (some #(card-is? % :side :corp) targets))
-                            :effect (req (let [amt-trashed (count (filter #(card-is? % :side :corp) targets))
-                                               sing-ab {:optional {:prompt "Place a virus counter on Consume?"
-                                                                   :autoresolve (autoresolve-lookup :auto-accept)
-                                                                   :yes-ability {:effect (effect (add-counter :runner card :virus 1))
-                                                                                 :msg "place 1 virus counter on Consume"}}}
-                                               mult-ab {:prompt "Place virus counters on Consume?"
-                                                        :choices {:number (req amt-trashed)
-                                                                  :default (req amt-trashed)}
-                                                        :msg (msg "place " (quantify target "virus counter") " on Consume")
-                                                        :effect (effect (add-counter :runner card :virus target))}
-                                               ab (if (= 1 amt-trashed) sing-ab mult-ab)]
-                                           (continue-ability state side ab card targets)))}}
-    :effect (effect (toast "Tip: You can toggle automatically adding virus counters by clicking Consume."))
-    :abilities [{:req (req (pos? (get-virus-counters state card)))
-                 :cost [:click 1]
-                 :label "Gain 2 [Credits] for each hosted virus counter, then remove all virus counters."
-                 :effect (req (gain-credits state side (* 2 (get-virus-counters state card)))
-                              (update! state side (assoc-in card [:counter :virus] 0))
-                              (when-let [hiveminds (filter #(= "Hivemind" (:title %)) (all-active-installed state :runner))]
-                                        (doseq [h hiveminds]
-                                               (update! state side (assoc-in h [:counter :virus] 0)))))
-                 :msg (msg (let [local-virus (get-counters card :virus)
-                                 global-virus (get-virus-counters state card)
-                                 hivemind-virus (- global-virus local-virus)]
-                             (str "gain " (* 2 global-virus) " [Credits], removing " (quantify local-virus "virus counter") " from Consume"
-                             (when (pos? hivemind-virus)
-                                   (str " (and " hivemind-virus " from Hivemind)")))))}
-                {:effect (autoresolve-toggler :auto-accept "adding virus counters")}]}
 
    "D4v1d"
    {:implementation "Does not check that ICE strength is 5 or greater"
