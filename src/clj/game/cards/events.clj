58dcb40e8bc8d2963b218f6ba9a49d9157a619ad
Karl Erik Holter <karl0erik@gmail.com>
2019-03-04 09:25:12 +0100

    Corrected indentation of Compile

--- src/clj/game/cards/events.clj
+++ src/clj/game/cards/events.clj
@@ -267,25 +267,25 @@
     :abilities [{:label "Install a program using Compile"
                  :async true
                  :effect (effect (resolve-ability
-                                   {:prompt "Install a program from Stack or Heap?"
-                                    :choices ["Stack" "Heap"]
-                                    :msg (msg "install " (:title target))
-                                    :effect (effect (resolve-ability
-                                                      (let [chosen-source target]
-                                                        {:prompt (str "Choose a program in your " chosen-source " to install")
-                                                         :choices (req (cancellable (filter #(is-type? % "Program")
-                                                                                            ((if (= chosen-source "Heap") :discard :deck) runner))))
-                                                         :effect (req (runner-install state side (assoc-in target [:special :compile-installed] true) {:ignore-all-cost true})
-                                                                      (when (= chosen-source "Stack")
-                                                                        (shuffle! state :runner :deck)))})
-                                                      card nil))}
-                                   card nil))}]
+                                  {:prompt "Install a program from Stack or Heap?"
+                                   :choices ["Stack" "Heap"]
+                                   :msg (msg "install " (:title target))
+                                   :effect (effect (resolve-ability
+                                                    (let [chosen-source target]
+                                                      {:prompt (str "Choose a program in your " chosen-source " to install")
+                                                       :choices (req (cancellable (filter #(is-type? % "Program")
+                                                                                          ((if (= chosen-source "Heap") :discard :deck) runner))))
+                                                       :effect (req (runner-install state side (assoc-in target [:special :compile-installed] true) {:ignore-all-cost true})
+                                                                    (when (= chosen-source "Stack")
+                                                                      (shuffle! state :runner :deck)))})
+                                                    card nil))}
+                                  card nil))}]
     :effect (effect (make-run target nil card)
                     (prompt! card (str "Click Compile in the Temporary Zone to install a Program") ["OK"] {})
                     (resolve-ability
                      {:effect (req (let [c (move state side (last (:discard runner)) :play-area)]
-                                         (card-init state side c {:resolve-effect false})))}
-                      card nil))
+                                     (card-init state side c {:resolve-effect false})))}
+                     card nil))
     :events {:run-ends {:effect (req
                                  (let [compile-installed (first (filter #(get-in % [:special :compile-installed]) (game.core/all-installed state :runner)))]
                                    (when (not (empty? compile-installed))
