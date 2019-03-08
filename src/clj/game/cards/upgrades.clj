aaef9639537a9e06c5e81d8ec91c2681d70abad0
Noah <nbtheduke@gmail.com>
2019-03-08 19:45:02 +0100

    Update src/clj/game/cards/upgrades.clj
    Co-Authored-By: presheaf <karl0erik@gmail.com>

--- src/clj/game/cards/upgrades.clj
+++ src/clj/game/cards/upgrades.clj
@@ -682,7 +682,7 @@
               :effect (req (if (= target "Take 1 tag")
                              (do (gain-tags state :runner 1)
                                  (system-msg state :corp (str "uses K. P. Lynn. Runner chooses to take 1 tag")))
-                             (do (end-run state side (make-eid state) card)
+                             (do (end-run state side eid card)
                                  (system-msg state :corp (str "uses K. P. Lynn. Runner chooses to end the run")))))}]
      {:events {:pass-ice {:req (req (and this-server (= (:position run) 1))) ; trigger when last ice passed
                           :async true
