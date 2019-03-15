dca1d9792a61e1f5878731ce971e3a312859cfd9
Noah Bogart <noah.bogart@ebth.com>
2019-03-15 17:24:30 -0400

    Change Cortez Chip to use new rez-additional-cost-bonus

--- src/clj/game/cards/hardware.clj
+++ src/clj/game/cards/hardware.clj
@@ -220,14 +220,12 @@
    "Cortez Chip"
    {:abilities [{:prompt "Select a piece of ICE"
                  :choices {:req ice?}
-                 :effect (req (let [ice target]
-                                (update! state side (assoc card :cortez-target ice))
-                                (trash state side (get-card state card) {:cause :ability-cost})
-                                (system-msg state side
-                                            (str "trashes Cortez Chip to increase the rez cost of " (card-str state ice)
-                                                 " by 2 [Credits] until the end of the turn"))))}]
+                 :msg (msg "trashes Cortez Chip to increase the rez cost of " (card-str state target)
+                           " by 2 [Credits] until the end of the turn")
+                 :effect (effect (update! (assoc card :cortez-target target))
+                                 (trash (get-card state card) {:cause :ability-cost}))}]
     :trash-effect {:effect (effect (register-events {:pre-rez {:req (req (= (:cid target) (:cid (:cortez-target card))))
-                                                               :effect (effect (rez-cost-bonus 2))}
+                                                               :effect (effect (rez-additional-cost-bonus [:credit 2]))}
                                                      :runner-turn-ends {:effect (effect (unregister-events card))}
                                                      :corp-turn-ends {:effect (effect (unregister-events card))}}
                                                     (get-card state card)))}
