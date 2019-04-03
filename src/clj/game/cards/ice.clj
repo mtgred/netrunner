994a8d5e5c62b31af07ab3d71316cbd4c40708e9
Noah Bogart <noah.bogart@ebth.com>
2019-04-03 12:40:27 -0400

    Fix Data Loop to let Corp use ability too

--- src/clj/game/cards/ice.clj
+++ src/clj/game/cards/ice.clj
@@ -746,20 +746,22 @@
                                             (effect-completed state side eid)))))})]})
 
    "Data Loop"
-   {:implementation "Encounter effect is manual"
-    :subroutines [end-the-run-if-tagged
-                  end-the-run]
-    :runner-abilities [{:label "Add 2 cards from your Grip to the top of the Stack"
-                        :req (req (pos? (count (:hand runner))))
-                        :effect (req (let [n (min 2 (count (:hand runner)))]
-                                       (resolve-ability state side
-                                                        {:prompt (msg "Choose " n " cards in your Grip to add to the top of the Stack (first card targeted will be topmost)")
-                                                         :choices {:max n :all true
-                                                                   :req #(and (in-hand? %) (= (:side %) "Runner"))}
-                                                         :effect (req (doseq [c targets]
-                                                                        (move state :runner c :deck {:front true}))
-                                                                      (system-msg state :runner (str "adds " n " cards from their Grip to the top of the Stack")))}
-                                                        card nil)))}]}
+   (let [ability {:label "Add 2 cards from your Grip to the top of the Stack"
+                  :req (req (pos? (count (:hand runner))))
+                  :effect (req (let [n (min 2 (count (:hand runner)))]
+                                 (resolve-ability state side
+                                                  {:prompt (msg "Choose " n " cards in your Grip to add to the top of the Stack (first card targeted will be topmost)")
+                                                   :choices {:max n :all true
+                                                             :req #(and (in-hand? %) (= (:side %) "Runner"))}
+                                                   :effect (req (doseq [c targets]
+                                                                  (move state :runner c :deck {:front true}))
+                                                                (system-msg state :runner (str "adds " n " cards from their Grip to the top of the Stack")))}
+                                                  card nil)))}]
+     {:implementation "Encounter effect is manual"
+      :subroutines [end-the-run-if-tagged
+                    end-the-run]
+      :abilities [ability]
+      :runner-abilities [ability]})
 
    "Data Mine"
    {:subroutines [{:msg "do 1 net damage"
