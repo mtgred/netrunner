00fa9670fd08322603823d076f70779527d2242b
Noah Bogart <noah.bogart@ebth.com>
2019-04-08 18:02:30 -0400

    Shore up the test

--- test/clj/game_test/engine/init_game.clj
+++ test/clj/game_test/engine/init_game.clj
@@ -6,7 +6,24 @@
             [clojure.test :refer :all]))
 
 (deftest default-identity
-  (do-game
-    (new-game)
-    (is (= "The Masque: Cyber General" (get-in @state [:runner :identity :title])))
-    (is (= "The Shadow: Pulling the String" (get-in @state [:corp :identity :title])))))
+  (testing "Both are chosen"
+    (do-game
+      (new-game {:corp {:id "Jinteki: Personal Evolution"}
+                 :runner {:id "Khan: Savvy Skiptracer"}})
+      (is (= "Jinteki: Personal Evolution" (get-in @state [:corp :identity :title])))
+      (is (= "Khan: Savvy Skiptracer" (get-in @state [:runner :identity :title])))))
+  (testing "Only the Corp ID is chosen"
+    (do-game
+      (new-game {:corp {:id "Jinteki: Personal Evolution"}})
+      (is (= "Jinteki: Personal Evolution" (get-in @state [:corp :identity :title])))
+      (is (= "The Masque: Cyber General" (get-in @state [:runner :identity :title])))))
+  (testing "Only the Runner ID is chosen"
+    (do-game
+      (new-game {:runner {:id "Khan: Savvy Skiptracer"}})
+      (is (= "The Shadow: Pulling the String" (get-in @state [:corp :identity :title])))
+      (is (= "Khan: Savvy Skiptracer" (get-in @state [:runner :identity :title])))))
+  (testing "Both are not chosen"
+    (do-game
+      (new-game)
+      (is (= "The Shadow: Pulling the String" (get-in @state [:corp :identity :title])))
+      (is (= "The Masque: Cyber General" (get-in @state [:runner :identity :title]))))))
