6fef990377d5af0a95bb60cfebeb6ba42f2884c1
Oguz Han Asnaz <github@lostgeek.de>
2019-06-04 20:40:51 +0200

    Swapped The Root with Off the Grid to avoid pay-credits prompt

--- test/clj/game_test/cards/upgrades.clj
+++ test/clj/game_test/cards/upgrades.clj
@@ -350,18 +350,18 @@
       (is (not (:run @state)) "Bio Vault fires with 2 advancement tokens")
       (is (= 1 (count (:discard (get-corp)))) "Bio Vault trashed"))))
 
-(deftest breaker-bay-grid
+(deftest ^:test-refresh/focus breaker-bay-grid
   ;; Breaker Bay Grid - Reduce rez cost of other cards in this server by 5 credits
   (do-game
-    (new-game {:corp {:deck [(qty "Breaker Bay Grid" 2) "The Root" "Strongbox"]}})
+    (new-game {:corp {:deck [(qty "Breaker Bay Grid" 2) "Off the Grid" "Strongbox"]}})
     (core/gain state :corp :click 1)
     (play-from-hand state :corp "Breaker Bay Grid" "New remote")
-    (play-from-hand state :corp "The Root" "Server 1")
+    (play-from-hand state :corp "Off the Grid" "Server 1")
     (let [bbg1 (get-content state :remote1 0)
-          root (get-content state :remote1 1)]
+          otg (get-content state :remote1 1)]
       (core/rez state :corp bbg1)
-      (core/rez state :corp root)
-      (is (= 4 (:credit (get-corp))) "Paid only 1 to rez The Root")
+      (core/rez state :corp otg)
+      (is (= 4 (:credit (get-corp))) "Paid only 1 to rez Off the Grid")
       (play-from-hand state :corp "Breaker Bay Grid" "R&D")
       (play-from-hand state :corp "Strongbox" "R&D")
       (let [bbg2 (get-content state :rd 0)
