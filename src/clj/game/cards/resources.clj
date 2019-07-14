3951acd01b982d104a985eafbdc384dd2bc69444
Oguz Han Asnaz <github@lostgeek.de>
2019-07-14 16:17:12 +0200

    Added comments to Taka and Fueno

--- src/clj/game/cards/resources.clj
+++ src/clj/game/cards/resources.clj
@@ -939,8 +939,10 @@
    "Fencer Fueno"
    (assoc
      (companion-builder
+       ;; companion-builder: ability-req
        (req (and (pos? (get-counters (get-card state card) :credit))
                  (:successful run)))
+       ;; companion-builder: turn-ends-effect
        (effect (show-wait-prompt :corp "Runner to take decision on Fencer Fueno")
                (continue-ability
                  {:prompt "Pay 1 [Credits] or trash Fencer Fueno?"
@@ -957,12 +959,12 @@
                                    (system-msg state :runner "pays 1 [Credits] to avoid trashing Fencer Fueno")))
                                (clear-wait-prompt state :corp))}
                  card nil))
+       ;; companion-builder: ability
        {:msg "take 1 [Credits]"
         :effect (effect (add-counter card :credit -1)
-                        (gain-run-credits 1))})
-     :interactions {:pay-credits {:req (req (:successful run))
-                                  :type :credit}})
-
+                        (gain-run-credits 1))}
+       :interactions {:pay-credits {:req (req (:successful run))
+                                    :type :credit}}))
 
    "Fester"
    {:events {:purge {:msg "force the Corp to lose 2 [Credits] if able"
@@ -2400,10 +2402,12 @@
    "Trickster Taka"
    (assoc
      (companion-builder
+       ;; companion-builder: ability-req
        (req (and (pos? (get-counters (get-card state card) :credit))
                  run
                  (not (:successful run))
                  (not (:unsuccessful run))))
+       ;; companion-builder: turn-ends-effect
        (effect (show-wait-prompt :corp "Runner to take decision on Trickster Taka")
                (continue-ability
                  {:prompt "Take 1 tag or trash Trickster Taka?"
@@ -2420,10 +2424,12 @@
                                    (system-msg state :runner "takes 1 tag to avoid trashing Trickster Taka")
                                    (gain-tags state :runner eid 1))))}
                  card nil))
+       ;; companion-builder: ability
        {:msg "take 1 [Credits]"
         :effect (effect (add-counter card :credit -1)
                         (trigger-event :spent-stealth-credit card)
                         (gain-credits 1))})
+     ;; assoc: arguments
      :interactions {:pay-credits {:req (req (and (= :ability (:source-type eid))
                                                  (program? target)
                                                  run))
