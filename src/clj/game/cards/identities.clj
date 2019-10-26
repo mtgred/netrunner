7c6137689908a6a9b63aca45f6e3b284572598a6
Oguz Han Asnaz <github@lostgeek.de>
2019-10-26 10:13:33 +0200

    Quetzal uses new :ignore-strength flag in break-sub

--- src/clj/game/cards/identities.clj
+++ src/clj/game/cards/identities.clj
@@ -1088,7 +1088,7 @@
               :effect (effect (gain-credits :corp 1))}]}
 
    "Quetzal: Free Spirit"
-   {:abilities [(assoc (break-sub nil 1 "Barrier" {:req (req true)}) :once :per-turn)]}
+   {:abilities [(assoc (break-sub nil 1 "Barrier" {:ignore-strength true}) :once :per-turn)]}
 
    "Reina Roja: Freedom Fighter"
    (letfn [(not-triggered? [state card] (not (get-in @state [:per-turn (:cid card)])))
