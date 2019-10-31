9d561f930858c0ebac20baf91c7a9694acecee80
Oguz Han Asnaz <github@lostgeek.de>
2019-10-31 16:29:40 +0100

    Remove event hooks correctly on derez

--- test/clj/game_test/cards/assets.clj
+++ test/clj/game_test/cards/assets.clj
@@ -1299,24 +1299,40 @@
 
 (deftest estelle-moon
   ;; Estelle Moon
-  (letfn [(estelle-test [number]
-            (do-game
-              (new-game {:corp {:deck ["Estelle Moon" (qty "Encryption Protocol" 20)]}})
-              (starting-hand state :corp (repeat 9 "Encryption Protocol"))
-              (core/move state :corp (find-card "Estelle Moon" (:deck (get-corp))) :hand)
-              (play-from-hand state :corp "Estelle Moon" "New remote")
-              (let [em (get-content state :remote1 0)]
-                (core/rez state :corp (refresh em))
-                (core/gain state :corp :click 10)
-                (dotimes [_ number]
-                  (play-from-hand state :corp "Encryption Protocol" "New remote"))
-                (let [credits (:credit (get-corp))
-                      hand (count (:hand (get-corp)))]
-                  (card-ability state :corp (refresh em) 0)
-                  (is (= (* 2 number) (- (:credit (get-corp)) credits)) (str "Should gain " (* 2 number) " credits"))
-                  (is (= number (- (count (:hand (get-corp))) hand)) (str "Should draw " number " cards"))
-                  (is (= 1 (-> (get-corp) :discard count)) "Estelle Moon should be trashed")))))]
-    (doall (map estelle-test (range 10)))))
+  (testing "Basic test"
+    (letfn [(estelle-test [number]
+              (do-game
+                (new-game {:corp {:deck ["Estelle Moon" (qty "Encryption Protocol" 20)]}})
+                (starting-hand state :corp (repeat 9 "Encryption Protocol"))
+                (core/move state :corp (find-card "Estelle Moon" (:deck (get-corp))) :hand)
+                (play-from-hand state :corp "Estelle Moon" "New remote")
+                (let [em (get-content state :remote1 0)]
+                  (core/rez state :corp (refresh em))
+                  (core/gain state :corp :click 10)
+                  (dotimes [_ number]
+                    (play-from-hand state :corp "Encryption Protocol" "New remote"))
+                  (let [credits (:credit (get-corp))
+                        hand (count (:hand (get-corp)))]
+                    (card-ability state :corp (refresh em) 0)
+                    (is (= (* 2 number) (- (:credit (get-corp)) credits)) (str "Should gain " (* 2 number) " credits"))
+                    (is (= number (- (count (:hand (get-corp))) hand)) (str "Should draw " number " cards"))
+                    (is (= 1 (-> (get-corp) :discard count)) "Estelle Moon should be trashed")))))]
+      (doall (map estelle-test (range 10)))))
+  (testing "Estelle Moon triggers multiple time after derez #4601"
+    (do-game
+      (new-game {:corp {:hand ["Estelle Moon" "Divert Power" (qty "Encryption Protocol" 3)]
+                        :deck [(qty "Encryption Protocol" 20)]}})
+      (core/gain state :corp :click 1)
+      (play-from-hand state :corp "Estelle Moon" "New remote")
+      (let [em (get-content state :remote1 0)]
+        (core/rez state :corp (refresh em))
+        (play-from-hand state :corp "Encryption Protocol" "New remote")
+        (is (= 1 (get-counters (refresh em) :power)) "Moon has one power token")
+        (play-from-hand state :corp "Divert Power")
+        (click-card state :corp (refresh em)) ;derez moon
+        (click-card state :corp (refresh em)) ;rez again
+        (play-from-hand state :corp "Encryption Protocol" "New remote")
+        (is (= 2 (get-counters (refresh em) :power)) "Moon has two power tokens")))))
 
 (deftest eve-campaign
   ;; Eve Campaign
