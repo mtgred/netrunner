9c766f478a81313ae71404104f6fcf5b862440c3
Noah <nbtheduke@gmail.com>
2019-12-06 00:00:41 -0500

    Fix card comparison functions, make Overseer Matrix work better

--- src/clj/game/core/misc.clj
+++ src/clj/game/core/misc.clj
@@ -79,32 +79,40 @@
 (defn same-server?
   "True if the two cards are IN or PROTECTING the same server."
   [card1 card2]
-  (let [zone1 (get-nested-zone card1)
-        zone2 (get-nested-zone card2)]
-    (= (second zone1) (second zone2))))
+  (and card1
+       card2
+       (let [zone1 (get-nested-zone card1)
+             zone2 (get-nested-zone card2)]
+         (= (second zone1) (second zone2)))))
 
 (defn protecting-same-server?
   "True if an ice is protecting the server that the card is in or protecting."
   [card ice]
-  (let [zone1 (get-nested-zone card)
-        zone2 (get-nested-zone ice)]
-    (and (= (second zone1) (second zone2))
-         (= :ices (last zone2)))))
+  (and card
+       ice
+       (let [zone1 (get-nested-zone card)
+             zone2 (get-nested-zone ice)]
+         (and (= (second zone1) (second zone2))
+              (= :ices (last zone2))))))
 
 (defn in-same-server?
   "True if the two cards are installed IN the same server, or hosted on cards IN the same server."
   [card1 card2]
   (let [zone1 (get-nested-zone card1)
         zone2 (get-nested-zone card2)]
-    (and (= zone1 zone2)
+    (and card1
+         card2
+         (= zone1 zone2)
          (is-remote? (second zone1)) ; cards in centrals are in the server's root, not in the server.
          (= :content (last zone1)))))
 
 (defn from-same-server?
   "True if the upgrade is in the root of the server that the target is in."
   [upgrade target]
-  (= (central->zone (:zone target))
-     (butlast (get-nested-zone upgrade))))
+  (and upgrade
+       target
+       (= (central->zone (:zone target))
+          (butlast (get-nested-zone upgrade)))))
 
 (defn all-installed
   "Returns a vector of all installed cards for the given side, including those hosted on other cards,
