1d9ca4f49f33733475af4709a598594964249a95
kubik <jakub.juroska@centrum.cz>
2019-12-22 12:11:19 +0100

    after merge conflict fix

--- src/clj/game/cards/agendas.clj
+++ src/clj/game/cards/agendas.clj
@@ -1103,8 +1103,9 @@
          :msg (msg "add 4 agenda counters on " (:title card))
          :effect (effect (add-counter (get-card state card) :agenda 4)
                          (update! (assoc-in (get-card state card) [:special :vacheron] true)))}]
-    {:agendapoints-runner (req (if (and (get-in card [:special :vacheron])
-                                        (zero? (get-counters card :agenda))) 3 0))
+    {:agendapoints-runner (req (if (or (= (first (:previous-zone card)) :discard)
+                                       (and (get-in card [:special :vacheron])
+                                            (zero? (get-counters card :agenda)))) 3 0))
      :stolen vacheron-ability
      :events [(assoc vacheron-ability :event :as-agenda)
               {:event :runner-turn-begins
