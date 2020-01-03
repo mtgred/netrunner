22a5037534a58fe179dbaa7218972da7e7da0e2a
kubik <jakub.juroska@centrum.cz>
2020-01-03 20:50:14 +0100

    Sensor net activation fix

--- src/clj/game/cards/agendas.clj
+++ src/clj/game/cards/agendas.clj
@@ -1401,6 +1401,7 @@
    :silent (req true)
    :abilities [{:cost [:agenda 1]
                 :req (req (some #(and (has-subtype? % "Bioroid") (not (rezzed? %))) (all-installed state :corp)))
+                :label "Choose a bioroid to rez, ignoring all costs"
                 :prompt "Choose a bioroid to rez, ignoring all costs"
                 :choices {:card #(and (has-subtype? % "Bioroid")
                                       (not (rezzed? %)))}
