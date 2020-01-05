e1836d6a5e7d24d4ca3e756f7cec5f78551c5f46
Noah <nbtheduke@gmail.com>
2020-01-04 20:41:27 -0500

    Fix The Maker's Eye #4864
    Co-authored-by: kubik <jakub.juroska@centrum.cz>

--- src/clj/game/cards/events.clj
+++ src/clj/game/cards/events.clj
@@ -2667,6 +2667,7 @@
    :async true
    :effect (effect (make-run eid :rd nil card))
    :events [{:event :successful-run
+             :unregister-once-resolved true
              :silent (req true)
              :req (req (= target :rd))
              :effect (effect (access-bonus :rd 2))}]})
