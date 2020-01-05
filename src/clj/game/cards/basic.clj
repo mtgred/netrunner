f8d4c682e1e4b6c3f428be3ad485d4d91566e7ce
Oguz Han Asnaz <github@lostgeek.de>
2020-01-05 10:27:53 +0100

    Fixed eids

--- src/clj/game/cards/basic.clj
+++ src/clj/game/cards/basic.clj
@@ -28,21 +28,21 @@
                 :async true
                 :effect (req (wait-for (trigger-event-simult state side (make-eid state eid) :pre-corp-click-draw nil nil)
                                        (trigger-event state side :corp-click-draw (->> @state side :deck (take 1)))
-                                       (draw state side)
                                        (swap! state update-in [:stats side :click :draw] (fnil inc 0))
-                                       (play-sfx state side "click-card")))}
+                                       (play-sfx state side "click-card")
+                                       (draw state side eid 1 nil)))}
                {:label "Install 1 agenda, asset, upgrade, or piece of ice from HQ"
                 :async true
                 :req (req (not-empty (:hand corp)))
                 :effect (req (let [target-card (first targets)
                                    server (second targets)]
-                               (corp-install state side (make-eid state {:source server :source-type :corp-install})
+                               (corp-install state side (assoc eid :source server :source-type :corp-install)
                                              target-card server {:base-cost [:click 1] :action :corp-click-install})))}
                {:label "Play 1 operation"
                 :async true
                 :req (req (not-empty (:hand corp)))
                 :effect (req (let [target-card (first targets)]
-                               (play-instant state side (make-eid state {:source :action :source-type :play})
+                               (play-instant state side (assoc eid :source :action :source-type :play)
                                              target-card {:base-cost [:click 1]})))}
                {:label "Advance 1 installed card"
                 :cost [:click 1 :credit 1]
@@ -54,6 +54,7 @@
                              (play-sfx state side "click-advance"))}
                {:label "Trash 1 resource if the Runner is tagged"
                 :cost [:click 1 :credit 2]
+                :async true
                 :req (req tagged)
                 :prompt "Choose a resource to trash"
                 :msg (msg "trash " (:title target))
@@ -61,7 +62,7 @@
                                              (> (count (filter resource? (all-active-installed state :runner))) 1))
                                       (and (resource? target) (not (untrashable-while-resources? target)))
                                       (resource? target)))}
-                :effect (effect (trash target))}
+                :effect (effect (trash eid target nil))}
                {:label "Purge virus counters"
                 :cost [:click 3]
                 :msg "purge all virus counters"
@@ -81,24 +82,24 @@
                 :msg "draw 1 card"
                 :effect (req (wait-for (trigger-event-simult state side (make-eid state eid) :pre-runner-click-draw nil nil)
                                        (trigger-event state side :runner-click-draw (->> @state side :deck (take 1)))
-                                       (draw state side)
                                        (swap! state update-in [:stats side :click :draw] (fnil inc 0))
-                                       (play-sfx state side "click-card")))}
+                                       (play-sfx state side "click-card")
+                                       (draw state side eid 1 nil)))}
                {:label "Install 1 program, resource, or piece of hardware from the grip"
                 :async true
                 :req (req (not-empty (:hand runner)))
                 :effect (req (let [target-card (first targets)]
-                               (runner-install state side (make-eid state {:source :action :source-type :runner-install})
+                               (runner-install state side (assoc eid :source :action :source-type :runner-install)
                                                target-card {:base-cost [:click 1]})))}
                {:label "Play 1 event"
                 :async true
                 :req (req (not-empty (:hand runner)))
                 :effect (req (let [target-card (first targets)]
-                               (play-instant state side (make-eid state {:source :action :source-type :play})
+                               (play-instant state side (assoc eid :source :action :source-type :play)
                                              target-card {:base-cost [:click 1]})))}
                {:label "Run any server"
                 :async true
-                :effect (effect (make-run (make-eid state) target nil nil {:click-run true}))}
+                :effect (effect (make-run eid target nil nil {:click-run true}))}
                {:label "Remove 1 tag"
                 :cost [:click 1 :credit 2]
                 :msg "remove 1 tag"
