e52a34c49bc6a7c192df753335bc4be5d56d1818
Oguz Han Asnaz <github@lostgeek.de>
2020-01-05 12:44:08 +0100

    Stargate message now includes position of ambiguous choices

--- test/clj/game/cards/programs_test.clj
+++ test/clj/game/cards/programs_test.clj
@@ -3634,7 +3634,24 @@
      (is (= "Troll" (-> (get-corp) :discard first :title)) "Troll was trashed")
      (is (= "Herald" (-> (get-corp) :deck first :title)) "Herald now on top of R&D")
      (card-ability state :runner (get-program state 0) 0)
-     (is (not (:run @state)) "No run ended, as Stargate is once per turn"))))
+     (is (not (:run @state)) "No run ended, as Stargate is once per turn")))
+  (testing "Different message on repeating cards"
+    (do-game
+     (new-game {:corp {:deck [(qty "Troll" 10)]
+                       :hand ["Herald" "Troll"]}
+                :runner {:deck ["Stargate"]}})
+     (core/move state :corp (find-card "Herald" (:hand (get-corp))) :deck {:front true})
+     (core/move state :corp (find-card "Troll" (:hand (get-corp))) :deck {:front true})
+     (is (= "Troll" (-> (get-corp) :deck first :title)) "Troll on top of deck")
+     (is (= "Herald" (-> (get-corp) :deck second :title)) "Herald 2nd")
+     (is (= "Troll" (-> (get-corp) :deck (#(nth % 2)) :title)) "Another Troll 3rd")
+     (take-credits state :corp)
+     (play-from-hand state :runner "Stargate")
+     (card-ability state :runner (get-program state 0) 0)
+     (run-continue state)
+     (run-successful state)
+     (click-prompt state :runner (-> (get-runner) :prompt first :choices first))
+     (is (last-log-contains? state "Runner uses Stargate to trash top Troll.") "Correct log"))))
 
 (deftest study-guide
   ;; Study Guide - 2c to add a power counter; +1 strength per counter
