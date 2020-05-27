a17b0795d35cc97df76f453a3f1827eb40021b98
Oguz Han Asnaz <github@lostgeek.de>
2020-05-27 10:44:54 +0200

    Split up Paule message into multiple parts.

--- src/clj/game/cards/resources.clj
+++ src/clj/game/cards/resources.clj
@@ -1889,13 +1889,10 @@
                                               (:uniqueness %)))
                                 count
                                 -)
-                           0))
-                       (edit-spend-msg [msg] ; adds 1 to the credit cost message
-                         (let [credit-msg (re-find #"\d+ \[Credits\]" msg)
-                               cost (Integer/parseInt (first (split credit-msg #" ")))]
-                           (clojure.string/replace msg (re-pattern (str cost #" \[Credits\]")) (str (inc cost) " [Credits]"))))]
+                           0))]
                  {:async true
                   :label "Install hosted card"
+                  :msg "install hosted card"
                   :cost [:credit 1]
                   :req (req (and (seq (:hosted card))
                                  (some #(can-pay? state :runner (assoc eid :source card :source-type :runner-install)
@@ -1911,7 +1908,7 @@
                                  (assoc eid :source card :source-type :runner-install)
                                  target
                                  {:cost-bonus (discount state card)
-                                  :custom-message #(str (build-spend-msg (edit-spend-msg %) "install") (:title target) " using " (:title card))})
+                                  :custom-message #(str (build-spend-msg % "install") (:title target) " using " (:title card))})
                             (swap! state assoc-in [:per-turn (:cid card)] true))})]})
 
 (define-card "Penumbral Toolkit"
