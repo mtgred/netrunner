d12028896256c5b4b3aa657e01beea11b315d763
Oguz Han Asnaz <github@lostgeek.de>
2020-06-02 03:17:58 +0200

    Fixed broken log message with /unique

--- test/clj/game/core/io_test.clj
+++ test/clj/game/core/io_test.clj
@@ -94,4 +94,24 @@
           (changes-val-macro
             1 (:link (get-runner))
             "Link increases by 1"
-            (core/say state :runner {:user user :text "/link 1"})))))))
+            (core/say state :runner {:user user :text "/link 1"}))))))
+
+  (testing "/unique"
+    (let [user {:username "Runner"}]
+      (testing "Works with Wireless Net Pavillion"
+        (do-game
+          (new-game {:runner {:hand [(qty "Wireless Net Pavilion" 2)]}})
+          (take-credits state :corp)
+          (let [wnp1 (nth (:hand (get-runner)) 0)
+                wnp2 (nth (:hand (get-runner)) 1)]
+            (core/play state :runner {:card wnp1})
+            (core/play state :runner {:card wnp2})
+            (is (= 1 (count (:hand (get-runner)))) "Second WNP was not installed")
+            (is (:uniqueness wnp2) "WNP is unique")
+            (core/say state :runner {:user user :text "/unique"})
+            (is (last-log-contains? state "\\[!\\]Runner uses a command: /unique") "Correct message")
+            (click-card state :runner wnp2)
+            (is (not (:uniqueness (refresh wnp2))) "WNP is not unique anymore")
+            (is (last-log-contains? state "Runner uses /unique command to make Wireless Net Pavilion not unique\\.") "Correct message")
+            (core/play state :runner {:card wnp2})
+            (is (zero? (count (:hand (get-runner)))) "Both cards have been installed")))))))
