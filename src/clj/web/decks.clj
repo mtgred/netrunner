70566a0b66bd4355304f9386ce13e98e836b223b
Noah <nbtheduke@gmail.com>
2020-07-21 20:02:22 -0400

    Hash existing decks

--- src/clj/web/decks.clj
+++ src/clj/web/decks.clj
@@ -30,29 +30,36 @@
       (response 200 (mc/insert-and-return db "decks" deck)))
     (response 401 {:message "Unauthorized"})))
 
+(defn update-card
+  [card]
+  (update card :card #(get @all-cards %)))
+
+(defn hash-deck
+  [deck]
+  (let [check-deck (-> deck
+                       (update :cards #(map update-card %))
+                       (update :identity #(get @all-cards (:title %))))
+        id (-> deck :identity :title)
+        sorted-cards (sort-by #(:code (:card %)) (:cards check-deck))
+        decklist (s/join (for [entry sorted-cards]
+                           (str (:qty entry) (:code (:card entry)))))
+        deckstr (str id decklist)
+        salt (byte-array (map byte (:name deck)))]
+    (last (s/split (pbkdf2/encrypt deckstr 100000 "HMAC-SHA1" salt) #"\$"))))
+
 (defn decks-save-handler [{{username :username} :user
                            deck                 :body}]
   (if (and username deck)
-    (let [update-card (fn [c] (update-in c [:card] @all-cards))
-          check-deck (-> deck
-                         (update-in [:cards] #(map update-card %))
-                         (update-in [:identity] #(@all-cards (:title %))))
-          id (-> deck :identity :title)
-          sorted-cards (sort-by #(:code (:card %)) (:cards check-deck))
-          decklist (s/join (for [entry sorted-cards] (str (:qty entry) (:code (:card entry)))))
-          deckstr (str id decklist)
-          salt (byte-array (map byte (:name deck)))
-          hash (last (s/split (pbkdf2/encrypt deckstr 100000 "HMAC-SHA1" salt) #"\$"))
+    (let [check-deck (-> deck
+                         (update :cards #(map update-card %))
+                         (update :identity #(get @all-cards (:title %))))
+          deck-hash (hash-deck deck)
           deck (-> deck
                    (update-in [:cards] (fn [cards] (mapv #(select-keys % [:qty :card :id :art]) cards)))
                    (assoc :username username)
-                   (assoc :hash hash))
+                   (assoc :hash deck-hash))
           status (calculate-deck-status check-deck)
           deck (assoc deck :status status)]
-      (when (nil? (:identity check-deck))
-        (println "NIL IDENTITY WHEN SAVING DECK")
-        (println "Deck:" deck)
-        (println "-----------------------------"))
       (if-let [deck-id (:_id deck)]
         (if (:identity deck)
           (do (mc/update db "decks"
@@ -62,6 +69,20 @@
           (response 409 {:message "Deck is missing identity"}))
         (response 409 {:message "Deck is missing _id"})))
     (response 401 {:message "Unauthorized"})))
+
+(defn hash-existing-decks []
+  (let [decks (mc/find-maps db "decks")]
+    (->> decks
+         (remove :hash)
+         (map #(assoc % :hash (hash-deck %)))
+         (map #(let [deck-id (:_id %)
+                     username (:username %)]
+                 (when deck-id
+                   (mc/update db "decks"
+                              {:_id (object-id deck-id) :username username}
+                              {"$set" (dissoc % :_id)}))))
+         (filter acknowledged?)
+         count)))
 
 (defn decks-delete-handler [{{username :username} :user
                              {id :id}             :params}]
