db06006c62f85062e9142731b6685a438965fee8
Noah <nbtheduke@gmail.com>
2020-09-25 12:29:49 -0400

    Remove unnecessary/unused dependencies

--- src/clj/web/utils.clj
+++ src/clj/web/utils.clj
@@ -1,7 +1,8 @@
 (ns web.utils
   (:require [ring.util.response :as resp]
             [monger.collection :as mc]
-            [web.db :refer [db]]))
+            [web.db :refer [db]])
+  (:import java.security.MessageDigest))
 
 (defn tick
   "Call f with args every ms. First call will be after ms"
@@ -13,3 +14,11 @@
 
 (defn response [status-code msg]
   (resp/status (resp/response msg) status-code))
+
+(defn md5
+  "Taken from here: https://gist.github.com/jizhang/4325757"
+  [^String s]
+  (->> (.getBytes s)
+       (.digest (MessageDigest/getInstance "MD5"))
+       (BigInteger. 1)
+       (format "%032x")))
