77b5ecbaebf61e83a559b75948bd842c582efdb3
osiccha <oliversiccha@gmx.de>
2021-01-19 02:13:53 +0100

    fix :req for :optional maps being outside of associated map

--- src/clj/game/cards/agendas.clj
+++ src/clj/game/cards/agendas.clj
@@ -954,11 +954,11 @@
 
 (defcard "NEXT Wave 2"
   {:not-when-scored true
-   :req (req (some #(and (rezzed? %)
-                         (ice? %)
-                         (has-subtype? % "NEXT"))
-                   (all-installed state :corp)))
    :optional {:prompt "Do 1 brain damage with NEXT Wave 2?"
+              :req (req (some #(and (rezzed? %)
+                                    (ice? %)
+                                    (has-subtype? % "NEXT"))
+                              (all-installed state :corp)))
               :yes-ability {:msg "do 1 brain damage"
                             :effect (effect (damage eid :brain 1 {:card card}))}}})
 
