5c877d41aef815e61dc01a4bec1a7da6c8526488
John Warwick <jwarwick@gmail.com>
2021-02-12 14:40:02 -0500

    Keep deckbuilding state when changing tabs

--- src/cljs/nr/deckbuilder.cljs
+++ src/cljs/nr/deckbuilder.cljs
@@ -964,6 +964,12 @@
             updated-card (add-params-to-card (:card line) id art)]
         [zoom-card-view updated-card s]))]])
 
+(defn- class-for-state [s]
+  (cond
+    (:edit @s) "edit"
+    (:delete @s) "delete"
+    :else ""))
+
 (defn deck-builder
   "Make the deckbuilder view"
   []
@@ -991,7 +997,8 @@
       (when (= "/deckbuilder" (first @active))
         [:div.container
          [:div.deckbuilder.blue-shade.panel
-          [:div.viewport {:ref #(swap! db-dom assoc :viewport %)}
+          [:div.viewport {:ref #(swap! db-dom assoc :viewport %)
+                          :class (class-for-state s)}
            [list-panel s user decks decks-loaded scroll-top]
            [selected-panel s]
            [edit-panel s]]]]))))
