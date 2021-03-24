11e864d298eee36fe5e4519e71b9516508724b6b
merrickbrown <merrickbrown@users.noreply.github.com>
2021-03-23 20:52:15 -0400

    Fixes #5669. Aligned can-pay? usage in make-run with get-runnable-zones.

--- src/clj/game/core/actions.clj
+++ src/clj/game/core/actions.clj
@@ -545,7 +545,7 @@
          permitted-zones (remove (set restricted-zones) (or zones (get-zones state)))]
      (if ignore-costs
        permitted-zones
-       (filter #(can-pay? state :runner eid (total-run-cost state side card {:server (unknown->kw %)}))
+       (filter #(can-pay? state :runner eid card nil (total-run-cost state side card {:server (unknown->kw %)}))
                permitted-zones)))))
 
 (defn generate-runnable-zones
