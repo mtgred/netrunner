eaad3a69681dac0aca22b8896e8499716ead259a
John Warwick <jwarwick@gmail.com>
2021-03-24 20:10:10 -0400

    Remove index list from db.clj

--- src/clj/tasks/db.clj
+++ src/clj/tasks/db.clj
@@ -80,29 +80,6 @@
                          (.printStackTrace e)))
     (finally (webdb/disconnect))))
 
-(def ^:const indexes
-  (let [case-insensitive-index-opts {:collation {:locale "en" :strength (int 2)}}]
-    [["cards" (array-map :code 1)]
-     ["cards" (array-map :previous-versions 1)]
-     ["cards" (array-map :type 1)]
-     ["decks" (array-map :username 1)]
-     ["game-logs" (array-map :gameid 1)]
-     ["game-logs" (array-map "corp.player.username" 1 :start-date -1)]
-     ["game-logs" (array-map "runner.player.username" 1 :start-date -1)]
-     ["messages" (array-map :channel 1 :date -1)]
-     ["messages" (array-map :username 1 :date -1)]
-     ["news" (array-map :date -1)]
-     ["users" (array-map :username 1)]
-     ["users" (array-map :username 1)
-      (assoc case-insensitive-index-opts :name "username_ci_1")]
-     ["users" (array-map :email 1)]
-     ["users" (array-map :email 1)
-      (assoc case-insensitive-index-opts :name "email_ci_1")]
-     ["users" (array-map :isadmin 1)]
-     ["users" (array-map :ismoderator 1)]
-     ["users" (array-map :special 1)]
-     ["users" (array-map :resetPasswordToken 1)]]))
-
 (defn- prepare-sample-decks
   [nrdb-urls]
   (vec
