mtgred/cljs-netrunner

Rearrange command parsing, add /install

src/clj/game/core/def_helpers.clj
@@ -133,6 +133,20 @@
    :effect (effect (system-msg (str "trashes " (:title card)))
                    (trash eid card {:unpreventable true}))})
 
+(defn corp-recur
+  ([] (corp-recur (constantly true)))
+  ([pred]
+   {:label "add card from Archives to HQ"
+    :prompt "Select a card to add to HQ"
+    :show-discard true
+    :choices {:card #(and (corp? %)
+                       (in-discard? %)
+                       (pred %))}
+    :msg (msg "add " (card-str state target {:visible (faceup? target)}) " to HQ")
+    :effect (effect (move :corp target :hand))}))
+
+;; defcard support
+
 (defn trash-or-rfg
   [state _ eid card]
   (let [side (to-keyword (:side card))
@@ -175,16 +189,17 @@
       (update ability :abilities #(conj (into [] %) recurring-ability)))
     ability))
 
+(defn assign-action
+  [ability]
+  (if (= :click (first (:cost ability)))
+    (assoc ability :action true)
+    ability))
+
 (defn make-click-abilities-actions
   [ability]
-  (let [paid-abilities (mapv
-                         (fn [ab]
-                           (if (and (:cost ab)
-                                    (= :click (first (:cost ab))))
-                             (assoc ab :action true)
-                             ab))
-                         (:abilities ability))]
-    (assoc ability :abilities paid-abilities)))
+  (->> (:abilities ability)
+       (mapv assign-action)
+       (assoc ability :abilities)))
 
 (defn add-default-abilities
   [title ability]
@@ -193,24 +208,12 @@
        (make-recurring-ability)
        (make-click-abilities-actions)))
 
-(defn corp-recur
-  ([] (corp-recur (constantly true)))
-  ([pred]
-   {:label "add card from Archives to HQ"
-    :prompt "Select a card to add to HQ"
-    :show-discard true
-    :choices {:card #(and (corp? %)
-                       (in-discard? %)
-                       (pred %))}
-    :msg (msg "add " (card-str state target {:visible (faceup? target)}) " to HQ")
-    :effect (effect (move :corp target :hand))}))
-
 (def card-defs-cache (atom {}))
 
 (defmacro defcard
   [title ability]
   `(defmethod ~'defcard-impl ~title [~'_]
-     (if-let [cached-ability# (get card-defs-cache ~title)]
+     (if-let [cached-ability# (get @card-defs-cache ~title)]
        cached-ability#
        (let [ability# (add-default-abilities ~title ~ability)]
          (swap! card-defs-cache assoc ~title ability#)

mtgred/netrunner

src/clj/game/core/def_helpers.clj
@@ -133,6 +133,20 @@
    :effect (effect (system-msg (str "trashes " (:title card)))
                    (trash eid card {:unpreventable true}))})
 
+(defn corp-recur
+  ([] (corp-recur (constantly true)))
+  ([pred]
+   {:label "add card from Archives to HQ"
+    :prompt "Choose a card to add to HQ"
+    :show-discard true
+    :choices {:card #(and (corp? %)
+                       (in-discard? %)
+                       (pred %))}
+    :msg (msg "add " (card-str state target {:visible (faceup? target)}) " to HQ")
+    :effect (effect (move :corp target :hand))}))
+
+;; defcard support
+
 (defn trash-or-rfg
   [state _ eid card]
   (let [side (to-keyword (:side card))
@@ -175,16 +189,17 @@
       (update ability :abilities #(conj (into [] %) recurring-ability)))
     ability))
 
+(defn assign-action
+  [ability]
+  (if (= :click (first (:cost ability)))
+    (assoc ability :action true)
+    ability))
+
 (defn make-click-abilities-actions
   [ability]
-  (let [paid-abilities (mapv
-                         (fn [ab]
-                           (if (and (:cost ab)
-                                    (= :click (first (:cost ab))))
-                             (assoc ab :action true)
-                             ab))
-                         (:abilities ability))]
-    (assoc ability :abilities paid-abilities)))
+  (->> (:abilities ability)
+       (mapv assign-action)
+       (assoc ability :abilities)))
 
 (defn add-default-abilities
   [title ability]
@@ -193,24 +208,12 @@
        (make-recurring-ability)
        (make-click-abilities-actions)))
 
-(defn corp-recur
-  ([] (corp-recur (constantly true)))
-  ([pred]
-   {:label "add card from Archives to HQ"
-    :prompt "Choose a card to add to HQ"
-    :show-discard true
-    :choices {:card #(and (corp? %)
-                       (in-discard? %)
-                       (pred %))}
-    :msg (msg "add " (card-str state target {:visible (faceup? target)}) " to HQ")
-    :effect (effect (move :corp target :hand))}))
-
 (def card-defs-cache (atom {}))
 
 (defmacro defcard
   [title ability]
   `(defmethod ~'defcard-impl ~title [~'_]
-     (if-let [cached-ability# (get card-defs-cache ~title)]
+     (if-let [cached-ability# (get @card-defs-cache ~title)]
        cached-ability#
        (let [ability# (add-default-abilities ~title ~ability)]
          (swap! card-defs-cache assoc ~title ability#)