38c954ce8a62fb7f93fe09e1e181a8a8a20cdf30
Artem Mikhalitsin <artemmikhalitsin@gmail.com>
2022-07-26 17:43:01 -0400

    use in-discard?

--- src/clj/game/cards/agendas.clj
+++ src/clj/game/cards/agendas.clj
@@ -1528,7 +1528,7 @@
 
 (defcard "Regenesis"
   {:on-score {:req (req (and (some #(not (faceup? %)) (:discard corp))
-                             (no-event? state side :card-moved #(and (= [:discard] (:zone (second %)))
+                             (no-event? state side :card-moved #(and (in-discard? (second %))
                                                                      (corp? (second %))))))
               ;; we want a prompt even if there are no valid targets,
               ;; to make sure we don't give away hidden info
