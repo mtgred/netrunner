41a2fddf296ae12bd26cd36523e3a3893a41f9ae
Francesco Pellegrini <francesco.pelle@gmail.com>
2022-10-21 18:53:46 +0200

    fix sabotage more cards than there are in HQ

--- test/clj/game/core/sabotage_test.clj
+++ test/clj/game/core/sabotage_test.clj
@@ -101,4 +101,14 @@
       (is (no-prompt? state :runner))
       (is (= 0 (count (:hand (get-corp)))) "HQ is empty")
       (is (= 0 (count (:deck (get-corp)))) "R&D is empty")
-      (is (= 7 (count (:discard (get-corp)))) "Archives has 7 cards"))))
+      (is (= 7 (count (:discard (get-corp)))) "Archives has 7 cards")))
+  (testing "Forced to trash more cards than there are in HQ"
+    (do-game
+      (new-game {:corp {:deck [(qty "Hedge Fund" 7)]}})
+      (core/resolve-ability state :runner (eid/make-eid state)
+                            (s/sabotage-ability 7) (:identity (get-runner)) nil)
+      (is (no-prompt? state :corp))
+      (is (no-prompt? state :runner))
+      (is (empty? (:hand (get-corp))))
+      (is (empty? (:deck (get-corp))))
+      (is (= 7 (count (:discard (get-corp))))))))
