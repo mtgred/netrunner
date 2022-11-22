fb9901c6cecf383c2d07c6671c16290271468f3f
NB Kelly <N.B.Kelly@protonmail.com>
2022-11-23 11:20:43 +1300

    clean up

--- src/clj/game/cards/assets.clj
+++ src/clj/game/cards/assets.clj
@@ -59,7 +59,7 @@
    [game.core.to-string :refer [card-str]]
    [game.core.toasts :refer [toast]]
    [game.core.update :refer [update!]]
-   [game.core.winning :refer [check-win-by-agenda]]
+   [game.core.winning :refer [check-win-by-agenda win]]
    [game.macros :refer [continue-ability effect msg req wait-for]]
    [game.utils :refer :all]
    [jinteki.utils :refer :all]))
@@ -2248,17 +2248,20 @@
                        :value [:credit 2]}]})
 
 (defcard "Superdeep Borehole"
-  {:on-rez {:effect (req (add-counter state side card :bad-publicity 6))}
+  ;; the "when it is empty" text is reliant on the card being loaded
+  {:on-rez {:effect (req (update! state side (assoc-in (get-card state card) [:special :borehole-valid] true))
+                         (add-counter state side card :bad-publicity 6))}
    :events [{:event :corp-turn-begins
              :msg (msg "take 1 bad publicity from " (:title card))
              :async true
              :effect (req (add-counter state side card :bad-publicity -1 nil)
-                          (wait-for (gain-bad-publicity state :corp 1)
-                                    (if (= 0 (get-counters (get-card state card) :bad-publicity))
-                                      (do (win state :corp "Superdeep Borehole extinction event")
-                                          (system-msg state side "win the game")
-                                          (effect-completed state side eid))
-                                      (effect-completed state side eid))))}]})
+                          (gain-bad-publicity state :corp eid 1))}
+            {:event :counter-added
+             :req (req (and (same-card? card target)
+                            (not (pos? (get-counters card :power)))
+                            (:borehole-valid (:special card))))
+             :msg "win the game"
+             :effect (req (win state :corp "Superdeep Borehole extinction event"))}]})
 
 (defcard "Sundew"
   ; If this a run event then handle in :begin-run as we do not know the server
