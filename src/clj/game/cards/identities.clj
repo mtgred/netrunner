96a6fe9149df62233ab1a4968f82873179914857
Julian Lane <lanejul@umich.edu>
2022-12-10 11:11:34 -0500

    replaced -1 with dec

--- src/clj/game/cards/identities.clj
+++ src/clj/game/cards/identities.clj
@@ -842,7 +842,7 @@
              :msg "put 1 charge counter on itself"
              :effect (req (add-counter state side card :power 1)
                           (swap! state assoc-in [:corp :agenda-point-req]
-                                 (- (get-in @state [:corp :agenda-point-req]) 1))
+                                 (dec (get-in @state [:corp :agenda-point-req])))
                           (check-win-by-agenda state))}]})
 
 (defcard "Jamie \"Bzzz\" Micken: Techno Savant"
