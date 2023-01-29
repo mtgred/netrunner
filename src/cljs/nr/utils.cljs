be9b3bac684f2a00ad01a2f34ccf3f81224a3c65
Clint Sbisa <csbisa@gmail.com>
2023-01-29 14:16:06 +0900

    utils: Enable card pattern matching for selected language
    When searching for card patterns, both English and the selected
    language is considered. The original language of the text is
    preserved.

--- src/cljs/nr/utils.cljs
+++ src/cljs/nr/utils.cljs
@@ -10,6 +10,7 @@
    [goog.string :as gstring]
    [goog.string.format]
    [nr.appstate :refer [app-state]]
+   [nr.translations :refer [tr-data]]
    [reagent.dom :as rd]))
 
 ;; Dot definitions
@@ -194,12 +195,18 @@
   "A sequence of card pattern pairs consisting of a regex, used to match a card
   name in text, and the span fragment that should replace it"
   []
-  (letfn [(span-of [title] [:span {:class "fake-link" :data-card-title title} title])]
-    (->> (:all-cards-and-flips @app-state)
-         (vals)
-         (remove :replaced_by)
-         (map (fn [c] [(:title c) (span-of (:title c))]))
-         (sort-by (comp count str first) >))))
+  (letfn [(span-of [title tr-title] [:span {:class "fake-link" :data-card-title title} tr-title])]
+    (distinct (concat
+     (->> (:all-cards-and-flips @app-state)
+          (vals)
+          (remove :replaced_by)
+          (map (fn [c] [(:title c) (span-of (:title c) (:title c))]))
+          (sort-by (comp count str first) >))
+     (->> (:all-cards-and-flips @app-state)
+          (vals)
+          (remove :replaced_by)
+          (map (fn [c] [(tr-data :title c) (span-of (:title c) (tr-data :title c))]))
+          (sort-by (comp count str first) >))))))
 
 (def card-patterns (memoize card-patterns-impl))
 
@@ -211,7 +218,7 @@
     (->> (:all-cards-and-flips @app-state)
          (vals)
          (filter #(not (:replaced_by %)))
-         (map (fn [k] (regex-escape (:title k))))
+         (map (fn [k] (join "|" (map regex-escape (distinct [(:title k) (tr-data :title k)])))))
          (join "|"))))
 
 (def contains-card-pattern (memoize contains-card-pattern-impl))
