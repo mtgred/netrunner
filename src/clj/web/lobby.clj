df220f40ddcd8d0b0fd17a64b0fcc0684d12069e
NB Kelly <N.B.Kelly@protonmail.com>
2024-07-18 17:51:03 +1200

    fix brackets

--- src/clj/web/lobby.clj
+++ src/clj/web/lobby.clj
@@ -601,7 +601,7 @@
                  (not (inst/is-after (inst/now) (inst/plus-seconds last-update (- time-inactive 29)))))
         (let [uids (keep :uid (get-players-and-spectators lobby))]
           (doseq [uid uids]
-            (when uid (ws/chsk-send! uid [:game/timeout-soon gameid]))))))
+            (when uid (ws/chsk-send! uid [:game/timeout-soon gameid])))))
       (when (and gameid (inst/is-after (inst/now) (inst/plus-seconds last-update time-inactive)))
         (let [uids (keep :uid (get-players-and-spectators lobby))]
           (vreset! changed? true)
