37c0a8ab3e6bdec1e12fcc290e28cb5cd8ed0191
NB Kelly <N.B.Kelly@protonmail.com>
2024-08-25 09:24:08 +1200

    test actually passes now

--- test/clj/game/cards/hardware_test.clj
+++ test/clj/game/cards/hardware_test.clj
@@ -3092,7 +3092,8 @@
     (click-card state :corp "Ice Wall")
     (is (= ["IPO" "IPO" "IPO" "Ice Wall"] (map :title (:discard (get-corp))))
         "3 Ipos and Ice Wall sabotaged")
-    (is (= ["Vanilla" "IPO"] (map :title (:deck (get-corp)))) "Just vanilla in deck")))
+    (is (= (sort ["Vanilla" "IPO"]) (sort (map :title (:deck (get-corp)))))
+        "Deck is Vanilla and IPO")))
 
 (deftest masterwork-v37
   ;; Masterwork (v37)
