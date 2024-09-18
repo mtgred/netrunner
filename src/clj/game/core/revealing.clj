12822821860e08b16b461fa0dedad2f86f32874c
NB Kelly <N.B.Kelly@protonmail.com>
2024-09-18 15:48:04 +1200

    reveal cards fn with log printout

--- src/clj/game/core/revealing.clj
+++ src/clj/game/core/revealing.clj
@@ -1,6 +1,11 @@
 (ns game.core.revealing
   (:require
-    [game.core.engine :refer [trigger-event-sync]]))
+   [clojure.string :as string]
+   [game.core.engine :refer [trigger-event-sync]]
+   [game.core.say :refer [system-msg]]
+   [game.core.servers :refer [name-zone]]
+   [game.utils :refer [enumerate-str]]
+   [jinteki.utils :refer [other-side]]))
 
 (defn reveal-hand
   "Reveals a side's hand to opponent and spectators."
@@ -16,3 +21,17 @@
   "Trigger the event for revealing one or more cards."
   [state side eid & targets]
   (apply trigger-event-sync state side eid (if (= :corp side) :corp-reveal :runner-reveal) (flatten targets)))
+
+(defn reveal-explicit
+  "Trigger the event for revealing one or more cards, and also handle the log printout"
+  [state side eid card {:keys [forced] :as args} & targets]
+  (let [cards-by-zone (group-by #(select-keys % [:side :zone]) (flatten targets))
+        strs (map #(str (enumerate-str (map :title (get cards-by-zone %)))
+                        " from " (name-zone (:side %) (:zone %)))
+                  (keys cards-by-zone))]
+    (if forced
+      (system-msg state (other-side side) (str " uses " (:title card) " to force the "
+                                               (string/capitalize (name side)) " to reveal "
+                                               (enumerate-str strs)))
+      (system-msg state side (str " uses " (:title card) " to reveal " (enumerate-str strs))))
+    (reveal state side eid targets)))
