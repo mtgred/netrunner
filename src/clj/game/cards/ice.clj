27c98b45e0deafafd3cde02c44617ea08d4be5d6
NB Kelly <N.B.Kelly@protonmail.com>
2024-11-29 14:32:58 +1300

    bran waits

--- src/clj/game/cards/ice.clj
+++ src/clj/game/cards/ice.clj
@@ -1067,6 +1067,7 @@
                   :label "Install an ice from HQ or Archives"
                   :prompt "Choose an ice to install from Archives or HQ"
                   :show-discard true
+                  :waiting-prompt true
                   :choices {:card #(and (ice? %)
                                         (or (in-hand? %)
                                             (in-discard? %)))}
