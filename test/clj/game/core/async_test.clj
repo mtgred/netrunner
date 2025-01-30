b520cf57703aec015db4504853ecf9a9e7211bca
NB Kelly <N.B.Kelly@protonmail.com>
2025-01-30 19:47:47 +1300

    pretty sure the test looks good now

--- test/clj/game/core/async_test.clj
+++ test/clj/game/core/async_test.clj
@@ -80,9 +80,10 @@
   [chunk depth]
   (some #(cond
            (string? %) (= % "eid")
-           (and (vector? %) (= (second %) "make-eid")) true
-           (and (vector? %) (= (second %) "assoc")) (contains-eid? % (inc depth))
-           (and (vector? %) (zero? depth)) (contains-eid? % 1)
+           (not (vector? %)) nil
+           (= (second %) "make-eid") true
+           (contains? #{"assoc" "assoc-in"} (second %)) (contains-eid? % (inc depth))
+           (zero? depth) (contains-eid? % 1)
            :else nil)
         chunk))
 
@@ -93,7 +94,8 @@
     (and (string? chunk) (contains? @memory chunk) (< depth 15))
     (completes-eid? (get @memory chunk) memory (inc depth))
     ;; if it's not a function, it doesn't complete
-    (not= sig :fn) (println "does not complete: " chunk)
+    (not= sig :fn) nil
+    ;; referring to a pre-deffed fn
     (and ide (contains? @memory ide) (< depth 15))
     (completes-eid? (get @memory ide) memory (inc depth))
     ;; both sides of the ifn should complete
@@ -122,9 +124,25 @@
     (or (= ide "continue-ability") (contains-eid? chunk 0))
     :maybe
     ;; leftover fn - check the RHS member completes
-    :else (and (> (count chunk) 2) (completes-eid? (last chunk) memory (inc depth)))
-    ))
+    :else (and (> (count chunk) 2) (completes-eid? (last chunk) memory (inc depth)))))
 
+;; TODO - add more things as needed, if issues arise
+(def terminal-fns #{"checkpoint" "complete-with-result" "continue-ability" "corp-install"
+                    "damage" "draw" "effect-completed" "gain-credits" "gain-tags" "make-run"
+                    "reveal" "rez" "resolve-ability" "runner-install"
+                    "trash" "trash-cards" "trigger-event-simult" "trigger-event-sync" "wait-for"})
+
+(defn- should-be-async?
+  "should a chunk (probably) complete an eid?"
+  [[sig ide :as chunk] memory depth]
+  (cond
+    (>= depth 15) nil
+    (contains? @memory chunk) (should-be-async? (get @memory chunk) memory (inc depth))
+    (and ide (contains? @memory ide)) (should-be-async? (get @memory ide) memory (inc depth))
+    (= :fn sig) (or (contains? terminal-fns ide)
+                    (some #(should-be-async? % memory (inc depth)) (drop 2 chunk)))))
+
+;; TODO - check function metadata for ignore-async-check, just incase we purposely break shit
 (defn- is-valid-chunk?
   "checks if a chunk of code is 'valid' in terms of sync/async classification.
    This is intended to be at least 99% accurate, but if something is getting missed,
@@ -132,18 +150,14 @@
   ([chunk] (is-valid-chunk? chunk (atom {})))
   ([[sig :as chunk] memory]
    (when (= :fn sig) (bank-fn! chunk memory))
-   (println "chunk: " chunk)
-   ;; (println "memory: " memory)
    (cond
      (contains? #{:string :keyword :number :character :truthy :list :comment} sig) :fine
      (string? chunk) :fine
      (contains? #{:fn :vector :set} sig) (every? #(is-valid-chunk? % memory) (rest chunk))
      ;; maps -> require more complicated logic
      (= :map sig) (is-valid-chunk? (rest chunk) memory :map)
-     :else (do (println "invalid chunk: " chunk)
-               nil)))
+     :else nil))
   ([chunk memory sig]
-   (println "sig: " sig " - mchunk: " chunk)
    (cond
      (= sig :map)
      (let [keypairs (partition 2 chunk)
@@ -161,191 +175,74 @@
            (every? #(is-valid-chunk? % memory) (map second keypairs))
            true)))
      ;; things that are async should be completing eids
-     (= sig :async) (and (completes-eid? chunk memory 0) (is-valid-chunk? chunk memory))
-     (= sig :sync)
-     true)))
+     (= sig :async) (and (completes-eid? chunk memory 0)
+                         (is-valid-chunk? chunk memory))
+     (= sig :sync) (and (not (should-be-async? chunk memory 0))
+                        (not (completes-eid? chunk memory 0))
+                        (is-valid-chunk? chunk memory))
+     :else true)))
+
+(defn- invalid-chunk?
+  [chunk]
+  (let [[sig body :as parsed] (clojure-grammar chunk)]
+    (if-not (is-valid-chunk? body)
+      (get-fn-name body)
+      nil)))
 
 (deftest cards-are-async-test
   (doseq [fname (take 15 relevant-cards-files)]
+    (println "checking" fname)
     (let [f (slurp (str card-base-str fname))
-          chunks (rest (stitch-and-split-card-files f))
-          name-if-invalid (fn [parsed] (when-not (is-valid-chunk? parsed) (get-fn-name parsed)))]
-      ;; final form should look like this
-      ;; (doseq [c (rest chunks)]
-      ;;   ;;(println c)
-      ;;   (let [parsed (clojure-grammar c)]
-      ;;     (println parsed)
-      ;;     (is (not ((comp name-if-invalid second) parsed)) (str "wrong: " parsed)))))))
-      (let [invalids (->> chunks
-                          (map (comp name-if-invalid second clojure-grammar))
-                          (filterv identity))]
+          chunks (rest (stitch-and-split-card-files f))]
+      (let [invalids (->> chunks (map invalid-chunk?) (filterv identity))]
         (is (empty? invalids)
             (str "the following definitions in " fname
                  " may have sync/async issues: " (str/join ", " invalids)))))))
 
-;; ;; TODO - can add a few more to these as errors get picked up down the line
-;; (def terminal-fns #{"checkpoint" "complete-with-result" "continue-ability" "corp-install" "damage" "draw" "effect-completed"
-;;                     "gain-credits" "gain-tags" "make-run" "reveal" "rez" "resolve-ability" "runner-install"
-;;                     "trash" "trash-cards" "trigger-event-simult" "trigger-event-sync" "wait-for"})
-;; (defn should-complete?
-;;   "Should a chunk (probably) complete an eid?"
-;;   [chunk depth]
-;;   (cond
-;;     (and (string? chunk) (zero? depth)) nil
-;;     (and (vector? chunk) (= 2 (count chunk)) (zero? depth)) nil
-;;     (and (vector? chunk) (= (first chunk) :FN))
-;;     (let [func-name (second chunk)]
-;;       (if (contains? terminal-fns func-name)
-;;         true
-;;         (some #(should-complete? % (inc depth)) (rest (rest chunk)))))
-;;     :else nil))
+(deftest async-test-defferred-fns-are-correct
+  (let [c1 "(defcard \"c1\" (let [x (req (do-something state side eid))] {:async true :effect x}))"
+        c2 "(defcard \"c2\" (let [x (req (do-something state side nil))] {:async true :effect x}))"
+        c3 "(defcard \"c3\" (let [x (req (do-something state side eid))] {:effect x}))"
+        c4 "(defcard \"c4\" (let [x (req (do-something state side nil))] {:effect x}))"
+        c5 "(defcard \"c5\" (letfn [(x [] (req (do-something state side eid)))] {:async true :effect (x)}))"
+        c6 "(defcard \"c6\" (letfn [(x [] (req (do-something state side nil)))] {:async true :effect (x)}))"
+        c7 "(defcard \"c7\" (letfn [(x [] (req (do-something state side eid)))] {:effect (x)}))"
+        c8 "(defcard \"c8\" (letfn [(x [] (req (do-something state side nil)))] {:effect (x)}))"]
+    (is (not (invalid-chunk? c1)) "deffered block c1 is picked up as being correct (x completes)")
+    (is (invalid-chunk? c2) "deffered block c2 is picked up as being wrong (x should complete)")
+    (is (invalid-chunk? c3) "deffered block c3 is picked up as being wrong (x should complete)")
+    (is (not (invalid-chunk? c4)) "deffered block c4 is picked up as being correct (x should not complete)")
+    (is (not (invalid-chunk? c5)) "deffered block c5 is picked up as being correct (x completes)")
+    (is (invalid-chunk? c6) "deffered block c6 is picked up as being wrong (x should complete)")
+    (is (invalid-chunk? c7) "deffered block c7 is picked up as being wrong (x is not async, but should be)")
+    (is (not (invalid-chunk? c8)) "deffered block c8 is picked up as being correct (x should not complete)")))
 
-;; (defn is-valid-chunk?
-;;   ([chunk]
-;;    (cond
-;;      (not (sequential? chunk)) true
-;;      (= :FN (first chunk)) (every? is-valid-chunk? (rest chunk))
-;;      (= :VEC (first chunk)) (every? is-valid-chunk? (rest chunk))
-;;      (= :SET (first chunk)) (every? is-valid-chunk? (rest chunk))
-;;      (= [:STR] chunk) true
-;;      (= :MAP (first chunk)) (is-valid-chunk? (rest chunk) :MAP)
-;;      (sequential? chunk) (every? is-valid-chunk? chunk)
-;;      :else true))
-;;   ([chunk conditional?]
-;;    (cond
-;;      (= conditional? :MAP)
-;;      (do (let [keypairs (partition 2 chunk)
-;;                mapped (zipmap (map first keypairs) (map second keypairs))]
-;;            ;; if it contains an :effect, then:
-;;            ;;   see if it contains :async true. If it does, effect must complete eid
-;;            ;;   if it does not, effect should not complete the eid
-;;            (and
-;;              (if (:effect mapped)
-;;                (if (:async mapped)
-;;                  (is-valid-chunk? (:effect mapped) :async)
-;;                  (is-valid-chunk? (:effect mapped) :sync))
-;;                (every? is-valid-chunk? (vals mapped)))
-;;              ;; note that cancel effects must always complete eids,
-;;              ;; as there is no provision for async-checking them baked into the engine
-;;              ;; this comment is valid as of Jan '25 -nbk
-;;              (if (:cancel-effect mapped)
-;;                (is-valid-chunk? (:cancel-effect mapped) :async)
-;;                true))))
-;;      (= conditional? :async)
-;;      (and (completes? chunk 0) (is-valid-chunk? chunk))
-;;      (= conditional? :sync)
-;;      (and (not (should-complete? chunk 0)) (is-valid-chunk? chunk)))))
+(deftest async-test-if-block-is-correct?
+  (let [c1 (invalid-chunk? "(defcard \"c1\" {:async true :effect (req (if (some corp-installable-type? (:hand corp)) (continue-ability state side select-ability card nil) (damage state)))})")
+        c2 (invalid-chunk? "(defcard \"c2\" {:async true :effect (req (if-not (some corp-installable-type? (:hand corp)) (damage 2) (damage state side eid 1)))})")
+        c3 (invalid-chunk? "(defcard \"c3\" {:async true :effect (req (if-let (some corp-installable-type? (:hand corp)) (continue-ability state side select-ability card nil) (damage state side eid 1)))})")]
+    (is c1       "If block C1 is picked up as being wrong (RHS does not complete)")
+    (is c2       "If block C2 is picked up as being wrong (LHS does not complete)")
+    (is (not c3) "If block C3 is picked up as being right (LHS and RHS both complete)")))
 
-;; (defn clean-chunks
-;;   "remove the empties and nils, and swaps keywords in"
-;;   [chunks]
-;;   (cond
-;;     (= "" chunks) nil
-;;     (= "true" chunks) true
-;;     (= "nil" chunks) :nil
-;;     (and (string? chunks) (str/starts-with? chunks ":")) (keyword (subs chunks 1))
-;;     (sequential? chunks) (filterv identity (map clean-chunks chunks))
-;;     :else chunks))
+(deftest async-test-when-block-is-correct?
+  (let [c1 "(defcard \"c1\" {:async true :effect (req (when x (effect-completed state side eid)))})"
+        c2 "(defcard \"c2\" {:async true :effect (req (do (when x y) (effect-completed state side eid)))})"]
+    (is (invalid-chunk? c1)       "When block C1 is picked up as being wrong (conditional may not complete)")
+    (is (not (invalid-chunk? c2)) "When block C2 is picked up as being right (conditional does not block completion)")))
 
-;; (defn validate-chunk [chunk] (->> chunk scuffed-grammar clean-chunks is-valid-chunk?))
+(deftest async-test-case-block-is-correct?
+  (let [c1 "(defcard \"c1\" {:async true :effect (req (case x a (effect-completed state side eid) (system-msg state side \"whoops\")))})"
+        c2 "(defcard \"c2\" {:async true :effect (req (case x a (system-msg state side \"whoops\") (effect-completed state side eid)))})"
+        c3 "(defcard \"c3\" {:async true :effect (req (case x a (effect-completed state side eid) (effect-completed state side eid)))})"]
+    (is (invalid-chunk? c1)       "Case block C1 is picked up as being wrong (terminal does not complete)")
+    (is (invalid-chunk? c2)       "Case block C2 is picked up as being wrong (LHS does not complete)")
+    (is (not (invalid-chunk? c3)) "Case block C3 is picked up as being right (LHS and terminal both complete)")))
 
-;; (deftest cards-are-async-test
-;;   (doseq [fname relevant-cards-files]
-;;     (let [f (slurp (str card-base-str fname))
-;;           chunks (stitch-and-split-card-files f)]
-;;       (let [invalid-chunks (filter (complement validate-chunk) chunks)
-;;             titles (map #(re-find #" \".+?\"" %) invalid-chunks)]
-;;         (when (seq titles)
-;;           (is nil (str "The following cards/fns in file '" fname "' may be invalid (async/sync): " (str/join ", " titles))))))))
-
-;; (deftest async-test-if-block-is-correct?
-;;   (let [c1 "{:async true :effect (req (if (some corp-installable-type? (:hand corp)) (continue-ability state side select-ability card nil) (damage state)))}"
-;;         c2 "{:async true :effect (req (if-not (some corp-installable-type? (:hand corp)) (damage 2) (damage state side eid 1)))}"
-;;         c3 "{:async true :effect (req (if-let (some corp-installable-type? (:hand corp)) (continue-ability state side select-ability card nil) (damage state side eid 1)))}"]
-;;     (is (not (validate-chunk c1)) "If block C1 is picked up as being wrong (RHS does not complete)")
-;;     (is (not (validate-chunk c2)) "If block C2 is picked up as being wrong (LHS does not complete)")
-;;     (is (validate-chunk c3)       "If block C3 is picked up as being right (LHS and RHS both complete)")))
-
-;; (deftest async-test-when-block-is-correct?
-;;   (let [c1 "{:async true :effect (req (when x (effect-completed state side eid)))}"
-;;         c2 "{:async true :effect (req (do (when x y) (effect-completed state side eid)))}"]
-;;     (is (not (validate-chunk c1)) "When block C1 is picked up as being wrong (conditional may not complete)")
-;;     (is (validate-chunk c2)       "When block C2 is picked up as being right (conditional does not block completion)")))
-
-;; (deftest async-test-case-block-is-correct?
-;;   (let [c1 "{:async true :effect (req (case x a (effect-completed state side eid) (system-msg state side \"whoops\")))}"
-;;         c2 "{:async true :effect (req (case x a (system-msg state side \"whoops\") (effect-completed state side eid)))}"
-;;         c3 "{:async true :effect (req (case x a (effect-completed state side eid) (effect-completed state side eid)))}"]
-;;     (is (not (validate-chunk c1)) "Case block C1 is picked up as being wrong (terminal does not complete)")
-;;     (is (not (validate-chunk c2)) "Case block C2 is picked up as being wrong (LHS does not complete)")
-;;     (is (validate-chunk c3)       "Case block C3 is picked up as being right (LHS and terminal both complete)")))
-
-;; (deftest async-test-cond+-is-correct?
-;;   (let [c1 "{:async true :effect (req (cond+ [a (damage state :runner)] [:else (effect-completed state side eid)]))}"
-;;         c2 "{:async true :effect (req (cond+ [a (effect-completed state :runner eid)] [:else (damage state side)]))}"
-;;         c3 "{:async true :effect (req (cond+ [a (effect-completed state :runner eid)] [:else (effect-completed state side eid)]))}"]
-;;     (is (not (validate-chunk c1)) "Cond+ block C1 is picked up as being wrong (RHS does not complete)")
-;;     (is (not (validate-chunk c2)) "Cond+ block C2 is picked up as being wrong (LHS does not complete)")
-;;     (is (validate-chunk c3)       "Cond+ block C3 is picked up as being right (LHS and RHS both complete)")))
-
-
-
-
-;; [:fn defcard
-;;  [:string "Deuces Wild"]
-;;  [:fn let
-;;   [:vector
-;;    all [:vector
-;;         [:map
-;;          [:keyword effect] [:fn effect [:fn gain-credits eid [:number]]]
-;;          [:keyword async] [:truthy]
-;;          [:keyword msg] [:string "gain 3 [Credits]"]]
-;;         [:map
-;;          [:keyword async] [:truthy]
-;;          [:keyword effect] [:fn effect [:fn draw eid [:number]]]
-;;          [:keyword msg] [:string "draw 2 cards"]]
-;;         [:map
-;;          [:keyword async] [:truthy]
-;;          [:keyword effect] [:fn effect [:fn lose-tags eid [:number]]]
-;;          [:keyword msg] [:string "remove 1 tag"]]
-;;         [:map
-;;          [:keyword prompt] [:string "Choose 1 piece of ice to expose"]
-;;          [:keyword msg] [:string "expose 1 ice and make a run"]
-;;          [:keyword choices] [:map
-;;                              [:keyword card] [:fn and [:fn installed? %] [:fn ice? %]]]
-;;          [:keyword async] [:truthy]
-;;          [:keyword effect] [:fn req
-;;                             [:fn wait-for
-;;                              [:fn expose state side target]
-;;                              [:fn continue-ability
-;;                               state side
-;;                               [:map
-;;                                [:keyword prompt] [:string "Choose a server"]
-;;                                [:keyword choices] [:fn req runnable-servers]
-;;                                [:keyword async] [:truthy]
-;;                                [:keyword effect] [:fn effect [:fn make-run eid target]]]
-;;                               card [:truthy]]]]
-;;          [:keyword cancel-effect] [:fn effect
-;;                                    [:fn continue-ability
-;;                                     [:map
-;;                                      [:keyword prompt] [:string "Choose a server"]
-;;                                      [:keyword choices] [:fn req runnable-servers]
-;;                                      [:keyword async] [:truthy]
-;;                                      [:keyword effect] [:fn effect [:fn make-run eid target]]]
-;;                                     card [:truthy]]]]]
-;;    choice [:fn fn
-;;            choice [:vector abis] [:map
-;;                                   [:keyword prompt] [:string "Choose an ability to resolve"]
-;;                                   [:keyword choices] [:fn map [:fn capitalize [:fn [:keyword msg] %]] abis]
-;;                                   [:keyword async] [:truthy]
-;;                                   [:keyword effect] [:fn req [:fn let [:vector chosen [:fn some [:fn when [:fn = target [:fn capitalize [:fn [:keyword msg] %]]] %] abis]]
-;;                                                               [:fn wait-for
-;;                                                                [:fn resolve-ability state side chosen card [:truthy]]
-;;                                                                [:fn if
-;;                                                                 [:fn = [:fn count abis] [:number]]
-;;                                                                 [:fn continue-ability state side [:fn choice [:fn remove-once [:fn = % chosen] abis]] card [:truthy]]
-;;                                                                 [:fn effect-completed state side eid]]]]]]]]
-;;   [:map
-;;    [:keyword on-play] [:map
-;;                        [:keyword async] [:truthy]
-;;                        [:keyword effect] [:fn effect [:fn continue-ability [:fn choice all] card [:truthy]]]]]]]
+(deftest async-test-cond+-is-correct?
+  (let [c1 "(defcard \"c1\" {:async true :effect (req (cond+ [a (damage state :runner)] [:else (effect-completed state side eid)]))})"
+        c2 "(defcard \"c2\" {:async true :effect (req (cond+ [a (effect-completed state :runner eid)] [:else (damage state side)]))})"
+        c3 "(defcard \"c3\" {:async true :effect (req (cond+ [a (effect-completed state :runner eid)] [:else (effect-completed state side eid)]))})"]
+    (is (invalid-chunk? c1)       "Cond+ block C1 is picked up as being wrong (RHS does not complete)")
+    (is (invalid-chunk? c2)       "Cond+ block C2 is picked up as being wrong (LHS does not complete)")
+    (is (not (invalid-chunk? c3)) "Cond+ block C3 is picked up as being right (LHS and RHS both complete)")))
