3c6695e74d9f85ff9df5d286f6971b17aca46a9e
NB Kelly <N.B.Kelly@protonmail.com>
2025-02-24 18:12:41 +1300

    simplified tags file

--- src/clj/game/core/tags.clj
+++ src/clj/game/core/tags.clj
@@ -36,22 +36,6 @@
                                                    :is-tagged is-tagged?}))
      changed?)))
 
-;; this can also be cut
-(defn tag-prevent
-  [state side eid n]
-  (swap! state update-in [:tag :tag-prevent] (fnil #(+ % n) 0))
-  (trigger-event-sync state side eid (if (= side :corp) :corp-prevent :runner-prevent) {:type :tag
-                                                                                        :amount n}))
-
-;; this can actually be cut entirely
-(defn- number-of-tags-to-gain
-  "Calculates the number of tags to give, taking into account prevention and boosting effects."
-  [state _ n {:keys [unpreventable unboostable]}]
-  (-> n
-      (+ (or (when-not unboostable (get-in @state [:tag :tag-bonus])) 0))
-      (- (or (when-not unpreventable (get-in @state [:tag :tag-prevent])) 0))
-      (max 0)))
-
 (defn- resolve-tag
   "Resolve runner gain tags. Always gives `:base` tags."
   [state side eid {:keys [card n suppress-checkpoint]}]
@@ -71,14 +55,11 @@
   "Attempts to give the runner n tags, allowing for boosting/prevention effects."
   ([state side eid n] (gain-tags state side eid n nil))
   ([state side eid n {:keys [unpreventable card suppress-checkpoint] :as args}]
-   (swap! state update :tag dissoc :tag-bonus :tag-prevent)
-   (wait-for (trigger-event-simult state side :pre-tag nil card)
-             (let [n (number-of-tags-to-gain state side n args)]
-               (wait-for
-                 (resolve-tag-prevention state side n args)
-                 (resolve-tag state side eid {:suppress-checkpoint suppress-checkpoint
-                                              :card card
-                                              :n (:remaining async-result)}))))))
+   (wait-for
+     (resolve-tag-prevention state side n args)
+     (resolve-tag state side eid {:suppress-checkpoint suppress-checkpoint
+                                  :card card
+                                  :n (:remaining async-result)}))))
 
 (defn lose-tags
   "Always removes `:base` tags"
