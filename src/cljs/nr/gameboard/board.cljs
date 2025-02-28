4dd7f299dd50dd19d8748c56fc46cb6e6a3b08e5
NB Kelly <N.B.Kelly@protonmail.com>
2025-03-01 12:25:09 +1300

    display selectable cards during a selection prompt

--- src/cljs/nr/gameboard/board.cljs
+++ src/cljs/nr/gameboard/board.cljs
@@ -172,13 +172,10 @@
 
 (defn- prompt-button-from-card?
   [clicked-card {:keys [card msg prompt-type choices] :as prompt-state}]
-  ;; HERE
   (when-not (or (some #{:counter :card-title :number} choices)
                 (= choices "credit")
                 (= prompt-type "trace"))
     (some (fn [{:keys [_ uuid value]}]
-            ;; (js/console.log (str "Value:" value))
-            ;; (js/console.log (str "Card:" clicked-card))
             (when (= (:cid value) (:cid clicked-card)) uuid))
           choices)))
 
@@ -186,13 +183,13 @@
   (let [side (:side @game-state)]
     (when (not-spectator?)
       (cond
-        ;; A prompt is open, and that card is among the prompt buttons
-        (prompt-button-from-card? card (get-in @game-state [side :prompt-state]))
-        (send-command "choice" {:choice {:uuid (prompt-button-from-card? card (get-in @game-state [side :prompt-state]))}})
-
         ;; Selecting card
         (= (get-in @game-state [side :prompt-state :prompt-type]) "select")
         (send-command "select" {:card (card-for-click card) :shift-key-held shift-key-held})
+
+        ;; A selectable card is clicked outside of a select prompt (ie it's a button on a choices prompt)
+        (contains? (into #{} (get-in @game-state [side :prompt-state :selectable])) (:cid card))
+        (send-command "choice" {:choice {:uuid (prompt-button-from-card? card (get-in @game-state [side :prompt-state]))}})
 
         ;; Card is an identity of player's side
         (and (= (:type card) "Identity")
@@ -715,10 +712,12 @@
            subtype-target corp-abilities]
     :as card} flipped disable-click]
   (let [title (get-title card)]
-    [:div.card-frame.menu-container
-     [:div.blue-shade.card {:class (str (cond selected "selected"
-                                              (same-card? card (:button @app-state)) "hovered"
-                                              (same-card? card (-> @game-state :encounters :ice)) "encountered"
+    (r/with-let [prompt-state (r/cursor game-state [(keyword (lower-case side)) :prompt-state])]
+      [:div.card-frame.menu-container
+       [:div.blue-shade.card {:class (str (cond selected "selected"
+                                                (contains? (into #{} (get-in @prompt-state [:selectable])) (:cid card)) "selectable"
+                                                (same-card? card (:button @app-state)) "hovered"
+                                                (same-card? card (-> @game-state :encounters :ice)) "encountered"
                                               (and (not (any-prompt-open? side)) (playable? card)) "playable"
                                               ghost "ghost"
                                               (graveyard-highlight-card? card) "graveyard-highlight"
@@ -807,7 +806,7 @@
            (for [card hosted]
              (let [flipped (draw-facedown? card)]
                ^{:key (:cid card)}
-               [card-view card flipped]))))])]))
+               [card-view card flipped]))))])])))
 
 (defn show-distinct-cards
   [distinct-cards]
