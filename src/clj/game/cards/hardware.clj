9c15774a97c4e1964cba0395aeb80a18e3629449
NB Kelly <N.B.Kelly@protonmail.com>
2025-05-31 14:10:30 +1200

    type

--- src/clj/game/cards/hardware.clj
+++ src/clj/game/cards/hardware.clj
@@ -766,7 +766,7 @@
                           :waiting-prompt true
                           :yes-ability {:msg (msg "access 1 card from HQ")
                                         :async true
-                                        :effect (req (access-card state :runner eid c (-> corp :hand shuffle first)))}}}
+                                        :effect (req (access-card state :runner eid (-> corp :hand shuffle first)))}}}
                         card nil))}]
     {:static-abilities [(mu+ 1)]
      :events [{:event :successful-run
