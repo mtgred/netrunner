449be485479e5aa705fa7c3068ee101db0397aea
NB Kelly <N.B.Kelly@protonmail.com>
2025-07-11 16:37:51 +1200

    restructed log files

--- src/clj/web/logs.clj
+++ src/clj/web/logs.clj
@@ -3,19 +3,32 @@
    [taoensso.timbre :as timbre]
    [clojure.java.io :as io]))
 
-(defn appender
-  [file min-level print?]
+(def default-appender
   {:enabled? true
    :async? true
-   :min-level min-level
+   :min-level :info
+   :output-fn timbre/default-output-fn
+   :fn (fn [{:keys [level output_ context]}]
+         (when-not (or (= level :error) (= level :fatal) (= (:type context) :moderator))
+           (spit "logs/clojure.log" (str @output_ "\n") :append true)))})
+
+(def exceptions-appender
+  {:enabled? true
+   :async? true
+   :min-level :error
    :output-fn timbre/default-output-fn
    :fn (fn [{:keys [output_]}]
-         (when print? (println @output_))
-         (spit file (str @output_ "\n") :append true))})
+         (println @output_)
+         (spit "logs/exceptions.log" (str @output_ "\n") :append true))})
 
-(def log-appender (appender "logs/clojure.log" :info nil))
-(def exceptions-appender (appender "logs/exceptions.log" :error true))
-(def mod-action-appender (appender "logs/mod-actions.log" :info nil))
+(def mod-action-appender
+  {:enabled? true
+   :async? true
+   :min-level :info
+   :output-fn timbre/default-output-fn
+   :fn (fn [{:keys [output_ context]}]
+         (when (= (:type context) :moderator))
+         (spit "logs/clojure.log" (str @output_ "\n") :append true))})
 
 (defn timbre-init!
   []
@@ -25,6 +38,6 @@
   ;; maybe we can actually just have an indexed html that points to different log files?
   ;; that would actually be sick as hell
   (timbre/merge-config!
-    {:appenders {:default log-appender
-                 :exceptions exceptions-appender
+    {:appenders {:default default-appender
+                 :error exceptions-appender
                  :mod-action mod-action-appender}}))
