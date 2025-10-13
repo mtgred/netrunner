3cc1ff7ccf15cf3f66fe7c924873cf35a678b5a1
NB Kelly <N.B.Kelly@protonmail.com>
2025-10-14 10:06:17 +1300

    end of game stat screen

--- src/cljs/nr/end_of_game_stats.cljs
+++ src/cljs/nr/end_of_game_stats.cljs
@@ -1,5 +1,5 @@
 (ns nr.end-of-game-stats
-  (:require [nr.translations :refer [tr tr-side]]
+  (:require [nr.translations :refer [tr tr-side tr-element]]
             [nr.utils :refer [map-longest]]))
 
 (defn computed-stat
@@ -17,46 +17,46 @@
 
 (defn corp-stats [s]
   (vec (remove nil?
-               [[(tr [:stats_clicks-gained "Clicks Gained"]) (get-in s [:gain :click])]
-                [(tr [:stats_credits-gained "Credits Gained"]) (get-in s [:gain :credit])]
-                [(tr [:stats_credits-spent "Credits Spent"]) (get-in s [:spent :credit])]
-                [(tr [:stats_credits-click "Credits by the Basic Action"]) (get-in s [:click :credit])]
-                [(tr [:stats_cards-drawn "Cards Drawn"]) (get-in s [:gain :card])]
-                [(tr [:stats_cards-click "Cards Drawn by the Basic Action"]) (get-in s [:click :draw])]
-                [(tr [:stats_damage-done "Damage Done"]) (get-in s [:damage :all])]
-                [(tr [:stats_cards-rezzed "Cards Rezzed"]) (get-in s [:cards :rezzed])]
-                (optional-stat s (tr [:stats_shuffle-count "Shuffle Count"]) [:shuffle-count])
-                (optional-stat s (tr [:stats_operations-played "Operations Played"]) [:cards-played :play-instant])
-                (optional-stat s (tr [:stats_rashida-count "Rashida Count"]) [:rashida-count])
+               [[[:stats_clicks-gained "Clicks Gained"] (get-in s [:gain :click])]
+                [[:stats_credits-gained "Credits Gained"] (get-in s [:gain :credit])]
+                [[:stats_credits-spent "Credits Spent"] (get-in s [:spent :credit])]
+                [[:stats_credits-click "Credits by the Basic Action"] (get-in s [:click :credit])]
+                [[:stats_cards-drawn "Cards Drawn"] (get-in s [:gain :card])]
+                [[:stats_cards-click "Cards Drawn by the Basic Action"] (get-in s [:click :draw])]
+                [[:stats_damage-done "Damage Done"] (get-in s [:damage :all])]
+                [[:stats_cards-rezzed "Cards Rezzed"] (get-in s [:cards :rezzed])]
+                (optional-stat s [:stats_shuffle-count "Shuffle Count"] [:shuffle-count])
+                (optional-stat s [:stats_operations-played "Operations Played"] [:cards-played :play-instant])
+                (optional-stat s [:stats_rashida-count "Rashida Count"] [:rashida-count])
                 ;; psi games
-                (optional-stat s (tr [:stats_psi-game-total "Psi Game: Games Played"]) [:psi-game :games-played])
-                (optional-stat s (tr [:stats_psi-game-total-wins "Psi Game: Wins"]) [:psi-game :wins])
-                (optional-stat s (tr [:stats_psi-game-total-bid-0 "Psi Game: Bid 0"]) [:psi-game :bet-0])
-                (optional-stat s (tr [:stats_psi-game-total-bid-1 "Psi Game: Bid 1"]) [:psi-game :bet-1])
-                (optional-stat s (tr [:stats_psi-game-total-bid-2 "Psi Game: Bid 2"]) [:psi-game :bet-2])
+                (optional-stat s [:stats_psi-game-total "Psi Game: Games Played"] [:psi-game :games-played])
+                (optional-stat s [:stats_psi-game-total-wins "Psi Game: Wins"] [:psi-game :wins])
+                (optional-stat s [:stats_psi-game-total-bid-0 "Psi Game: Bid 0"] [:psi-game :bet-0])
+                (optional-stat s [:stats_psi-game-total-bid-1 "Psi Game: Bid 1"] [:psi-game :bet-1])
+                (optional-stat s [:stats_psi-game-total-bid-2 "Psi Game: Bid 2"] [:psi-game :bet-2])
                 ])))
 
 (defn runner-stats [s]
   (vec (remove nil?
-               [[(tr [:stats_clicks-gained "Clicks Gained"]) (get-in s [:gain :click])]
-                [(tr [:stats_credits-gained "Credits Gained"]) (get-in s [:gain :credit])]
-                [(tr [:stats_credits-spent "Credits Spent"]) (get-in s [:spent :credit])]
-                [(tr [:stats_credits-click "Credits by the Basic Action"]) (get-in s [:click :credit])]
-                [(tr [:stats_cards-drawn "Cards Drawn"]) (get-in s [:gain :card])]
-                [(tr [:stats_cards-click "Cards Drawn by the Basic Action"]) (get-in s [:click :draw])]
-                [(tr [:stats_tags-gained "Tags Gained"]) (get-in s [:gain :tag :base])]
-                [(tr [:stats_runs-made "Runs Made"]) (get-in s [:runs :started])]
-                [(tr [:stats_cards-accessed "Cards Accessed"]) (get-in s [:access :cards])]
-                (optional-stat s (tr [:stats_shuffle-count "Shuffle Count"]) [:shuffle-count])
-                (optional-stat s (tr [:stats_cards-sabotaged "Sabotage Count"]) [:cards-sabotaged])
-                (optional-stat s (tr [:stats_events-played "Events Played"]) [:cards-played :play-instant])
-                (computed-stat s (tr [:stats_unique-accesses "Unique Cards Accessed"]) [:access :unique-cards] count)
+               [[[:stats_clicks-gained "Clicks Gained"] (get-in s [:gain :click])]
+                [[:stats_credits-gained "Credits Gained"] (get-in s [:gain :credit])]
+                [[:stats_credits-spent "Credits Spent"] (get-in s [:spent :credit])]
+                [[:stats_credits-click "Credits by the Basic Action"] (get-in s [:click :credit])]
+                [[:stats_cards-drawn "Cards Drawn"] (get-in s [:gain :card])]
+                [[:stats_cards-click "Cards Drawn by the Basic Action"] (get-in s [:click :draw])]
+                [[:stats_tags-gained "Tags Gained"] (get-in s [:gain :tag :base])]
+                [[:stats_runs-made "Runs Made"] (get-in s [:runs :started])]
+                [[:stats_cards-accessed "Cards Accessed"] (get-in s [:access :cards])]
+                (optional-stat s [:stats_shuffle-count "Shuffle Count"] [:shuffle-count])
+                (optional-stat s [:stats_cards-sabotaged "Sabotage Count"] [:cards-sabotaged])
+                (optional-stat s [:stats_events-played "Events Played"] [:cards-played :play-instant])
+                (computed-stat s [:stats_unique-accesses "Unique Cards Accessed"] [:access :unique-cards] count)
                 ;; psi games
-                (optional-stat s (tr [:stats_psi-game-total "Psi Game: Games Played"]) [:psi-game :games-played])
-                (optional-stat s (tr [:stats_psi-game-total-wins "Psi Game: Wins"]) [:psi-game :wins])
-                (optional-stat s (tr [:stats_psi-game-total-bid-0 "Psi Game: Bid 0"]) [:psi-game :bet-0])
-                (optional-stat s (tr [:stats_psi-game-total-bid-1 "Psi Game: Bid 1"]) [:psi-game :bet-1])
-                (optional-stat s (tr [:stats_psi-game-total-bid-2 "Psi Game: Bid 2"]) [:psi-game :bet-2])
+                (optional-stat s [:stats_psi-game-total "Psi Game: Games Played"] [:psi-game :games-played])
+                (optional-stat s [:stats_psi-game-total-wins "Psi Game: Wins"] [:psi-game :wins])
+                (optional-stat s [:stats_psi-game-total-bid-0 "Psi Game: Bid 0"] [:psi-game :bet-0])
+                (optional-stat s [:stats_psi-game-total-bid-1 "Psi Game: Bid 1"] [:psi-game :bet-1])
+                (optional-stat s [:stats_psi-game-total-bid-2 "Psi Game: Bid 2"] [:psi-game :bet-2])
                 ])))
 
 (defn show-stat
@@ -78,6 +78,6 @@
        (doall (map-indexed
                 (fn [i [corp runner]]
                   [:tr {:key i}
-                   [:td (first corp)] [:td (show-stat corp)]
-                   [:td (first runner)] [:td (show-stat runner)]])
+                   (if (first corp) [tr-element :td (first corp)] [:td]) [:td (show-stat corp)]
+                   (if (first runner) [tr-element :td (first runner)] [:td]) [:td (show-stat runner)]])
                 stats))]]]))
