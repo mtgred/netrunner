45d30e9d1bc6f949288bdeecb8a35323ea13e906
NB Kelly <N.B.Kelly@protonmail.com>
2025-10-17 14:00:54 +1300

    you can insert content into translations

--- src/cljs/nr/translations.cljs
+++ src/cljs/nr/translations.cljs
@@ -23,9 +23,28 @@
   [params]
   (into {} (map (fn [[k v]] [(keyword (str "data-i18n-param-" (name k))) (str v)])) params))
 
-(defn tr-element
-  ([element resource] (tr-element element resource nil))
-  ([element resource params]
+(defn- embed-content
+  [translation content]
+  ;; note - it's very awkward to do translations where there is an embedded element on the inside
+  ;; this will allow us to do that by passing in a content map, like:
+  ;; {:link [:a {:href ....}]}
+  (if (or (not content) (empty? content))
+    translation
+    (do
+      (loop [elements [translation]
+             [pattern & patterns] (keys content)]
+        (let [insert (get content pattern)
+              reg (re-pattern (str "\\[" (name pattern) "]"))
+              new-elements (mapcat #(if-not (string? %)
+                                      [%] (interpose (get content pattern) (str/split % reg)))
+                                   elements)]
+          (if-not patterns
+            (into [:span] new-elements)
+            (recur new-elements patterns)))))))
+
+(defn tr-element-with-embedded-content
+  ([element resource content] (tr-element-with-embedded-content element resource content nil))
+  ([element resource content params]
    ;; note - sometimes a nil value will be passed into a tr, or the key is computed on the frontend
    ;; and the 'else' value is nil (ie no run phase), so we need to make sure there is no black
    ;; screen in a case like this.
@@ -34,8 +53,12 @@
             success? :target-language} (tr-with-info resource params)]
        [element
         (merge {:data-i18n-key (first resource) :data-i18n-success success?} (i18n-keys params))
-        (or translation "-")])
+        (or (embed-content translation content) "-")])
      [element {:data-i18n-failure true} "[no resource]"])))
+
+(defn tr-element
+  ([element resource] (tr-element element resource nil))
+  ([element resource params] (tr-element-with-embedded-content element resource nil params)))
 
 (defn tr-span
   ([resource] (tr-element :span resource nil))
