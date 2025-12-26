93b7d6aef568d2ac16e5b2ed55b8f6ffc6fb245a
NB Kelly <N.B.Kelly@protonmail.com>
2025-12-26 15:08:43 +1300

    allow reading rules inserts

--- src/cljs/nr/cardbrowser.cljs
+++ src/cljs/nr/cardbrowser.cljs
@@ -361,8 +361,9 @@
        [:div.heading (tr [:card-browser_implementation-note "Implementation note"] {:impl impl})])
 
      [:div.text.card-body
-      [:p [:span.type (tr-type (:type card))]
-       (if-not subtypes "" (str ": " subtypes))]
+      (when (:type card)
+        [:p [:span.type (tr-type (:type card))]
+         (if-not subtypes "" (str ": " subtypes))])
       [:pre (render-icons (tr-data :text (get @all-cards (:title card))))]
 
       (when show-extra-info
