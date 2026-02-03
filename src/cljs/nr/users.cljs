db0cf44e36113e1fdd57ad9422342667cb17ecce
NB Kelly <N.B.Kelly@protonmail.com>
2026-02-03 17:46:01 +1300

    ip ban framework

--- src/cljs/nr/users.cljs
+++ src/cljs/nr/users.cljs
@@ -23,6 +23,33 @@
         {}
         success))))
 
+(defmethod ws/event-msg-handler :admin/fetch-ip-bans
+  [{{success :success} :?data}]
+  (when success
+    (swap! users-state assoc :ip-banned success)))
+
+(defmethod ws/event-msg-handler :admin/look-up-ip
+  [{{:keys [error success]} :?data}]
+  (if success
+    (let [{:keys [username last-ip-address]} success]
+      (prn "success: " success)
+      (swap! users-state assoc :ip-lookup-name username :ip-lookup-result last-ip-address))
+    (non-game-toast error nil)))
+
+(defmethod ws/event-msg-handler :admin/ip-ban-user
+  [{{:keys [error success]} :?data}]
+  (if success
+    (let [{:keys [username ip-address]} success]
+      (swap! users-state update :ip-banned conj {:username username :ip-address ip-address}))
+    (non-game-toast error nil)))
+
+(defmethod ws/event-msg-handler :admin/ip-unban-user
+  [{{:keys [error success]} :?data}]
+  (if success
+    (let [{:keys [username ip-address]} success]
+      (swap! users-state update :ip-banned #(filterv (fn [m] (not= username (:username m))) %)))
+    (non-game-toast error nil)))
+
 (defmethod ws/event-msg-handler :admin/user-edit
   [{{:keys [error success]} :?data}]
   (cond
@@ -70,6 +97,36 @@
            "Remove"]]
          [:span.title (:username d "")]]))]])
 
+(defn- ip-bans-list [users]
+  ;; here it's a list of {:username ... :address ...}
+  [:div.users-box.panel.blue-shade
+   [:ul.list
+    (doall
+      (for [d (sort-by #(str/lower-case (:username %)) @users)]
+        [:li.users-item
+         {:key (:username d)}
+         [:span
+          [:button.delete
+           {:on-click #(ws/ws-send! [:admin/ip-unban-user {:username (:username d)}])}
+           "Remove"]]
+         [:span.title (:username d "") " - " (:ip-address d)]]))]])
+
+(defn- ip-ban-user-add [state state-key]
+  [:form.msg-box {:on-submit #(let [username (state-key @state "")]
+                                (.preventDefault %)
+                                (when-not (str/blank? username)
+                                  (ws/ws-send! [:admin/ip-ban-user {:username username}])
+                                  (swap! state assoc state-key "")))}
+   [:input {:type "text"
+            :placeholder "Type username"
+            :value (state-key @state "")
+            :on-change #(swap! state assoc state-key (-> % .-target .-value))}]
+   (let [username (state-key @state "")
+         disabled (str/blank? username)]
+     [:button {:disabled disabled
+               :class (if disabled "disabled" "")}
+      "IP Ban User"])])
+
 (defn- user-add [state state-key add-fn]
   [:form.msg-box {:on-submit #(let [username (state-key @state "")]
                                 (.preventDefault %)
@@ -86,11 +143,35 @@
                :class (if disabled "disabled" "")}
       "Add"])])
 
+(defn- ip-lookup-box [state state-key]
+  (fn []
+    [:form.msg-box {:on-submit #(let [username (state-key @state "")]
+                                  (.preventDefault %)
+                                  (when-not (str/blank? username)
+                                    (ws/ws-send! [:admin/look-up-ip {:username username}])))}
+     [:input {:type "text"
+              :placeholder "Type username"
+              :value (state-key @state "")
+              :on-change #(swap! state assoc state-key (-> % .-target .-value))}]
+     [:input {:type "text"
+              :read-only true
+              :value (:ip-lookup-result @state "")
+              :placeholder "Look up an IP"}]
+    (let [username (state-key @state "")
+          disabled (str/blank? username)]
+      [:button {:disabled disabled
+                :class (if disabled "disabled" "")}
+       "Search"])]))
+
+
+
+
 (defn users-container []
   (r/with-let [mods (r/cursor users-state [:mods])
                specials (r/cursor users-state [:specials])
                tos (r/cursor users-state [:tos])
-               banned (r/cursor users-state [:banned])]
+               banned (r/cursor users-state [:banned])
+               ip-bans (r/cursor users-state [:ip-banned])]
     (let [s (r/atom {})
           rows [{:h3 "Moderators"
                  :cursor mods
@@ -109,19 +190,30 @@
                  :key :banned
                  :h4 "Ban user"}]]
       (fn []
-        (into [:div.container.panel.blue-shade.content-page]
-              (->> (for [row rows]
-                     [[:h3 (:h3 row)]
-                      (users-list (:cursor row) (remove-user (:key row)))
-                      [:h4 (:h4 row)]
-                      (user-add s (:key row) (add-user (:key row)))])
-                   (interpose [[:br]])
-                   (mapcat identity)
-                   (map-indexed (fn [idx itm] ^{:key idx} itm))))))))
+        [:div.container.panel.blue-shade.content-page
+         (into [:div]
+               (->> (for [{:keys [h3 cursor key h4] :as row} rows]
+                      ;; (if (= key :ip-bans)
+                      ;;   [[:h3 (:h3 row)]
+                      [[:h3 h3]
+                       (users-list cursor (remove-user key))
+                       [:h4 h4]
+                       (user-add s key (add-user key))])
+                    (interpose [[:br]])
+                    (mapcat identity)
+                    (map-indexed (fn [idx itm] ^{:key idx} itm))))
+         [:br]
+         [:h3 "IP Lookup"]
+         [ip-lookup-box users-state :ip-lookup-name]
+         [:br]
+         [:h3 "Ip Bans"]
+         [ip-bans-list ip-bans]
+         [ip-ban-user-add users-state :ip-ban-name]]))))
 
 (defn users []
   (r/with-let [user (r/cursor app-state [:user])]
     (ws/ws-send! [:admin/fetch-users])
+    (ws/ws-send! [:admin/fetch-ip-bans])
     [:div.page-container
      [:div.account-bg]
      (when (or (:isadmin @user) (:ismoderator @user))
